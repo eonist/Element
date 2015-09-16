52e59954396de79f287d3b4d1aeaeb9aaf256779
Eonist <unicornhive@gmail.com>
2015-09-16 23:45:14 +0200

    Files modified: 1
    Modified 1 file:SwiftTest.swift

--- SwiftTest.swift
+++ SwiftTest.swift
@@ -137,7 +137,7 @@ func testingTuples2()->(name:String,num:Int){
 	return ("test",55)
 }
 
-var res2 = testingTuples2()
+
 Println("your value: \(res.name) and \(res.num)")
 
 //you cant use values that arent set
@@ -145,7 +145,10 @@ var someVal:Int
 Println("your value: \(someVal)")//throws error
 var someVal2:Int?//optional value the same as assigning = nil
 Println("your value: \(someVal2)")//nil
-
+someVal2 = 3
+if someVal2 != nil {
+	Println("your value: \(someVal2!)")//forced unwrapping with !, you do this if you know the value isnt nil, even though it isnt assigned on init of the var
+}
 /**
  * //the code bellow is a way swift can do basically this:
  * var res = theDict["someKeyNameThatDoesNotExist"] 

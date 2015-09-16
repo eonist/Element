1b1978ab1d50df1009a1d26da2bddcd291418ee9
Eonist <unicornhive@gmail.com>
2015-09-17 00:51:20 +0200

    Files modified: 1
    Modified 1 file:SwiftTest.swift

--- SwiftTest.swift
+++ SwiftTest.swift
@@ -208,8 +208,9 @@ func lowestToHighest(a:Int,b:Int)->Bool{
 	return a < b;
 }
 //convert this to a clouser method:
-}(a:Int,b:Int)->Bool in
+let lowToHig{(a:Int,b:Int)->Bool in
 	return a < b;
 }
 
-var someInts = [500,63,99,23]
+var someInts:[Int] = [500,63,99,23]
+var sortedInts:[Int] = sorted(lowToHig)//sorts the ints

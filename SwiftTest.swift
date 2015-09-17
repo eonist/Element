8b8a3a40bb0d0ff1a45712988ccc86815c3e402d
Eonist <unicornhive@gmail.com>
2015-09-17 22:44:44 +0200

    Files modified: 1
    Modified 1 file:SwiftTest.swift

--- SwiftTest.swift
+++ SwiftTest.swift
@@ -351,3 +351,21 @@ function, a structure's value is copied
 when assigned to another variable, or passed to a
 function, a reference to the original object is passed
 */
+
+// Playground - Pass By Reference
+class SimpleClass {
+	var value : Int = 99
+}
+// Simple function that changes an object
+func changeObject(var object : SimpleClas){
+object.value += 1000
+	return obj ect.value
+}
+// create a new instance (reference type)
+var myobject = SimpleClass()
+// pass the object into the function - Pass by Reference
+changeobject(myobject)
+// the original object has been changed.
+myobject.value//
+5) -> Int {
+as By

2d6087dc8e01b292662101d006c2eb86ce4b8f07
Eonist <unicornhive@gmail.com>
2015-09-17 10:09:07 +0200

    Files modified: 1
    Modified 1 file:SwiftTest.swift

--- SwiftTest.swift
+++ SwiftTest.swift
@@ -221,7 +221,7 @@ class Person{
 	var name:String = "Paul"//values must be set if you use a class aka initialized
 	let age:Int = 77;
 	func init(name:String,age:Int){//class initializer w/ params
-		self.name = name;//self refers to properties outside the method bit inside the class
+		self.name = name;//self refers to properties outside the method bit inside the class, aka this in other languages
 		self.age = age;
 	}
 	//methods accessable outside a class:

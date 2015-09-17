f98f7c20445f88f8e1c86b552cd1b875fbde58d5
Eonist <unicornhive@gmail.com>
2015-09-18 00:06:49 +0200

    Files modified: 1
    Modified 1 file:SwiftTest.swift

--- SwiftTest.swift
+++ SwiftTest.swift
@@ -442,13 +442,13 @@ var website = personalSite ?? defaultSite
 //modulo 
 //NOTE: modulo works with floats and negative numbers in swift
 
+//importing:
 import Foundation
-let someString =Hello world, how are you today?"
+let someString ="Hello world, how are you today?"
 someString.uppercaseString
 let words = someString.componentsSeparatedByString(" ")
 let someDate = NSDate()
-let someURL = NSURL(string: "http://www.lynda.com/
-Simon-Allardice/21-1.html")
+let someURL = NSURL(string: "http://www.lynda.com/Simon-Allardice/21-1.html")
 someURL.host
 someURL.scheme
 someURL.lastPathComponent

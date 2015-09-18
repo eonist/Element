7d59e8e42448bf44cbaa2e46e5633a7d059924f6
Eonist <unicornhive@gmail.com>
2015-09-18 23:30:24 +0200

    Files modified: 1
    Modified 1 file:SwiftTest.swift

--- SwiftTest.swift
+++ SwiftTest.swift
@@ -538,3 +538,20 @@ Extensions in Swift
  Doesn't require source code for the type
 ' Can be added to classes, structures and enumerations
 */
+
+//NOTE: extensions are almost like prototypes in other languages
+//example of an extension:
+//import UIKit
+extension String {
+	func reversewords() -> String {
+		let wordsArray = self.componentsSeparatedByString(" ")
+		let reversedArray = wordsArray.reverse()
+		var newString = ""
+		for eachword in reversedArray {
+			newString += eachword + " "
+		}
+		return newString
+	}
+}
+var message = "I want to reverse all the words in this string"
+message.reversewords()

0f35c69387c818af53c189f6bb3805df526f264a
Eonist <unicornhive@gmail.com>
2015-09-20 01:38:08 +0200

    New files added: 1, Files modified: 1
    Added 1 file:test/Downcast.swiftModified 1 file:SwiftTest.swift

--- SwiftTest.swift
+++ SwiftTest.swift
@@ -483,27 +483,7 @@ let myTextFie1d = UITextFie1d()
 let myDatePicker = UIDatePicker()
 let controls = [myButton, mySlider, myTextField, myDatePicker]
 
-//type casting
-for item in controls {
-	// option 1: check type with "is"
-	if item is UIDatePicker {
-		println("We have a UIDatePicker")
-	}
-	// downcast with "as"
-	let picker = item as UIDatePicker
-		picker.datePickerMode = UIDatePickerMode.CountDownTimer
-	}
-	// option 2: try to downcast with as?
-	let picker = item as? UIDatePicker
-	// then check if it worked, and unwrap the optional
-	if picker != nil {
-		picker!.datePickerMode = UIDatePickerMode.CountDownTimer
-	}
-	// option 3 - combine
-	if let picker = item as? UIDatePicker {//this is like doing: var picker = picker is UIDatePicker ? picker as UIDatePicker : picker; in another language
-		picker.datePickerMode = UIDatePickerMode.CountDownTimer
-	}                                      I
-}
+
 
 //shorthand in swift:
 let x = 10, y = 20; 
---
+++ test/Downcast.swift
@@ -0,0 +1,21 @@
+//type casting
+for item in controls {
+	// option 1: check type with "is"
+	if item is UIDatePicker {
+		println("We have a UIDatePicker")
+	}
+	// downcast with "as"
+	let picker = item as UIDatePicker
+		picker.datePickerMode = UIDatePickerMode.CountDownTimer
+	}
+	// option 2: try to downcast with as?
+	let picker = item as? UIDatePicker
+	// then check if it worked, and unwrap the optional
+	if picker != nil {
+		picker!.datePickerMode = UIDatePickerMode.CountDownTimer
+	}
+	// option 3 - combine
+	if let picker = item as? UIDatePicker {//this is like doing: var picker = picker is UIDatePicker ? picker as UIDatePicker : picker; in another language
+		picker.datePickerMode = UIDatePickerMode.CountDownTimer
+	}                                      I
+}

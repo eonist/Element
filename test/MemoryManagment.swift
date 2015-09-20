832c508f66c588d05aac3acf6f9920411bbcdaf9
Eonist <unicornhive@gmail.com>
2015-09-20 18:34:51 +0200

    Files modified: 1
    Modified 1 file:test/MemoryManagment.swift

--- test/MemoryManagment.swift
+++ test/MemoryManagment.swift
@@ -1,3 +1,4 @@
-//automatic reference counting
+//automatic reference counting, is similar to other languages that uses garbage collectors
 //prefix variables with weak if you need objects to reference each other
-//you can also use the prefix unknown
+//you can also use the prefix unknown if you set the reference from the initializer
+//

e1069d9f499bf9ba947b34c989aee5097f5da22f
Eonist <unicornhive@gmail.com>
2015-09-20 11:38:31 +0200

    New files added: 1, Files modified: 1
    Added 1 file:test/Operators.swiftModified 1 file:SwiftTest.swift

---
+++ test/Operators.swift
@@ -0,0 +1,24 @@
+//operators and their operandees
+var a : 11:55
+var b : 11:55
+var c : 11:55
+var highScore = 50o080
+// arithmetic operators:  + - * /
+var result = a + b
+result = b - 20
+var x = c * 3.23
+x = c / 1.1
+highScore = highScore + 5
+// compound assignment: += -=
+highScore += 10
+highScore -= 10
+
+// increment, decrement: ++ --
+var score : 99
+
+// POSTfix
+score++
+score--
+// PREfiX
+++score
+--score

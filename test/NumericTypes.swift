02440c7ac65972df3707e8da7d6a934eda13bcd3
Eonist <unicornhive@gmail.com>
2015-09-21 12:44:57 +0200

    Files modified: 1
    Modified 1 file:test/NumericTypes.swift

--- test/NumericTypes.swift
+++ test/NumericTypes.swift
@@ -4,3 +4,40 @@ someInt.successor()//34
 someInt.advanceBy(2)//35,returns the value doesnt set the value
 someInt.advanceBy(-2)//31,returns the value doesnt set the value
 someInt.distanceTo(40)//7
+
+
+var (result, overflowed) =result overflowed
+( result, overflowed) = Int.
+result
+overflowed
+(result, overflowed) = Int.
+result
+overflowed
+(result, overflowed) = Int.
+result
+overflowed
+Int.max
+Int.min
+x
+n
+(result, overflowed) = Int.
+Int.max)
+result
+overflowed
+Int8.m
+IntB.m
+in
+ax
+Int16.min
+Int16.max
+Int32.
+min
+Int32.max
+Int64.
+min
+Int64.max
+Int.addWithoverflow(12, 34)
+subtractWithoverflow(-128, 256)
+multiplyWithoverflow(128, 1024)
+divideWithoverflow(22, 7)
+multiplyWithoverflow(Int.max,

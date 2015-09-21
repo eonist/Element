77608281b84d562514192ca21fece6ac4b6358ad
Eonist <unicornhive@gmail.com>
2015-09-21 12:41:53 +0200

    Files modified: 1
    Modified 1 file:test/NumericTypes.swift

--- test/NumericTypes.swift
+++ test/NumericTypes.swift
@@ -3,3 +3,4 @@ someInt.predessor()//32
 someInt.successor()//34
 someInt.advanceBy(2)//35,returns the value doesnt set the value
 someInt.advanceBy(-2)//31,returns the value doesnt set the value
+someInt.distanceTo(40)//7

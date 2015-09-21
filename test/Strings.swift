8d982312be6cb663de650e245651a4a8d2ad08f0
Eonist <unicornhive@gmail.com>
2015-09-21 12:05:08 +0200

    Files modified: 1
    Modified 1 file:test/Strings.swift

--- test/Strings.swift
+++ test/Strings.swift
@@ -5,3 +5,6 @@ someStr.hasSuffix("123")//true
 
 var anotherStr = "66"
 anotherStr.toInt()//some int
+var theInt = anotherInt.toInt() as? Int//this unwraps the variable and assigns it, 
+//the better solution would be to do:
+var theInt = anotherInt.toInt() ?? 0//whoch would set the theInt to 0 of the anotherStr.toInt() doesnt yield an int

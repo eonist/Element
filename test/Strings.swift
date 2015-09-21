629047ac03fb96eb6d23528394c96d52256b27aa
Eonist <unicornhive@gmail.com>
2015-09-21 12:08:13 +0200

    Files modified: 1
    Modified 1 file:test/Strings.swift

--- test/Strings.swift
+++ test/Strings.swift
@@ -8,3 +8,6 @@ anotherStr.toInt()//some int
 var theInt = anotherInt.toInt() as? Int//this unwraps the variable and assigns it, 
 //the better solution would be to do:
 var theInt = anotherInt.toInt() ?? 0//whoch would set the theInt to 0 of the anotherStr.toInt() doesnt yield an int
+
+var thirdStr = "test"
+thirdStr += "abc"//concat the string

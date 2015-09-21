8bab0fe961938032851673dc13b8caafdb4072a6
Eonist <unicornhive@gmail.com>
2015-09-21 12:40:19 +0200

    Files modified: 1
    Modified 1 file:test/NumericTypes.swift

--- test/NumericTypes.swift
+++ test/NumericTypes.swift
@@ -1,3 +1,5 @@
 var someInt:Int = 33
 someInt.predessor()//32
-someInt.sucsessor()//34
+someInt.successor()//34
+someInt.advanceBy(2)//35,returns the value doesnt set the value
+someInt.advanceBy(-2)//31,returns the value doesnt set the value

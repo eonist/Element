d40c9287bd3b005deea3b50e79c56c113191f0f1
Eonist <unicornhive@gmail.com>
2015-09-22 17:47:20 +0200

    Files modified: 1
    Modified 1 file:test/ObjC.swift

--- test/ObjC.swift
+++ test/ObjC.swift
@@ -1,5 +1,10 @@
-Type Mapping
+/*
+Type Mapping:
 id » Anyobject
 NSString » String
 NSDictionary Dictionary
-*--4 ?
+* >> ?
+*/
+
+UITableViewCell *cell = [[UITab1eViewCe1l alloc] initWithStyle:UITab1eViewCe1lSty1eDefault reuseldentifier:@"Cell"];
+let cell = UITableViewCell(style: .Default, reuseldentifierz "Cell")

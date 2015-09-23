688dc73c1c54298d2ec793a281c48d233c5a29d8
Eonist <unicornhive@gmail.com>
2015-09-23 18:59:43 +0200

    Files modified: 1
    Modified 1 file:test/Errors.swift

--- test/Errors.swift
+++ test/Errors.swift
@@ -1,3 +1,19 @@
+
+
+
+
+
+
+//Alternatively in function that is itself marked with throws keyword like this
+
+throws {
+    try rideTheDragon(quest.dragon)
+} 
+
+
+//In order to throw an error you use throw keyword like this
+
+
 throw DragonError.DragonIsMissing
 
 enum DragonError: ErrorType {

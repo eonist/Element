7d05d9567ec5b8c81e2381bf5fea2fb66fc89671
Eonist <unicornhive@gmail.com>
2015-09-23 20:26:28 +0200

    Files modified: 1
    Modified 1 file:test/Errors.swift

--- test/Errors.swift
+++ test/Errors.swift
@@ -64,4 +64,19 @@ Will be automatically propagated to the caller:
 throw NSError(domain: NSURLErrorDomain, code: NSURLErrorCannotOpenFile, userInfo: nil)
 
 
-//From Apple books, The Swift Programming Language it's seems erros should be handle using enum.
+//From Apple books, The Swift Programming Language it's seems erros should be handle using enum
+enum ServerResponse {
+    case Result(String, String)
+    case Error(String)
+}
+
+let success = ServerResponse.Result("6:00 am", "8:09 pm")
+let failure = ServerResponse.Error("Out of cheese.")
+
+switch success {
+case let .Result(sunrise, sunset):
+    let serverResponse = "Sunrise is at \(sunrise) and sunset is at \(sunset)."
+case let .Error(error):
+    let serverResponse = "Failure...  \(error)"
+}
+

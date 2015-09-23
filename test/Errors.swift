f2e7b64fe2164af8910bf53e325b7be4b189e6de
Eonist <unicornhive@gmail.com>
2015-09-23 19:04:38 +0200

    Files modified: 1
    Modified 1 file:test/Errors.swift

--- test/Errors.swift
+++ test/Errors.swift
@@ -1,7 +1,7 @@
 
 
 //NOTE: Syntax is a bit different: do-catch + try + defer vs traditional try-catch-finally syntax.
-
+//NOTE: Swift exceptions are not compatible with ObjC exceptions. Your do-catch block will not catch any NSException, and vice versa, for that you must use ObjC.
 //this line should normally be present do-catch block like this
 
 

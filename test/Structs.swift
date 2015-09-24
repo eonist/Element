2c83c40a78ba526e955b126d0c7b477e65cccbcf
Eonist <unicornhive@gmail.com>
2015-09-24 13:25:54 +0200

    New files added: 1
    Added 1 file:test/Structs.swift

---
+++ test/Structs.swift
@@ -0,0 +1,11 @@
+// CGSize struct
+var size : CGSize = CGSizeMake(100, 100)
+// access struct members
+size.height
+size.width
+// CGPoint struct
+var screenLocation : CGPoint = CGPointMake(45,100)
+screenLocat1on.x
+screenLocation.y
+// CGRect struct
+var rectangle : CGRect = CGRectMake(10, 10, 400, 400)

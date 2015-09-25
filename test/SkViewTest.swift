731d025830ecb801702ee34fea836a8342b6edb5
Eonist <unicornhive@gmail.com>
2015-09-25 09:13:27 +0200

    Files modified: 1
    Modified 1 file:test/SkViewTest.swift

--- test/SkViewTest.swift
+++ test/SkViewTest.swift
@@ -3,3 +3,8 @@ cild.node
 child.node.removeFromParent()
 
 private let skView:SKView = self.view as! SKView)
+
+//width and height
+var size = CGSize(width: self.skView.bounds.width, height: self.skView.bounds.height)
+        
+        

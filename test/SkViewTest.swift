faa9a3f8014b2676289d7aa7ac5e6d7ddfbecc3b
Eonist <unicornhive@gmail.com>
2015-09-25 09:37:52 +0200

    Files modified: 1
    Modified 1 file:test/SkViewTest.swift

--- test/SkViewTest.swift
+++ test/SkViewTest.swift
@@ -16,10 +16,19 @@ self.node.insertChild(child.node, atIndex: Int(index))
 self.numChildren
 
 
-//SKShapeNode
+//SKShapeNode:
 //circle
 let shapeNode = GraphicsNode(circleOfRadius: radius)
 //ellipse
 let shapeNode = GraphicsNode(ellipseOfSize: CGSize(width: width, height: height))
 //rect
 let shapeNode = GraphicsNode(rect: CGRect(x: x, y: Stage.size.height - (y + height), width: width, height: height))
+//triangle
+let path = CGPathCreateMutable()//path
+CGPathMoveToPoint(path, nil, x1, Stage.size.height - y1)
+CGPathAddLineToPoint(path, nil, x2, Stage.size.height - y2)
+CGPathAddLineToPoint(path, nil, x3, Stage.size.height - y3)
+CGPathCloseSubpath(path)
+let shapeNode = GraphicsNode(path: path)
+//adding to view
+self.node.addChild(shapeNode)

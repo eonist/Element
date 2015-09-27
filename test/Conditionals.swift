4da2f751486165dc6edc75c8239b544f95e99cf8
Eonist <unicornhive@gmail.com>
2015-09-27 10:04:00 +0200

    Files modified: 1
    Modified 1 file:test/Conditionals.swift

--- test/Conditionals.swift
+++ test/Conditionals.swift
@@ -25,9 +25,14 @@ switch someVal{
 		break;
 }
 
+let johnsAddress = Address()
+johnsAddress.buildingName = "The Larches"
+johnsAddress.street = "Laurel Street"
+john.residence?.address = johnsAddress
+ 
 if let johnsStreet = john.residence?.address?.street {
     print("John's street name is \(johnsStreet).")
 } else {
     print("Unable to retrieve the address.")
 }
-// prints "Unable to retrieve the address."
+// prints "John's street name is Laurel Street."

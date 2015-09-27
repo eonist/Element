fd5f6603165a6e80ef9dd97d2d427b2c1a89c543
Eonist <unicornhive@gmail.com>
2015-09-27 09:23:10 +0200

    Files modified: 1
    Modified 1 file:test/Dictionaries.swift

--- test/Dictionaries.swift
+++ test/Dictionaries.swift
@@ -42,10 +42,10 @@ a == c//false,
 a != c//true, 
 
 You can explicitly type your of key:value pairs. The preferred way to do this is:
-
+//Dictionary<string,double>
 let piePrice:[String:Double] = ["Apple":3.99,"Raspberry":3.35]
 
 
 There are times you need an empty dictionary. In those cases, call the dictionary initializer, which for a dictionary is the key:value pair enclosed in square brackets:
-//Dictionary<string,double>
+//var pizzaSizes = Dictionary<Int, String>()
 var pieToppings = [String:Double]()

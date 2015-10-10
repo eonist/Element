ba36238006305113be7bb5748f6bc64ff6d0c53b
Eonist <unicornhive@gmail.com>
2015-10-10 09:03:07 +0200

    Files modified: 1
    Modified 1 file:src/utils/ElementParser.swift

--- src/utils/ElementParser.swift
+++ src/utils/ElementParser.swift
@@ -3,7 +3,7 @@ class ElementParser{
 	 * returns all children in @param element that is of type IElement
 	 */
 	func children(element:IElement){
-
+		//continue
 	}
 	/**
 	 * Returns an Array instance comprised of Selector instances for each (element,classId,id and state) in the element "cascade" (the spesseficity)
@@ -16,7 +16,7 @@ class ElementParser{
 	 * Returns a String instance of Selectors from @param element (i.e: "Window Button#first Text#input")
 	 */
 	func string(element:IElement){
-
+		//continue
 	}
 	/**
 	 * Returns an array populated with IElement parents of the target (Basically the ancestry)
@@ -33,7 +33,7 @@ class ElementParser{
 	/**
 	 * 
 	 */
-	class function elementById(elements:Array,id:String)->IElement {
+	class function elementById(elements:Array,id:String)->IElement? {
 		for element in elements as! [IElement] {if(id == element.id) {return element}}
 		return nil;
 	}

a8a5531bfb8729eacecb1afa449001c8991162ea
Eonist <unicornhive@gmail.com>
2015-10-28 20:11:31 +0100

    Files modified: 1
    Modified 1 file:src/style/utils/StyleParser.swift

--- src/style/utils/StyleParser.swift
+++ src/style/utils/StyleParser.swift
@@ -1,1 +1,23 @@
 import Foundation
+
+/**
+*
+*/
+class StyleParser {
+    /**
+    * // :TODO: write java doc
+    */
+    class func describe(style:IStyle){
+        Swift.print(style.name+": ");
+        for styleProperty : IStyleProperty in style.styleProperties {
+            var value:String = ""
+            if(styleProperty.value is String || styleProperty.value is Double || styleProperty.value is Bool || styleProperty.value is UInt || styleProperty.value is Int){
+                value = String(styleProperty.value)
+            }
+            else {
+                value = String(styleProperty.value)//ObjectParser.parse(styleProperty.value);//was if(styleProperty.value is Object)  //if the property is an object parse it
+            }
+            Swift.print(" " + styleProperty.name + ":" + value/* + " depth:"+styleProperty.depth*/);
+        }
+    }
+}

2886f3431537b3841f5de939ada24cf5fc0ef99d
Eonist <unicornhive@gmail.com>
2015-10-28 13:32:56 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/GraphicModifier.swift

--- src/skin/graphic/utils/GraphicModifier.swift
+++ src/skin/graphic/utils/GraphicModifier.swift
@@ -1,1 +1,18 @@
-import Foundation
+import Cocoa
+
+class GraphicModifier {
+    /**
+    *
+    */
+    class func applyProperties(path:NSPath,fillStyle:IFillStyle,lineStyle:ILineStyle) -> NSPath {
+        path
+        decoratable.graphic.fillStyle = fillStyle;
+        decoratable.graphic.lineStyle = lineStyle;
+        nsFillColor.setFill();
+        nsLineColor.setStroke();
+        
+        path.fill()
+        path.stroke()
+        return path;
+    }
+}

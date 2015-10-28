c268170257896e1f09518f32be3feaac852da559
Eonist <unicornhive@gmail.com>
2015-10-28 15:38:49 +0100

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyAsserter.swift

--- src/style/property/utils/StylePropertyAsserter.swift
+++ src/style/property/utils/StylePropertyAsserter.swift
@@ -6,14 +6,24 @@ class StylePropertyAsserter {
         var fillet:Fillet = StylePropertyParser.fillet(skin,depth);
         return !(fillet.topLeft == 0 && fillet.topRight == 0 && fillet.bottomLeft == 0 && fillet.bottomRight == 0);
         */
+        return false
     }
     class func hasGradient(style:IStyle)->Bool {
+        /*
         return StylePropertyParser.value(skin, CSSConstants.FILL, depth) is Gradient || StylePropertyParser.value(skin, "line", depth) is Gradient;
+        */
+        return false
     }
     class func hasAsset(style:IStyle)->Bool {
+        /*
         return StylePropertyParser.value(skin, CSSConstants.FILL, depth) is Array;
+        */
+        return false
     }
     class func hasDropShadow(style:IStyle)->Bool {
+        /*
         return StylePropertyParser.value(skin, CSSConstants.DROP_SHADOW, depth) is DropShadowFilter;
+        */
+        return false
     }
 }

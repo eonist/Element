bc572f9d0035cd549beb74254e17df118b03bd85
Eonist <unicornhive@gmail.com>
2015-10-29 20:19:08 +0100

    Files modified: 1
    Modified 1 file:src/ui/Element.swift

--- src/ui/Element.swift
+++ src/ui/Element.swift
@@ -24,6 +24,8 @@ class Element: FlippedView,IElement {
         
         //continue here
         
+        
+        //read chap 10 here:(this may also be in ed.4 you have) http://www.amazon.com/Cocoa-Objective-C-Running-Scott-Stevenson/dp/0596804792
         //ok your kinda stuck, lets complete alot of research, books, websites etc. 
         //TODO: To figure out how t do line gradient. Make sure you actually need this. check your prev Element lib
         //TODO: quartz, nsbezierpath or layers? do alot of cross research, write stuff down etc.

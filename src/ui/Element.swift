f3d594149177b7428a7a1c59cf4a3d9b4a3f8726
Eonist <unicornhive@gmail.com>
2015-10-29 12:19:33 +0100

    Files modified: 1
    Modified 1 file:src/ui/Element.swift

--- src/ui/Element.swift
+++ src/ui/Element.swift
@@ -10,6 +10,7 @@ class Element: FlippedView,IElement {
         
         self.layer = CALayer() // Set view to be layer-hosting:
         self.wantsLayer = true//need for the updateLayer method to be called internally
+        needsDisplay = true
         //layerWithColor()
         layerWithGradient()
     }

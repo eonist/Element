32c9988da995e71608bfa27580a1d70441c1e136
Eonist <unicornhive@gmail.com>
2015-10-29 15:59:07 +0100

    Files modified: 1
    Modified 1 file:src/ui/Element.swift

--- src/ui/Element.swift
+++ src/ui/Element.swift
@@ -216,7 +216,31 @@ class Element: FlippedView,IElement {
         
         
     }
-   
+    
+    /**
+    *
+    */
+    func drawRadialGradient(rect:CGRect){
+       
+            CGContextRef context = UIGraphicsGetCurrentContext();
+            
+        /*
+        size_t gradLocationsNum = 2;
+        CGFloat gradLocations[2] = {0.0f, 1.0f};
+        CGFloat gradColors[8] = {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.5f};
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+        CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, gradColors, gradLocations, gradLocationsNum);
+        CGColorSpaceRelease(colorSpace);
+        
+        CGPoint gradCenter= CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+        float gradRadius = MIN(self.bounds.size.width , self.bounds.size.height) ;
+        
+        CGContextDrawRadialGradient (context, gradient, gradCenter, 0, gradCenter, gradRadius, kCGGradientDrawsAfterEndLocation);
+        
+        CGGradientRelease(gradient);
+        */
+        
+    }
     
     /**
     * Returns the class type of the Class instance

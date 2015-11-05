aa3b12a7e9e23fc4dbfe26a80d98e9c6e0773d05
Eonist <unicornhive@gmail.com>
2015-11-05 16:09:47 +0100

    Files modified: 1
    Modified 1 file:src/ui/Element.swift

--- src/ui/Element.swift
+++ src/ui/Element.swift
@@ -85,10 +85,8 @@ extension IElement {
         
         
         //this is how you seperate the states with a space. 
-        //basicalley create an array of states from  a space seperated string TODO: check Button on how it composes the state
-        
-        
-        //from here: ElementParser.selectors(element):
+            //basicalley create an array of states from  a space seperated string TODO: check Button on how it composes the state
+            //from here: ElementParser.selectors(element):
             //selector.states = (e.skin != null ? e.skin.state : e.getSkinState()).match(/\b\w+\b/g);/*Matches words with spaces between them*/
         
   

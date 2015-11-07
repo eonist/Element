aeca042ab02d294e89617dd9ddacc1fc07891440
Eonist <unicornhive@gmail.com>
2015-11-07 12:00:20 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:src/style/selector/Selector.swift

--- src/style/selector/Selector.swift
+++ src/style/selector/Selector.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-class Selector:ISelector{// :TODO: impliment a ISelector interface, you should probably use a struct instead of class in the future
+class Selector:ISelector{// :TODO: you should probably use a struct instead of class in the future
     var element:String/*Button*///should be nil
-    var classIds:Array<String>//should be nil
-    var id:String//should be nil
-    var states:Array<String>//should be nil
+    var classIds:Array<String>/*.customButton*///should be nil
+    var id:String/*#customButton*///should be nil
+    var states:Array<String>/*:down*///should be nil
     init(_ element:String = "",_ classIds:Array<String> = [],_ id:String = "",_ states:Array<String> = [""]) {
         self.element = element;
         self.classIds = classIds;

ea86f335854d2600fab415474f18b72101964994
Eonist <unicornhive@gmail.com>
2015-11-07 20:22:19 +0100

    Files modified: 1
    Modified 1 file:src/style/collection/IStyleCollection.swift

--- src/style/collection/IStyleCollection.swift
+++ src/style/collection/IStyleCollection.swift
@@ -1,1 +1,10 @@
 import Foundation
+
+protocol IStyleCollection {
+    var styles:Array<IStyle> {get}
+    func addStyle(style:IStyle)
+    func addStyles(styles:Array<IStyle>)
+    func removeStyle(name:String)->IStyle
+    func getStyle(name:String)->IStyle
+    func getStyleAt(index:Int)->IStyle
+}

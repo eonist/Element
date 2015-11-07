98d8e27cf22936f587c53b4b8302d2dd3671917d
Eonist <unicornhive@gmail.com>
2015-11-07 20:26:36 +0100

    Files modified: 1
    Modified 1 file:src/style/collection/StyleCollection.swift

--- src/style/collection/StyleCollection.swift
+++ src/style/collection/StyleCollection.swift
@@ -2,6 +2,24 @@ import Foundation
 
 class StyleCollection:IStyleCollection{
     var styles:Array<IStyle> = [];//use obj instead?
-    init() {
+    
+    /**
+    * Adds every style in an array to the_styles array (uses the addStyle method to do it so that it checks for duplicates)
+    * @Note the reason we dont move the following core methods into StyleCollectionModifier is because they are used alot and are not that complex
+    */
+    func addStyles(styles:Array<IStyle>){
+        for style in styles{ addStyle(style) }
+    }
+    func addStyle(style:IStyle){
+        
+    }
+    func removeStyle(name:String)->IStyle{
+        
+    }
+    func getStyle(name:String)->IStyle{
+        
+    }
+    func getStyleAt(index:Int)->IStyle{
+        
     }
 }

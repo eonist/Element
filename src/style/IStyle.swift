64973dd68bf9da2b0d46168881999c311fbb2a54
Eonist <unicornhive@gmail.com>
2015-11-07 17:40:07 +0100

    Files modified: 1
    Modified 1 file:src/style/IStyle.swift

--- src/style/IStyle.swift
+++ src/style/IStyle.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 protocol IStyle {
-    var selectors:ISelector {get}
     var name:String {get}
+    var selectors:Array<ISelector> {get}
     var styleProperties:Array<IStyleProperty> {get}
     func getStyleProperty(name:String)->IStyleProperty?
     func addStyleProperty(styleProperty:IStyleProperty)

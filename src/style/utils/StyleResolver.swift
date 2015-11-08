797122be4b581d797e2d5c072b2be06fb8d3cec8
Eonist <unicornhive@gmail.com>
2015-11-08 08:20:20 +0100

    Files modified: 2
    Modified 2 files:src/style/utils/StyleResolver.swiftsrc/utils/ElementParser.swift

--- src/style/utils/StyleResolver.swift
+++ src/style/utils/StyleResolver.swift
@@ -1,1 +1,33 @@
 import Foundation
+
+class StyleResolver{
+    /**
+     *
+     */
+    class func style(element:IElement)->IStyle{
+        var styleComposition:IStyle = Style("styleComp")
+        let classType:String = element.getClassType()//gets the classtype from the component
+
+        Swift.print("styleComposition")
+        Swift.print(StyleManager.styles.count)
+        for style in StyleManager.styles{//loop through styles
+            //Swift.print("style.selector.element: " + style.selector.element)
+            for selector in style.selectors{
+                if(selector.element == classType){ //if style.selector == classType
+                    Swift.print("  element match found")
+                    for state in selector.states{//loop style.selector.states
+                        if(state == element.skinState){//if state == any of the current states TODO: figure out how the statemaschine works and impliment that
+                            Swift.print("    state match found")
+                            StyleModifier.combine(&styleComposition, style)//gracefully append this style to styleComposition, forced overwrite
+                        }
+                    }
+                }
+            }
+            
+        }
+        
+        Swift.print("styleComposition.styleProperties.count: " + "\(styleComposition.styleProperties.count)")
+        StyleParser.describe(styleComposition)
+        return styleComposition
+    }
+}
--- src/utils/ElementParser.swift
+++ src/utils/ElementParser.swift
@@ -1,1 +1,13 @@
 import Foundation
+
+
+class ElementParser{
+    /**
+     *
+     */
+    class func parents(element:IElement)->Array<IElement>{
+        var parents:Array<IElement> = []
+        element.getClassType()
+        return []
+    }
+}

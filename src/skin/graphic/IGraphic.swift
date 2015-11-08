5d71d2adeff018efabdba2495c47ae49869fdcc2
Eonist <unicornhive@gmail.com>
2015-11-08 19:36:31 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/IGraphic.swift

--- src/skin/graphic/IGraphic.swift
+++ src/skin/graphic/IGraphic.swift
@@ -1,1 +1,10 @@
 import Foundation
+
+class IGraphic{
+    func lineShape():Shape;
+    func  lineStyle():ILineStyle{get};
+    func  fillStyle():IFillStyle{get}
+    func  lineOffsetType():OffsetType[get];
+    func  lineOffsetType(offsetType : OffsetType) : void
+    func get graphics():Graphics;
+}

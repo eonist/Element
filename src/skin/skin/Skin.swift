6a7a47c41efe87e52eefa1c10522430638046fec
Eonist <unicornhive@gmail.com>
2015-11-08 16:46:10 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/Skin.swift

--- src/skin/skin/Skin.swift
+++ src/skin/skin/Skin.swift
@@ -2,9 +2,12 @@ import Foundation
 
 
 class Skin{
+    private var style:IStyle?
+    private var state:String
+    private var element:IElement?
     init(style:IStyle? = nil, state:String = "", element:IElement? = nil){
-        _style = style;
-        _state = state;
-        _element = element;
+        self.style = style;
+        self.state = state;
+        self.element = element;
     }
 }

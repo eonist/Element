d50fee3757d89c4886d996faef0217b61506e4f4
Eonist <unicornhive@gmail.com>
2015-11-08 20:13:23 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -4,6 +4,14 @@ import Foundation
 class GraphicSkin:Skin{
     override init(_ style:IStyle? = nil, _ state:String = "", _ element:IElement? = nil){
         super.init(style, state, element)
+        super.decoratable = nil//GraphicSkinParser.configure(this);
+        
+        
+        
+        //Continue here, implement the GraphicSkinParser.configure method
+        
+        
+        
         
     }
 }

db9a68494781fc5afcc73a8af15305e123d0972d
Eonist <unicornhive@gmail.com>
2015-11-08 08:38:04 +0100

    Files modified: 1
    Modified 1 file:src/ui/IElement.swift

--- src/ui/IElement.swift
+++ src/ui/IElement.swift
@@ -2,6 +2,7 @@ import Foundation
 protocol IElement:class, IView{
     func resolveSkin()
     var style:IStyle{get set}
+    func getParent()->IElement//TODO: maybe use weak?
     func getClassType()->String
     var skinState:String{get}
 }

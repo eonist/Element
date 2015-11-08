6e2365fa1317a9a971266f8336bb310ed7219a73
Eonist <unicornhive@gmail.com>
2015-11-08 14:51:46 +0100

    Files modified: 1
    Modified 1 file:src/style/selector/Selector.swift

--- src/style/selector/Selector.swift
+++ src/style/selector/Selector.swift
@@ -5,7 +5,7 @@ class Selector:ISelector{// :TODO: you should probably use a struct instead of c
     var classIds:Array<String>/*.customButton*///should be nil
     var id:String/*#customButton*///should be nil
     var states:Array<String>/*:down*///should be nil
-    init(_ element:String = "",_ classIds:Array<String> = [],_ id:String = "",_ states:Array<String> = []/*TODO: ""<-init with idle state?*/) {
+    init(_ element:String = "",_ classIds:Array<String> = [],_ id:String = "",_ states:Array<String> = [""]/*TODO: ""<-init with idle state?*/) {
         self.element = element;
         self.classIds = classIds;
         self.id = id;

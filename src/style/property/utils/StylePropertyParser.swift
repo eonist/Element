fb1a630a664e574bad1e10fcb20be8a3120666a2
Eonist <unicornhive@gmail.com>
2015-11-08 07:49:00 +0100

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyParser.swift

--- src/style/property/utils/StylePropertyParser.swift
+++ src/style/property/utils/StylePropertyParser.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+class StylePropertyParser{
+    /**
+     * Returns a property from @param skin and @param property
+     * @Note the reason that depth defaults to 0 is because if the exact depth isnt found there should only be depth 0, if you have more than 1 depth in a property then you must supply at all depths or just the 1 that will work for all depths
+     * // :TODO: should probably also support when state is know and depth is defaulted to 0 ?!?!?
+     */
+    class func value(){
+        
+    }
+}

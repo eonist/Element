7f09f7611a89b108c6037b8ca75a92c977912595
Eonist <unicornhive@gmail.com>
2015-11-08 08:48:32 +0100

    Files modified: 1
    Modified 1 file:src/utils/Section.swift

--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -4,22 +4,10 @@ class Section:Element {//Unlike Container, section can have a style applied
         super.init(width,height,parent)
     }
     /*
-    * Required by super class
-    */
+     * Required by super class 
+     * TODO: could we add it thorugh extions instead?
+     */
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    /*
-    *  This class exists because one shouldnt use the Element class as a holder of content
-    */
-    /*
-    override init(_ width: Int, _ height: Int/*, _ style: IGraphicStyle*/) {
-        super.init(width, height/*, style*/)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    */
-   
 }

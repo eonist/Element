bc887079382e86309e21b781d4909fb043d45258
Eonist <unicornhive@gmail.com>
2015-11-08 16:44:06 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/SkinStates.swift

--- src/skin/skin/utils/SkinStates.swift
+++ src/skin/skin/utils/SkinStates.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+
+class SkinStates {
+    static var none:String = "";/*aka idle*/
+    static var over:String = "over";
+    static var down:String = "down";
+    
+    static var selected:String = "selected";
+    static var checked:String = "checked";
+    static var focused:String = "focused";
+    static var disabled:String = "disabled";
+}
+
+/*
+enum SkinStates:String
+case None = ""
+case Over = "over"
+//etc
+*/

651144c651f153c9dde922bc90dfafa3f0e9218c
Eonist <unicornhive@gmail.com>
2015-11-08 08:35:48 +0100

    Files modified: 1
    Modified 1 file:src/utils/ElementParser.swift

--- src/utils/ElementParser.swift
+++ src/utils/ElementParser.swift
@@ -2,12 +2,17 @@ import Foundation
 
 
 class ElementParser{
+
     /**
-     *
+     * Returns an array populated with IElement parents of the target (Basically the ancestry)
      */
-    class func parents(element:IElement)->Array<IElement>{
-        var parents:Array<IElement> = []
-        element.getParent()
-        return []
+    class func parents(element:IElement)->Array<IElement> {
+        var parents:Array = [];
+        var parent:IElement = element.getParent()// :TODO: seperate this into a check if its DO then that, if its NativeWindow then do that //note to self: this was styleable.parent as DisplayObject but it cant be anymore since it needs suport for NativeWindow
+        while(parent is IElement) {//loops up the object hierarchy as long as the parent is a StylableObject supertype
+            ArrayModifier.unshift(&parents,parent)
+            parent = (parent as IElement).getParent();
+        }
+        return parents;
     }
 }

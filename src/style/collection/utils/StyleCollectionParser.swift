f3922511c005e9ed56387c1153446a9874dfed9b
Eonist <unicornhive@gmail.com>
2015-11-08 06:37:08 +0100

    Files modified: 1
    Modified 1 file:src/style/collection/utils/StyleCollectionParser.swift

--- src/style/collection/utils/StyleCollectionParser.swift
+++ src/style/collection/utils/StyleCollectionParser.swift
@@ -1,25 +1,25 @@
 import Foundation
 
-class StyleCollectionDescriber {
+class StyleCollectionParser {
     /**
-    * Describes the stylecollection content
-    * Note can you use the ObjectDescriber in place of this class?
-    */
+     * Describes the stylecollection content
+     * Note can you use the ObjectDescriber in place of this class?
+     */
     class func describe(styleCollection:IStyleCollection) {
-        var stylesLength:Int = styleCollection.styles.count;
-        for (var i : Int = 0; i < stylesLength; i++) {
-            var style:IStyle = styleCollection.styles[i];
-            printStyleProperties(style);
-        }
         func printStyleProperties(style:IStyle) {
             Swift.print("printStyleProperties <style.name>:"+style.name);
             var propertyNames:Array = StyleParser.stylePropertyNames(style);
-            var propertyLength:Int = style.styleProperties.count;
+            let propertyLength:Int = style.styleProperties.count;
             for (var e : Int = 0; e < propertyLength; e++) {
-                var property:Any = style.getValueAt(e);
-                var name:String = propertyNames[e];
-                trace("name:" + name + ", property: " +  property);
+                let property:Any = style.getValueAt(e);
+                let name:String = propertyNames[e];
+                Swift.print("name:" + name + ", property: " +  String(property));
             }
+        }
+        let stylesLength:Int = styleCollection.styles.count;
+        for (var i : Int = 0; i < stylesLength; i++) {
+            let style:IStyle = styleCollection.styles[i];
+            printStyleProperties(style);
         }
     }
 }

cf7bbc11f4641750c138c2e1911348a61998ad20
Eonist <unicornhive@gmail.com>
2015-11-08 06:26:16 +0100

    Files modified: 1
    Modified 1 file:src/style/utils/StyleParser.swift

--- src/style/utils/StyleParser.swift
+++ src/style/utils/StyleParser.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 /**
-*
-*/
+ *
+ */
 class StyleParser {
     /**
-    * // :TODO: write java doc
-    */
+     * // :TODO: write java doc
+     */
     class func describe(style:IStyle){
         Swift.print(style.name+": ");
         for styleProperty : IStyleProperty in style.styleProperties {
@@ -19,5 +19,13 @@ class StyleParser {
             }
             Swift.print(" " + styleProperty.name + ":" + value/* + " depth:"+styleProperty.depth*/);
         }
+    }
+    /**
+     * Returns an array populated with style property names
+     */
+    class func stylePropertyNames(style:IStyle) -> Array<String>{
+        var propertyNames:Array<String> = [];
+        for styleProperty : IStyleProperty in style.styleProperties{ propertyNames.append(styleProperty.name) }
+        return propertyNames;
     }
 }

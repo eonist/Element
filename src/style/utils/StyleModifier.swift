fa5054e52948fcf30c6f8787134d6f933c42bd58
Eonist <unicornhive@gmail.com>
2015-11-08 05:47:49 +0100

    Files modified: 1
    Modified 1 file:src/style/utils/StyleModifier.swift

--- src/style/utils/StyleModifier.swift
+++ src/style/utils/StyleModifier.swift
@@ -5,8 +5,8 @@ class StyleModifier {
      * CSSParser.as, StyleHeritageResolver.as uses this function
      * // :TODO: explain what newSelectors does
      */
-    class func clone(style:IStyle, _ newName:String? = nil, newSelectors:Array = null)->IStyle{
-        let returnStyle:IStyle = Style(newName ?? style.name);
+    class func clone(style:IStyle, _ newName:String? = nil, newSelectors:Array<IStyle>? = nil)->IStyle{
+        let returnStyle:IStyle = Style(newName ?? style.name, newSelectors ?? style.selectors,[]);
         for styleProperty : IStyleProperty in style.styleProperties{
             returnStyle.addStyleProperty(StyleProperty(styleProperty.name, styleProperty.value));
         }

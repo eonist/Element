b7d4a0f79b917eb447ab0eb6c28461a88ae185c5
Eonist <unicornhive@gmail.com>
2015-11-08 16:44:06 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file: src/skin/skin/SkinStates.swiftModified 1 file:src/skin/skin/Skin.swift

--- src/skin/skin/Skin.swift
+++ src/skin/skin/Skin.swift
@@ -1,1 +1,10 @@
 import Foundation
+
+
+class Skin{
+    init(style:IStyle? = nil, state:String = "", element:IElement? = nil){
+        _style = style;
+        _state = state;
+        _element = element;
+    }
+}

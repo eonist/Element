497a0980a9f3ceaf1fe639382b24971ac4e91a25
Eonist <unicornhive@gmail.com>
2015-11-08 06:21:37 +0100

    Files modified: 1
    Modified 1 file:src/style/collection/utils/StyleCollectionParser.swift

--- src/style/collection/utils/StyleCollectionParser.swift
+++ src/style/collection/utils/StyleCollectionParser.swift
@@ -1,1 +1,25 @@
 import Foundation
+
+class StyleCollectionDescriber {
+    /**
+    * Describes the stylecollection content
+    * Note can you use the ObjectDescriber in place of this class?
+    */
+    class func describe(styleCollection:IStyleCollection) {
+        var stylesLength:Int = styleCollection.styles.count;
+        for (var i : Int = 0; i < stylesLength; i++) {
+            var style:IStyle = styleCollection.getStyleAt(i);
+            printStyleProperties(style);
+        }
+        func printStyleProperties(style:IStyle) {
+            Swift.print("printStyleProperties <style.name>:"+style.name);
+            var propertyNames:Array = StyleParser.stylePropertyNames(style);
+            var propertyLength:int = style.styleProperties.length;
+            for (var e : int = 0; e < propertyLength; e++) {
+                var property:* = style.getValueAt(e);
+                var name:String = propertyNames[e];
+                trace("name:" + name + ", property: " +  property);
+            }
+        }
+    }
+}

b70d01373fc1083b0c353de10cb327fe45249889
Eonist <unicornhive@gmail.com>
2015-11-08 09:25:08 +0100

    Files modified: 1
    Modified 1 file:src/utils/ElementParser.swift

--- src/utils/ElementParser.swift
+++ src/utils/ElementParser.swift
@@ -3,6 +3,28 @@ import Foundation
 
 class ElementParser{
     /**
+     * Returns an Array instance comprised of Selector instances for each (element,classId,id and state) in the element "cascade" (the spesseficity)
+     * @Note to get the stackString use: trace(SelectorUtils.toString(StyleResolver.stack(checkButton)));
+     */
+    class func selectors(element:IElement)->Array<ISelector>{
+        
+        
+        //continue here
+        
+        
+        var elements:Array<IElement> = parents(element) += element
+        var selectors:Array = [];
+        for each (var e : IElement in elements) {
+            var selector:Selector = new Selector();
+            selector.element = e.getClassType();
+            if(e.classId != null) selector.classIds = e.classId.indexOf(" ") != -1 ? e.classId.split(" ") : [e.classId];
+            selector.id = e.id;
+            selector.states = (e.skin != null ? e.skin.state : e.getSkinState()).match(/\b\w+\b/g);/*Matches words with spaces between them*/
+            selectors.push(selector);
+        }
+        return selectors;
+    }
+    /**
      * Returns an array populated with IElement parents of the target (Basically the ancestry)
      */
     class func parents(element:IElement)->Array<IElement> {

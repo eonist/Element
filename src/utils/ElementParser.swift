3d2a10fb80d17752e0ad90ffcfd2bf1df67791ed
Eonist <unicornhive@gmail.com>
2015-11-08 09:48:35 +0100

    Files modified: 1
    Modified 1 file:src/utils/ElementParser.swift

--- src/utils/ElementParser.swift
+++ src/utils/ElementParser.swift
@@ -7,15 +7,15 @@ class ElementParser{
      * @Note to get the stackString use: trace(SelectorUtils.toString(StyleResolver.stack(checkButton)));
      */
     class func selectors(element:IElement)->Array<ISelector>{
-        var elements:Array<IElement> = ArrayModifier.append(parents(element),element)
+        let elements:Array<IElement> = ArrayModifier.append(parents(element),element)
         var selectors:Array<ISelector> = []
         for  e : IElement in elements {
-            var selector:Selector = Selector();
+            let selector:Selector = Selector();
             selector.element = e.getClassType();
-            if(e.classId != null) selector.classIds = e.classId.indexOf(" ") != -1 ? e.classId.split(" ") : [e.classId];
-            selector.id = e.id;
-            selector.states = (e.skin != null ? e.skin.state : e.getSkinState()).match(/\b\w+\b/g);/*Matches words with spaces between them*/
-            selectors.push(selector);
+            //if(e.classId != null) selector.classIds = e.classId.indexOf(" ") != -1 ? e.classId.split(" ") : [e.classId];
+            //selector.id = e.id;
+            //selector.states = (e.skin != null ? e.skin.state : e.getSkinState()).match(/\b\w+\b/g);/*Matches words with spaces between them*/
+            selectors.append(selector);
         }
         return selectors;
     }

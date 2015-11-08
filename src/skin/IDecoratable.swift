d3cda300fd638889c714f3f27598c2af72c1771b
Eonist <unicornhive@gmail.com>
2015-11-08 19:44:57 +0100

    Files modified: 1
    Modified 1 file:src/skin/IDecoratable.swift

--- src/skin/IDecoratable.swift
+++ src/skin/IDecoratable.swift
@@ -1,5 +1,14 @@
 import Foundation
 
 protocol IDecoratable{
-    
+    func getGraphic()-> Graphic
+    func fill()
+    func line()
+    func drawFill()
+    func drawLine()
+    func clear()
+    func clearLine()
+    func beginFill()
+    func applyLineStyle(graphics:Graphics,lineStyle:ILineStyle)
+    func endFill()
 }

00c0f43910a133c36eb57261e81e21235b242a48
Eonist <unicornhive@gmail.com>
2015-11-08 08:40:09 +0100

    Files modified: 2
    Modified 2 files:src/ui/IElement.swiftsrc/utils/ElementParser.swift

--- src/ui/IElement.swift
+++ src/ui/IElement.swift
@@ -2,7 +2,7 @@ import Foundation
 protocol IElement:class, IView{
     func resolveSkin()
     var style:IStyle{get set}
-    func getParent()->IElement//TODO: maybe use weak?
+    func getParent()->IElement?//TODO: maybe use weak?
     func getClassType()->String
     var skinState:String{get}
 }
--- src/utils/ElementParser.swift
+++ src/utils/ElementParser.swift
@@ -7,11 +7,11 @@ class ElementParser{
      * Returns an array populated with IElement parents of the target (Basically the ancestry)
      */
     class func parents(element:IElement)->Array<IElement> {
-        var parents:Array = [];
-        var parent:IElement = element.getParent()// :TODO: seperate this into a check if its DO then that, if its NativeWindow then do that //note to self: this was styleable.parent as DisplayObject but it cant be anymore since it needs suport for NativeWindow
-        while(parent is IElement) {//loops up the object hierarchy as long as the parent is a StylableObject supertype
-            ArrayModifier.unshift(&parents,parent)
-            parent = (parent as IElement).getParent();
+        var parents:Array<IElement> = [];
+        var parent:IElement? = element.getParent()// :TODO: seperate this into a check if its DO then that, if its Window then do that
+        while(parent is IElement) {//loops up the object hierarchy as long as the parent is a Element supertype
+            ArrayModifier.unshift(&parents,parent!)
+            parent = parent!.getParent()
         }
         return parents;
     }

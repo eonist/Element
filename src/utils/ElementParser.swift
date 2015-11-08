7ee2d4be81c16dd56e24d58b25b460eb73179e40
Eonist <unicornhive@gmail.com>
2015-11-08 08:42:13 +0100

    Files modified: 1
    Modified 1 file:src/utils/ElementParser.swift

--- src/utils/ElementParser.swift
+++ src/utils/ElementParser.swift
@@ -2,14 +2,13 @@ import Foundation
 
 
 class ElementParser{
-
     /**
      * Returns an array populated with IElement parents of the target (Basically the ancestry)
      */
     class func parents(element:IElement)->Array<IElement> {
         var parents:Array<IElement> = [];
         var parent:IElement? = element.getParent()// :TODO: seperate this into a check if its DO then that, if its Window then do that
-        while(parent is IElement) {//loops up the object hierarchy as long as the parent is a Element supertype
+        while(parent != nil) {//loops up the object hierarchy as long as the parent is a Element supertype
             ArrayModifier.unshift(&parents,parent!)
             parent = parent!.getParent()
         }

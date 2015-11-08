b422135c655ad13b220a1c47d409795973845c0b
Eonist <unicornhive@gmail.com>
2015-11-08 16:50:24 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/Skin.swift

--- src/skin/skin/Skin.swift
+++ src/skin/skin/Skin.swift
@@ -10,4 +10,30 @@ class Skin:ISkin{
         self.state = state;
         self.element = element;
     }
+    /**
+     * Sets the style instance to apply to the skin also forces a redraw.
+     * @Note this is a great way to update an skin without querying StyleManager
+     */
+    func setStyle(style:IStyle){
+        hasStyleChanged = true;
+        self.style = style;
+        draw();
+    }
+    /**
+     * sets the skin state and forces a redraw
+     * @Note forces a lookup of the style in the StyleManager, since it has to look for the correct state of the style
+     */
+    func setState(state:String){
+        hasStateChanged = true;
+        self.state = state;
+        style = StyleResolver.style(element);/*looping through the entire styleManager isnt a good idea for just a state change*/
+        draw();
+    }
+    /**
+     * Sets the width and height of skin also forces a redraw.
+     * @Note similar to setStyle, this does not querry the styleManger when called
+     */
+    func setSize(width:Int, height:Int){
+        
+    }
 }

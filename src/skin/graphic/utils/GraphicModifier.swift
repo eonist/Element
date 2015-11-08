3e0abfd89e60d12dfff016e210f61de9ff1b7523
Eonist <unicornhive@gmail.com>
2015-11-08 14:38:36 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/GraphicModifier.swift

--- src/skin/graphic/utils/GraphicModifier.swift
+++ src/skin/graphic/utils/GraphicModifier.swift
@@ -5,24 +5,27 @@ class GraphicModifier {
     * TODO: fill and linestyle should be graphic spessific see original code
     */
     class func applyProperties(path:CGPath, _ graphics:Graphics,_ fillStyle:IStyle,_ lineStyle:IStyle, _ skinState:String) -> CGPath {
-        /*
         let fillColor:String = fillStyle.getStyleProperty("fillColor")!.value as! String
         let fillAlpha:Double = fillStyle.getStyleProperty("fillAlpha")!.value as! Double
+        /*
+        
+        
         */
         //let fillGradientType:String = fillStyle.getStyleProperty("fillGradientType")!.value as! String
         
+        /*
         let fillGradientColorA:String = fillStyle.getStyleProperty("fillGradientColorA")!.value as! String
         let fillGradientColorB:String = fillStyle.getStyleProperty("fillGradientColorB")!.value as! String
         
         let lineColor:String = lineStyle.getStyleProperty("lineColor")!.value as! String
         let lineAlpha:Double = lineStyle.getStyleProperty("lineAlpha")!.value as! Double
         let lineWidth:Int = lineStyle.getStyleProperty("lineWidth")!.value as! Int
+        */
         
-        //Conitnue here replace some of these for the gradient ones
         
         
-        //let nsFillColor = ColorParser.nsColor(fillColor, Float(fillAlpha))//fill
-        let nsLineColor = ColorParser.nsColor(lineColor, Float(lineAlpha))//line
+        let nsFillColor = ColorParser.nsColor(fillColor, Float(fillAlpha))//fill
+        //let nsLineColor = ColorParser.nsColor(lineColor, Float(lineAlpha))//line
         //ViewModifier.applyColor(self as! NSView, nsFillColor, nsLineColor, lineWidth)
         
         //path.lineWidth = CGFloat(lineWidth)
@@ -30,15 +33,16 @@ class GraphicModifier {
         //nsFillColor.setFill();
         //nsLineColor.setStroke();
         
-        
+        /*
         let nsFillGradientColorA = ColorParser.nsColor(fillGradientColorA, Float(1))
         let nsFillGradientColorB = ColorParser.nsColor(fillGradientColorB, Float(1))
         graphics.line(CGFloat(lineWidth),nsLineColor)//Stylize the line
-        //graphics.fill(nsFillColor)//Stylize the fill
+
+        
         let gradient:Gradient = Gradient([nsFillGradientColorA.CGColor,nsFillGradientColorB.CGColor])
         graphics.gradientFill(gradient)
-        
-        
+        */
+        graphics.fill(nsFillColor)//Stylize the fill
         return path;
         
         

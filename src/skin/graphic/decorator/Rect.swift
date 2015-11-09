e67913043ae4c41aba0ec2695d7cd56fddbb17fa
Eonist <unicornhive@gmail.com>
2015-11-09 08:40:18 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/Rect.swift

--- src/skin/graphic/decorator/Rect.swift
+++ src/skin/graphic/decorator/Rect.swift
@@ -1,11 +1,17 @@
 import Foundation
 /*
-* // :TODO: impliment margin padding ?!? maybe not
-* // :TODO: Try to impliment Rect3 into Element2 and do tests
-* // :TODO: add example in the javadoc
-*/
+ * // :TODO: impliment margin padding ?!? maybe not
+ * // :TODO: Try to impliment Rect3 into Element2 and do tests
+ * // :TODO: add example in the javadoc
+ */
 class Rect : Graphic{
+    var _width:Int;
+    var _height:Int;
     override init(_ fillStyle:IFillStyle? = nil, _ lineStyle:ILineStyle? = nil){
         super.init(fillStyle, lineStyle )
+        self.width = width;
+        self.height = height;
+        fill();
+        line();
     }
 }

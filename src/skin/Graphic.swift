314856f072c4f103d91ed5e25d48a5b054377458
Eonist <unicornhive@gmail.com>
2015-11-09 05:45:07 +0100

    Files modified: 1
    Modified 1 file:src/skin/Graphic.swift

--- src/skin/Graphic.swift
+++ src/skin/Graphic.swift
@@ -2,6 +2,7 @@ import Foundation
 /*
 * The graphic class is an Element DataObject class that holds the lineshape, lineStyle and fillStyle
 * // :TODO: possibly get rid of the setters for the fillStyle and Line style and use implicit setFillStyle and setLineStyle?
+* NOTE: We dont need a line mask, just subclass the Graphics class so it supports masking of the line aswell (will require some effort)
 */
 class Graphic:IGraphic{//this will extend Graphics in the future
     var fillStyle:IFillStyle?

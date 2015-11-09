00e2bbca4b37afe01bde68edb1d66c04c33b6afc
Eonist <unicornhive@gmail.com>
2015-11-09 09:53:01 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/SkinManager.swift

--- src/skin/skin/SkinManager.swift
+++ src/skin/skin/SkinManager.swift
@@ -1,13 +1,13 @@
 import Foundation
 class SkinManager{
     /**
-    * Returns a new SkinClass instance
-    */
+     * Returns a new SkinClass instance
+     */
     func getSkinInstance(skinName:String,element:IElement,style:IStyle)->ISkin{
         switch skinName{
-            case SkinFactory.graphicsSkin : return GraphicSkin(style,element.skin!.state!,element)
+            case SkinFactory.graphicsSkin : return GraphicSkin(style, element.skin!.state, element)
             case SkinFactory.textSkin : fatalError("NOT IMPLEMENTED YET")
-            default: break;
+            default: fatalError("NOT IMPLEMENTED YET")
         }
     }
 }

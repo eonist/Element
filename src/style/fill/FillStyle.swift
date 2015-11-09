128f08cbfc5d52db17a18a29274e1f90023a7112
Eonist <unicornhive@gmail.com>
2015-11-09 06:29:59 +0100

    Files modified: 2
    Modified 2 files:src/style/fill/FillStyle.swiftsrc/style/fill/GradientFillStyle.swift

--- src/style/fill/FillStyle.swift
+++ src/style/fill/FillStyle.swift
@@ -1,5 +1,17 @@
-import Foundation
+
+import Cocoa
 
 class FillStyle {
+    
+    var color:NSColor
+    /**
+     *
+     */
+    init(_ color:NSColor = NSColor.clearColor()){
+        self.color = color
+    }
+}
 
+extension IFillStyle {
+    var cgColor: CGColor {return NSColorParser.cgColor(color)}
 }
--- src/style/fill/GradientFillStyle.swift
+++ src/style/fill/GradientFillStyle.swift
@@ -3,6 +3,7 @@ import Cocoa
 class GradientFillStyle:FillStyle{
     var gradient:IGradient;
     init(gradient:IGradient = nil, color:NSColor){
-        
+        super.init(color,alpha);
+        _gradient = gradient;
     }
 }

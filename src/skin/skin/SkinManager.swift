38025503c80b546549c7f65a6bb9ecd07c356113
Eonist <unicornhive@gmail.com>
2015-11-09 09:40:25 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/SkinManager.swift

--- src/skin/skin/SkinManager.swift
+++ src/skin/skin/SkinManager.swift
@@ -4,7 +4,9 @@ class SkinManager{
     * Returns a new SkinClass instance
     */
     func getSkinInstance(skinName:String,element:IElement,style:IStyle):Skin{
-        var skin:ISkin
+        switch skinName{
+            case SkinFactory.graphicsSkin : SkinClass(element.width,element.height,style,element.skin.state);
+        }
     
         if(skin is Function) return skin(element,style);
         else if (skin is Class) return Util.skinClassInstance(skin,element,style);

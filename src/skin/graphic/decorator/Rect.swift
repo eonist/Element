e60755e51cf786391fdc65f9c90ffabc4df386d0
Eonist <unicornhive@gmail.com>
2015-11-09 08:50:22 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/Rect.swift

--- src/skin/graphic/decorator/Rect.swift
+++ src/skin/graphic/decorator/Rect.swift
@@ -5,13 +5,49 @@ import Foundation
  * // :TODO: add example in the javadoc
  */
 class Rect : Graphic{
-    var _width:Int;
-    var _height:Int;
+    var width:Int;
+    var height:Int;
     override init(_ fillStyle:IFillStyle? = nil, _ lineStyle:ILineStyle? = nil){
         super.init(fillStyle, lineStyle )
         self.width = width;
         self.height = height;
         fill();
         line();
+    }
+    func fill() {
+      beginFill();
+      drawFill();
+      endFill();
+    }
+    func beginFill() {
+      if(fillStyle != nil && fillStyle.color != NSColor.clearColor() {
+         //beginFill drawing;/*Updates only if fillStyle is of class FillStyle*/
+      }
+    }
+    /**
+     * // :TODO: does this function need arguments?
+     */
+    func applyLineStyle(graphics:Graphics,lineStyle:ILineStyle) {
+      if(lineStyle != nil) {
+         //apply lineStyle here /*updates only if lineStyle of class LineStyle*/
+      }
+    }
+    func drawFill() {
+      //do fill drawing here
+    }
+    func drawLine() {
+      if(lineStyle != nil){
+          //do line drawing here, keep in mind line mask
+      }
+    }
+    func endFill(){
+      //print("Rect3.endFill");
+      fillShape.graphics.endFill();
+    }
+    func setSize(width:Number,height:Number) {
+      self.width = width;
+      self.height = height;
+      fill();
+      line();
     }
 }

9a468fd34d78fa8d0cab1181abfe12b7535ba2e0
Eonist <unicornhive@gmail.com>
2015-11-09 05:34:06 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/GraphicSkinParser.swift

--- src/skin/skin/utils/GraphicSkinParser.swift
+++ src/skin/skin/utils/GraphicSkinParser.swift
@@ -1,1 +1,16 @@
 import Foundation
+
+
+class GraphicSkinParser{
+   static var rect:String = "rect";
+   static var gradient:String = "gradient";
+   static var icon:String = "icon";// :TODO: rename to asset or svgasset or alike (keep in mind that you will need to support png assets soon enough)
+   static var fillet:String = "fillet";// :TODO: rename to ROUNDED?
+   static var dropshadow:String = "dropshadow";
+    /**
+    * Returns a Decoratable instance based on what stylePropertyValues is found in @param skin at @param depth
+    */
+    public static function configure(skin:ISkin,depth:int):IDecoratable3 {
+    
+    }
+}

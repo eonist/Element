bb2813add25294030905f24ab4becbd87fe4ad80
Eonist <unicornhive@gmail.com>
2015-11-09 09:57:07 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/SkinResolver.swift

--- src/skin/skin/SkinResolver.swift
+++ src/skin/skin/SkinResolver.swift
@@ -18,6 +18,16 @@ class SkinResolver{
         
         return nil
     }
+    /**
+     * Throws an error message if a skin cant be resolved (with usefull information for debugging)
+     */
+    private static function resolveError(style:IStyle,element:IElement):* {// :FIXME: argument type should be IAurora2
+    trace("with parent: " + (element.parent || ""));
+    trace("with skin: "+element.skin || "");
+    trace("with style.selector: "+style.name || "");//could have to be altred
+    throw new Error( "SKINRESOLVER: NO SKIN COULD BE RESOLVED FOR ELEMENT BY THE ID: "+element.id);
+    return null;
+    }
 }
 
 private class Utils{

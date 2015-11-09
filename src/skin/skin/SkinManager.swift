a1bba6c936da884abd3f140229d141f66d1bb798
Eonist <unicornhive@gmail.com>
2015-11-09 09:36:16 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/SkinManager.swift

--- src/skin/skin/SkinManager.swift
+++ src/skin/skin/SkinManager.swift
@@ -1,4 +1,4 @@
 import Foundation
 class SkinManager{
-    static var skinClasses:Dictionary<String,String> = Dictionary<String,String>();
+    static var skinClasses:Dictionary<String,ISkin> = [SkinFactory.graphicsSkin = SkinFactory.graphicSkin(<#T##element: IElement##IElement#>, style: <#T##IStyle#>),SkinFactory.textSkin]
 }

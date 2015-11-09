2b10a90fa71967f0534addc010dbc90ca392f696
Eonist <unicornhive@gmail.com>
2015-11-09 07:30:55 +0100

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyParser.swift

--- src/style/property/utils/StylePropertyParser.swift
+++ src/style/property/utils/StylePropertyParser.swift
@@ -6,7 +6,7 @@ class StylePropertyParser{
      * @Note the reason that depth defaults to 0 is because if the exact depth isnt found there should only be depth 0, if you have more than 1 depth in a property then you must supply at all depths or just the 1 that will work for all depths
      * // :TODO: should probably also support when state is know and depth is defaulted to 0 ?!?!?
      */
-    class func value(skin:ISkin, propertyName:String/*, depth:int = 0*/)->Any?{
+    class func value(skin:ISkin, _ propertyName:String/*, depth:int = 0*/)->Any?{
         return nil
     }
     class func fillStyle(skin:ISkin):IFillStyle {
@@ -16,10 +16,10 @@ class StylePropertyParser{
     * Returns a FillStyle instance
     */
     class func colorFillStyle(skin:ISkin)->IFillStyle {
-        var value:String = StylePropertyParser.value(skin, CSSConstants.fill);
+        let value:String = StylePropertyParser.value(skin, CSSConstants.fill);
         //print("value: " + value);
-        var alpha:String = StylePropertyParser.value(skin, CSSConstants.fillAlpha);
-        var color:NSColor = value == CSSConstants.none ? NSColor.clearColor() : ColorParser.nsColor(fillColor, Float(alpha))//fill
+        let alpha:String = StylePropertyParser.value(skin, CSSConstants.fillAlpha);
+        let color:NSColor = value == CSSConstants.none ? NSColor.clearColor() : ColorParser.nsColor(value, Float(alpha)!)//fill
         
         return FillStyle(color)
     }

fe5090d9d05d52a68243c60daaf84c696bd5c5d1
Eonist <unicornhive@gmail.com>
2015-11-09 09:46:37 +0100

    Files modified: 2
    Modified 2 files:src/skin/skin/ISkin.swiftsrc/skin/skin/SkinManager.swift

--- src/skin/skin/ISkin.swift
+++ src/skin/skin/ISkin.swift
@@ -4,5 +4,9 @@ protocol ISkin{
     var style:IStyle?{get}
     var state:String{get set}
     var element:IElement?{get}/*We use IElement here instead of Element because sometimes we need to use Window which is not an Element but impliments IElement*/
-
+    var width:Int?{get}
+    var height:Int?{get}
+    var hasStyleChanged:Bool{get}
+    var hasStateChanged:Bool{get}
+    var hasSizeChanged:Bool{get}
 }
--- src/skin/skin/SkinManager.swift
+++ src/skin/skin/SkinManager.swift
@@ -5,13 +5,9 @@ class SkinManager{
     */
     func getSkinInstance(skinName:String,element:IElement,style:IStyle)->ISkin{
         switch skinName{
-            case SkinFactory.graphicsSkin : return GraphicSkin(element.width,element.height,style,element.skin.state)
+            case SkinFactory.graphicsSkin : return GraphicSkin(element.width,element.height,style,element.skin!.state!)
             case SkinFactory.textSkin : fatalError("NOT IMPLEMENTED YET")
             default: break;
         }
-    
-        if(skin is Function) return skin(element,style);
-        else if (skin is Class) return Util.skinClassInstance(skin,element,style);
-        else return null;
     }
 }

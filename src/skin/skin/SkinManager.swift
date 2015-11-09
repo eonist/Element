02a22060ca2ca8f21fc5edf1dbc8a7ea3864d744
Eonist <unicornhive@gmail.com>
2015-11-09 09:38:20 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/SkinManager.swift

--- src/skin/skin/SkinManager.swift
+++ src/skin/skin/SkinManager.swift
@@ -1,4 +1,13 @@
 import Foundation
 class SkinManager{
-    static var skinClasses:Dictionary<String,ISkin> = [SkinFactory.graphicsSkin = SkinFactory.graphicSkin(<#T##element: IElement##IElement#>, style: <#T##IStyle#>),SkinFactory.textSkin]
+    /**
+    * Returns a new SkinClass instance
+    */
+    func getSkinInstance(skinName:String,element:IElement,style:IStyle):Skin{
+        var skin:ISkin
+    
+        if(skin is Function) return skin(element,style);
+        else if (skin is Class) return Util.skinClassInstance(skin,element,style);
+        else return null;
+    }
 }

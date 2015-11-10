e694c3757d489093f09cee43ddf338c38287f911
Eonist <unicornhive@gmail.com>
2015-11-10 06:25:31 +0100

    Files modified: 1
    Modified 1 file:src/skin/Graphic.swift

--- src/skin/Graphic.swift
+++ src/skin/Graphic.swift
@@ -4,14 +4,16 @@ import Foundation
 * // :TODO: possibly get rid of the setters for the fillStyle and Line style and use implicit setFillStyle and setLineStyle?
 * NOTE: We dont need a line mask, just subclass the Graphics class so it supports masking of the line aswell (will require some effort)
 */
-class Graphic:IGraphic{//this will extend Graphics in the future
+class Graphic:IGraphic{//this will extend Graphics in the future or just have it
     var fillStyle:IFillStyle?
     var lineStyle:ILineStyle?
+    let graphics:Graphics
     //var lineOffsetType:OffsetType
     init(_ fillStyle:IFillStyle? = nil, _ lineStyle:ILineStyle? = nil/*, _ lineOffsetType:OffsetType = OffsetType()*/){
         self.fillStyle = fillStyle
         self.lineStyle = lineStyle
         /*self.lineOffsetType = lineOffsetType*/
+        graphics = Graphics()
     }
     func setPosition(position:CGPoint){
         //TODO:translate the graphics to position

3476fb5c9c7dadf7dde053b794b957aa41b3c44f
Eonist <unicornhive@gmail.com>
2015-11-10 19:23:46 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RoundRectGraphic.swift

--- src/skin/graphic/decorator/RoundRectGraphic.swift
+++ src/skin/graphic/decorator/RoundRectGraphic.swift
@@ -6,14 +6,21 @@ class RoundRectGraphic:Decoratable,IRoundRectGraphic{
         self.fillet = fillet ?? Fillet();
         super.init(decoratable);
         fill();
-        line();
+        //line();
     }
     override func fill() {
         beginFill();
         drawFill();
     }
     override func line() {
-        applyLineStyle(graphic.graphics, graphic.lineStyle);
-        drawLine();
+        
+    }
+    /**
+     * Draws the fill
+     */
+    override func drawFill():void {
+        var x:Number = graphic.lineOffsetType.left == OffsetType.OUTSIDE ? graphic.lineStyle.thickness : 0;
+        var y:Number = graphic.lineOffsetType.top == OffsetType.OUTSIDE ? graphic.lineStyle.thickness : 0;
+        graphic.fillShape.graphics.drawRoundRectComplex(x, y, graphic.width, graphic.height, _fillet.topLeft, _fillet.topRight, _fillet.bottomLeft, _fillet.bottomRight);
     }
 }

4b80e769dc45cbcab8ab514aecfc48b999161d97
Eonist <unicornhive@gmail.com>
2015-11-11 08:16:32 +0100

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyAsserter.swift

--- src/style/property/utils/StylePropertyAsserter.swift
+++ src/style/property/utils/StylePropertyAsserter.swift
@@ -1,26 +1,26 @@
 import Foundation
 
 class StylePropertyAsserter {
-    class func hasFillet(style:IStyle)->Bool {
-        /*
-        var fillet:Fillet = StylePropertyParser.fillet(skin,depth);
+    class func hasFillet(skin:ISkin)->Bool {
+       
+        var fillet:Fillet = StylePropertyParser.fillet(skin);
         return !(fillet.topLeft == 0 && fillet.topRight == 0 && fillet.bottomLeft == 0 && fillet.bottomRight == 0);
-        */
+        
         return false
     }
-    class func hasGradient(style:IStyle)->Bool {
+    class func hasGradient(skin:ISkin)->Bool {
         /*
         return StylePropertyParser.value(skin, CSSConstants.FILL, depth) is Gradient || StylePropertyParser.value(skin, "line", depth) is Gradient;
         */
         return false
     }
-    class func hasAsset(style:IStyle)->Bool {
+    class func hasAsset(skin:ISkin)->Bool {
         /*
         return StylePropertyParser.value(skin, CSSConstants.FILL, depth) is Array;
         */
         return false
     }
-    class func hasDropShadow(style:IStyle)->Bool {
+    class func hasDropShadow(skin:ISkin)->Bool {
         /*
         return StylePropertyParser.value(skin, CSSConstants.DROP_SHADOW, depth) is DropShadowFilter;
         */

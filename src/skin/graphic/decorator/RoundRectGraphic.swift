bc976555ad7df5012f314ebf8e2710b4f71c9d89
Eonist <unicornhive@gmail.com>
2015-11-12 08:39:27 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RoundRectGraphic.swift

--- src/skin/graphic/decorator/RoundRectGraphic.swift
+++ src/skin/graphic/decorator/RoundRectGraphic.swift
@@ -21,11 +21,11 @@ class RoundRectGraphic:Decoratable,IRoundRectGraphic{
      */
     override func drawFill(){
         Swift.print("RoundRectGraphic.drawFill() ")
-        let w:Double = (decoratable as! RectGraphic).width
+        let w:Double = 300//(decoratable as! RectGraphic).width
         Swift.print("w: " + "\(w)")
         let h:Double = (decoratable as! RectGraphic).height
-        Swift.print("w: " + "\(h)")
-        
+        Swift.print("h: " + "\(h)")
+        Swift.print("fillet.topLeft: " + "\(fillet.topLeft)")
         getGraphic().path = CGPathParser.roundRect(0,0,CGFloat(w), CGFloat(h),CGFloat(fillet.topLeft), CGFloat(fillet.topRight), CGFloat(fillet.bottomLeft), CGFloat(fillet.bottomRight))//Shapes
         GraphicModifier.stylize(getGraphic().path,getGraphic().graphics)//realize style on the graphic
     }

f104befdc2b890251cf6100969546efba190adb7
Eonist <unicornhive@gmail.com>
2015-11-13 07:43:22 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RectGraphic.swift

--- src/skin/graphic/decorator/RectGraphic.swift
+++ src/skin/graphic/decorator/RectGraphic.swift
@@ -7,22 +7,22 @@ import Cocoa
 class RectGraphic : Decoratable{
     var width:Double;
     var height:Double;
-    init(_ width:Double = 100, _ height:Double = 100){
+    init(_ decoratable:IDecoratable, _ width:Double = 100, _ height:Double = 100){
         self.width = width;
         self.height = height;
         super.init(decoratable);
     }
-    
     override func drawFill() {
         Swift.print("RectGraphic.drawFill()")
         //do fill drawing here
         getGraphic().path = CGPathParser.rect(CGFloat(width), CGFloat(height))//Shapes
         super.drawFill()
     }
-    func drawLine() {
-      if(lineStyle != nil){
+    override func drawLine() {
+      if(getGraphic().lineStyle != nil){
           //do line drawing here, keep in mind line mask
       }
+      super.drawLine()
     }
     func setSize(width:Double,height:Double) {
       self.width = width;
@@ -30,5 +30,4 @@ class RectGraphic : Decoratable{
       fill();
       line();
     }
-    
 }

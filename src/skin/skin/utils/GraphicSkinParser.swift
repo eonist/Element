37e7d40ba1eb68e7b50a91a0b9a212e23d593ec3
Eonist <unicornhive@gmail.com>
2015-11-14 14:58:05 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/GraphicSkinParser.swift

--- src/skin/skin/utils/GraphicSkinParser.swift
+++ src/skin/skin/utils/GraphicSkinParser.swift
@@ -14,6 +14,7 @@ class GraphicSkinParser{
         let fillStyle:IFillStyle = StylePropertyParser.fillStyle(skin);
         var decoratable:IDecoratable = Utils.shape(fillStyle)
         decoratable = Utils.rectGraphic(decoratable,skin);
+        decoratable.getShape().path = CGPathParser.rect(CGFloat(250), CGFloat(250))//Shapes
         //decoratable.getShape().clear()
         if(StylePropertyAsserter.hasFillet(skin)) { decoratable = Utils.fillet(decoratable, StylePropertyParser.fillet(skin)) }
         return decoratable

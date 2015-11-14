9b66c729b3b17cc946551ee872aaa1e4cb8aaefa
Eonist <unicornhive@gmail.com>
2015-11-14 10:03:26 +0100

    Files modified: 1
    Modified 1 file:src/ui/Element.swift

--- src/ui/Element.swift
+++ src/ui/Element.swift
@@ -19,8 +19,6 @@ class Element: FlippedView,IElement {
         let frame = NSRect(x: 0, y: 0, width: width, height: height)
         super.init(frame: frame)
         self.wantsLayer = false//this avoids calling drawLayer() and enables drawingRect()
-        
-        
     }
     /**
      * Note: if you overide drawRect then update layers wont work

83e0b2b545cca07596da8b452ef91bfd51e0c49f
Eonist <unicornhive@gmail.com>
2015-11-14 07:29:25 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/IShape.swift

--- src/skin/graphic/IShape.swift
+++ src/skin/graphic/IShape.swift
@@ -2,7 +2,7 @@ import Foundation
 /*
 * 
 */
-protocol IGraphic{
+protocol IShape{
     var lineStyle:ILineStyle?{get}
     var fillStyle:IFillStyle?{get}
     /*var lineOffsetType:OffsetType{get set}*/

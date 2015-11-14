e7fcc2b26cd2e8e50db8d1a4c87ef5f4b2490912
Eonist <unicornhive@gmail.com>
2015-11-14 06:34:20 +0100

    Files modified: 1
    Modified 1 file:src/skin/Graphic.swift

--- src/skin/Graphic.swift
+++ src/skin/Graphic.swift
@@ -15,7 +15,7 @@ import Cocoa
 //That are also only concerning themselfs with just making the path etc. less code!
 
 
-
+//extend nsview,
 
 class Graphic:IGraphic,IDecoratable{//this will extend Graphics in the future or just have it
     var fillStyle:IFillStyle?

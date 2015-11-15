eafab2c232afe4cefb4df15af30b3e2337c2ed59
Eonist <unicornhive@gmail.com>
2015-11-15 07:53:56 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/IBaseGraphic.swift

--- src/skin/graphic/decorator/IBaseGraphic.swift
+++ src/skin/graphic/decorator/IBaseGraphic.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 protocol IBaseGraphic {
-
+    func getGraphic() -> BaseGraphic
+    func fill()
+    func beginFill()
+    func drawFill()
+    func stylizeFill()
+    func initialize()
 }

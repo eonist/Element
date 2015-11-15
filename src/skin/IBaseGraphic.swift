0e7df0e588a973b35ff89b8b801ee8f9ad0e10bd
Eonist <unicornhive@gmail.com>
2015-11-15 08:43:42 +0100

    Files modified: 2
    Modified 2 files:src/skin/IBaseGraphic.swiftsrc/skin/graphic/decorator/BaseGraphic.swift

--- src/skin/IBaseGraphic.swift
+++ src/skin/IBaseGraphic.swift
@@ -4,5 +4,7 @@ import Foundation
 protocol IBaseGraphic {
     var lineStyle:ILineStyle?{get}
     var fillStyle:IFillStyle?{get}
+    func setProperties(fillStyle:IFillStyle?, lineStyle:ILineStyle?)
+    func setPosition(position:CGPoint)
     /*var lineOffsetType:OffsetType{get set}*/
 }
--- src/skin/graphic/decorator/BaseGraphic.swift
+++ src/skin/graphic/decorator/BaseGraphic.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-class BaseGraphic :AbstractGraphicDecoratable{
+class BaseGraphic :AbstractGraphicDecoratable,IBaseGraphic{
     var fillStyle:IFillStyle?
     var lineStyle:ILineStyle?
     lazy var graphics:Graphics = Graphics()
@@ -15,15 +15,14 @@ class BaseGraphic :AbstractGraphicDecoratable{
     override func stylizeFill(){
         GraphicModifier.stylize(getGraphic().path,getGraphic().graphics)//realize style on the graphic
     }
-    override func getGraphic()->BaseGraphic{
-        return self
-    }
     func setPosition(position:CGPoint){
-        //TODO:translate the graphics to position
         CGPathModifier.translate(&path,position.x,position.y)//Transformations
     }
     func setProperties(fillStyle:IFillStyle? = nil, lineStyle:ILineStyle? = nil){// :TODO: remove this and replace with setLineStyle and setFillStyle ?
         self.fillStyle = fillStyle;
         self.lineStyle = lineStyle;
+    }
+    override func getGraphic()->BaseGraphic{
+        return self
     }
 }

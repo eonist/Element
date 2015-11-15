8b8cd4c8fb1b85b73757d8fc23727c8b820a621b
Eonist <unicornhive@gmail.com>
2015-11-15 08:41:48 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/BaseGraphic.swift

--- src/skin/graphic/decorator/BaseGraphic.swift
+++ src/skin/graphic/decorator/BaseGraphic.swift
@@ -5,7 +5,10 @@ class BaseGraphic :AbstractGraphicDecoratable{
     var lineStyle:ILineStyle?
     lazy var graphics:Graphics = Graphics()
     var path:CGPath = CGPathCreateMutable()
-    
+    init(_ fillStyle:IFillStyle? = nil, _ lineStyle:ILineStyle? = nil/*, _ lineOffsetType:OffsetType = OffsetType()*/) {
+        self.fillStyle = fillStyle
+        self.lineStyle = lineStyle
+    }
     override func beginFill(){
         GraphicModifier.applyProperties(getGraphic().graphics, FillStyle(NSColor.purpleColor())/*, lineStyle*/)//apply style
     }
@@ -14,5 +17,13 @@ class BaseGraphic :AbstractGraphicDecoratable{
     }
     override func getGraphic()->BaseGraphic{
         return self
+    }
+    func setPosition(position:CGPoint){
+        //TODO:translate the graphics to position
+        CGPathModifier.translate(&path,position.x,position.y)//Transformations
+    }
+    func setProperties(fillStyle:IFillStyle? = nil, lineStyle:ILineStyle? = nil){// :TODO: remove this and replace with setLineStyle and setFillStyle ?
+        self.fillStyle = fillStyle;
+        self.lineStyle = lineStyle;
     }
 }

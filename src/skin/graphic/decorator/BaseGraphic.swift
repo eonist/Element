e54ff770c0712e6a290f8a4e2940cc75a1bcdbd7
Eonist <unicornhive@gmail.com>
2015-11-15 08:01:34 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/BaseGraphic.swift

--- src/skin/graphic/decorator/BaseGraphic.swift
+++ src/skin/graphic/decorator/BaseGraphic.swift
@@ -1,5 +1,16 @@
-import Foundation
+import Cocoa
 
-class BaseGraphic {
-
+class BaseGraphic :AbstractGraphicDecorator{
+    lazy var graphics:Graphics = Graphics()
+    var path:CGPath = CGPathCreateMutable()
+    
+    override func beginFill(){
+        GraphicModifier.applyProperties(getGraphic().graphics, FillStyle(NSColor.purpleColor())/*, lineStyle*/)//apply style
+    }
+    override func stylizeFill(){
+        GraphicModifier.stylize(getGraphic().path,getGraphic().graphics)//realize style on the graphic
+    }
+    override func getGraphic()->BaseGraphic{
+        return self
+    }
 }

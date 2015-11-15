eb1e8875e7a4bc4d1328d54f8323d9ebf85f8fae
Eonist <unicornhive@gmail.com>
2015-11-15 08:22:33 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RoundRectGraphic2.swift

--- src/skin/graphic/decorator/RoundRectGraphic2.swift
+++ src/skin/graphic/decorator/RoundRectGraphic2.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-class RoundRectGraphic2 {
-
+class RoundRectGraphic2:GraphicDecorator{//adds round-rectangular path
+    override func drawFill() {
+        let fillet:Fillet = Fillet(20)
+        getGraphic().path = CGPathParser.roundRect(0,0,CGFloat(200), CGFloat(200),CGFloat(fillet.topLeft), CGFloat(fillet.topRight), CGFloat(fillet.bottomLeft), CGFloat(fillet.bottomRight))//Shapes
+    }
 }

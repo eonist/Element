ff4a4666fc855a15baa70c55faac5bb34496716f
Eonist <unicornhive@gmail.com>
2015-11-15 08:09:16 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RectGraphic2.swift

--- src/skin/graphic/decorator/RectGraphic2.swift
+++ src/skin/graphic/decorator/RectGraphic2.swift
@@ -1,5 +1,7 @@
 import Foundation
 
-class RectGraphic2 {
-
+class RectGraphic2:GraphicDecorator{
+    override func drawFill() {
+        getGraphic().path = CGPathParser.rect(CGFloat(100), CGFloat(100))//Shapes
+    }
 }

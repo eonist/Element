cb13632dc87a3ddfa2d2fc328b6da8887095a0ab
Eonist <unicornhive@gmail.com>
2015-11-15 08:01:34 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/AbstractGraphicDecorator.swift

--- src/skin/graphic/decorator/AbstractGraphicDecorator.swift
+++ src/skin/graphic/decorator/AbstractGraphicDecorator.swift
@@ -1,5 +1,19 @@
 import Foundation
-
-class AbstractGrapixDecorator {
-
+/*
+* The AbstractDecorator has all the decorator methods
+*/
+class AbstractGraphicDecorator:IGraphicDecorator{
+    func initialize(){
+    }
+    func fill(){
+    }
+    func beginFill(){
+    }
+    func drawFill(){
+    }
+    func stylizeFill(){
+    }
+    func getGraphic() -> BaseGraphic{
+        fatalError("Must be overridden in subClass")
+    }
 }

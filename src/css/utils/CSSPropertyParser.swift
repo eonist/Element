d42caf420524a58b5e76a7e39cff0de259168d14
Eonist <unicornhive@gmail.com>
2015-11-15 09:26:00 +0100

    Files modified: 1
    Modified 1 file:src/css/utils/CSSPropertyParser.swift

--- src/css/utils/CSSPropertyParser.swift
+++ src/css/utils/CSSPropertyParser.swift
@@ -15,6 +15,8 @@ class CSSPropertyParser {
             case StringAsserter.boolean(string):return StringParser.boolean(string);/*true or false*/
             case StringAsserter.color(string):return StringParser.color(string);/*#00ff00 or 00ff00*/
             case StringAsserter.webColor(string):return StringParser.color(string);/*green red etc*/
+            
+            case RegExp.test(string,"^linear-gradient\\b"):return linearGradient(string);/*linear-gradient*/// :TODO: create a more complte exprrison for this test
             case RegExp.test(string,"^([\\w\\d\\/\\%\\-\\.]+?\\\040)+?(\\b|\\B|$)"):return array(string);/*corner-radius, line-offset-type, margin, padding, offset*/// :TODO: shouldnt the \040 be optional?
             default : fatalError("CSSPropertyParser.property() THE: " + string + " PROPERTY IS NOT SUPPORTED");
         }

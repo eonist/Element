260d1ef741805d24dffdb710f33952ce8f639692
Eonist <unicornhive@gmail.com>
2015-11-15 09:27:56 +0100

    Files modified: 1
    Modified 1 file:src/css/utils/CSSPropertyParser.swift

--- src/css/utils/CSSPropertyParser.swift
+++ src/css/utils/CSSPropertyParser.swift
@@ -22,6 +22,19 @@ class CSSPropertyParser {
         }
     }
     /**
+    * // :TODO: possibly use the RegExp.exec to loop the properties!!
+    * @param string "linear-gradient(top,gray 1 0,white 1 1);"// 2 color gradient
+    * @Note setting the gradientType isnt necessary since its the default setting
+    */
+    class func linearGradient(string:String)->IGradient{
+        var propertyString:String = string.match(/(?<=linear-gradient\().+?(?=\);?)/).toString();
+        var properties:Array = propertyString.split(",");
+        var rotation:Number = Utils.rotation(properties.shift());/*the first item is always the rotation, top or left or top left etc*/
+        var gradient:IGradient = Utils.gradient(properties);/*add colors, opacities and ratios*/
+        gradient.rotation = rotation*Trig.RAD;// :TODO: rotations should be applied in the matrix
+        return gradient;
+    }
+    /**
      * Returns an array comprised of values if the individual value is a digit then it is processed as a digit if its not a digit then its just processed as a string
      * // :TODO: does this support comma delimited lists?
      * EXAMPLE: a corner-radius "10 20 10 20"

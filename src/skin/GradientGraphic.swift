a1124a1e9a5656e10a920ea7b498c0dc343c70d3
Eonist <unicornhive@gmail.com>
2015-11-16 09:07:14 +0100

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -3,7 +3,7 @@ import Cocoa
 class GradientGraphic:GraphicDecoratable {
     override func beginFill(){
         if(getGraphic().fillStyle.dynamicType is GradientFillStyle.Type){
-            FillStyleModifier.beginGradientFill(getGraphic().fillShape.graphics, GradientFillStyle(getGraphic().fillStyle).gradient)
+            FillStyleModifier.beginGradientFill(getGraphic().graphics, GradientFillStyle(getGraphic().fillStyle,NSColor.clearColor()).gradient)
         }
     }
 }

6efbf7f0b209c231e61edd26fdc1de6b4d99b894
Eonist <unicornhive@gmail.com>
2015-11-16 08:37:41 +0100

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 class GradientGraphic:GraphicDecoratable {
     override func beginFill(){
-        
-        ObjectAsserter.ofClass(graphic.fillStyle, GradientFillStyle) ? FillStyleModifier.beginGradientFill(graphic.fillShape.graphics, GradientFillStyle(graphic.fillStyle).gradient) : decoratable.beginFill();/*If there is no GradienFillStyle then beginFill with with whatever is in the decoratable*/;
+        graphic.fillStyle is GradientFillStyle
+        ObjectAsserter.ofClass(, ) ? FillStyleModifier.beginGradientFill(graphic.fillShape.graphics, GradientFillStyle(graphic.fillStyle).gradient) : decoratable.beginFill();/*If there is no GradienFillStyle then beginFill with with whatever is in the decoratable*/;
     }
 }

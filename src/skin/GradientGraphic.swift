89fa1762dc9b514a231f4f4072acc871e717dfe7
Eonist <unicornhive@gmail.com>
2015-11-16 07:41:46 +0100

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -1,5 +1,9 @@
-import Foundation
+import Cocoa
 
-class GradientGraphic {
-
+class GradientGraphic:GraphicDecoratable {
+    override func beginFill(){
+        if(fillStyle != nil && fillStyle!.color != NSColor.clearColor() ) {/*Updates only if fillStyle is of class FillStyle*/
+            getGraphic().graphics.fill(fillStyle!.color)//Stylize the fill
+        }
+    }
 }

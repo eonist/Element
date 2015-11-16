3dc30d2f22b53db266652f84963ffaa40ee46aff
Eonist <unicornhive@gmail.com>
2015-11-16 06:06:58 +0100

    Files modified: 1
    Modified 1 file:src/css/utils/CSSPropertyParser.swift

--- src/css/utils/CSSPropertyParser.swift
+++ src/css/utils/CSSPropertyParser.swift
@@ -88,7 +88,7 @@ class Utils{
         else if(RegExp.test(rotationMatch,directionPattern)){
             let angleType:String = RegExp.match(rotationMatch,directionPattern)[0]
             rotation = Trig.angleType(angleType)+180;// :TODO: Create support for top left and other corners
-        }
+        }else{fatalError("Error")}
         //		trace("rotation: " + rotation);
         return rotation;
     }

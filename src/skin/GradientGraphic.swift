396c43830418986d4e430358f684e42500683577
Eonist <unicornhive@gmail.com>
2015-11-16 09:09:07 +0100

    Files modified: 2
    Modified 2 files:src/skin/GradientGraphic.swiftsrc/style/fill/utils/FillStyleModifier.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -3,7 +3,7 @@ import Cocoa
 class GradientGraphic:GraphicDecoratable {
     override func beginFill(){
         if(getGraphic().fillStyle.dynamicType is GradientFillStyle.Type){
-            FillStyleModifier.beginGradientFill(getGraphic().graphics, GradientFillStyle(getGraphic().fillStyle,NSColor.clearColor()).gradient)
+            FillStyleModifier.beginGradientFill(getGraphic().graphics, (getGraphic().fillStyle as! GradientFillStyle).gradient)
         }
     }
 }
--- src/style/fill/utils/FillStyleModifier.swift
+++ src/style/fill/utils/FillStyleModifier.swift
@@ -4,7 +4,7 @@ class FillStyleModifier {
     /**
      * Applies @param gradient on to @param graphics
      */
-    class func beginGradientFill(graphics:Graphics,gradient:IGradient) {
+    class func beginGradientFill(graphics:Graphics,_ gradient:IGradient) {
         let nsFillGradientColorA = ColorParser.nsColor("FF0000"/*fillGradientColorA*/, Float(1))
         let nsFillGradientColorB = ColorParser.nsColor("0000FF"/*fillGradientColorB*/, Float(1))
         let gradient:Gradient = Gradient([nsFillGradientColorA.CGColor,nsFillGradientColorB.CGColor])

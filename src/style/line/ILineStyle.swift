888ea1066060f2e3ecd7c5e03827c10423554284
Eonist <unicornhive@gmail.com>
2015-11-17 17:42:37 +0100

    Files modified: 1
    Modified 1 file:src/style/line/ILineStyle.swift

--- src/style/line/ILineStyle.swift
+++ src/style/line/ILineStyle.swift
@@ -1,9 +1,8 @@
 import Cocoa
 
-class ILineStyle{
+protocol ILineStyle{
     var color: NSColor { get set }
     var thickness: CGFloat { get set }
-    var lineCap: CGFloat { get set }
     var lineCap: CGFloat { get set }
     var lineJoin: CGFloat { get set }
     var miterLimit: CGFloat { get set }

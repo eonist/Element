8dcf476dfff4db97a89950b493ee6533c1379960
Eonist <unicornhive@gmail.com>
2015-11-17 20:25:36 +0100

    Files modified: 2
    Modified 2 files:src/skin/graphic/decorator/BaseGraphic.swiftsrc/skin/graphic/decorator/RectGraphic.swift

--- src/skin/graphic/decorator/BaseGraphic.swift
+++ src/skin/graphic/decorator/BaseGraphic.swift
@@ -5,14 +5,15 @@ import Cocoa
 * NOTE: We dont need a line mask, just subclass the Graphics class so it supports masking of the line aswell (will require some effort)
 */
 class BaseGraphic :AbstractGraphicDecoratable,IBaseGraphic{
+    lazy var graphics:Graphics = Graphics()
+    override var graphic:IBaseGraphic {return self}
     var width:Double;
     var height:Double;
     var fillStyle:IFillStyle?
     var lineStyle:ILineStyle?
     var lineOffsetType:OffsetType?
-    lazy var graphics:Graphics = Graphics()
     var path:CGMutablePath = CGPathCreateMutable()
-    override var graphic:IBaseGraphic {return self}
+    
     init(_ width:Double = 100, _ height:Double = 100,_ fillStyle:IFillStyle? = nil, _ lineStyle:ILineStyle? = nil, _ lineOffsetType:OffsetType = OffsetType()) {
         self.width = width;
         self.height = height;

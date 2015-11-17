8e657854da1759024c95c45c6dcdd903fc592014
Eonist <unicornhive@gmail.com>
2015-11-17 17:46:27 +0100

    Files modified: 2
    Modified 2 files:src/style/line/ILineStyle.swiftsrc/style/line/LineStyle.swift

--- src/style/line/ILineStyle.swift
+++ src/style/line/ILineStyle.swift
@@ -3,7 +3,7 @@ import Cocoa
 protocol ILineStyle{
     var color: NSColor { get set }
     var thickness: CGFloat { get set }
-    var lineCap: CGFloat { get set }
-    var lineJoin: CGFloat { get set }
+    var lineCap: CGLineCap { get set }
+    var lineJoin: CGLineJoin { get set }
     var miterLimit: CGFloat { get set }
 }
--- src/style/line/LineStyle.swift
+++ src/style/line/LineStyle.swift
@@ -1,5 +1,16 @@
-import Foundation
+import Cocoa
 
-class LineStyle {
-
+class LineStyle:ILineStyle {
+    var color: NSColor
+    var thickness: CGFloat
+    var lineCap: CGLineCap
+    var lineJoin: CGLineJoin
+    var miterLimit: CGFloat
+    init(thickness:CGFloat = 1,_ color:NSColor = NSColor.blackColor(), _ lineCap:CGLineCap = CGLineCap.Butt, _ lineJoin:CGLineJoin =  CGLineJoin.Miter, _ miterLimit:CGFloat = 10){
+        self.thickness = thickness
+        self.color = color
+        self.lineCap = lineCap
+        self.lineJoin = lineJoin
+        self.miterLimit = miterLimit
+    }
 }

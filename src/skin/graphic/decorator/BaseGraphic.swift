1910eb2be9030a23b7bea8f1b35894a14d8465aa
Eonist <unicornhive@gmail.com>
2015-11-17 14:43:31 +0100

    Files modified: 2
    Modified 2 files:src/skin/graphic/decorator/BaseGraphic.swiftsrc/skin/graphic/decorator/RectGraphic.swift

--- src/skin/graphic/decorator/BaseGraphic.swift
+++ src/skin/graphic/decorator/BaseGraphic.swift
@@ -5,15 +5,18 @@ import Cocoa
 * NOTE: We dont need a line mask, just subclass the Graphics class so it supports masking of the line aswell (will require some effort)
 */
 class BaseGraphic :AbstractGraphicDecoratable,IBaseGraphic{
+    var width:Double;
+    var height:Double;
     var fillStyle:IFillStyle?
     var lineStyle:ILineStyle?
     lazy var graphics:Graphics = Graphics()
     var path:CGMutablePath = CGPathCreateMutable()
-    init(_ fillStyle:IFillStyle? = nil, _ lineStyle:ILineStyle? = nil/*, _ lineOffsetType:OffsetType = OffsetType()*/) {
-        self.fillStyle = fillStyle
-        self.lineStyle = lineStyle
+    init(_ width:Double = 100, _ height:Double = 100,_ fillStyle:IFillStyle? = nil, _ lineStyle:ILineStyle? = nil/*, _ lineOffsetType:OffsetType = OffsetType()*/) {
         self.width = width;
         self.height = height;
+        self.fillStyle = fillStyle
+        self.lineStyle = lineStyle
+        
     }
     /**
      * TODO:color cant be uint since uint cant be NaN, use Double
@@ -28,6 +31,10 @@ class BaseGraphic :AbstractGraphicDecoratable,IBaseGraphic{
     }
     func setPosition(position:CGPoint){
         CGPathModifier.translate(&path,position.x,position.y)//Transformations
+    }
+    func setSize(width:Double,height:Double) {
+        self.width = width;
+        self.height = height;
     }
     func setProperties(fillStyle:IFillStyle? = nil, lineStyle:ILineStyle? = nil){// :TODO: remove this and replace with setLineStyle and setFillStyle ?
         self.fillStyle = fillStyle;
--- src/skin/graphic/decorator/RectGraphic.swift
+++ src/skin/graphic/decorator/RectGraphic.swift
@@ -1,17 +1,10 @@
 import Foundation
 
 class RectGraphic:GraphicDecoratable{
-    var width:Double;
-    var height:Double;
-    init(_ decoratable: IGraphicDecoratable, _ width:Double = 100, _ height:Double = 100) {
-        
-        super.init(decoratable)
-    }
+    /**
+     *
+     */
     override func drawFill() {
-        getGraphic().path = CGPathParser.rect(CGFloat(width), CGFloat(height))
-    }
-    func setSize(width:Double,height:Double) {
-        self.width = width;
-        self.height = height;
+        getGraphic().path = CGPathParser.rect(CGFloat(getGraphic().width), CGFloat(getGraphic().height))
     }
 }

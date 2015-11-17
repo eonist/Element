df5c61f8ce2a24184156bb89e243a5feca2cea0e
Eonist <unicornhive@gmail.com>
2015-11-17 20:13:53 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RectGraphic.swift

--- src/skin/graphic/decorator/RectGraphic.swift
+++ src/skin/graphic/decorator/RectGraphic.swift
@@ -11,7 +11,11 @@ class RectGraphic:GraphicDecoratable{
      *
      */
     func drawLine(){
-        
-        let maskRect:CGRect = RectGraphicUtils.maskRect(CGRect(0,0, getGraphic().width,getGraphic().height), getGraphic().lineStyle!, getGraphic().lineOffsetType!);
+        if(getGraphic().lineStyle != nil){
+            let rect:CGRect = RectGraphicUtils.offsetRect(CGRect(0, 0, getGraphic().width, getGraphic().height), getGraphic().lineStyle!, getGraphic().lineOffsetType!);
+            //drawLine
+            let maskRect:CGRect = RectGraphicUtils.maskRect(CGRect(0,0, getGraphic().width,getGraphic().height), getGraphic().lineStyle!, getGraphic().lineOffsetType!);
+            //draw the mask line
+        }
     }
 }

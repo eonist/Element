3030132ba28e99774d579575c9478889ff9c9b10
Eonist <unicornhive@gmail.com>
2015-11-17 20:19:45 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/IGraphicDecoratable.swift

--- src/skin/graphic/decorator/IGraphicDecoratable.swift
+++ src/skin/graphic/decorator/IGraphicDecoratable.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 protocol IGraphicDecoratable {
+    var graphic:IBaseGraphic{get}
     func getGraphic() -> BaseGraphic/*Dont revert this variable to a protocol*/
     func fill()
     func beginFill()

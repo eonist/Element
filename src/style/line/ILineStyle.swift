c75472fc346620cbe7827fa01cef416546aaa980
Eonist <unicornhive@gmail.com>
2015-11-17 17:40:41 +0100

    Files modified: 1
    Modified 1 file:src/style/line/ILineStyle.swift

--- src/style/line/ILineStyle.swift
+++ src/style/line/ILineStyle.swift
@@ -1,21 +1,10 @@
-import Foundation
+import Cocoa
 
 class ILineStyle{
-
-    var color():NSColor
-    function set color(color : Number) : void
-    function get alpha():Number
-    function set alpha(alpha : Number) : void
-    function get thickness():Number
-    function set thickness(thickness : Number) : void
-    function get pixelHinting():Boolean
-    function set pixelHinting(pixelHinting : Boolean) : void
-    function get lineScaleMode():String
-    function set lineScaleMode(lineScaleMode : String) : void
-    function get capStyle():String
-    function set capStyle(capStyle : String) : void
-    function get jointStyle():String
-    function set jointStyle(jointStyle : String) : void
-    function get miterLimit():Number
-    function set miterLimit(miterLimit : Number) : void
+    var color: NSColor { get set }
+    var thickness: CGFloat { get set }
+    var lineCap: CGFloat { get set }
+    var lineCap: CGFloat { get set }
+    var lineJoin: CGFloat { get set }
+    var miterLimit: CGFloat { get set }
 }

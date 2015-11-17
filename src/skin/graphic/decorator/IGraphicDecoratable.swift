47ed3af1eabf72326a09a62cd3a68d67f84af1bb
Eonist <unicornhive@gmail.com>
2015-11-17 20:31:22 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/IGraphicDecoratable.swift

--- src/skin/graphic/decorator/IGraphicDecoratable.swift
+++ src/skin/graphic/decorator/IGraphicDecoratable.swift
@@ -1,5 +1,8 @@
 import Foundation
-
+/**
+ * These are the methods the GraphicDecoratable must implement
+ * Differs from IBaseGraphic in that IBaseGraphic has some variables that the decoratables isnt dependent on when it comes to the creation procedure
+ */
 protocol IGraphicDecoratable {
     var graphic:IBaseGraphic{get}
     func getGraphic() -> BaseGraphic/*Dont revert this variable to a protocol*/

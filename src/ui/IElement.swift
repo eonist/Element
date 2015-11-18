d1ca7db50d61d97e92ef1bedddccde83050cb13c
Eonist <unicornhive@gmail.com>
2015-11-18 21:35:58 +0100

    Files modified: 1
    Modified 1 file:src/ui/IElement.swift

--- src/ui/IElement.swift
+++ src/ui/IElement.swift
@@ -4,6 +4,8 @@ protocol IElement:class, IView{
     var skinState:String{get}
     var style:IStyle{get set}
     var skin:ISkin?{get set}
+    var x:CGFloat{get set}
+    var y:CGFloat{get set}
     var width:Double?{get}
     var height:Double?{get}
     func resolveSkin()

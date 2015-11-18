c2c1c1ceb5cc13528ddc9521e57dfcab8011a959
Eonist <unicornhive@gmail.com>
2015-11-18 07:38:51 +0100

    Files modified: 1
    Modified 1 file:src/style/line/GradientLineStyle.swift

--- src/style/line/GradientLineStyle.swift
+++ src/style/line/GradientLineStyle.swift
@@ -1,5 +1,9 @@
-import Foundation
+import Cocoa
 
-class GradientLineStyle {
-
+class GradientLineStyle:LineStyle {
+    private var gradient:IGradient
+    init(gradient:IGradient,thickness:CGFloat = 1,_ color:NSColor = NSColor.blackColor(), _ lineCap:CGLineCap = CGLineCap.Butt, _ lineJoin:CGLineJoin =  CGLineJoin.Miter, _ miterLimit:CGFloat = 10) {
+        self.gradient = gradient
+        super.init(thickness, color, lineCap, lineJoin, miterLimit)
+    }
 }

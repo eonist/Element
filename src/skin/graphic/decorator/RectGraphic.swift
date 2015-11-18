1aae1d2228e4e1971ae086cf4b1936a5d0d4a474
Eonist <unicornhive@gmail.com>
2015-11-18 15:24:10 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RectGraphic.swift

--- src/skin/graphic/decorator/RectGraphic.swift
+++ src/skin/graphic/decorator/RectGraphic.swift
@@ -8,23 +8,27 @@ class RectGraphic:GraphicDecoratable{
         let x:CGFloat = graphic.lineOffsetType!.left == OffsetType.outside ? graphic.lineStyle!.thickness : 0;
         let y:CGFloat = graphic.lineOffsetType!.top == OffsetType.outside ? graphic.lineStyle!.thickness : 0;
         let rect:CGRect = CGRect(x,y,graphic.width, graphic.height)
-        Swift.print("rect: " + String(rect))
+        
         graphic.path = rect.path
     }
     /**
      *
      */
     override func drawLine(){
-        //Swift.print("RectGraphic.drawLine()")
+        Swift.print("RectGraphic.drawLine()")
         if(graphic.lineStyle != nil){
             let graphicRect:CGRect = CGRect(0, 0, graphic.width, graphic.height)
             
-            //Swift.print("graphicRect: " + String(graphicRect))
+            Swift.print("graphicRect: " + String(graphicRect))
             let rect:CGRect = RectGraphicUtils.offsetRect(graphicRect, graphic.lineStyle!, graphic.lineOffsetType!);
             Swift.print("rect: " + String(rect))
             
-      
-            
+            //drawLine
+            /*
+            let x:CGFloat = graphic.lineOffsetType!.left == OffsetType.outside ? graphic.lineStyle!.thickness : 0;
+            let y:CGFloat = graphic.lineOffsetType!.top == OffsetType.outside ? graphic.lineStyle!.thickness : 0;
+            graphic.linePath = CGRect(x,y,graphic.width, graphic.height).path
+            */
             graphic.linePath = rect.path
             
             

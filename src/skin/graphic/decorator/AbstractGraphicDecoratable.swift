7233d2c25f43addd1b82f2b3b90ab65639f625b8
Eonist <unicornhive@gmail.com>
2015-11-18 06:24:39 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/AbstractGraphicDecoratable.swift

--- src/skin/graphic/decorator/AbstractGraphicDecoratable.swift
+++ src/skin/graphic/decorator/AbstractGraphicDecoratable.swift
@@ -3,15 +3,17 @@ import Foundation
  * The AbstractDecorator has all the decorator methods
  */
 class AbstractGraphicDecoratable:IGraphicDecoratable{
-    var graphic:IBaseGraphic {fatalError("Must be overridden in subClass")}
+    var graphic:IBaseGraphic {get {fatalError("Must be overridden in subClass")} set{fatalError("Must be overridden in subClass")}}
+    func getGraphic() -> BaseGraphic{fatalError("Must be overridden in subClass")}
     func initialize(){}
+    /*Fill*/
     func fill(){}
+    func beginFill(){}
+    func drawFill(){}
+    func stylizeFill(){}
+    /*Line*/
     func line(){}
     func applyLineStyle(){}
     func drawLine(){}
     func stylizeLine(){}
-    func beginFill(){}
-    func drawFill(){}
-    func stylizeFill(){}
-    func getGraphic() -> BaseGraphic{fatalError("Must be overridden in subClass")}
 }

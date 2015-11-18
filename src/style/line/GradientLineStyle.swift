0d45fe3ec887d9420ec0161a060a3bb0d660292b
Eonist <unicornhive@gmail.com>
2015-11-18 07:44:37 +0100

    Files modified: 2
    Modified 2 files:src/style/line/GradientLineStyle.swiftsrc/style/line/ILineStyle.swift

--- src/style/line/GradientLineStyle.swift
+++ src/style/line/GradientLineStyle.swift
@@ -1,14 +1,14 @@
 import Cocoa
 
 class GradientLineStyle:LineStyle {
-    private var gradient:IGradient
-    init(gradient:IGradient,thickness:CGFloat = 1,_ color:NSColor = NSColor.blackColor(), _ lineCap:CGLineCap = CGLineCap.Butt, _ lineJoin:CGLineJoin =  CGLineJoin.Miter, _ miterLimit:CGFloat = 10) {
+    var gradient:IGradient
+    init(_ gradient:IGradient,_ thickness:CGFloat = 1,_ color:NSColor = NSColor.blackColor(), _ lineCap:CGLineCap = CGLineCap.Butt, _ lineJoin:CGLineJoin =  CGLineJoin.Miter, _ miterLimit:CGFloat = 10) {
         self.gradient = gradient
         super.init(thickness, color, lineCap, lineJoin, miterLimit)
     }
 }
 extension GradientLineStyle{
-    func gradientLineStyle(gradient:IGradient,lineStyle:ILineStyle)->GradientLineStyle {
-        return new GradientLineStyle(gradient,lineStyle.thickness,lineStyle.color,lineStyle.alpha,lineStyle.pixelHinting, lineStyle.lineScaleMode,lineStyle.capStyle,lineStyle.jointStyle, lineStyle.miterLimit);
+    convenience init(gradient:IGradient,lineStyle:ILineStyle) {
+        self.init(gradient,lineStyle.thickness,lineStyle.color,lineStyle.lineCap,lineStyle.lineJoin, lineStyle.miterLimit);
     }
 }
--- src/style/line/ILineStyle.swift
+++ src/style/line/ILineStyle.swift
@@ -3,7 +3,7 @@ import Cocoa
 protocol ILineStyle{
     var color: NSColor { get set }
     var thickness: CGFloat { get set }
-    var lineCap: CGLineCap { get set }
-    var lineJoin: CGLineJoin { get set }
+    var lineCap: CGLineCap { get set }//TODO: rename to capStyle?
+    var lineJoin: CGLineJoin { get set }//TODO: rename to jointStyle
     var miterLimit: CGFloat { get set }
 }

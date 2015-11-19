18ebd25bceafeed169dbe6641106128236a66be2
Eonist <unicornhive@gmail.com>
2015-11-19 08:34:15 +0100

    Files modified: 2
    Modified 2 files:src/skin/GradientGraphic.swiftsrc/style/line/utils/LineStyleModifier.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -26,7 +26,6 @@ class GradientGraphic:GraphicDecoratable {
         
         if(getGraphic().lineStyle!.dynamicType is GradientLineStyle.Type){
             LineStyleModifier.lineGradientStyle(graphic.graphics, (graphic.lineStyle as! GradientLineStyle).gradient);//Updates only if _lineGradient is not null, and _lineGradient.colors[0] and (_lineGradient.colors[1] are valid colors)
-            
         }else{
             Swift.print(String(getGraphic().fillStyle))
             fatalError("NOT CORRECT lineStyle")
--- src/style/line/utils/LineStyleModifier.swift
+++ src/style/line/utils/LineStyleModifier.swift
@@ -5,6 +5,10 @@ class LineStyleModifier {
     *
     */
     class func lineGradientStyle(graphics:Graphics,_ gradient:IGradient) {
+        let nsFillGradientColorA = ColorParser.nsColor("FF00FF"/*fillGradientColorA*/, Float(1))
+        let nsFillGradientColorB = ColorParser.nsColor("00FF00"/*fillGradientColorB*/, Float(1))
+        let gradient:Gradient = Gradient([nsFillGradientColorA.CGColor,nsFillGradientColorB.CGColor])
+        graphics.gradientLine(gradient)
         //graphics.lineGradientStyle(gradient.type, gradient.colors, gradient.alphas, gradient.ratios, gradient.matrix, gradient.spreadMethod, gradient.interpolationMethod, gradient.focalPointRatio);
     }
 }

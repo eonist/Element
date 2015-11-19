f510acb0d58149ba5025f491e36ea9df43111f76
Eonist <unicornhive@gmail.com>
2015-11-19 08:43:59 +0100

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -4,7 +4,11 @@ import Cocoa
 * @Note there may not be a need to include a getter function for the fillStyle, since if this instance is edited with a Utility class the new fillstyle is applied but not stored in _fillStyle, same goes for lineStyle
 */
 class GradientGraphic:GraphicDecoratable {
+    /**
+     *
+     */
     override func beginFill(){
+        Swift.print("beginFill()")
         if(getGraphic().fillStyle!.dynamicType is GradientFillStyle.Type){
             FillStyleModifier.beginGradientFill(getGraphic().graphics, (getGraphic().fillStyle as! GradientFillStyle).gradient)
         }else{
@@ -16,6 +20,7 @@ class GradientGraphic:GraphicDecoratable {
      * // :TODO: could possibly be renamed to applyGradientLinestyle
      */
     override func applyLineStyle() {
+        Swift.print("GradientGraphic.applyLineStyle()")
         decoratable.applyLineStyle()//call the BaseGraphic to set the stroke-width, cap, joint etc
         if(getGraphic().lineStyle!.dynamicType is GradientLineStyle.Type){
             LineStyleModifier.lineGradientStyle(graphic.graphics, (graphic.lineStyle as! GradientLineStyle).gradient);//Updates only if _lineGradient is not null, and _lineGradient.colors[0] and (_lineGradient.colors[1] are valid colors)

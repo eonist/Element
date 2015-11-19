8679fab828e3a1ceac3c7af8640b0c8b29e30e23
Eonist <unicornhive@gmail.com>
2015-11-19 18:37:23 +0100

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -25,9 +25,6 @@ class GradientGraphic:GraphicDecoratable {
         if(getGraphic().lineStyle!.dynamicType is GradientLineStyle.Type){
             Swift.print("lineStyle is GradientLineStyle")
             LineStyleModifier.lineGradientStyle(graphic.graphics, (graphic.lineStyle as! GradientLineStyle).gradient);//Updates only if _lineGradient is not null, and _lineGradient.colors[0] and (_lineGradient.colors[1] are valid colors)
-        }else{
-            Swift.print(String(getGraphic().lineStyle))
-            fatalError("NOT CORRECT lineStyle")
-        }
+        }//else{fatalError("NOT CORRECT lineStyle")}
     }
 }

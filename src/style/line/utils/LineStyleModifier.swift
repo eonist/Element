883fd67a14bd77e256abbff5f2719689df04c485
Eonist <unicornhive@gmail.com>
2015-11-19 08:22:20 +0100

    New files added: 1
    Added 1 file:src/style/line/utils/

---
+++ src/style/line/utils/LineStyleModifier.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class LineStyleModifier {
+    /**
+    *
+    */
+    class func lineGradientStyle(graphics:Graphics,_ gradient:IGradient) {
+        //graphics.lineGradientStyle(gradient.type, gradient.colors, gradient.alphas, gradient.ratios, gradient.matrix, gradient.spreadMethod, gradient.interpolationMethod, gradient.focalPointRatio);
+    }
+}

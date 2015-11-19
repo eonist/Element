9451e52a001376fa3b1742de1813db5288b512cd
Eonist <unicornhive@gmail.com>
2015-11-19 08:45:57 +0100

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -8,7 +8,7 @@ class GradientGraphic:GraphicDecoratable {
      *
      */
     override func beginFill(){
-        Swift.print("beginFill()")
+        Swift.print("GradientGraphic.beginFill()")
         if(getGraphic().fillStyle!.dynamicType is GradientFillStyle.Type){
             FillStyleModifier.beginGradientFill(getGraphic().graphics, (getGraphic().fillStyle as! GradientFillStyle).gradient)
         }else{
@@ -23,6 +23,7 @@ class GradientGraphic:GraphicDecoratable {
         Swift.print("GradientGraphic.applyLineStyle()")
         decoratable.applyLineStyle()//call the BaseGraphic to set the stroke-width, cap, joint etc
         if(getGraphic().lineStyle!.dynamicType is GradientLineStyle.Type){
+            Swift.print("lineStyle is GradientLineStyle")
             LineStyleModifier.lineGradientStyle(graphic.graphics, (graphic.lineStyle as! GradientLineStyle).gradient);//Updates only if _lineGradient is not null, and _lineGradient.colors[0] and (_lineGradient.colors[1] are valid colors)
         }else{
             Swift.print(String(getGraphic().lineStyle))

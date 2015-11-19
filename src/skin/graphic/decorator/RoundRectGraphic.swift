1cadd28620310c9724ce8a1b34b44c603a15e3cb
Eonist <unicornhive@gmail.com>
2015-11-19 07:25:19 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RoundRectGraphic.swift

--- src/skin/graphic/decorator/RoundRectGraphic.swift
+++ src/skin/graphic/decorator/RoundRectGraphic.swift
@@ -1,8 +1,8 @@
 import Foundation
 /*
-* // :TODO: the mask doesnt have an exact fitting fillet (figure out how to solve this)
-* // :TODO: impliment elliptical round corners corner-radius:50/25; creates an elliptical roundcornered rect
-* @Note we might need to create a method named drawAdvanceRoundRect, which would draw a round rect with individual corners that also could have 2 radiuses ech so you would have elliptical corners,
+* TODO: the mask doesnt have an exact fitting fillet (figure out how to solve this)
+* TODO: impliment elliptical round corners corner-radius:50/25; creates an elliptical roundcornered rect
+* NOTE we might need to create a method named drawAdvanceRoundRect, which would draw a round rect with individual corners that also could have 2 radiuses ech so you would have elliptical corners,
 * google for Drawing Circles with Rational Quadratic Bezier Curves.pdf or use a value named kappa which is basically kappa = 4 * (sqrt(2) - 1) / 3
 * NOTE: CGRect has a roundRect method with eliptical corners. You have this code in your research folder
 */
@@ -27,26 +27,13 @@ class RoundRectGraphic:GraphicDecoratable{//adds round-rectangular path
      *
      */
     override func drawLine(){
-        
         if(graphic.lineStyle != nil){/*updates only if lineStyle and lineStyle.color are valid*/// :TODO: this check could possibly be redundant
             let lineOffsetType:OffsetType = graphic.lineOffsetType!;
             let rect:CGRect = RectGraphicUtils.offsetRect(CGRect(0, 0, graphic.width, graphic.height), graphic.lineStyle!, lineOffsetType);
             let fillet:Fillet = FilletParser.config(self.fillet, lineOffsetType, graphic.lineStyle!);
             CGPathParser.roundRect(rect.x,rect.y,rect.width,rect.height,fillet.topLeft, fillet.topRight, fillet.bottomLeft, fillet.bottomRight)
             /*mask*/
-            let maskRect:CGRect = RectGraphicUtils.maskRect(CGRect(0,0,graphic.width,graphic.height), graphic.lineStyle!, lineOffsetType);
+            //let maskRect:CGRect = RectGraphicUtils.maskRect(CGRect(0,0,graphic.width,graphic.height), graphic.lineStyle!, lineOffsetType);
         }
-        /*
-        
-        
-        
-        
-        graphic.lineMask.graphics.beginFill(Color.GREEN,0.5);
-        var maskFillet:Fillet = Rect3Utils.configMaskFillet(_fillet, lineOffsetType, graphic.lineStyle);
-        graphic.lineMask.graphics.drawRoundRectComplex(maskRect.x, maskRect.y, maskRect.width, maskRect.height, maskFillet.topLeft, maskFillet.topRight, maskFillet.bottomLeft, maskFillet.bottomRight);
-        graphic.lineMask.graphics.endFill();
-        }
-
-        */
     }
 }

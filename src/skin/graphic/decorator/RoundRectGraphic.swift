a2df024a0d3ae6a1aac87ccf63025eb87499e055
Eonist <unicornhive@gmail.com>
2015-11-19 05:56:18 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/RoundRectGraphic.swift

--- src/skin/graphic/decorator/RoundRectGraphic.swift
+++ src/skin/graphic/decorator/RoundRectGraphic.swift
@@ -7,14 +7,14 @@ class RoundRectGraphic:GraphicDecoratable{//adds round-rectangular path
         super.init(decoratable)
     }
     override func drawFill() {
-        var x:Number = graphic.lineOffsetType.left == OffsetType.OUTSIDE ? graphic.lineStyle.thickness : 0;
-        var y:Number = graphic.lineOffsetType.top == OffsetType.OUTSIDE ? graphic.lineStyle.thickness : 0;
+        let x:CGFloat = graphic.lineOffsetType!.left == OffsetType.outside ? graphic.lineStyle!.thickness : 0;
+        let y:CGFloat = graphic.lineOffsetType!.top == OffsetType.outside ? graphic.lineStyle!.thickness : 0;
         //Swift.print("RoundRectGraphic2.drawFill() ")
         let w:CGFloat = getGraphic().width
         //Swift.print("w: " + "\(w)")
         let h:CGFloat = getGraphic().height
         //Swift.print("h: " + "\(h)")
         //Swift.print("fillet.topLeft: " + "\(fillet.topLeft)")
-        getGraphic().path = CGPathParser.roundRect(0,0,w, h,CGFloat(fillet.topLeft), CGFloat(fillet.topRight), CGFloat(fillet.bottomLeft), CGFloat(fillet.bottomRight))//Shapes
+        getGraphic().path = CGPathParser.roundRect(x,y,w, h,CGFloat(fillet.topLeft), CGFloat(fillet.topRight), CGFloat(fillet.bottomLeft), CGFloat(fillet.bottomRight))//Shapes
     }
 }

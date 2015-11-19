df3fea56772af4a95122b0f46ad3d485e690a0a9
Eonist <unicornhive@gmail.com>
2015-11-19 08:10:49 +0100

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -1,5 +1,8 @@
 import Cocoa
-
+/*
+* @Note: Use FillStyleUtils.beginGradientFill(_gradientRect.graphic.graphics, _gradient); if you need to modifiy
+* @Note there may not be a need to include a getter function for the fillStyle, since if this instance is edited with a Utility class the new fillstyle is applied but not stored in _fillStyle, same goes for lineStyle
+*/
 class GradientGraphic:GraphicDecoratable {
     override func beginFill(){
         if(getGraphic().fillStyle!.dynamicType is GradientFillStyle.Type){

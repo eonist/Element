676f86c419803cc9d3157756dc530190d921ed05
Eonist <unicornhive@gmail.com>
2015-11-19 18:47:11 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/GraphicSkinParser.swift

--- src/skin/skin/utils/GraphicSkinParser.swift
+++ src/skin/skin/utils/GraphicSkinParser.swift
@@ -28,7 +28,7 @@ private class Utils{
         let width:CGFloat = (StylePropertyParser.width(skin) ?? skin.width!);
         let height:CGFloat = (StylePropertyParser.height(skin) ?? skin.height!);
         let lineOffsetType:OffsetType = StylePropertyParser.lineOffsetType(skin);
-        Swift.print("lineOffsetType: " + String(lineOffsetType))
+        Swift.print("lineOffsetType: top:" + lineOffsetType.top + "  left:" + lineOffsetType.left + " bottom: " + lineOffsetType.bottom + " right: "+lineOffsetType.right)
         return BaseGraphic(width,height,fillStyle,lineStyle,lineOffsetType)
     }
     /**

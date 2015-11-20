de905b2a9c8d82a7657bed051233bbba7fb1477c
Eonist <unicornhive@gmail.com>
2015-11-20 18:20:03 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/Text.swift

--- src/ui/text/Text.swift
+++ src/ui/text/Text.swift
@@ -4,7 +4,7 @@ class Text:Element,IText {
     var initText:String;
     var textField:NSText{get{return (skin as! ITextSkin).textField}}
     /**
-     * Sets text to the textfield, remember to set textformat after
+     * Returns the textField text and Sets text to the textfield, remember to set textformat after
      * @Note: to access htmlText: ITextSkin2(_skin).textField.htmlText = htmlText;
      */
     var text:String{get{return textField.string!} set{(skin as! ITextSkin).text = newValue}}
@@ -13,6 +13,7 @@ class Text:Element,IText {
         initText = text
         super.init(width, height,0,0, parent, id)
     }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     /**
      * Returns "Text"
      * @Note This function is used to find the correct class type when synthezing the element cascade, in the event that a class subclasses this class

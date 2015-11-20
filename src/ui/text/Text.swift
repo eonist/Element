82cb677ae86144804136849d6c158e8bd00e3977
Eonist <unicornhive@gmail.com>
2015-11-20 18:10:09 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/Text.swift

--- src/ui/text/Text.swift
+++ src/ui/text/Text.swift
@@ -1,11 +1,15 @@
 import Cocoa
 
 class Text:Element,IText {
-    /**
-    * Sets text to the textfield, remember to set textformat after
-    * @Note: to access htmlText: ITextSkin2(_skin).textField.htmlText = htmlText;
-    */
+    private var initText:String;
     var textField:NSText{get{return (skin as! ITextSkin).textField}}
+    /**
+     * Sets text to the textfield, remember to set textformat after
+     * @Note: to access htmlText: ITextSkin2(_skin).textField.htmlText = htmlText;
+     */
     var text:String{get{return textField.string!} set{(skin as! ITextSkin).text = newValue}}
-    var initText:String{get{return }}
+    var initText:String{get{return initText}}
+    init(width:Number, height:Number, text:String = "dafaultText", parent:IElement? = nil, id:String? = nil){
+        
+    }
 }

2afec13a3c98a1e7673062e86d7f48557cc73d42
Eonist <unicornhive@gmail.com>
2015-11-20 12:33:32 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/text/TextSkin.swift

--- src/skin/skin/text/TextSkin.swift
+++ src/skin/skin/text/TextSkin.swift
@@ -5,11 +5,14 @@ class TextSkin:Skin,ITextSkin{
     var textField:NSText;
     //func setText(text:String)
     init(style:IStyle, text:String, state:String = SkinStates.none, element:IElement? = nil){
-        textField = NSText(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
+        textField = NSText(frame: NSRect(x: 0, y: 0, width: 100, height: 100))//set w and h to 0
+        textField.string = text;
         super.init(style, state, element)
     }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+    override func draw() {
+        //do aligning here
+        super.draw()
     }
 
 }

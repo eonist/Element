20e66c6841579271c9763f0153a7eb3c2c9c6555
Eonist <unicornhive@gmail.com>
2015-11-21 14:46:44 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -7,33 +7,34 @@ class Button:Element {
         //acceptsTouchEvents = false//only for swipes,pinch etc
         
         trackingRectTag = addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
-        
+        //removeTrackingRect(trackingRectTag!)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func getClassType()->String{
         return String(Button)
     }
     override func mouseEntered( event: NSEvent){
+        removeTrackingRect(trackingRectTag!)
         skinState = SkinStates.over
         Swift.print("mouseEntered: " + "\(super.skinState)")
-        super.mouseEntered(event)
-        removeTrackingRect(trackingRectTag!)
+        //super.mouseEntered(event)
+        
         needsDisplay = true;
     }
     override func mouseExited(event: NSEvent){
         Swift.print("mouseExited: " + "\(self.skinState)")
-        super.mouseExited(event)
+        //super.mouseExited(event)
         
     }
     override func mouseDown(theEvent: NSEvent) {
         skinState = SkinStates.down
         Swift.print("mouseDownEvent: " + "\(self.skinState)")
-        super.mouseDown(theEvent)
+        //super.mouseDown(theEvent)
     }
     override func mouseUp(theEvent: NSEvent) {
         skinState = SkinStates.none
         Swift.print("mouseUpEvent: " + "\(self.skinState)")
-        super.mouseDown(theEvent)
+        //super.mouseDown(theEvent)
         
     }
 }

65a353c7709cd409c62fbf826222714a2c414812
Eonist <unicornhive@gmail.com>
2015-11-21 18:28:27 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -8,16 +8,13 @@ class Button:Element {
         //acceptsTouchEvents = false//only for swipes,pinch etc
         
         addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
-        
-        
+  
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func getClassType()->String{
         return String(Button)
     }
     override func mouseEntered( event: NSEvent){
-        
-        
         
         skinState = SkinStates.over
         Swift.print("mouseEntered: " + "\(super.skinState)")

b4a092ecdb8d7782c7faaf2c9bc01b7d9a2145af
Eonist <unicornhive@gmail.com>
2015-11-21 19:52:47 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -5,8 +5,6 @@ class Button:Element {
     override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = nil){
         //Swift.print("Button.init()")
         super.init(width, height, parent, id)
-        //acceptsTouchEvents = false//only for swipes,pinch etc
-        
         addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
@@ -49,6 +47,7 @@ class Button:Element {
      * @Note: bubbling= true was added to make Stepper class dragable
      */
     func mouseUpInside(theEvent: NSEvent){
+        skinState = SkinStates.over;// :TODO: why in two lines like this?
         Swift.print("mouseUpInside: " + "\(self.skinState)")
     }
     /**

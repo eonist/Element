1faa2d5711482ab67a034247d18e4ada009d4645
Eonist <unicornhive@gmail.com>
2015-11-21 16:31:53 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -9,6 +9,12 @@ class Button:Element {
         
         addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: false)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
         //
+        
+        
+        //The problem is that you create the Items in superviews drawrect method. move the interface creation to didLoadWin or alike
+        //remove swift prints in the current implementation
+        
+        
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func getClassType()->String{

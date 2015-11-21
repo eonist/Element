5adaaea7de8c079fd4420f74efa59f458f6c25f4
Eonist <unicornhive@gmail.com>
2015-11-21 08:41:49 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-class Button {
-
+class Button:Element {
+    override init(_ width: CGFloat, _ height: CGFloat, _ parent:IElement? = nil,_ id:String? = nil){
+        super(width,height,parent,id)
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

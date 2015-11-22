8ea09e1f370f84bbf78c4f4016e7b9c6acb68beb
Eonist <unicornhive@gmail.com>
2015-11-22 09:48:15 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/text/TextSkin.swift

--- src/skin/skin/text/TextSkin.swift
+++ src/skin/skin/text/TextSkin.swift
@@ -19,6 +19,7 @@ class TextSkin:Skin,ITextSkin{
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func draw() {
         //do aligning here
+        applyProperties(textField);
         super.draw()
     }
     func applyProperties(textField:NSText){
@@ -34,6 +35,7 @@ class TextSkin:Skin,ITextSkin{
         super.frame.width = width//quick fix
         super.frame.height = height//quick fix
         let textFormat:TextFormat = StylePropertyParser.textFormat(self)
+        Swift.print("TextSkin.applyProperties() textFormat.color: " + String(textFormat.color))
         TextFieldModifier.applyTextFormat(textField,textFormat);
     }
     /**

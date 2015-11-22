97460997a7df13d34281d95f0abd96fb14daa9f6
Eonist <unicornhive@gmail.com>
2015-11-22 08:34:16 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/Text.swift

--- src/ui/text/Text.swift
+++ src/ui/text/Text.swift
@@ -5,6 +5,9 @@ class Text:Element,IText {
     var textField:NSText{get{return (skin as! ITextSkin).textField}}
     var text:String{get{return textField.string!} set{}}
     //override var wantsDefaultClipping:Bool{return true}//avoids clipping the view
+    override func hitTest(aPoint: NSPoint) -> NSView? {return nil}//avoids 
+        
+    
     init(_ width:CGFloat, _ height:CGFloat, _ text:String = "dafaultText", _ parent:IElement? = nil, _ id:String? = nil){
         initText = text
         super.init(width, height, parent, id)

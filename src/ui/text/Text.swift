57e3a660dca5c2a20c026499247b759d7b56b04d
Eonist <unicornhive@gmail.com>
2015-11-22 08:40:00 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/Text.swift

--- src/ui/text/Text.swift
+++ src/ui/text/Text.swift
@@ -5,10 +5,7 @@ class Text:Element,IText {
     var textField:NSText{get{return (skin as! ITextSkin).textField}}
     var text:String{get{return textField.string!} set{}}
     //override var wantsDefaultClipping:Bool{return true}//avoids clipping the view
-    //override func hitTest(aPoint: NSPoint) -> NSView? {return nil}//Avoids covering the graphic behind when dealing with mouse down events
-    override func acceptsFirstMouse(theEvent: NSEvent?) -> Bool {
-        return false
-    }
+    override func hitTest(aPoint: NSPoint) -> NSView? {return nil}//Avoids covering the graphic behind when dealing with mouse down events
     
     init(_ width:CGFloat, _ height:CGFloat, _ text:String = "dafaultText", _ parent:IElement? = nil, _ id:String? = nil){
         initText = text

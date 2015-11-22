a14a5f90a3f36ebff8d882536d1cf310f7eaa67d
Eonist <unicornhive@gmail.com>
2015-11-22 07:01:31 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/TextButton.swift

--- src/ui/button/TextButton.swift
+++ src/ui/button/TextButton.swift
@@ -2,9 +2,9 @@ import Foundation
 
 class TextButton:Button {
     init(_ title:String = "", _ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = nil) {
-        super.init(width, height, parent)
+        super.init(width, height, parent, id)
     }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        
+    
 }

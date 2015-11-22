3e76dfb151a93add24f8b1533bae721cb4b7c8c5
Eonist <unicornhive@gmail.com>
2015-11-22 10:21:23 +0100

    Files modified: 1
    Modified 1 file:src/style/StyleManager.swift

--- src/style/StyleManager.swift
+++ src/style/StyleManager.swift
@@ -1,17 +1,20 @@
 import Foundation
+/**
+ * TODO: This class can be a struct
+ */
 class StyleManager{
     static var styles:Array<IStyle> = []
     /**
-    * Adds a style to the styleManager class
-    * @param style: IStyle
-    */
+     * Adds a style to the styleManager class
+     * @param style: IStyle
+     */
     class func addStyle(style:IStyle){
         styles.append(style);
     }
     /**
-    * Locates and returns a Style by the @param name.
-    * @return a Style
-    */
+     * Locates and returns a Style by the @param name.
+     * @return a Style
+     */
     class func getStyle(name:String)->IStyle?{
         let numOfStyles:Int = styles.count;
         for(var i:Int = 0;i < numOfStyles;i++) {
@@ -21,7 +24,6 @@ class StyleManager{
         }
         return nil;
     }
-    
 }
 //convenince methods
 extension StyleManager{

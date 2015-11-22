4486012ab20f5125a0978bd716df116193c479b3
Eonist <unicornhive@gmail.com>
2015-11-22 06:11:19 +0100

    Files modified: 2
    Modified 2 files:src/skin/skin/GraphicSkin.swiftsrc/utils/ElementParser.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -4,6 +4,9 @@ import Foundation
 class GraphicSkin:Skin{
     override init(_ style:IStyle? = nil, _ state:String = "", _ element:IElement? = nil){
         super.init(style, state, element)
+        
+    }
+    override func drawRect(dirtyRect: NSRect) {
         GraphicSkinParser.configure(self)
     }
     /**

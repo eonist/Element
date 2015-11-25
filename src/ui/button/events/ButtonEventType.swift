73a625b0db13fab7637befc0366544c746e7eeeb
Eonist <unicornhive@gmail.com>
2015-11-25 08:16:13 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/events/ButtonEventType.swift

--- src/ui/button/events/ButtonEventType.swift
+++ src/ui/button/events/ButtonEventType.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class ButtonEvent {
-    
+class ButtonEventType {
+    public static const DOWN:String = "buttonEventDown";
+    public static const RELEASE_INSIDE:String = "buttonEventReleaseInside";
+    public static const RELEASE_OUTSIDE:String = "buttonEventReleaseOutside";
+    public static const ROLL_OUT:String = "buttonEventRollOut";
+    public static const ROLL_OVER:String = "buttonEventRollOut";
 }

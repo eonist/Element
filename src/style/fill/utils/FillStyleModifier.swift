154a187c7aac5bd0558ad1c2d0f47f2066e0f278
Eonist <unicornhive@gmail.com>
2015-11-26 07:09:27 +0100

    Files modified: 1
    Modified 1 file:src/style/fill/utils/FillStyleModifier.swift

--- src/style/fill/utils/FillStyleModifier.swift
+++ src/style/fill/utils/FillStyleModifier.swift
@@ -1,28 +1,7 @@
 import Foundation
 
 class FillStyleModifier {
-    /**
-     * Applies @param gradient on to @param graphics
-     */
-    class func beginGradientFill(graphics:Graphics,_ gradient:Gradient/*IGradient*/) {
-        
-        
-        //continue here: figure out why the bellow nsColor works and the color in gradient doesnt. and change the types to IGradient again
-        
-       
-        //let nsFillGradientColorA = ColorParser.nsColor("FF0000"/*fillGradientColorA*/, Float(1))
-        //let nsFillGradientColorB = ColorParser.nsColor("0000FF"/*fillGradientColorB*/, Float(1))
-        
-        Swift.print("___")
-        Swift.print(gradient.locations[0])
-        Swift.print(gradient.locations[1])
-        Swift.print("___")
-
-        //Swift.print(gradient.locations.count)
-        //let gradient:Gradient = Gradient([gradient.colors[0]/*nsFillGradientColorA.CGColor*/,gradient.colors[1]/*nsFillGradientColorB.CGColor*/])
-         /**/
-        graphics.gradientFill(gradient)
-    }
+    
 }
 
 

7c2a17534f4699e8e3fdacae98539fd862eff2fd
Eonist <unicornhive@gmail.com>
2015-11-26 05:56:49 +0100

    Files modified: 1
    Modified 1 file:src/css/utils/CSSPropertyParser.swift

--- src/css/utils/CSSPropertyParser.swift
+++ src/css/utils/CSSPropertyParser.swift
@@ -128,7 +128,7 @@ private class Utils{
                 //Swift.print("ratio: " + ratio)
                 
                 var ratioValue:Double = Utils.ratio(ratio)
-                if(ratioValue.isNaN) { ratioValue = (Double(i) / (Double(properties.count)-1.0)) * 255.0 }/*if there is no ratio then set the ratio to its natural progress value and then multiply by 255 to get valid ratio values*/
+                if(ratioValue.isNaN) { ratioValue = (Double(i) / (Double(properties.count)-1.0)) * 1.0/*255.0*/ }/*if there is no ratio then set the ratio to its natural progress value and then multiply by 255 to get valid ratio values*/
                 //Swift.print("gradient.locations start: " + "\(gradient.locations.count)")
                 gradient.locations.append(CGFloat(Float(ratioValue)))//append ratioValue
                 //Swift.print("gradient.locations end: " + "\(gradient.locations.count)")

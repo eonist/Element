214bf9207f61e2cf4eb7dfe599c6b6bd0317dcd3
Eonist <unicornhive@gmail.com>
2015-11-27 16:23:47 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/LineGraphic.swift

--- src/skin/graphic/decorator/LineGraphic.swift
+++ src/skin/graphic/decorator/LineGraphic.swift
@@ -5,7 +5,7 @@ import Foundation
 class LineGraphic:GraphicDecoratable {
     var p1:CGPoint;
     var p2:CGPoint;
-    init(_ p1:CGPoint = CGPoint(), _ p2:CGPoint = CGPoint(), _ decoratable: IGraphicDecoratable) {
+    init(_ p1:CGPoint = CGPoint(), _ p2:CGPoint = CGPoint(), _ decoratable: IGraphicDecoratable = BaseGraphic(nil,LineStyle())) {
         self.p1 = p1
         self.p2 = p2
         super.init(decoratable)

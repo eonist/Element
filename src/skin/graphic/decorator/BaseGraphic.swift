9b7a347971fb2ba786fd87b64decf3673fd69d66
Eonist <unicornhive@gmail.com>
2015-11-27 08:42:25 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/BaseGraphic.swift

--- src/skin/graphic/decorator/BaseGraphic.swift
+++ src/skin/graphic/decorator/BaseGraphic.swift
@@ -4,7 +4,7 @@ import Cocoa
 * // :TODO: possibly get rid of the setters for the fillStyle and Line style and use implicit setFillStyle and setLineStyle?
 * NOTE: We dont need a line mask, just subclass the Graphics class so it supports masking of the line aswell (will require some effort)
 */
-class BaseGraphic :GraphicDecoratable,IBaseGraphic{
+class BaseGraphic :AbstractGraphicDecoratable,IBaseGraphic{
     lazy var graphics:Graphics = Graphics()
     override var graphic:BaseGraphic {return self}
     var x:CGFloat = 0
@@ -16,11 +16,9 @@ class BaseGraphic :GraphicDecoratable,IBaseGraphic{
     var linePath:CGMutablePath = CGPathCreateMutable()
     
     init(_ fillStyle:IFillStyle? = nil, _ lineStyle:ILineStyle? = nil, _ lineOffsetType:OffsetType? = nil) {
-        super.init(self)
         self.fillStyle = fillStyle
         self.lineStyle = lineStyle
         self.lineOffsetType = lineOffsetType
-        
     }
     
     /**

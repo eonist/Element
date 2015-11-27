67d30e6e56fe619aac739a63a35e09727cf18a75
Eonist <unicornhive@gmail.com>
2015-11-27 16:19:52 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/LineGraphic.swift

--- src/skin/graphic/decorator/LineGraphic.swift
+++ src/skin/graphic/decorator/LineGraphic.swift
@@ -1,5 +1,27 @@
 import Foundation
-
-class LineGraphic {
-
+/**
+ * TODO: you should also probably create a class named PathGraphic
+ */
+class LineGraphic:GraphicDecoratable {
+    var p1:CGPoint;
+    var p2:CGPoint;
+    init(_ p1:CGPoint = CGPoint(), _ p2:CGPoint = CGPoint(), _ decoratable: IGraphicDecoratable) {
+        self.p1 = p1
+        self.p2 = p2
+        super.init(decoratable)
+    }
+    /**
+     *
+     */
+    override func drawLine() {
+        Swift.print("LineGraphic.drawLine()")
+        graphic.linePath = CGPathParser.line(p1, p2)
+    }
+    /**
+     *
+     */
+    func setPoints(p1:CGPoint, p2:CGPoint) {
+        self.p1 = p1;
+        self.p2 = p2;
+    }
 }

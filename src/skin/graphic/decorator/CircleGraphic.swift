0ffcc98f59a1a2b77426df7c29767405634f642e
Eonist <unicornhive@gmail.com>
2015-11-27 06:07:44 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/CircleGraphic.swift

--- src/skin/graphic/decorator/CircleGraphic.swift
+++ src/skin/graphic/decorator/CircleGraphic.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class CircleGraphic {
-
+class CircleGraphic:BaseGraphic {
+    override func drawFill() {
+        //CGPathParser.circle(100, x, x)
+    }
+    override func drawLine() {
+        //needs code
+        
+    }
 }

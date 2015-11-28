2e1d8ccaf48746af945a3a1eacbe3a7db0932536
Eonist <unicornhive@gmail.com>
2015-11-28 13:29:19 +0100

    Files modified: 1
    Modified 1 file:src/skin/IPositionalGraphic.swift

--- src/skin/IPositionalGraphic.swift
+++ src/skin/IPositionalGraphic.swift
@@ -3,16 +3,15 @@ import Foundation
 protocol IPositionalGraphic {
     var x:CGFloat{get set}
     var y:CGFloat{get set}
-    func getGraphic()->Graphic
+    func getGraphic() -> BaseGraphic
 }
 extension IPositionalGraphic{
+    var x:CGFloat{get{return getGraphic().x}set{getGraphic().x = newValue}}
+    var y:CGFloat{get{return getGraphic().y}set{getGraphic().y = newValue}}
     /**
-     * TODO: remove the x and y values from this class, some graphics may not have a natural x and y pos like LineGraphic or PathGraphic
+     *
      */
     func setPosition(position:CGPoint){
         getGraphic().setPosition(position)
-        getGraphic().y = position.y
-        //path = CGPathModifier.translate(&path,position.x,position.y)//Transformations
-        //linePath = CGPathModifier.translate(&linePath,position.x,position.y)//Transformations
     }
 }

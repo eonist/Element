de77d2b9a827ef88cd8ccbf22d6202dbc5b63b1d
Eonist <unicornhive@gmail.com>
2015-11-28 17:01:24 +0100

    Files modified: 1
    Modified 1 file:src/skin/SizeableGraphic.swift

--- src/skin/SizeableGraphic.swift
+++ src/skin/SizeableGraphic.swift
@@ -10,6 +10,9 @@ class SizeableGraphic:PositionalGraphic,ISizeableGraphic {
     //use cgsize and cgpoint as the variables and then provide convenince init for the width,height,x,y props
     //implement size and point variables for easy access aswell that can set and get
     
+    //Do the bellow!!!!
+    //actually you could solve all this Sizable and positionable nonsence by adding everything to BaseGraphic again and just set it getters for classes that cant be set and setters and getters for all others
+    
     
     init(_ x:CGFloat = 0, _ y:CGFloat = 0, _ width:CGFloat = 100,_ height:CGFloat = 100,_ decoratable: IGraphicDecoratable) {
         self.width = width

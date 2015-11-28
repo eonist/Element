8aeafd51155a849b388abc9e9be01688eed1c6e7
Eonist <unicornhive@gmail.com>
2015-11-28 19:52:32 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/CircleGraphic.swift

--- src/skin/graphic/decorator/CircleGraphic.swift
+++ src/skin/graphic/decorator/CircleGraphic.swift
@@ -5,7 +5,7 @@ class CircleGraphic:PositionalGraphic{
     
     //var width:CGFloat {get{return radius!}set{fatalError("NOT SUPPORTED")}}
     //var height:CGFloat {get{return radius!}set{fatalError("NOT SUPPORTED")}}
-    init(_ radius:CGFloat = 50,_ decoratable: IGraphicDecoratable = BaseGraphic(FillStyle(NSColor.redColor()))) {
+    init(_ radius:CGFloat = 50,_ decoratable: IGraphicDecoratable = BaseGraphic(FillStyle(NSColor.greenColor()))) {
         //Swift.print("CircleGraphic.init()")
         self.radius = radius
         super.init(CGPoint(0,0),decoratable)

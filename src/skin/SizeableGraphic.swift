0f297c97759188228f885a1d8b03ad4d47835ba2
Eonist <unicornhive@gmail.com>
2015-11-28 19:42:42 +0100

    Files modified: 1
    Modified 1 file:src/skin/SizeableGraphic.swift

--- src/skin/SizeableGraphic.swift
+++ src/skin/SizeableGraphic.swift
@@ -8,6 +8,9 @@ class SizeableGraphic:PositionalGraphic,ISizeable {
         self.size = size
         super.init(position,decoratable)
     }
+    convenience init(_ width:CGFloat = 100, _ height:CGFloat = 100,decoratable: IGraphicDecoratable){
+        self.init(CGPoint(0,0),CGSize(width,height),decoratable)
+    }
     convenience init(_ width:CGFloat = 100, _ height:CGFloat = 100){
         self.init(CGPoint(0,0),CGSize(width,height))
     }

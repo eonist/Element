3dfcba044bafa33853374c36330837c7601ed500
Eonist <unicornhive@gmail.com>
2015-11-28 17:55:36 +0100

    Files modified: 2
    Modified 2 files:src/skin/SizeableGraphic.swiftsrc/skin/graphic/decorator/PositionalGraphic.swift

--- src/skin/SizeableGraphic.swift
+++ src/skin/SizeableGraphic.swift
@@ -3,18 +3,10 @@ import Foundation
  * All SizableGraphics are also positionable
  */
 class SizeableGraphic:PositionalGraphic,ISizeableGraphic {
-    override var size:CGSize
     
-    //continue here:
-    //use cgsize and cgpoint as the variables and then provide convenince init for the width,height,x,y props
-    //implement size and point variables for easy access aswell that can set and get
-    
-    //Do the bellow!!!!
-    //actually you could solve all this Sizable and positionable nonsence by adding everything to BaseGraphic again and just set it getters for classes that cant be set and setters and getters for all others
-    //remove the getPositional etc
-    
+    override var size:CGSize{get{return }set{}}
     init(position:CGPoint, size:CGSize,_ decoratable: IGraphicDecoratable) {
-        self.size = size
+        //self.size = size
         super.init(position,decoratable)
     }
     override func getSizeableGraphic() -> ISizeableGraphic {
--- src/skin/graphic/decorator/PositionalGraphic.swift
+++ src/skin/graphic/decorator/PositionalGraphic.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 class PositionalGraphic:GraphicDecoratable,IPositionalGraphic {
-    override var position:CGPoint
+    //var position:CGPoint
     init(_ position:CGPoint,_ decoratable: IGraphicDecoratable) {
-        self.position = position
+        //self.position = position
         super.init(decoratable)
     }
     override func getPositionalGraphic() -> IPositionalGraphic {

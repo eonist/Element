3c366e7f0f0c5674849ae505bf93087dcba36076
Eonist <unicornhive@gmail.com>
2015-11-28 08:45:16 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -9,37 +9,7 @@ class GraphicSkin:Skin{
         Swift.print("GraphicSkin.drawRect()")
         
         
-        
-        
-        //Continue here:
-        
-        
-        
-        
-        //move configure to its original place, and add initialize here
-        
-            //the path creation should not update when the drawRect is called
-        
-            //the path creation should happen on init and when a update method is excplicitly called
-        
-            //reorganize the code to reflect this new scheme
-        
-        //Actually all graphics data needs to be read from simple variables 
-        
-            //And if a graphics needs to change, then only this graphics will change, and not all graphics in all NSViews.
-        
-            //a hasStyleChanged Boolean will be set to true if graphic style has changed
-        
-                //-> then the cached style data will be updated
-        
-            //a hasPathDataChanged Boolean will be set to true if path data has been changed
-        
-                //-> then the cached pathdata will be updated
-        
-        //todo: test what happends if you set a if flag in the drawRect method, will the graphics be deleted?
-        
-        
-        
+               
         
         
         GraphicSkinParser.configure(self)/*this call is here because CGContext is only accessible after drawRect is called*/

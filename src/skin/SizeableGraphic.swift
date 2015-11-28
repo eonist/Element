8cfe17c06e486d60ea44e61c97c055fbd02929f1
Eonist <unicornhive@gmail.com>
2015-11-28 16:31:22 +0100

    Files modified: 1
    Modified 1 file:src/skin/SizeableGraphic.swift

--- src/skin/SizeableGraphic.swift
+++ src/skin/SizeableGraphic.swift
@@ -5,6 +5,12 @@ import Foundation
 class SizeableGraphic:PositionalGraphic,ISizeableGraphic {
     var width:CGFloat;
     var height:CGFloat;
+    
+    
+    //use cgsize and cgpoint as the variables and then provide convenince init for the width,height,x,y props
+    //implement size and point variables for easy access aswell that can set and get
+    
+    
     init(_ x:CGFloat = 0, _ y:CGFloat = 0, _ width:CGFloat = 100,_ height:CGFloat = 100,_ decoratable: IGraphicDecoratable) {
         self.width = width
         self.height = height

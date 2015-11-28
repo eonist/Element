ac7a32a31810c6fe398f1bb02f298284c7afee43
Eonist <unicornhive@gmail.com>
2015-11-28 09:18:41 +0100

    Files modified: 1
    Modified 1 file:src/skin/PositionalGraphic.swift

--- src/skin/PositionalGraphic.swift
+++ src/skin/PositionalGraphic.swift
@@ -1,5 +1,15 @@
 import Foundation
 
-class PositionalGraphic {
-
+class PositionalGraphic:GraphicDecoratable {
+    var x:CGFloat = 0
+    var y:CGFloat = 0
+    /**
+     *
+     */
+    override func setPosition(position:CGPoint){
+        x = position.x
+        y = position.y
+        //path = CGPathModifier.translate(&path,position.x,position.y)//Transformations
+        //linePath = CGPathModifier.translate(&linePath,position.x,position.y)//Transformations
+    }
 }

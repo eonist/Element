abe9388dcbfb525c9584eaec4cb5f676d6ee4ed4
Eonist <unicornhive@gmail.com>
2015-11-28 15:33:02 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/CircleGraphic.swift

--- src/skin/graphic/decorator/CircleGraphic.swift
+++ src/skin/graphic/decorator/CircleGraphic.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-class CircleGraphic:GraphicDecoratable,IPositionalGraphic,ISizeable {
+class CircleGraphic:GraphicDecoratable,IPositionalGraphic {
     var radius:CGFloat?
     init(_ radius:CGFloat,_ decoratable: IGraphicDecoratable = BaseGraphic(FillStyle(NSColor.redColor()))) {
         //Swift.print("CircleGraphic.init()")

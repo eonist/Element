cdf7a444d57662b42dafdaa250831a8b4aa9b1b0
Eonist <unicornhive@gmail.com>
2015-11-28 12:58:19 +0100

    Files modified: 1
    Modified 1 file:src/skin/IPositionalGraphic.swift

--- src/skin/IPositionalGraphic.swift
+++ src/skin/IPositionalGraphic.swift
@@ -9,8 +9,8 @@ extension IPositionalGraphic{
      * TODO: remove the x and y values from this class, some graphics may not have a natural x and y pos like LineGraphic or PathGraphic
      */
     mutating func setPosition(position:CGPoint){
-        x = position.x
-        y = position.y
+        self.x = position.x
+        self.y = position.y
         //path = CGPathModifier.translate(&path,position.x,position.y)//Transformations
         //linePath = CGPathModifier.translate(&linePath,position.x,position.y)//Transformations
     }

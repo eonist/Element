6bc6aac6d9385e8fc5e8b18722b481b1e142b9dd
Eonist <unicornhive@gmail.com>
2015-11-28 16:02:42 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PositionalGraphic.swift

--- src/skin/graphic/decorator/PositionalGraphic.swift
+++ src/skin/graphic/decorator/PositionalGraphic.swift
@@ -1,10 +1,12 @@
 import Foundation
 
 class PositionalGraphic:GraphicDecoratable,IPositionalGraphic {
-    var x:CGFloat = 0
-    var y:CGFloat = 0
-    init(_ decoratable: IGraphicDecoratable) {
-        <#code#>
+    var x:CGFloat
+    var y:CGFloat
+    init(x:CGFloat,y:CGFloat,_ decoratable: IGraphicDecoratable) {
+        self.x = x
+        self.y = y
+        super.init(decoratable)
     }
     override func getPositionalGraphic() -> IPositionalGraphic {
         return self

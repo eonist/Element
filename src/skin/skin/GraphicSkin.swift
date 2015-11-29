ee700a5c74430dad565928e26d92cd2d52bfe9ad
Eonist <unicornhive@gmail.com>
2015-11-29 20:43:01 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -20,6 +20,7 @@ class GraphicSkin:Skin{
         //Swift.print("GraphicSkin.draw() NOT IMPLEMENTED YET")
         if(hasStateChanged || hasSizeChanged || hasStyleChanged){
             applyProperties(decoratable);
+            SkinModifier.align(self,IDecoratable3(decoratables[depth]).graphic,depth);
         }
         super.draw();
     }

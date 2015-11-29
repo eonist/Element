c0cf03c3c2ffa43b202a3263dc9f2e66186716cd
Eonist <unicornhive@gmail.com>
2015-11-29 20:04:52 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/SkinModifier.swift

--- src/skin/skin/utils/SkinModifier.swift
+++ src/skin/skin/utils/SkinModifier.swift
@@ -1,5 +1,18 @@
-import Foundation
-
+import Cocoa
+/**
+ * // :TODO: Make the methods more Element cetric less skin centric
+ */
 class SkinModifier {
-
+    /**
+     * Aligns @param view
+     */
+    class func align(skin:ISkin, view:NSView) {
+        //var offset:CGPoint = StylePropertyParser.offset(skin,depth);
+        //var padding:Padding2 = StylePropertyParser.padding(skin,depth);
+        var margin:Margin = StylePropertyParser.margin(skin);
+        //var floatType:String = SkinParser.float(skin,depth);
+        //if(floatType == CSSConstants.LEFT || floatType == "" || floatType == null) DisplayObjectModifier.position(displayObject, new Point(margin.left + offset.x, margin.top + offset.y));
+        //else if(floatType == CSSConstants.RIGHT) DisplayObjectModifier.position(displayObject, new Point(padding.right + margin.right + offset.x, margin.top + padding.top + offset.y));
+        //else /*floatType == CSSConstants.NONE*/DisplayObjectModifier.position(displayObject, new Point(margin.left + offset.x, margin.top + offset.y));// :TODO: this is temp for testing
+    }
 }

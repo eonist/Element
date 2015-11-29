6209324031f1dcc0981ea4f7ab7c338503b3c7e5
Eonist <unicornhive@gmail.com>
2015-11-29 18:21:05 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/GraphicModifier.swift

--- src/skin/graphic/utils/GraphicModifier.swift
+++ src/skin/graphic/utils/GraphicModifier.swift
@@ -4,35 +4,11 @@ class GraphicModifier {
     /**
     * TODO: fill and linestyle should be graphic spessific see original code
     */
-    public static function applyProperties(decoratable:IDecoratable3,fillStyle:IFillStyle,lineStyle:ILineStyle,offsetType:OffsetType):IDecoratable3 {
-    decoratable.graphic.fillStyle = fillStyle;
-    decoratable.graphic.lineStyle = lineStyle;
-    decoratable.graphic.lineOffsetType = offsetType;
-    return decoratable;
-    }
-    class func applyProperties(graphics:Graphics,_ fillStyle:IFillStyle ,_ lineStyle:ILineStyle*//*, _ skinState:String*/){
-        //Swift.print("GraphicModifier.applyProperties() " + String(fillStyle.getStyleProperty("fillColor")!.value))
-        //let fillColor:String = fillStyle.getStyleProperty("fillColor")!.value as! String
-        //let fillAlpha:Double = Double(String(fillStyle.getStyleProperty("fillAlpha")!.value))!
-        /*
-        let lineColor:String = lineStyle.getStyleProperty("lineColor")!.value as! String
-        let lineAlpha:Double = lineStyle.getStyleProperty("lineAlpha")!.value as! Double
-        let lineWidth:Int = lineStyle.getStyleProperty("lineWidth")!.value as! Int
-        */
-        
-        //let nsFillColor = ColorParser.nsColor(fillColor, Float(fillAlpha))//fill
-        //Swift.print(nsFillColor)
-        //let nsLineColor = ColorParser.nsColor(lineColor, Float(lineAlpha))//line
-        //ViewModifier.applyColor(self as! NSView, nsFillColor, nsLineColor, lineWidth)
-        
-        //path.lineWidth = CGFloat(lineWidth)
-        
-        //nsFillColor.setFill();
-        //nsLineColor.setStroke();
-        ////graphics.line(CGFloat(lineWidth),nsLineColor)//Stylize the line
-        
-
-        graphics.fill(fillStyle.color)//Stylize the fill
+    class func applyProperties(decoratable:GraphicDecoratable,fillStyle:IFillStyle,lineStyle:ILineStyle,offsetType:OffsetType)->GraphicDecoratable {
+        decoratable.graphic.fillStyle = fillStyle;
+        decoratable.graphic.lineStyle = lineStyle;
+        decoratable.graphic.lineOffsetType = offsetType;
+        return decoratable;
     }
     /**
      * Finalizes the fill style to the path

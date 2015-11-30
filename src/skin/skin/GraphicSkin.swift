e94cd39874e20729091b61389e2d6ec7fd84d3b1
Eonist <unicornhive@gmail.com>
2015-11-30 09:21:34 +0100

    Files modified: 2
    Modified 2 files:src/skin/skin/GraphicSkin.swiftsrc/skin/skin/utils/SkinModifier.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -6,7 +6,7 @@ class GraphicSkin:Skin{
         super.init(style, state, element)
         decoratable = GraphicSkinParser.configure(self)/*this call is here because CGContext is only accessible after drawRect is called*/
         
-        SkinModifier.align(self,&decoratable.positional);
+        SkinModifier.align(self);
         
         //continue here investigat ewhy you cant use inout with protocols in playground, try struct if classes wont work
     }

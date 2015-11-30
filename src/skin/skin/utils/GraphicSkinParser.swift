0107d21d2b7c519ca520e2878d6f3233ef1c50bc
Eonist <unicornhive@gmail.com>
2015-11-30 06:16:08 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/GraphicSkinParser.swift

--- src/skin/skin/utils/GraphicSkinParser.swift
+++ src/skin/skin/utils/GraphicSkinParser.swift
@@ -14,7 +14,7 @@ class GraphicSkinParser{
         let fillStyle:IFillStyle = StylePropertyParser.fillStyle(skin);
         let lineStyle:ILineStyle = StylePropertyParser.lineStyle(skin);
         var graphic:IGraphicDecoratable = Utils.baseGraphic(skin,fillStyle,lineStyle)
-        graphic = Utils.rectGraphic(skin,graphic)
+        graphic = Utils.rectGraphic(skin,graphic).setPosition(<#T##position: CGPoint##CGPoint#>)
         if(StylePropertyAsserter.hasFillet(skin)) { graphic = Utils.fillet(graphic, StylePropertyParser.fillet(skin)) }
         if(StylePropertyAsserter.hasGradient(skin)) { graphic = Utils.gradient(graphic) }
         return graphic

d9f194c683f26cccfcc7165cc83e961f48ce2821
Eonist <unicornhive@gmail.com>
2015-11-30 12:18:38 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/ButtonTest.swift

--- src/ui/button/ButtonTest.swift
+++ src/ui/button/ButtonTest.swift
@@ -1,7 +1,16 @@
 import Cocoa
 
-extension NSButton:IElement{
+protocol IButton{
+    func someMethod()
+}
+
+//continue testing how you can add protocol to Classes that cant extend
+
+extension NSButton:IButton{
     public override func drawRect(dirtyRect: NSRect) {
         //do something
+    }
+    func someMethod() {
+        //do somthing
     }
 }

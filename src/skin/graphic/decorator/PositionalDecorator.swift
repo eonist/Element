6464abff76a4e8e28a54cef36bc0affda13482e5
Eonist <unicornhive@gmail.com>
2015-12-01 10:30:34 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PositionalDecorator.swift

--- src/skin/graphic/decorator/PositionalDecorator.swift
+++ src/skin/graphic/decorator/PositionalDecorator.swift
@@ -1,5 +1,20 @@
 import Foundation
 
-class PositionalDecorator {
-
+/**
+ *
+ */
+class PositionalDecorator:GraphicDecoratable,IPositional{
+    var position:CGPoint{
+        get{
+            if(decoratable is PositionalGraphic){return (decoratable as! PositionalGraphic).position}
+            else{fatalError("Must subclass PositionalGraphic")}
+        }
+        set{
+            if(decoratable is PositionalGraphic){(decoratable as! PositionalGraphic).position = newValue}
+            else{fatalError("Must subclass PositionalGraphic")}
+        }
+    }
+    func getPosition() -> CGPoint {
+        return position
+    }
 }

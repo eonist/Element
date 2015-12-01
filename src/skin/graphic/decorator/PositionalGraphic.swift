cbf0fc694c0de97f9af30ac7e9fcd15bcb6a9746
Eonist <unicornhive@gmail.com>
2015-12-01 14:35:00 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PositionalGraphic.swift

--- src/skin/graphic/decorator/PositionalGraphic.swift
+++ src/skin/graphic/decorator/PositionalGraphic.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 class PositionalGraphic:GraphicDecoratable,IPositional {
-    var position:CGPoint
+    var pos:CGPoint
     init(_ position:CGPoint,_ decoratable: IGraphicDecoratable) {
-        self.position = position
+        self.pos = position
         super.init(decoratable)
     }
     /**
      * NOTE: This method must remain an instance method so that other decorators can override it (Circle, Line, Path, etc)
      */
     func getPosition() -> CGPoint {
-        return position
+        return pos
     }
     func setPosition(position: CGPoint) {
-        self.position = position
+        self.pos = position
     }
 }

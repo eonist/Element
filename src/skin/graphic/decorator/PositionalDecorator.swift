09bb860e836dd7e9e801a455ff906425db502a71
Eonist <unicornhive@gmail.com>
2015-12-01 14:42:35 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PositionalDecorator.swift

--- src/skin/graphic/decorator/PositionalDecorator.swift
+++ src/skin/graphic/decorator/PositionalDecorator.swift
@@ -5,14 +5,14 @@ import Foundation
  * NOTE: the if else clauses doesnt look good, but swift doesnt allow setting values via protocols that uses extensions for its functionality, so its eigther this or implementing implicit setter method in each subclass conforming to IPositional
  */
 class PositionalDecorator:GraphicDecoratable,IPositional{
-    var position:CGPoint{
+    var pos:CGPoint{
         get{
             if(decoratable is IPositional){return (decoratable as! IPositional).position}
             else{fatalError("Must subclass PositionalGraphic")}
         }
         set{
             if(decoratable is PositionalGraphic){(decoratable as! PositionalGraphic).position = newValue}
-            else if(decoratable is PositionalDecorator){(decoratable as! PositionalDecorator).position = newValue}
+            else if(decoratable is PositionalDecorator){(decoratable as! PositionalDecorator).pos = newValue}
             else{fatalError("Must subclass PositionalGraphic")}
         }
     }
@@ -20,9 +20,9 @@ class PositionalDecorator:GraphicDecoratable,IPositional{
      * NOTE: This method must remain an instance method so that other decorators can override it (Circle, Line, Path, etc)
      */
     func getPosition() -> CGPoint {
-        return position
+        return pos
     }
     func setPosition(position: CGPoint) {
-        self.position = position
+        self.pos = position
     }
 }

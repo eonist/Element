6fc7ac12646403edaf170edeb9509057d5f04fe9
Eonist <unicornhive@gmail.com>
2015-12-01 08:15:02 +0100

    Files modified: 2
    Modified 2 files:src/skin/graphic/decorator/AbstractGraphicDecoratable.swiftsrc/skin/graphic/decorator/IGraphicDecoratable.swift

--- src/skin/graphic/decorator/IGraphicDecoratable.swift
+++ src/skin/graphic/decorator/IGraphicDecoratable.swift
@@ -3,7 +3,7 @@ import Foundation
  * These are the methods the GraphicDecoratable must implement
  * Differs from IBaseGraphic in that IBaseGraphic has some variables that the decoratables isnt dependent on when it comes to the creation procedure
  */
-protocol IGraphicDecoratable/*:IPositional,ISizeable*/{
+protocol IGraphicDecoratable:IPositional,ISizeable{
     var graphic:BaseGraphic{get}
     func getGraphic() -> BaseGraphic/*Dont revert this variable to a protocol*/
     /*Design*/
@@ -17,6 +17,8 @@ protocol IGraphicDecoratable/*:IPositional,ISizeable*/{
     func stylizeLine()
     func initialize()
     /*Size & position*/
+    var size:CGSize{get set}
+    var position:CGPoint{get set}
     func getSize()->CGSize
     func getPosition()->CGPoint
     func setSize(size:CGSize)

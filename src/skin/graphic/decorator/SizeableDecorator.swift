2c82c01da556bf6c15eb0992e08a064422fd499a
Eonist <unicornhive@gmail.com>
2015-12-01 10:13:08 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/SizeableDecorator.swift

--- src/skin/graphic/decorator/SizeableDecorator.swift
+++ src/skin/graphic/decorator/SizeableDecorator.swift
@@ -1,5 +1,25 @@
 import Foundation
-
+/**
+ * TODO: Shorten the bellow code with if shorthand
+ */
 class SizeableDecorator {
-
+    var size:CGSize{
+        get{
+            if(decoratable is SizeableGraphic){
+                return (decoratable as! SizeableGraphic).size
+            }else{
+                fatalError("Must subclass SizeableGraphic")
+            }
+        }
+        set{
+            if(decoratable is SizeableGraphic){
+                (decoratable as! SizeableGraphic).size = newValue
+            }else{
+                fatalError("Must subclass SizeableGraphic")
+            }
+        }
+    }
+    func getSize() -> CGSize {
+        return size
+    }
 }

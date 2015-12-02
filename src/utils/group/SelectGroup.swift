8f037aebd5b6ff7ff33f908ea14196eb2d653d9a
Eonist <unicornhive@gmail.com>
2015-12-02 07:02:03 +0100

    Files modified: 1
    Modified 1 file:src/utils/group/SelectGroup.swift

--- src/utils/group/SelectGroup.swift
+++ src/utils/group/SelectGroup.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 class SelectGroup {
-
+    private var selectables:Array<ISelectable> = [];
+    private var selected:ISelectable;
+    init(selectables:Array<ISelectable>, selected:ISelectable = nil){
+        
+    }
 }

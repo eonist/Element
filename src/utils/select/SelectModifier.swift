6fe7a0e0350d36abee58c5bfce753519ac4a29c9
Eonist <unicornhive@gmail.com>
2015-12-02 07:51:33 +0100

    Files modified: 1
    Modified 1 file:src/utils/select/SelectModifier.swift

--- src/utils/select/SelectModifier.swift
+++ src/utils/select/SelectModifier.swift
@@ -2,8 +2,8 @@ import Foundation
 
 class SelectModifier {
     /**
-    * Unselects all in @param items except @param target
-    */
+     * Unselects all in @param items except @param target
+     */
     class func unSelectAllExcept(exceptionItem:ISelectable, _ selectables:Array<ISelectable>) {// :TODO: refactor this function// :TODO: rename to unSelectAllExcept
         for selectable : ISelectable in selectables {if(selectable !== exceptionItem && selectable.isSelected) {selectable.setSelected(false)}}
     }

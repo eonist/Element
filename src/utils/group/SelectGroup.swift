afba5a73646e12035215ef6b60374b7421e9f58a
Eonist <unicornhive@gmail.com>
2015-12-02 07:53:29 +0100

    Files modified: 2
    Modified 2 files:src/utils/group/SelectGroup.swiftsrc/utils/group/event/SelectGroupEvent.swift

--- src/utils/group/SelectGroup.swift
+++ src/utils/group/SelectGroup.swift
@@ -14,14 +14,15 @@ class SelectGroup {
      * @Note useWeakReference is set to true so that we dont have to remove the event if the selectable is removed from the SelectGroup or view
      */
     func addSelectable(selectable:ISelectable) {
-        NSNotificationCenter.defaultCenter().addObserver((selectable as? AnyObject)!, selector: "onSelect:", name: SelectEvent.select, object: (selectable as? AnyObject)!)
-        NSNotificationCenter.defaultCenter().addObserver((selectable as? AnyObject)!, selector: "onSelect:", name: SelectEvent.deSelect, object: (selectable as? AnyObject)!)
+        let anyObj:AnyObject = selectable
+        NSNotificationCenter.defaultCenter().addObserver(anyObj, selector: "onSelect:", name: SelectEvent.select, object: anyObj)
+        NSNotificationCenter.defaultCenter().addObserver(anyObj, selector: "onSelect:", name: SelectEvent.deSelect, object: anyObj)
         selectables.append(selectable);
     }
     func onSelect(sender: AnyObject) {// :TODO: make this as protected since you may want to impose different functionaly when clicked, like multi select etc
-        NSNotificationCenter.defaultCenter().postNotificationName(SelectGroupEvent.selectGroupSelect, object:self)/*bubbles:true because i.e: radioBulet may be added to RadioButton and radioButton needs to dispatch Select event if the SelectGroup is to work*/
+        NSNotificationCenter.defaultCenter().postNotificationName(SelectGroupEvent.select, object:self)/*bubbles:true because i.e: radioBulet may be added to RadioButton and radioButton needs to dispatch Select event if the SelectGroup is to work*/
         selected = (sender as! NSNotification).object as? ISelectable
-        SelectModifier.unSelectAllExcept(selected, selectables);
-        dispatchEvent(new SelectGroupEvent(SelectGroupEvent.SELECT_GROUP_CHANGE,_selected));
+        SelectModifier.unSelectAllExcept(selected!, selectables);
+        NSNotificationCenter.defaultCenter().postNotificationName(SelectGroupEvent.change, object:self)
     }
 }

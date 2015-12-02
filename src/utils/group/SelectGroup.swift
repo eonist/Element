634fbb128deff078f53849ce52ab6a4edc44cc98
Eonist <unicornhive@gmail.com>
2015-12-02 07:03:57 +0100

    Files modified: 1
    Modified 1 file:src/utils/group/SelectGroup.swift

--- src/utils/group/SelectGroup.swift
+++ src/utils/group/SelectGroup.swift
@@ -2,8 +2,12 @@ import Foundation
 
 class SelectGroup {
     private var selectables:Array<ISelectable> = [];
-    private var selected:ISelectable;
-    init(selectables:Array<ISelectable>, selected:ISelectable = nil){
-        
+    private var selected:ISelectable?;
+    init(selectables:Array<ISelectable>, selected:ISelectable? = nil){
+        addSelectables(selectables);
+        self.selected = selected
+    }
+    func addSelectables(selectables:Array){
+        for item : ISelectable in selectables {addSelectable(item)}
     }
 }

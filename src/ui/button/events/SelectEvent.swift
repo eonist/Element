b70e93e369c038d2aabb2c743003e032780cc4b9
Eonist <unicornhive@gmail.com>
2015-12-02 07:17:32 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/events/SelectEvent.swift

--- src/ui/button/events/SelectEvent.swift
+++ src/ui/button/events/SelectEvent.swift
@@ -1,5 +1,7 @@
 import Foundation
-
+/**
+ * “Unselect” vs “Deselect”: Unselected is used to qualify something that has not been selected, not something that was selected and isn't anymore.
+ */
 class SelectEvent {
     static var select:String = "select"
     static var deSelect:String = "deSelect"

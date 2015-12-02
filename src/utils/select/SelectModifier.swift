6dc7985e4b3ee7a7590dd6e73d9f865e18ffad69
Eonist <unicornhive@gmail.com>
2015-12-02 07:25:47 +0100

    Files modified: 1
    Modified 1 file:src/utils/select/SelectModifier.swift

--- src/utils/select/SelectModifier.swift
+++ src/utils/select/SelectModifier.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class SelectModifier {
-    
+    /**
+    * Unselects all in @param items except @param target
+    */
+    class func unSelectAllExcept(exceptionItem:ISelectable, selectables:*):void {// :TODO: refactor this function// :TODO: rename to unSelectAllExcept
+        if(selectables is DisplayObjectContainer) selectables = SelectParser.selectables(selectables);
+        else if((selectables is Array) == false) throw new IllegalOperationError("type not supported");
+        if(exceptionItem as ISelectable == null) throw new IllegalOperationError("item does not implement ISelectable: "+exceptionItem);
+        for each (var selectable : ISelectable in selectables) if(selectable != exceptionItem && selectable.selected) selectable.setSelected(false);
+    }
 }

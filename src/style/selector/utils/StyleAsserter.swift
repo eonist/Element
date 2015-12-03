5fc47498ea2c2579aaf12fe75f87732c57371341
Eonist <unicornhive@gmail.com>
2015-12-03 12:29:19 +0100

    Files modified: 1
    Modified 1 file:src/style/selector/utils/StyleAsserter.swift

--- src/style/selector/utils/StyleAsserter.swift
+++ src/style/selector/utils/StyleAsserter.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class StyleAsserter {
-
+    /**
+    * Asserts if @param style is a direct style of the @param stack (the stack is derived from an Element instance)
+    * @Note the opposite would be an indirect style
+    */
+    class func direct(stack:Array<>,style:IStyle)->Bool {//rename to isDirect
+        return SelectorAsserter.hasSimilarity(stack[stack.length-1], style.selectors[style.selectors.length-1]);
+    }
 }

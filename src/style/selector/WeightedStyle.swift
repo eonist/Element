6cabfb1d8df4469bad47203d79105498c57eaebd
Eonist <unicornhive@gmail.com>
2015-12-03 11:58:42 +0100

    Files modified: 1
    Modified 1 file:src/style/selector/WeightedStyle.swift

--- src/style/selector/WeightedStyle.swift
+++ src/style/selector/WeightedStyle.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class WeightedStyle {
-
+class WeightedStyle:Style{// :TODO: move into its own class?
+    var styleWeight:StyleWeight;
+    init(style:IStyle, styleWeight:StyleWeight) {
+        self.styleWeight = styleWeight;
+        super.init(style.name,style.selectors,style.styleProperties);
+    }
 }

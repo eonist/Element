14142b4491868117c3f6d3f4017908c721039342
Eonist <unicornhive@gmail.com>
2015-12-03 11:29:43 +0100

    Files modified: 1
    Modified 1 file:src/style/selector/StyleWeight.swift

--- src/style/selector/StyleWeight.swift
+++ src/style/selector/StyleWeight.swift
@@ -1,5 +1,20 @@
 import Foundation
-
+/**
+ * // :TODO: move into its own class? if other classses uses this code yes if not then no!
+ */
 class StyleWeight {
-
+    var selectorWeights:Array;
+    var idWeight:uint;
+    var elementWeight:uint;
+    var classWeight:uint;
+    var stateWeight:uint;
+    public function StyleWeight(selectorWeights:Array) {
+       self.selectorWeights = selectorWeights;
+       for each (var selectorItemWeight : SelectorWeight in _selectorWeights) {
+          self.elementWeight += selectorItemWeight.hasElement ? selectorItemWeight.weight : 0;
+          self.classWeight += selectorItemWeight.hasClassId ? selectorItemWeight.weight : 0;
+          self.idWeight += selectorItemWeight.hasId ? selectorItemWeight.weight : 0;
+          self.stateWeight += selectorItemWeight.hasState ? selectorItemWeight.weight : 0;
+       }
+    }
 }

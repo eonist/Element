f7009574b60abc3cac73970077019d495cdfa7d9
Eonist <unicornhive@gmail.com>
2015-12-03 11:25:54 +0100

    Files modified: 1
    Modified 1 file:src/style/utils/StyleResolver.swift

--- src/style/utils/StyleResolver.swift
+++ src/style/utils/StyleResolver.swift
@@ -7,16 +7,16 @@ class StyleResolver{
      * // :TODO: should only inherit when property is marked inherit or from * universal selectors!?!?
      */
     class func style2(element:IElement)->IStyle{
-        var querrySelectors:Array<ISelector> = ElementParser.selectors(element);// :TODO: possibly move up in scope for optimizing
-        var weightedStyles:Array<IStyle> = [];
+        let querrySelectors:Array<ISelector> = ElementParser.selectors(element);// :TODO: possibly move up in scope for optimizing
+        let weightedStyles:Array<IStyle> = [];
         for style : IStyle in StyleManager.styles {/*This loop disregards styles that dont apply to the elements cascade*/
             if(style.selectors.count > querrySelectors.count) {continue;}/*if there are more selectors in style.selectors than in cascade the final styleWeight.weight is 0 and there for it is not included in the weightedStyles array*/
             //print("style: " + style.name);
             
             //continue here: add the selectorWeight class
             
-            var selectorWeights:Array<SelectorWeight> = SelectorParser.selectorWeights(style,querrySelectors);
-            //if(selectorWeights != null) weightedStyles.push(new WeightedStyle(style, new StyleWeight(selectorWeights)));
+            let selectorWeights:Array<SelectorWeight>? = SelectorParser.selectorWeights(style,querrySelectors);
+            if(selectorWeights != nil) {weightedStyles.push(WeightedStyle(style, StyleWeight(selectorWeights)))}
         }
         let finalStyle:IStyle = Style()
         return finalStyle

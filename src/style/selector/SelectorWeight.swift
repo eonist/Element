2c55992099d66448a3615bb0265ec5792b6d827e
Eonist <unicornhive@gmail.com>
2015-12-03 11:53:01 +0100

    Files modified: 1
    Modified 1 file:src/style/selector/SelectorWeight.swift

--- src/style/selector/SelectorWeight.swift
+++ src/style/selector/SelectorWeight.swift
@@ -3,13 +3,13 @@ import Foundation
  * @Note "Selectorparser","WeightdStyleAsserter" and "StyleWeight" all make use of this class
  */
 class SelectorWeight {
-    private var weight:UInt;
-    private var hasId:Bool;
-    private var hasElement:Bool;
-    private var hasClassId:Bool;
-    private var hasState:Bool;
-    private var numOfSimilarClassIds:UInt;
-    private var stateWeight:UInt;
+    var weight:UInt;
+    var hasId:Bool;
+    var hasElement:Bool;
+    var hasClassId:Bool;
+    var hasState:Bool;
+    var numOfSimilarClassIds:UInt;
+    var stateWeight:UInt;
     init(_ weight:UInt,_ hasId:Bool,_ hasElement:Bool,_ hasClassId:Bool,_ hasState:Bool,_ numOfSimilarClassIds:UInt,_ stateWeight:UInt) {// :TODO: shouldnt this be in this order: hasElement,hasClassId,classmatchcount,hasId,hasState has stateweight??
        self.weight = weight;
        self.hasId = hasId;

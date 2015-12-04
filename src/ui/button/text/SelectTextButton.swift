41dc80646411f9541db38f8e24b762257734d4a0
Eonist <unicornhive@gmail.com>
2015-12-04 06:33:16 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/text/SelectTextButton.swift

--- src/ui/button/text/SelectTextButton.swift
+++ src/ui/button/text/SelectTextButton.swift
@@ -2,11 +2,11 @@ import Cocoa
 
 class SelectTextButton:TextButton {
     var isSelected:Bool;
-    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
-    init(text : String = "defaultText", width : CGFloat, height : CGFloat, isSelected : Bool = false, parent : IElement? = nil, id : String? = nil){
+    init(_ text : String = "defaultText", _ width : CGFloat, _ height : CGFloat, _ isSelected : Bool = false, _ parent : IElement? = nil, _ id : String? = nil){
         self.isSelected = isSelected;
         super.init(text, width, height, parent, id);
     }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func mouseUpInside(theEvent: NSEvent) {
         isSelected = true
         super.mouseUpInside(theEvent)

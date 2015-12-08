48196c7ed6aa50826531694729fa8ee945e30681
Eonist <unicornhive@gmail.com>
2015-12-08 18:59:55 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/GraphicSkinParser.swift

--- src/skin/skin/utils/GraphicSkinParser.swift
+++ src/skin/skin/utils/GraphicSkinParser.swift
@@ -18,6 +18,7 @@ class GraphicSkinParser{
         if(StylePropertyAsserter.hasFillet(skin)) { graphic = Utils.fillet(graphic, StylePropertyParser.fillet(skin)) }
         if(StylePropertyAsserter.hasGradient(skin)) { graphic = Utils.gradient(graphic) }
         if(StylePropertyAsserter.hasGradient(skin)) { graphic = Utils.gradient(graphic) }
+        if(StylePropertyAsserter.hasDropShadow(skin)) { graphic = Utils.dropShadow(graphic, StylePropertyParser.dropShadow(skin)) }
         return graphic
     }
 }

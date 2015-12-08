63333733829f1e97c3c296e3954ae4205207a7bd
Eonist <unicornhive@gmail.com>
2015-12-08 19:20:28 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/DropShadowDecorator.swift

--- src/skin/graphic/decorator/DropShadowDecorator.swift
+++ src/skin/graphic/decorator/DropShadowDecorator.swift
@@ -1,6 +1,8 @@
 import Foundation
-
-class DropShadowDecorator:GraphicDecoratable{
+/**
+ * NOTE: It isnt ideal that you have to extend PositionalDecorator instead of simply GraphicDecorable, but in the spirit of moving on we keep it as is
+ */
+class DropShadowDecorator:PositionalDecorator{
     var dropShadow:DropShadow
     init(_ decoratable: IGraphicDecoratable,_ dropShadow:DropShadow) {
         self.dropShadow = dropShadow

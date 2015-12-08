3b89e1552fc6ae1370ed31e1d4e9af19a1dac4de
Eonist <unicornhive@gmail.com>
2015-12-08 19:03:56 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/DropShadowDecorator.swift

--- src/skin/graphic/decorator/DropShadowDecorator.swift
+++ src/skin/graphic/decorator/DropShadowDecorator.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 class DropShadowDecorator:GraphicDecoratable{
-    
+    var dropShadow:DropShadow
+    init(_ decoratable: IGraphicDecoratable,_ dropShadow:DropShadow) {
+        self.dropShadow = dropShadow
+        super.init(decoratable)
+    }
 }

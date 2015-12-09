40bb151fad202cfa3496045b381f1d5592eb6d63
Eonist <unicornhive@gmail.com>
2015-12-09 16:09:41 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/utils/DecoratorAsserter.swift

--- src/skin/graphic/decorator/utils/DecoratorAsserter.swift
+++ src/skin/graphic/decorator/utils/DecoratorAsserter.swift
@@ -4,7 +4,7 @@ class DecoratorAsserter {
     /**
      * Asserts if @param decoratable has a Decorator instance of "Class type" @param classType
      */
-    class func hasDecoratable(decoratable:GraphicDecoratable,classType:AnyClass)-> Bool {
+    class func hasDecoratable(decoratable:IGraphicDecoratable,_ classType:AnyClass)-> Bool {
         return DecoratorParser.decoratable(decoratable, classType) != nil;
     }
 }

a9ffaa0a7a636480d34ef87eb94c973fc80082e7
Eonist <unicornhive@gmail.com>
2015-12-09 13:55:39 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/utils/DecoratorParser.swift

--- src/skin/graphic/decorator/utils/DecoratorParser.swift
+++ src/skin/graphic/decorator/utils/DecoratorParser.swift
@@ -1,5 +1,23 @@
 import Foundation
 
 class DecoratorParser {
-
+    /**
+     * Returns a Decorator instance from @param decoratable by Class type @param classType if it exists, if it doesnt it returns null
+     */
+    class func decoratable(decoratable:IGraphicDecoratable,classType:Any)->IGraphicDecoratable {
+        
+        //continue here, assert a decoratable with you class assertion code
+        
+        if(decoratable is classType) {
+            return decoratable;
+        }
+        var current:IGraphicDecoratable = decoratable;
+        while(current.decoratable != current) {
+            if(current.decoratable is classType) {
+                return current.decoratable;
+            }
+            current = current.decoratable;
+        }
+        return nil;
+    }
 }

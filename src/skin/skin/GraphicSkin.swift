9c61d814f4722177598ccd679104ceeb56bbfb9d
Eonist <unicornhive@gmail.com>
2015-12-15 20:14:00 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -5,12 +5,19 @@ class GraphicSkin:Skin{
         super.init(style, state, element)
         decoratable = GraphicSkinParser.configure(self)/*this call is here because CGContext is only accessible after drawRect is called*/
         /*decoratable = */SkinModifier.align(self,decoratable as! IPositional);/*the argument now becomes a reference to the orgiginal instance, but it also becomes immutable unfortunatly,not to worry, the implicit settermethod isnt defined by swift as mutable, even though it is. I guess indirectly, so the values are mutated on the orginal instance and all is well*/        
+        
         let layerA = CALayer()
         layerA.frame = CGRectMake(20, 20, 100, 100);
         layerA.masksToBounds = false//finally it works
         //layerA.position = CGPointMake(10, 10);
         layerA.backgroundColor = NSColor.greenColor().CGColor
         layer!.addSublayer(layerA)
+        
+        let layerD = CustomLayer(NSColor.blueColor())
+        layerD.frame = CGRect(120,120,50,50);
+        //layerD.display()
+        //layerD.masksToBounds = false
+        layer!.addSublayer(layerD)
     }
     override func layout() {
         Swift.print("GraphicSkin.layout()")

57933773897aecce89f09b0f05d9759eb190ec6e
Eonist <unicornhive@gmail.com>
2015-12-16 07:22:34 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file: src/skin/graphic/decorator/concrete/BaseGraphic2.swiftModified 1 file:src/skin/IBaseGraphic.swift

--- src/skin/IBaseGraphic.swift
+++ src/skin/IBaseGraphic.swift
@@ -6,11 +6,13 @@ protocol IBaseGraphic{
     var width:CGFloat{get}
     var height:CGFloat{get}
     */
+    /*
     var lineStyle:ILineStyle?{get}
     var fillStyle:IFillStyle?{get}
     func setProperties(fillStyle:IFillStyle?, lineStyle:ILineStyle?)
     var lineOffsetType:OffsetType{get set}
     var path:CGMutablePath{get set}
     var linePath:CGMutablePath{get set}
+    */
 
 }

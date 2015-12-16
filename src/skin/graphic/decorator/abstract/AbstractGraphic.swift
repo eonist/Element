41e274cf40317f43d676c91fb354daec9ed1bf09
Eonist <unicornhive@gmail.com>
2015-12-16 08:56:51 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/abstract/AbstractGraphic.swift

--- src/skin/graphic/decorator/abstract/AbstractGraphic.swift
+++ src/skin/graphic/decorator/abstract/AbstractGraphic.swift
@@ -1,5 +1,19 @@
 import Foundation
 
-class AbstractGraphic {
-
+class AbstractGraphic:Graphic {
+    let errMsg:String = "Must be overridden in subClass"
+    func getDecoratable()->IGraphicDecoratable{fatalError(errMsg)}/*news*/
+    var graphic:BaseGraphic {fatalError(errMsg)}/*This is only a getter, for now, and dont revert to IBaseGraphic*/
+    func getGraphic() -> BaseGraphic{fatalError(errMsg)}
+    func initialize(){}
+    /*Fill*/
+    func fill(){}
+    func beginFill(){fatalError(errMsg)}
+    func drawFill(){fatalError(errMsg)}
+    func stylizeFill(){fatalError(errMsg)}
+    /*Line*/
+    func line(){}
+    func applyLineStyle(){fatalError(errMsg)}
+    func drawLine(){fatalError(errMsg)}
+    func stylizeLine(){fatalError(errMsg)}
 }

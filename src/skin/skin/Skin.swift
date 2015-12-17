26d3d6f1cb3a880dc14ebd3774af3e53585e8152
Eonist <unicornhive@gmail.com>
2015-12-17 16:24:54 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/Skin.swift

--- src/skin/skin/Skin.swift
+++ src/skin/skin/Skin.swift
@@ -29,6 +29,7 @@ class Skin:FlippedView,ISkin{
         //Swift.print("element!.width: " + "\(element!.width)")
         super.init(frame: NSRect(x: 0, y: 0, width: element!.width+2.0, height: element!.height+2.0))/*this used to be a generic size, but since wants deault clipping doesnt work anymore we have to set this size to something as big as the skin needs to be*/
         self.wantsLayer = true/*if true then view is layer backed*/
+        
     }
     /**
      * Required by super class

87c0a6b3535367e8f29c3e74af58241304ffeb1d
Eonist <unicornhive@gmail.com>
2015-12-19 12:59:41 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/RectGraphicUtils2.swift

--- src/skin/graphic/utils/RectGraphicUtils2.swift
+++ src/skin/graphic/utils/RectGraphicUtils2.swift
@@ -10,20 +10,21 @@ class RectGraphicUtils2 {
         var lineFrameRect:CGRect = rect.copy()
         var lineRect:CGRect
         var fillOffsetRect:CGRect
+        //Expand by 2x the thickness of the border
+        
+        if(offsetType.left == OffsetType.outside){
+            lineFrameRect = rect.expand(lineStyle.thickness, lineStyle.thickness)
+        }
+        if(offsetType.right == OffsetType.outside){
+            lineFrameRect = rect.expand(lineStyle.thickness, lineStyle.thickness)
+        }
+        //offset by half of thickness in x & y, then you expand it 1x the thickness
+        lineRect = rect.offset(lineStyle.thickness / 2, lineStyle.thickness / 2).expand(lineStyle.thickness, lineStyle.thickness)
+        //offset the fillRect with the thickness of the border in x & y dir
+        fillOffsetRect = rect.offset(lineStyle.thickness, lineStyle.thickness)
+
         if(offsetType == OffsetType(OffsetType.outside)){//outside
-            //Expand by 2x the thickness of the border
-            
-            if(offsetType.left == OffsetType.outside){
-                lineFrameRect = rect.expand(lineStyle.thickness, lineStyle.thickness)
-            }
-            if(offsetType.right == OffsetType.outside){
-                lineFrameRect = rect.expand(lineStyle.thickness, lineStyle.thickness)
-            }
-            //offset by half of thickness in x & y, then you expand it 1x the thickness
-            lineRect = rect.offset(lineStyle.thickness / 2, lineStyle.thickness / 2).expand(lineStyle.thickness, lineStyle.thickness)
-            //offset the fillRect with the thickness of the border in x & y dir
-            fillOffsetRect = rect.offset(lineStyle.thickness, lineStyle.thickness)
-        }else{//inside
+        }else if(offsetType == OffsetType(OffsetType.inside)){//inside
             //leave it as is
             lineFrameRect = rect.copy()
             //uniformally outset the lineFrameRect by half the border thickness

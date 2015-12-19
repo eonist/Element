445477ad4ddd16da84ee3d8a434248c82470dd75
Eonist <unicornhive@gmail.com>
2015-12-19 12:38:06 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/RectGraphicUtils2.swift

--- src/skin/graphic/utils/RectGraphicUtils2.swift
+++ src/skin/graphic/utils/RectGraphicUtils2.swift
@@ -22,12 +22,30 @@ class RectGraphicUtils2 {
         //Swift.print("lineRect: after" + "\(convertedLineRect)")
         return (convertedLineRect,frameRect)
     }
+    
+}
+class Utils{
+    /**
+     * NOTE: only supports topLeft and bottomRight
+     * TODO: This code isnt Optimized, to optimize see the old code. (Requires individual side calculation and also some sides use the same math so some sides can be squasehd etc. Also reuse similar math etc) you can optimize by storing halfsizes etc
+     */
+    class func corner(rect:CGRect,_ lineStyle:ILineStyle,_ offsetType:OffsetType,_ cornerType:String)->(line:CGPoint,frame:CGPoint,fill:CGPoint){
+        if(cornerType == Alignment.topLeft){
+            let topOffsetRect = offsetRect(rect, lineStyle, OffsetType(offsetType.top))
+            let leftOffsetRect = offsetRect(rect, lineStyle, OffsetType(offsetType.left))
+            return (CGPoint(leftOffsetRect.lineRect.x,topOffsetRect.lineRect.y), CGPoint(leftOffsetRect.frameRect.x,topOffsetRect.frameRect.y),CGPoint(leftOffsetRect.fillRect.x,topOffsetRect.fillRect.y))
+        }else{/*bottomRight*/
+            let bottomOffsetRect = offsetRect(rect, lineStyle, OffsetType(offsetType.bottom))
+            let rightOffsetRect = offsetRect(rect, lineStyle, OffsetType(offsetType.right))
+            return (CGPoint(rightOffsetRect.lineRect.bottomRight.x,bottomOffsetRect.lineRect.bottomRight.y), CGPoint(rightOffsetRect.frameRect.bottomRight.x,bottomOffsetRect.frameRect.bottomRight.y),CGPoint(rightOffsetRect.fillRect.bottomRight.x,bottomOffsetRect.fillRect.bottomRight.y))
+        }
+    }
     /**
      * New
      * NOTE: you actually need two CGRects returned, one is for the stroke rect, and one is for framing the stroke rect
      * NOTE: you also need to return a CGRect for the frame of the skin and the frame of the Element aswell. (same frame)
      */
-    class func offsetRect2(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->(frame:CGRect,line:CGRect,fill:CGRect){
+    class func offsetRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->(frameRect:CGRect,lineRect:CGRect,fill:CGRect){
         var lineFrameRect:CGRect
         var lineRect:CGRect
         var fillOffsetRect:CGRect
@@ -49,4 +67,24 @@ class RectGraphicUtils2 {
         return (lineFrameRect,lineRect, fillOffsetRect)
     }
 }
-class 
+private class Converter{
+    /**
+     *
+     */
+    class func pointToSpace(p:CGPoint, _ from:CGRect, _ to:CGRect)->CGPoint{
+        let difference = PointParser.difference(to.topLeft,from.topLeft)
+        //Swift.print("difference: " + "\(difference)")
+        return p + difference
+    }
+    
+    /**
+     * Converts topLeft corner and topRight corner to a CGRect instance
+     */
+    class func convert(tl:CGPoint,_ br:CGPoint)->CGRect{
+        let x:CGFloat = tl.x;
+        let y:CGFloat = tl.y;
+        let width:CGFloat = br.x - tl.x;
+        let height:CGFloat = br.y - tl.y;
+        return CGRect(x, y, width, height);
+    }
+}

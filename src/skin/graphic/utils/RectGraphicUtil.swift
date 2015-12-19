d71513c5fcc26243703fff0031c5e3e25a4ac5af
Eonist <unicornhive@gmail.com>
2015-12-19 12:14:56 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/RectGraphicUtil.swift

--- src/skin/graphic/utils/RectGraphicUtil.swift
+++ src/skin/graphic/utils/RectGraphicUtil.swift
@@ -6,35 +6,26 @@ class RectGraphicUtil {
      * NOTE: you actually need two CGRects returned, one is for the stroke rect, and one is for framing the stroke rect
      * NOTE: you also need to return a CGRect for the frame of the skin and the frame of the Element aswell. (same frame)
      */
-    class func lineOffsetRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->(frame:CGRect,line:CGRect){
+    class func offsetRect2(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->(frame:CGRect,line:CGRect,fill:CGRect){
         var lineFrameRect:CGRect
         var lineRect:CGRect
+        var fillOffsetRect:CGRect
         if(offsetType == OffsetType(OffsetType.outside)){//outside
             //Expand by 2x the thickness of the border
             lineFrameRect = rect.expand(lineStyle.thickness * 2, lineStyle.thickness * 2)
             //offset by half of thickness in x & y, then you expand it 1x the thickness
             lineRect = rect.offset(lineStyle.thickness / 2, lineStyle.thickness / 2).expand(lineStyle.thickness, lineStyle.thickness)
+            //offset the fillRect with the thickness of the border in x & y dir
+            fillOffsetRect = rect.offset(lineStyle.thickness, lineStyle.thickness)
         }else{//inside
             //leave it as is
             lineFrameRect = rect.copy()
             //uniformally outset the lineFrameRect by half the border thickness
             lineRect = rect.outset(lineStyle.thickness / 2, lineStyle.thickness / 2)
-        }
-        return (lineFrameRect,lineRect)
-    }
-    /**
-     * New
-     */
-    class func fillOffsetRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->CGRect{
-        var fillOffsetRect:CGRect
-        if(offsetType == OffsetType(OffsetType.outside)){//outside
-            //offset the fillRect with the thickness of the border in x & y dir
-            fillOffsetRect = rect.offset(lineStyle.thickness, lineStyle.thickness)
-        }else{//inside
             //dont do anything
             fillOffsetRect = rect.copy()
         }
-        return fillOffsetRect
+        return (lineFrameRect,lineRect, fillOffsetRect)
     }
     /**
      * Returns a Tuple with "frame and line rects" by offsetting @param rect with @param lineOffset

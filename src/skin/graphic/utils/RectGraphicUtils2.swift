02ac489a6dcaed289553cc0e8649e2765507a1a1
Eonist <unicornhive@gmail.com>
2015-12-19 12:34:14 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/RectGraphicUtils2.swift

--- src/skin/graphic/utils/RectGraphicUtils2.swift
+++ src/skin/graphic/utils/RectGraphicUtils2.swift
@@ -1,6 +1,27 @@
 import Foundation
 
 class RectGraphicUtils2 {
+    class func offsetRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->(lineRect:CGRect, frameRect:CGRect) {
+        let topLeft = Utils.corner(rect, lineStyle,offsetType,Alignment.topLeft);//cornerPoint(rect, Alignment.TOP_LEFT, offsetType.left, offsetType.top, lineStyle);
+        //print("topLeft.frame: " + String(topLeft.frame));
+        //print("topLeft.line: " + String(topLeft.line));
+        let bottomRight = Utils.corner(rect, lineStyle,offsetType,Alignment.bottomRight);//cornerPoint(rect, Alignment.BOTTOM_RIGHT, offsetType.right, offsetType.bottom, lineStyle);
+        //print("bottomRight.frame: " + String(bottomRight.frame));
+        //print("bottomRight.line: " + String(bottomRight.line));
+        let frameRect:CGRect = Converter.convert(topLeft.frame,bottomRight.frame)
+        let lineRect:CGRect = Converter.convert(topLeft.line,bottomRight.line)
+        let convertedTopLeftPoint = Converter.pointToSpace(topLeft.line,lineRect,frameRect)
+        //Swift.print("convertedTopLeftPoint: " + "\(convertedTopLeftPoint)")
+        /*convert the point to the correct point space*/
+        let convertedBottomRightPoint = Converter.pointToSpace(bottomRight.line,lineRect,frameRect)
+        //Swift.print("convertedBottomRightPoint: " + "\(convertedBottomRightPoint)")
+        var convertedLineRect:CGRect = Converter.convert(convertedTopLeftPoint,convertedBottomRightPoint)
+        //Swift.print("lineRect: before" + "\(convertedLineRect)")
+        /*convert to 0,0 pointspace*/
+        convertedLineRect -= topLeft.line
+        //Swift.print("lineRect: after" + "\(convertedLineRect)")
+        return (convertedLineRect,frameRect)
+    }
     /**
      * New
      * NOTE: you actually need two CGRects returned, one is for the stroke rect, and one is for framing the stroke rect
@@ -28,3 +49,4 @@ class RectGraphicUtils2 {
         return (lineFrameRect,lineRect, fillOffsetRect)
     }
 }
+class 

41860c39a76b992642ee83398b35e4327851bd43
Eonist <unicornhive@gmail.com>
2015-12-19 14:09:22 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/RectGraphicUtils2.swift

--- src/skin/graphic/utils/RectGraphicUtils2.swift
+++ src/skin/graphic/utils/RectGraphicUtils2.swift
@@ -7,15 +7,15 @@ class RectGraphicUtils2 {
      * NOTE: you also need to return a CGRect for the frame of the skin and the frame of the Element aswell. (same frame)
      */
     class func offsetRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->(lineFrameRect:CGRect,lineRect:CGRect,fillRect:CGRect){
-        var lineFrameRect:CGRect = CGRect()
-        var lineRect:CGRect = rect
-        var fillRect:CGRect = rect
+        var lineFrameRect:CGRect = rect.copy()
+        var lineRect:CGRect = rect.copy()
+        var fillRect:CGRect = rect.copy()
         
-        
+        Swift.print("lineRect: " + "\(lineRect)")
         /*Left*/
         if(offsetType.left == OffsetType.outside){
             //lineFrameRect = rect.expand(lineStyle.thickness, lineStyle.thickness)
-            lineRect = lineRect.offset(lineStyle.thickness / 2, lineStyle.thickness / 2).expand(-lineStyle.thickness/2, 0)
+            lineRect = lineRect.offset(lineStyle.thickness / 2, 0).expand(-lineStyle.thickness/2, 0)
             fillRect = fillRect.offset(lineStyle.thickness, 0)
         }else { //inside
             lineRect = lineRect.offset(-lineStyle.thickness/2, 0).expand(lineStyle.thickness/2 , 0)
@@ -29,9 +29,7 @@ class RectGraphicUtils2 {
         }
         /*Top*/
         if(offsetType.top == OffsetType.outside){
-            Swift.print("lineRect: " + "\(lineRect)")
-            lineRect = lineRect.offset(0, lineStyle.thickness/2).expand(0,-lineStyle.thickness/2)
-            
+            lineRect = lineRect.offset(0, lineStyle.thickness/2)//.expand(0,-lineStyle.thickness/2)
             fillRect = fillRect.offset(0,lineStyle.thickness)
             
         }else{//inside

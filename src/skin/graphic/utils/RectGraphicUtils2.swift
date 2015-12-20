e41eb82f44b63224509e518f95c4e4d73c4c90c8
Eonist <unicornhive@gmail.com>
2015-12-20 07:36:20 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/RectGraphicUtils2.swift

--- src/skin/graphic/utils/RectGraphicUtils2.swift
+++ src/skin/graphic/utils/RectGraphicUtils2.swift
@@ -1,12 +1,21 @@
 import Foundation
 
 class RectGraphicUtils2 {
-    
     /**
      *
      */
     class func fillOffsetRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->CGRect{
-        
+        var fillFrameRect:CGRect = rect.copy()
+        //Swift.print("lineRect: " + "\(lineRect)")
+        /*Left*/
+        if(offsetType.left == OffsetType.outside){
+            fillFrameRect = fillFrameRect.offset(lineStyle.thickness, 0)
+        }
+        /*Top*/
+        if(offsetType.top == OffsetType.outside){
+            fillFrameRect = fillFrameRect.offset(0,lineStyle.thickness)
+        }
+        return fillFrameRect
     }
     /**
      *
@@ -14,14 +23,11 @@ class RectGraphicUtils2 {
     class func lineOffsetRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->(lineFrameRect:CGRect,lineRect:CGRect){
         var lineFrameRect:CGRect = rect.copy()
         var lineRect:CGRect = CGRect(0,0,rect.width,rect.height)
-        var fillFrameRect:CGRect = rect.copy()
-        
         //Swift.print("lineRect: " + "\(lineRect)")
         /*Left*/
         if(offsetType.left == OffsetType.outside){
             lineFrameRect = lineFrameRect.expand(lineStyle.thickness, 0)
             lineRect = lineRect.offset(lineStyle.thickness / 2, 0)//.expand(-lineStyle.thickness/2, 0)
-            fillFrameRect = fillFrameRect.offset(lineStyle.thickness, 0)
         }else { //inside
             lineRect = lineRect.offset(-lineStyle.thickness/2, 0)//.expand(lineStyle.thickness/2 , 0)
         }
@@ -36,8 +42,6 @@ class RectGraphicUtils2 {
         if(offsetType.top == OffsetType.outside){
             lineFrameRect = lineFrameRect.expand(0,lineStyle.thickness)
             lineRect = lineRect.offset(0, lineStyle.thickness/2)//.expand(0,-lineStyle.thickness/2)
-            fillFrameRect = fillFrameRect.offset(0,lineStyle.thickness)
-            
         }else{//inside
             lineRect = lineRect.offset(0, -lineStyle.thickness/2)//.expand(0,+lineStyle.thickness/2)
         }
@@ -48,6 +52,6 @@ class RectGraphicUtils2 {
         }else{//inside
             lineRect = lineRect.expand(0,lineStyle.thickness)
         }
-        return (lineFrameRect,lineRect, fillFrameRect)
+        return (lineFrameRect,lineRect)
     }
 }

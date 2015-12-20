6e5074bedecd71eb1589b6852072c8c74577db1d
Eonist <unicornhive@gmail.com>
2015-12-20 15:16:46 +0100

    Files modified: 1
    Modified 1 file:src/css/utils/CSSPropertyParser.swift

--- src/css/utils/CSSPropertyParser.swift
+++ src/css/utils/CSSPropertyParser.swift
@@ -36,8 +36,8 @@ class CSSPropertyParser {
         var properties:Array<String> = StringModifier.split(propertyString, ",")
         let rotation:CGFloat = Utils.rotation(ArrayModifier.shift(&properties));/*the first item is always the rotation, top or left or top left etc*/
         var gradient:IGradient = Utils.gradient(properties);/*add colors, opacities and ratios*/
-        gradient.rotation = Trig.normalize2(rotation * ㎭);// :TODO: rotations should be applied in the matrix
-        Swift.print("gradient.rotation: " + "\(gradient.rotation)")
+        gradient.rotation = Trig.normalize2(rotation * ㎭);/*should pin the angle between -π and +π*/// :TODO: rotations should be applied in the matrix
+        Swift.print("CSSPropertyParser.linearGradient.rotation: " + "\(gradient.rotation)")
         return gradient;
     }
     /**

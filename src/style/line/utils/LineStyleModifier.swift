d2c186e655bb48b868056912265d4cd8bb51fe18
Eonist <unicornhive@gmail.com>
2015-12-20 15:18:42 +0100

    Files modified: 1
    Modified 1 file:src/style/line/utils/LineStyleModifier.swift

--- src/style/line/utils/LineStyleModifier.swift
+++ src/style/line/utils/LineStyleModifier.swift
@@ -7,7 +7,7 @@ class LineStyleModifier {
     class func lineGradientStyle(graphics:Graphics,_ gradient:IGradient) {
         let nsFillGradientColorA:NSColor = NSColor.magentaColor()
         let nsFillGradientColorB:NSColor = NSColor.cyanColor()
-        let gradient:Gradient = Gradient([nsFillGradientColorA.CGColor,nsFillGradientColorB.CGColor])
+        let gradient:Gradient = Gradient(gradient.colors)//[nsFillGradientColorA.CGColor,nsFillGradientColorB.CGColor]
         graphics.gradientLine(gradient)
         //graphics.lineGradientStyle(gradient.type, gradient.colors, gradient.alphas, gradient.ratios, gradient.matrix, gradient.spreadMethod, gradient.interpolationMethod, gradient.focalPointRatio);
     }

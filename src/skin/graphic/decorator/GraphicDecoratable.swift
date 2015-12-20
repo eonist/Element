b474d5e123086c0187ec2cb7fe2bbdf106b069c3
Eonist <unicornhive@gmail.com>
2015-12-20 10:54:24 +0100

    Files modified: 2
    Modified 2 files:src/skin/graphic/decorator/GraphicDecoratable.swiftsrc/skin/skin/GraphicSkin.swift

--- src/skin/graphic/decorator/GraphicDecoratable.swift
+++ src/skin/graphic/decorator/GraphicDecoratable.swift
@@ -1,4 +1,4 @@
-import Cocoa
+import Foundation
 /**
  * Manifesto:
  * 1. This class should only provide access to the graphic instance
@@ -11,9 +11,7 @@ class GraphicDecoratable:AbstractGraphicDecoratable {
     override var graphic:BaseGraphic {return decoratable.graphic}
     init(_ decoratable:IGraphicDecoratable){
         self.decoratable = decoratable
-        super.init()
-        graphic.fillShape.delegate = self
-        graphic.lineShape.delegate = self
+        
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func initialize(){
@@ -23,36 +21,6 @@ class GraphicDecoratable:AbstractGraphicDecoratable {
     override func draw() {//new
         if(getGraphic().fillStyle != nil){drawFill()}/*setup the fill geometry*/
         if(getGraphic().lineStyle != nil){drawLine()}/*setup the line geometry*/
-    }
-    
-    /**
-     * This is a delegate handler method
-     * TODO: use the other delegate method that doesnt pass in the context, for simpler code!?!
-     */
-    func drawLayer(layer: CALayer, inContext ctx: CGContext) {
-        Swift.print("GraphicSkin.drawLayer(layer,inContext)")
-        if(layer === decoratable.graphic.fillShape){
-            Swift.print("fillShape")
-            decoratable.graphic.fillShape.graphics.context = ctx
-            if(decoratable.getGraphic().fillStyle != nil){decoratable.fill()}
-            
-            //TODO:you only need to call the draw method from here, the fill setting etc can be done in the decoratable classes
-            
-            //decoratable.graphic.fillShape.graphics.fill(decoratable.graphic.fillStyle!.color)//Stylize the fill
-            //Swift.print("inside drawInContext")
-            //decoratable.graphic.fillShape.graphics.draw(decoratable.graphic.fillShape.path)//draw everything
-            
-        }else if(layer === decoratable.graphic.lineShape){
-            Swift.print("lineShape")
-            decoratable.graphic.lineShape.graphics.context = ctx
-            if(decoratable.getGraphic().lineStyle != nil){decoratable.line()}
-            
-            //TODO:you only need to call the draw method from here, the line setting etc can be done in the decoratable classes
-            
-            //decoratable.graphic.lineShape.graphics.line(decoratable.graphic.lineStyle!.thickness,decoratable.graphic.lineStyle!.color/*,lineStyle!.lineCap, lineStyle!.lineJoin, lineStyle!.miterLimit*/)//Stylize the line
-            //decoratable.graphic.lineShape.graphics.draw(decoratable.graphic.lineShape.path)//draw everything
-            
-        }
     }
     override func fill(){
         //Swift.print("GraphicDecoratable.fill()")
--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -17,7 +17,8 @@ class GraphicSkin:Skin{
         decoratable.graphic.fillShape.display()/*draw the fileShape*/
         decoratable.graphic.lineShape.display()/*draw the fileShape*/
         
-        
+        decoratable.graphic.fillShape.delegate = self
+        decoratable.graphic.lineShape.delegate = self
         
         //continue here: one draw call in graphic decoratable and two initializeFill and initializeLine calls
         
@@ -40,6 +41,35 @@ class GraphicSkin:Skin{
         
     }
     
+    /**
+     * This is a delegate handler method
+     * TODO: use the other delegate method that doesnt pass in the context, for simpler code!?!
+     */
+    override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
+        Swift.print("GraphicSkin.drawLayer(layer,inContext)")
+        if(layer === decoratable.graphic.fillShape){
+            Swift.print("fillShape")
+            decoratable.graphic.fillShape.graphics.context = ctx
+            if(decoratable.getGraphic().fillStyle != nil){decoratable.fill()}
+            
+            //TODO:you only need to call the draw method from here, the fill setting etc can be done in the decoratable classes
+            
+            //decoratable.graphic.fillShape.graphics.fill(decoratable.graphic.fillStyle!.color)//Stylize the fill
+            //Swift.print("inside drawInContext")
+            //decoratable.graphic.fillShape.graphics.draw(decoratable.graphic.fillShape.path)//draw everything
+            
+        }else if(layer === decoratable.graphic.lineShape){
+            Swift.print("lineShape")
+            decoratable.graphic.lineShape.graphics.context = ctx
+            if(decoratable.getGraphic().lineStyle != nil){decoratable.line()}
+            
+            //TODO:you only need to call the draw method from here, the line setting etc can be done in the decoratable classes
+            
+            //decoratable.graphic.lineShape.graphics.line(decoratable.graphic.lineStyle!.thickness,decoratable.graphic.lineStyle!.color/*,lineStyle!.lineCap, lineStyle!.lineJoin, lineStyle!.miterLimit*/)//Stylize the line
+            //decoratable.graphic.lineShape.graphics.draw(decoratable.graphic.lineShape.path)//draw everything
+            
+        }
+    }
     /*
     override func layout() {
     //Swift.print("GraphicSkin.layout()")

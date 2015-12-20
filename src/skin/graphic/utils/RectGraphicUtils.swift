ee1be0de20aca0096fbc916ca6e58e7f0b6e741f
Eonist <unicornhive@gmail.com>
2015-12-20 16:34:05 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/RectGraphicUtils.swift

--- src/skin/graphic/utils/RectGraphicUtils.swift
+++ src/skin/graphic/utils/RectGraphicUtils.swift
@@ -4,7 +4,7 @@ class RectGraphicUtils2 {
     /**
      *
      */
-    class func fillOffsetRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->CGRect{
+    class func fillFrameRect(rect:CGRect, _ lineStyle:ILineStyle, _ offsetType:OffsetType)->CGRect{
         var fillFrameRect:CGRect = rect.copy()
         //Swift.print("lineRect: " + "\(lineRect)")
         /*Left*/

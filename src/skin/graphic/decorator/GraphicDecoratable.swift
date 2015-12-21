0ef7de26203a889b4462eddada1f98a6e5a1a9e9
Eonist <unicornhive@gmail.com>
2015-12-21 06:50:24 +0100

    Files modified: 2
    Modified 2 files:src/skin/graphic/decorator/GraphicDecoratable.swiftsrc/skin/skin/utils/GraphicSkinParser.swift

--- src/skin/graphic/decorator/GraphicDecoratable.swift
+++ src/skin/graphic/decorator/GraphicDecoratable.swift
@@ -30,11 +30,11 @@ class GraphicDecoratable:AbstractGraphicDecoratable {
     override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
         Swift.print("GraphicDecoratable.drawLayer(layer,inContext)")
         if(layer === graphic.fillShape){
-            //Swift.print("fillShape: ")
+            Swift.print("fillShape: ")
             graphic.fillShape.graphics.context = ctx
             if(getGraphic().fillStyle != nil){fill()}
         }else if(layer === graphic.lineShape){
-            //Swift.print("lineShape")
+            Swift.print("lineShape")
             graphic.lineShape.graphics.context = ctx
             if(getGraphic().lineStyle != nil){line()}
         }

4e4e8fc19c1047e0d875c08fcedb472e3f2b184d
Eonist <unicornhive@gmail.com>
2015-12-22 09:53:59 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -7,36 +7,12 @@ import Cocoa
  */
 
 class GraphicSkin:Skin{
-    var rectGraphic:RectGraphic?
     override init(_ style:IStyle? = nil, _ state:String = "", _ element:IElement? = nil){
         super.init(style, state, element)
         decoratable = GraphicSkinParser.configure(self)/*this call is here because CGContext is only accessible after drawRect is called*/
         addSubview(decoratable.graphic)
         /*decoratable = */SkinModifier.align(self,decoratable as! IPositional);/*the argument now becomes a reference to the orgiginal instance, but it also becomes immutable unfortunatly,not to worry, the implicit settermethod isnt defined by swift as mutable, even though it is. I guess indirectly, so the values are mutated on the orginal instance and all is well*/
         decoratable.draw()/*Setup the geometry and init the display process of fill and line*/
-        
-        
-        
-        let fillStyle = FillStyle(NSColor.yellowColor().alpha(0.5))
-        //let gradient = Gradient([NSColor.blueColor().CGColor,NSColor.redColor().CGColor],[],GradientType.Radial,1.5707963267949,CGPoint(0,0),CGPoint(0,0),CGSize(0,0),CGSize(1,1))
-        //gradient.gradientType = GradientType.Axial
-        //try to make a Linear gradient and see if that gets clipped
-        
-        //fillStyle = GradientFillStyle(gradient,NSColor.clearColor());
-        
-        //let lineStyle = LineStyle(20,NSColor.blueColor().alpha(0.5))
-        //a = Graphic(fillStyle,nil)
-        
-        //test how easy it is to create a rectGraphic etc
-        let baseGraphic:BaseGraphic = BaseGraphic(fillStyle,nil,OffsetType(OffsetType.outside))
-        rectGraphic = RectGraphic(200,200,baseGraphic)
-        //baseGraphic.lineShape.delegate = self
-        baseGraphic.fillShape.delegate = rectGraphic
-        //baseGraphic.layer!.delegate = self
-        
-        addSubview(baseGraphic)
-        rectGraphic!.draw()
-        
     }
     override func draw(){
         //Swift.print("GraphicSkin.draw()")

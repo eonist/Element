9236db97f7ffe669e8392cbf89c02c2f6e08a9eb
Eonist <unicornhive@gmail.com>
2015-12-22 09:08:53 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -12,14 +12,6 @@ class GraphicSkin:Skin{
         addSubview(decoratable.graphic)
         /*decoratable = */SkinModifier.align(self,decoratable as! IPositional);/*the argument now becomes a reference to the orgiginal instance, but it also becomes immutable unfortunatly,not to worry, the implicit settermethod isnt defined by swift as mutable, even though it is. I guess indirectly, so the values are mutated on the orginal instance and all is well*/
         decoratable.draw()/*Setup the geometry and init the display process of fill and line*/
-    
-    
-        
-        //continue here, try to update the layer on button interaction, maybe make a simple example and work from there
-        //try to call the update twice. or from aother place.
-        //or see history, it seemd to work before
-        //just make the example
-        
     }
     override func draw(){
         //Swift.print("GraphicSkin.draw()")

b05d2fbf97aabcedc3924dc7aff028e96b601331
Eonist <unicornhive@gmail.com>
2015-12-22 09:48:09 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -5,6 +5,43 @@ import Cocoa
  * TODO: See if you cant add drawLayer in the LineShape class after all. doesnt delegate work then?
  * TODO: Graphic is currently an NSVIew, it doesnt have to be. it can be a CALAyer that you attach to skin, SKin it self could be a CALayer, then Text skin would need its own subclass that extends NSView, but they could have a common protocol. 
  */
+
+class TempGraphix:FlippedView{
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        self.wantsLayer = true/*if true then view is layer backed*/
+        layer = CALayer()/*needs to be layer-hosted so that we dont get clipping of children*/
+        layer!.masksToBounds = false//this is needed!!!
+        
+        
+        let fillStyle = FillStyle(NSColor.yellowColor().alpha(0.5))
+        //let gradient = Gradient([NSColor.blueColor().CGColor,NSColor.redColor().CGColor],[],GradientType.Radial,1.5707963267949,CGPoint(0,0),CGPoint(0,0),CGSize(0,0),CGSize(1,1))
+        //gradient.gradientType = GradientType.Axial
+        //try to make a Linear gradient and see if that gets clipped
+        
+        //fillStyle = GradientFillStyle(gradient,NSColor.clearColor());
+        
+        //let lineStyle = LineStyle(20,NSColor.blueColor().alpha(0.5))
+        //a = Graphic(fillStyle,nil)
+        
+        //test how easy it is to create a rectGraphic etc
+        let baseGraphic:BaseGraphic = BaseGraphic(fillStyle,nil,OffsetType(OffsetType.outside))
+        let rectGraphic:RectGraphic = RectGraphic(200,200,baseGraphic)
+        //baseGraphic.lineShape.delegate = self
+        baseGraphic.fillShape.delegate = rectGraphic
+        //baseGraphic.layer!.delegate = self
+        rectGraphic.draw()
+        addSubview(baseGraphic)
+        
+        
+    }
+    override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
+        Swift.print("TempGraphix.drawLayer")
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 class GraphicSkin:Skin{
     override init(_ style:IStyle? = nil, _ state:String = "", _ element:IElement? = nil){
         super.init(style, state, element)
@@ -12,6 +49,7 @@ class GraphicSkin:Skin{
         addSubview(decoratable.graphic)
         /*decoratable = */SkinModifier.align(self,decoratable as! IPositional);/*the argument now becomes a reference to the orgiginal instance, but it also becomes immutable unfortunatly,not to worry, the implicit settermethod isnt defined by swift as mutable, even though it is. I guess indirectly, so the values are mutated on the orginal instance and all is well*/
         decoratable.draw()/*Setup the geometry and init the display process of fill and line*/
+        addSubview(TempGraphix(frame: NSRect(10,10,400,400)))
     }
     override func draw(){
         //Swift.print("GraphicSkin.draw()")

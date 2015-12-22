2c71e17ff504ffe118aac46774ad8dea74ecc82d
Eonist <unicornhive@gmail.com>
2015-12-22 20:20:31 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/CircleGraphic.swift

--- src/skin/graphic/decorator/CircleGraphic.swift
+++ src/skin/graphic/decorator/CircleGraphic.swift
@@ -15,7 +15,7 @@ class CircleGraphic:SizeableGraphic{
         Swift.print("CircleGraphic.drawFill()")
         let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!, graphic.lineOffsetType) : CGRect(x,y,width,height)
         graphic.fillShape.frame = fillFrame/*,position and set the size of the frame*/
-        graphic.fillShape.path = CGPathParser.circ(radius, 0, 0)
+        graphic.fillShape.path = CGPathParser.ellipse(CGRect(0,0,width,height))
     }
     override func drawLine() {
         Swift.print("CircleGraphic.drawLine()")

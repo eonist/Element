ddbebfffdde612df3b5ce21339918756d6f89e95
Eonist <unicornhive@gmail.com>
2015-12-22 16:01:17 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/CircleGraphic.swift

--- src/skin/graphic/decorator/CircleGraphic.swift
+++ src/skin/graphic/decorator/CircleGraphic.swift
@@ -2,16 +2,19 @@ import Cocoa
 
 class CircleGraphic:SizeableGraphic{
     var radius:CGFloat
-    //var width:CGFloat {get{return radius!}set{fatalError("NOT SUPPORTED")}}
-    //var height:CGFloat {get{return radius!}set{fatalError("NOT SUPPORTED")}}
+
+    //TODO:should have x and y, for the simpler line bellow use extension
+    
     init(_ radius:CGFloat = 50,_ decoratable: IGraphicDecoratable = BaseGraphic(FillStyle(NSColor.greenColor()))) {
-        //Swift.print("CircleGraphic.init()")
+        Swift.print("CircleGraphic.init()")
         self.radius = radius
         super.init(CGPoint(0,0),CGSize(radius,radius),decoratable)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func drawFill() {
         Swift.print("CircleGraphic.drawFill()")
+        let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!, graphic.lineOffsetType) : CGRect(x,y,width,height)
+        graphic.fillShape.frame = fillFrame/*,position and set the size of the frame*/
         getGraphic().fillShape.path = CGPathParser.circle(radius, x, y)
     }
     override func drawLine() {
@@ -23,7 +26,7 @@ class CircleGraphic:SizeableGraphic{
     }
 }
 extension CircleGraphic{
-    //does not work anymore
+    //does not work anymore, works or?
     
     convenience init(_ radius:CGFloat,_ fillColor:NSColor){
         self.init(radius,BaseGraphic(FillStyle(fillColor)))

45969d874ddb0b52acde1e918c4d9628961c1c9c
Eonist <unicornhive@gmail.com>
2015-12-22 16:43:29 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/CircleGraphic.swift

--- src/skin/graphic/decorator/CircleGraphic.swift
+++ src/skin/graphic/decorator/CircleGraphic.swift
@@ -8,14 +8,14 @@ class CircleGraphic:SizeableGraphic{
     init(_ x:CGFloat, _ y:CGFloat, _ radius:CGFloat ,_ decoratable: IGraphicDecoratable = BaseGraphic(FillStyle(NSColor.greenColor()))) {
         Swift.print("CircleGraphic.init()")
         self.radius = radius
-        super.init(CGPoint(0,0),CGSize(radius*2,radius*2),decoratable)
+        super.init(CGPoint(x,y),CGSize(radius*2,radius*2),decoratable)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func drawFill() {
         Swift.print("CircleGraphic.drawFill()")
         let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!, graphic.lineOffsetType) : CGRect(x,y,width,height)
         graphic.fillShape.frame = fillFrame/*,position and set the size of the frame*/
-        getGraphic().fillShape.path = CGPathParser.circ(radius, x, y)
+        graphic.fillShape.path = CGPathParser.circ(radius, 0, 0)
     }
     override func drawLine() {
         Swift.print("CircleGraphic.drawLine()")

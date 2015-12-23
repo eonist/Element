078245addc3fbae37d85afa258de0a5a419a6573
Eonist <unicornhive@gmail.com>
2015-12-23 14:55:23 +0100

    Files modified: 2
    Modified 2 files:src/skin/SizeableGraphic.swiftsrc/style/fill/GradientFillStyle.swift

--- src/style/fill/GradientFillStyle.swift
+++ src/style/fill/GradientFillStyle.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-class GradientFillStyle:FillStyle,IGradientFillStyle{
+class GradientFillStyle:FillStyle{
     var gradient:Gradient/*IGradient*/;//TODO:change to IGradient
     init(_ gradient:Gradient/*IGradient*/, _ color:NSColor){
         self.gradient = gradient;

0b1597e8a9c3096bb734c092aeb7cea4b2c17e36
Eonist <unicornhive@gmail.com>
2015-12-23 06:17:23 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/RectGraphicUtils.swift

--- src/skin/graphic/utils/RectGraphicUtils.swift
+++ src/skin/graphic/utils/RectGraphicUtils.swift
@@ -28,28 +28,30 @@ class RectGraphicUtils {
         if(offsetType.left == OffsetType.outside){
             lineFrameRect = lineFrameRect.expand(lineStyle.thickness, 0)
             lineRect = lineRect.offset(lineStyle.thickness / 2, 0)//.expand(-lineStyle.thickness/2, 0)
-        }else { //inside
+        }else if(offsetType.left == OffsetType.inside){ //inside
             lineRect = lineRect.offset(-lineStyle.thickness/2, 0)//.expand(lineStyle.thickness/2 , 0)
         }
         /*Right*/
         if(offsetType.right == OffsetType.outside){
             lineFrameRect = lineFrameRect.expand(lineStyle.thickness, 0)
             lineRect = lineRect.expand(lineStyle.thickness , 0)
-        }else{//inside
+        }else if(offsetType.left == OffsetType.inside){//inside
             lineRect = lineRect.expand(lineStyle.thickness , 0)
+        }else{
+            
         }
         /*Top*/
         if(offsetType.top == OffsetType.outside){
             lineFrameRect = lineFrameRect.expand(0,lineStyle.thickness)
             lineRect = lineRect.offset(0, lineStyle.thickness/2)//.expand(0,-lineStyle.thickness/2)
-        }else{//inside
+        }else if(offsetType.left == OffsetType.inside){//inside
             lineRect = lineRect.offset(0, -lineStyle.thickness/2)//.expand(0,+lineStyle.thickness/2)
         }
         /*Bottom*/
         if(offsetType.bottom == OffsetType.outside){//outside
             lineFrameRect = lineFrameRect.expand(0,lineStyle.thickness)
             lineRect = lineRect.expand(0,lineStyle.thickness)
-        }else{//inside
+        }else if(offsetType.left == OffsetType.inside){//inside
             lineRect = lineRect.expand(0,lineStyle.thickness)
         }
         return (lineFrameRect,lineRect)

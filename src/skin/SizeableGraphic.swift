2f3eebae10216a3103948c449eee2fdc2d88e2c9
Eonist <unicornhive@gmail.com>
2015-12-23 13:41:27 +0100

    Files modified: 1
    Modified 1 file:src/skin/SizeableGraphic.swift

--- src/skin/SizeableGraphic.swift
+++ src/skin/SizeableGraphic.swift
@@ -21,9 +21,7 @@ class SizeableGraphic:PositionalGraphic,ISizeable {
 }
 extension SizeableGraphic{
     convenience init(_ x:CGFloat,_ y:CGFloat,_ width:CGFloat, _ height:CGFloat,_ fillStyle:GradientFillStyle, _ lineStyle:GradientLineStyle, _ lineOffset:OffsetType = OffsetType(OffsetType.center)){
-        let b = BaseGraphic(fillStyle,lineStyle,lineOffset)
-        let gradientGraphic = GradientGraphic()
-        self.init(CGPoint(x,y),CGSize(width,height),)
+        self.init(CGPoint(x,y),CGSize(width,height),GradientGraphic(BaseGraphic(fillStyle,lineStyle,lineOffset)))
     }
     convenience init(_ x:CGFloat,_ y:CGFloat,_ width:CGFloat, _ height:CGFloat,_ fillStyle:IFillStyle, _ lineStyle:ILineStyle, _ lineOffset:OffsetType = OffsetType(OffsetType.center)){
         self.init(CGPoint(x,y),CGSize(width,height),BaseGraphic(fillStyle,lineStyle,lineOffset))

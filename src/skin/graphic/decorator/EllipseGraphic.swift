72280194b96d7f44facf683d675bd866cb541b87
Eonist <unicornhive@gmail.com>
2015-12-23 07:26:44 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/EllipseGraphic.swift

--- src/skin/graphic/decorator/EllipseGraphic.swift
+++ src/skin/graphic/decorator/EllipseGraphic.swift
@@ -7,20 +7,19 @@ class EllipseGraphic:SizeableGraphic{
     //TODO: possibly add a diameter var
     //TODO: should have x and y, for the simpler line bellow use extension
     
-    init(_ x:CGFloat, _ y:CGFloat, _ radius:CGFloat ,_ decoratable: IGraphicDecoratable = BaseGraphic(FillStyle(NSColor.greenColor()))/*<--this shoul d be provided through an extension not here*/) {
-        Swift.print("CircleGraphic.init()")
-        self.radius = radius
+    init(_ decoratable: IGraphicDecoratable = BaseGraphic(FillStyle(NSColor.greenColor()))/*<--this shoul d be provided through an extension not here*/) {
+        Swift.print("EllipseGraphic.init()")
         super.init(CGPoint(x,y),CGSize(radius*2,radius*2),decoratable)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func drawFill() {
-        Swift.print("CircleGraphic.drawFill()")
+        Swift.print("EllipseGraphic.drawFill()")
         let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!, graphic.lineOffsetType) : CGRect(x,y,width,height)
         graphic.fillShape.frame = fillFrame/*,position and set the size of the frame*/
         graphic.fillShape.path = CGPathParser.ellipse(CGRect(0,0,width,height))
     }
     override func drawLine() {
-        Swift.print("CircleGraphic.drawLine()")
+        Swift.print("EllipseGraphic.drawLine()")
         let lineOffsetRect = RectGraphicUtils.lineOffsetRect(CGRect(x,y,width,height), graphic.lineStyle!, graphic.lineOffsetType)
         graphic.lineShape.frame = lineOffsetRect.lineFrameRect
         Swift.print("lineOffsetRect.lineFrameRect: " + "\(lineOffsetRect.lineFrameRect)")
@@ -33,6 +32,7 @@ class EllipseGraphic:SizeableGraphic{
 }
 extension EllipseGraphic{
     //does not work anymore, works or?
+    /*
     convenience init(_ radius:CGFloat,_ fillColor:NSColor){
         self.init(0,0,radius,BaseGraphic(FillStyle(fillColor)))
     }
@@ -48,4 +48,5 @@ extension EllipseGraphic{
     convenience init(_ x:CGFloat,_ y:CGFloat,_ radius:CGFloat,_ fillStyle:IFillStyle, _ lineStyle:ILineStyle, _ lineOffset:OffsetType = OffsetType(OffsetType.center)){
         self.init(x,y,radius,BaseGraphic(fillStyle,lineStyle,lineOffset))
     }
+    */
 }

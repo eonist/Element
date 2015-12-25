b75a59ef1a377a16d96e9fd7671a0cf022452c1a
Eonist <unicornhive@gmail.com>
2015-12-25 12:55:03 +0100

    Files modified: 1
    Modified 1 file:src/style/fill/FillStyle.swift

--- src/style/fill/FillStyle.swift
+++ src/style/fill/FillStyle.swift
@@ -18,11 +18,13 @@ protocol Copyable{
     init(_ instance:Copyable)
     func copy()->Copyable
 }
-
-extension FillStyle:Copyable{
-    required convenience init(_ instance:Copyable){
+class ConcreteCopyable:Copyable{
+    required init(_ instance: Copyable) {
         self.init((instance as! FillStyle).color)
     }
+}
+extension FillStyle:Copyable{
+    
     func clone(){
         
     }

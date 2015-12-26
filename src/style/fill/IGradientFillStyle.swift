3c74be438eb9cd42e2a76b597b082783ecabee29
Eonist <unicornhive@gmail.com>
2015-12-26 08:00:37 +0100

    Files modified: 1
    Modified 1 file:src/style/fill/IGradientFillStyle.swift

--- src/style/fill/IGradientFillStyle.swift
+++ src/style/fill/IGradientFillStyle.swift
@@ -1,9 +1,9 @@
 import Foundation
-protocol IGradientFillStyle{
+protocol IGradientFillStyle:IFillStyle{
     var gradient:IGradient{get set}
 }
 extension IGradientFillStyle{
-    func copy() -> GradientFillStyle {
-        return GradientFillStyle(self.gradient.copy(),(self as! IFillStyle).color)
+    func copy() -> IGradientFillStyle {
+        return GradientFillStyle(self.gradient.copy(),(self as IFillStyle).color)
     }
 }

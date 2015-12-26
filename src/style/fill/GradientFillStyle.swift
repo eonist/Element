f744888218a9ae8583b0f510c8fe81765839b00f
Eonist <unicornhive@gmail.com>
2015-12-26 10:43:03 +0100

    Files modified: 1
    Modified 1 file:src/style/fill/GradientFillStyle.swift

--- src/style/fill/GradientFillStyle.swift
+++ src/style/fill/GradientFillStyle.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 class GradientFillStyle:FillStyle,IGradientFillStyle{
     var gradient:IGradient/*IGradient*/;//TODO:change to IGradient
-    init(_ gradient:IGradient/*IGradient*/, _ color:NSColor){
+    init(_ gradient:IGradient/*IGradient*/, _ color:NSColor = NSColor.clearColor()){
         self.gradient = gradient;
         super.init(color);
     }

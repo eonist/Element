bb3b4a7865cd82fe4989a3ea66f1facf14324e62
Eonist <unicornhive@gmail.com>
2015-12-28 15:39:59 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/GraphicDecoratable.swift

--- src/skin/graphic/decorator/GraphicDecoratable.swift
+++ src/skin/graphic/decorator/GraphicDecoratable.swift
@@ -17,7 +17,7 @@ class GraphicDecoratable:AbstractGraphicDecoratable {
     /**
      * Setup the geometry and init the display process of fill and line
      */
-    override func draw() {//new
+    override func draw() {
         //Swift.print("GraphicDecoratable.draw()" )
         if(getGraphic().fillStyle != nil){drawFill();graphic.fillShape.setNeedsDisplay();}/*setup the fill geometry*//*draw the fileShape*/
         if(getGraphic().lineStyle != nil){drawLine();graphic.lineShape.setNeedsDisplay();}/*setup the line geometry*//*draw the fileShape*/

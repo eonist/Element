3edfff64b6fcc548c1614f2a8acbb68c5e757c77
Eonist <unicornhive@gmail.com>
2016-01-15 20:35:28 +0100

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -27,13 +27,20 @@ class GradientGraphic:PositionalDecorator/*<--recently changed from GraphicDecor
         //Swift.print("GradientGraphic.applyLineStyle()")
         super.applyLineStyle()/*call the BaseGraphic to set the stroke-width, cap, joint etc*/
         if(getGraphic().lineStyle!.dynamicType is GradientLineStyle.Type){//<--the dynamicType may not be needed
-            let gradient = (graphic.lineStyle as! GradientLineStyle).gradient
+            let gradient:IGradient = (graphic.lineStyle as! GradientLineStyle).gradient
             var boundingBox:CGRect = CGPathGetBoundingBox(graphic.lineShape.path) // this method can be moved up one level if its better for performance, but wait untill you impliment matrix etc
             boundingBox = boundingBox.outset(graphic.lineStyle!.thickness/2, graphic.lineStyle!.thickness/2)/*Outset the boundingbox to cover the entire stroke*/
+            if(gradient is LinearGradient){
+                
+            }else if(gradient is RadialGradient){
+                
+            }else{
+                
+            }
             let rg = RadialGradientUtils.radialGradient(boundingBox,gradient)/*Creates and configs the radial gradient*/
             //Swift.print("lineStyle is GradientLineStyle")
-            graphics.gradientLine(gradient)
-            LineStyleModifier.lineGradientStyle(graphic.lineShape.graphics, (graphic.lineStyle as! GradientLineStyle).gradient);//Updates only if _lineGradient is not null, and _lineGradient.colors[0] and (_lineGradient.colors[1] are valid colors)
+            graphics.gradientLine(LinearGraphicsGradient())
+            
         }//else{fatalError("NOT CORRECT lineStyle")}
     }
 }

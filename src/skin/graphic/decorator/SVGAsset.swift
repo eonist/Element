6b603a0905749fff6f66bc9e89c7506fad4b6468
Eonist <unicornhive@gmail.com>
2016-01-18 15:59:15 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/SVGAsset.swift

--- src/skin/graphic/decorator/SVGAsset.swift
+++ src/skin/graphic/decorator/SVGAsset.swift
@@ -2,18 +2,19 @@ import Foundation
 
 class SVGAsset:SizeableGraphic {
     var svg:SVG;
-    override init(_ path:String, _ position: CGPoint, _ size: CGSize, _ decoratable: IGraphicDecoratable) {
+    init(_ path:String, _ position: CGPoint, _ size: CGSize, _ decoratable: IGraphicDecoratable) {
         //var xml:XML = FileParser.xml(new File(File.applicationDirectory.url+path));
         let content = FileParser.content(path.tildePath)
         let xmlDoc:NSXMLDocument = try! NSXMLDocument(XMLString: content!, options: 0)
         let rootElement:NSXMLElement = xmlDoc.rootElement()!
-
-        //Swift.print("xml: " + xml.toXMLString());
-        svg = SVGParser.svg(xml)
-        addSubView(svg);
+        svg = SVGParser.svg(rootElement);
         super.init(position, size, decoratable)
+        graphic.addSubview(svg)
     }
-
+    public function draw(x : Number, y : Number, width : Number, height : Number) : void {
+        var scale:Point = new Point(width/_svg.width,height/_svg.height);
+        SVGModifier.scale(_svg, new Point(x,y), scale);
+    }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
         
     

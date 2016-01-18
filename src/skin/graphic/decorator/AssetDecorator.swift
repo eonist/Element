b14d0662bfb4938ffd26e389344bf5854662c382
Eonist <unicornhive@gmail.com>
2016-01-18 17:07:54 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/AssetDecorator.swift

--- src/skin/graphic/decorator/AssetDecorator.swift
+++ src/skin/graphic/decorator/AssetDecorator.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class AssetDecorator {
-
+class AssetDecorator:SizeableDecorator{
+    init(_ path:String, _ position: CGPoint, _ size: CGSize, _ decoratable: IGraphicDecoratable) {
+        Swift.print("SVGAsset.init()")
+        //var xml:XML = FileParser.xml(new File(File.applicationDirectory.url+path));
+        let content = FileParser.content(path.tildePath)
+        let xmlDoc:NSXMLDocument = try! NSXMLDocument(XMLString: content!, options: 0)
+        let rootElement:NSXMLElement = xmlDoc.rootElement()!
+        svg = SVGParser.svg(rootElement);
+        super.init(position, size, decoratable)
+        graphic.addSubview(svg)
+    }
 }

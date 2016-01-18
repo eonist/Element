8065f589536561ff63e79f79923c7df8efabdcc0
Eonist <unicornhive@gmail.com>
2016-01-18 15:53:19 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/SVGAsset.swift

--- src/skin/graphic/decorator/SVGAsset.swift
+++ src/skin/graphic/decorator/SVGAsset.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class SVGAsset:SizeableGraphic {
+    var _svg:SVG;
+    override init(_ position: CGPoint, _ size: CGSize, _ decoratable: IGraphicDecoratable) {
+        super.init(position, size, decoratable)
+    }
+
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        
     
 }

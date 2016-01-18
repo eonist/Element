214a2d18d3513ffe71d292cdb6be33d52b7b48b7
Eonist <unicornhive@gmail.com>
2016-01-18 17:11:58 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/AssetDecorator.swift

--- src/skin/graphic/decorator/AssetDecorator.swift
+++ src/skin/graphic/decorator/AssetDecorator.swift
@@ -1,7 +1,12 @@
 import Foundation
-
+/*
+ * // :TODO: this solution isnt perfect but it works for now
+ * @Note asset is svg for now but in the future it should support png
+ */
 class AssetDecorator:SizeableDecorator{
-    init(_ decoratable: IGraphicDecoratable) {//this shoul d be provided through an extension not here->  = BaseGraphic(FillStyle(NSColor.greenColor())
+    var asset : SVGAsset;
+    var assetURL : String;
+    override init(_ decoratable: IGraphicDecoratable) {//this shoul d be provided through an extension not here->  = BaseGraphic(FillStyle(NSColor.greenColor())
         
         super.init(decoratable)
     }

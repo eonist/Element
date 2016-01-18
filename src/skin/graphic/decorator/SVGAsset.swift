4d34af70ceb1442bd2aef15163928b968327f3cd
Eonist <unicornhive@gmail.com>
2016-01-18 16:01:13 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/SVGAsset.swift

--- src/skin/graphic/decorator/SVGAsset.swift
+++ src/skin/graphic/decorator/SVGAsset.swift
@@ -15,6 +15,15 @@ class SVGAsset:SizeableGraphic {
         var scale:Point = new Point(width/_svg.width,height/_svg.height);
         SVGModifier.scale(_svg, new Point(x,y), scale);
     }
+    
+    override func drawFill() {
+        //Swift.print("EllipseGraphic.drawFill()")
+        let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!.thickness, graphic.lineOffsetType) : CGRect(x,y,width,height)
+        graphic.fillShape.frame = fillFrame/*,position and set the size of the frame*/
+        graphic.fillShape.path = CGPathParser.ellipse(CGRect(0,0,width,height))
+    }
+    
+    
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
         
     

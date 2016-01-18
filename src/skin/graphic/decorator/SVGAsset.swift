44845b38fd569c5bcf4cc888d5eab8188c3708f7
Eonist <unicornhive@gmail.com>
2016-01-18 15:55:18 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/SVGAsset.swift

--- src/skin/graphic/decorator/SVGAsset.swift
+++ src/skin/graphic/decorator/SVGAsset.swift
@@ -1,8 +1,15 @@
 import Foundation
 
 class SVGAsset:SizeableGraphic {
-    var _svg:SVG;
+    var svg:SVG;
     override init(_ position: CGPoint, _ size: CGSize, _ decoratable: IGraphicDecoratable) {
+        var xml:XML = FileParser.xml(new File(File.applicationDirectory.url+path));
+        let xmlDoc:NSXMLDocument = try! NSXMLDocument(XMLString: content!, options: 0)
+        let rootElement:NSXMLElement = xmlDoc.rootElement()!
+
+        //Swift.print("xml: " + xml.toXMLString());
+        svg = SVGParser.svg(xml)
+        addSubView(svg);
         super.init(position, size, decoratable)
     }
 

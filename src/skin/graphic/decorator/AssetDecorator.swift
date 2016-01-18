e305bf075147c552ed6be9a6b3e9a5f9c1fd31d1
Eonist <unicornhive@gmail.com>
2016-01-18 19:20:52 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/AssetDecorator.swift

--- src/skin/graphic/decorator/AssetDecorator.swift
+++ src/skin/graphic/decorator/AssetDecorator.swift
@@ -1,17 +1,17 @@
-import Foundation
+import Cocoa
 /*
  * // :TODO: this solution isnt perfect but it works for now
  * @Note asset is svg for now but in the future it should support png
  */
 class AssetDecorator:SizeableDecorator{
-    var asset : SVGAsset;
+    var asset : SVGAsset?
     var assetURL : String;
     init(_ decoratable: IGraphicDecoratable,_ iconURL:String) {//this shoul d be provided through an extension not here->  = BaseGraphic(FillStyle(NSColor.greenColor())
         assetURL = iconURL;
         super.init(decoratable)
     }
     override func drawFill() {
-        if(asset != nil) {graphic.addSubview(self)};/*temp solution*/
+        if(asset != nil) {(asset! as! NSView).removeFromSuperview()};/*temp solution*/
         _asset = graphic.addChild(new SVGAsset(assetURL) as Sprite) as SVGAsset;
         if(!isNaN(graphic.fillStyle.color)) _asset.applyStyle(graphic.fillStyle,graphic.lineStyle);
     }

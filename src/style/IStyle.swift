af43cd182553c634c47ac717412422464d03e58c
Eonist <unicornhive@gmail.com>
2016-01-19 19:10:34 +0100

    Files modified: 1
    Modified 1 file:src/style/IStyle.swift

--- src/style/IStyle.swift
+++ src/style/IStyle.swift
@@ -1,15 +1,17 @@
 import Foundation
 
 protocol IStyle {
+    /*getters / setters*/
     var name:String {get}
     var selectors:Array<ISelector> {get}
     var styleProperties:Array<IStyleProperty> {get set}
     /*core methods*/
-    func getStyleProperty(name:String)->IStyleProperty?
     func addStyleProperty(styleProperty:IStyleProperty)
     func addStyleProperty(styleProperties:Array<IStyleProperty>)
     func addStyleProperties(styleProperties:Array<IStyleProperty>)
-    func getValueAt(index:Int)->Any
+    /*implicit getters / setters*/
+    func getStyleProperty(name:String,depth:Int)->IStyleProperty?
+    func getValueAt(index:Int,depth:Int)->Any
     func getValue(name:String)->Any?
     func getStyleProperties(name:String)->Array<IStyleProperty>
     func getStylePropertyAt(index:Int)->IStyleProperty

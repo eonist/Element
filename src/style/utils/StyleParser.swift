046c3a776dac00f138bfb69e1117e0534e2718e8
Eonist <unicornhive@gmail.com>
2016-01-19 17:21:31 +0100

    Files modified: 1
    Modified 1 file:src/style/utils/StyleParser.swift

--- src/style/utils/StyleParser.swift
+++ src/style/utils/StyleParser.swift
@@ -2,8 +2,11 @@ import Foundation
 
 /**
  *
+ * @Note: One could use  Vector<String> for speed etc, but that would make the framework less readable for now
+ * @Note if you ever need to find the absolute herarchy path to an instance use this: StyleParser.hierarchicalStyleName(someInstance);//SomeClass someInstance AnotherClass anotherInstance etc
+ * // :TODO: make support for int and uint values for styles that are not numbers
  */
-class StyleParser {
+class StyleParser {// :TODO: rename to StyleResolver, it doesnt feel like a normal parser class not all the functions here!?!?
     /**
      * // :TODO: write java doc
      */

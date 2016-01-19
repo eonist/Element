6e4eac2381ca11ef6410323be62c5ab372c2797b
Eonist <unicornhive@gmail.com>
2016-01-19 19:06:24 +0100

    Files modified: 1
    Modified 1 file:src/style/property/StyleProperty.swift

--- src/style/property/StyleProperty.swift
+++ src/style/property/StyleProperty.swift
@@ -3,11 +3,13 @@ import Foundation
 class StyleProperty:IStyleProperty {
     var name : String
     var value : Any
+    var depth : Int/*Depth should really be UInt, but since apple doesnt use/like UInt and the support isnt that great we use Int*/
     /**
     *
     */
-    init(_ name:String,_ value:Any){
+    init(_ name:String,_ value:Any,_ depth:Int){
         self.name = name
         self.value = value
+        self.depth = depth
     }
 }

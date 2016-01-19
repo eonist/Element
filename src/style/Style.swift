c0780dfe7cf4987b2239747e8a11a90cfe242728
Eonist <unicornhive@gmail.com>
2016-01-19 20:39:21 +0100

    Files modified: 1
    Modified 1 file:src/style/Style.swift

--- src/style/Style.swift
+++ src/style/Style.swift
@@ -21,17 +21,17 @@ class Style:IStyle{
         for styleProperty in styleProperties { addStyleProperty(styleProperty) }
     }
     /**
-    * Add styleProperty
-    * @Note will throw an error if a styleProperty with the same name is allready added
-    * @Note the reason that the following methods are not moved into StyleModifier is because they arent that complex, a minimal set of core methods are ok in a bean style class like this, more advance methods can go into StyleModifier
-    * // :TODO: but this method is cognativly taxing just look how long it is, makes more sense to move it to a modifier class, only simple methods should be in Type classes
-    */
+     * Add styleProperty
+     * @Note will throw an error if a styleProperty with the same name is allready added
+     * @Note the reason that the following methods are not moved into StyleModifier is because they arent that complex, a minimal set of core methods are ok in a bean style class like this, more advance methods can go into StyleModifier
+     * // :TODO: but this method is cognativly taxing just look how long it is, makes more sense to move it to a modifier class, only simple methods should be in Type classes
+     */
     func addStyleProperty(styleProperty:IStyleProperty) {
         styleProperties.append(styleProperty)//TODO:this method is more elaborate, it checks if the item is original, if its not throw error, implement this when its time
     }
     /**
-    * Adds styleProperties
-    */
+     * Adds styleProperties
+     */
     func addStyleProperty(styleProperties:Array<IStyleProperty>){
         for styleProperty : IStyleProperty in styleProperties{addStyleProperty(styleProperty) }
     }
@@ -57,21 +57,21 @@ class Style:IStyle{
         return styleProperties;
     }
     /**
-    * @Note a benefit of having this method is that when used you can use the interface of the return type instantly
-    */
+     * @Note a benefit of having this method is that when used you can use the interface of the return type instantly
+     */
     func getStylePropertyAt(index:Int)->IStyleProperty{
         return styleProperties[index]
     }
     /**
-    * @Note this function is not redundant, its usefull for qucik access in some methods
-    */
+     * @Note this function is not redundant, its usefull for qucik access in some methods
+     */
     func getValue(name:String,_ depth:Int = 0)->Any?{
         var styleProperty:IStyleProperty? = getStyleProperty(name,depth);
         return styleProperty != nil ? styleProperty?.value : nil;
     }
     /**
-    * @Note this method is here for convenience (methods that should be contained in an Utils class are thous that are seldom used)
-    */
+     * @Note this method is here for convenience (methods that should be contained in an Utils class are thous that are seldom used)
+     */
     func getValueAt(index:Int)->Any{
         return getStylePropertyAt(index).value;
     }

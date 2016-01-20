f6aea0d61a24bbd9fe7c3823b5a9bcec7665c8a6
Eonist <unicornhive@gmail.com>
2016-01-20 19:51:18 +0100

    Files modified: 2
    Modified 2 files:src/skin/skin/GraphicSkin.swiftsrc/skin/skin/utils/SkinModifier.swift

--- src/skin/skin/utils/SkinModifier.swift
+++ src/skin/skin/utils/SkinModifier.swift
@@ -6,11 +6,11 @@ class SkinModifier {
     /**
      * Aligns @param view
      */
-    class func align(skin:ISkin, _ positional:IPositional)->IPositional {
+    class func align(skin:ISkin, _ positional:IPositional,_ depth:Int = 0)->IPositional {
         //Swift.print("SkinModifier.align()")
         //var offset:CGPoint = StylePropertyParser.offset(skin,depth);
         //var padding:Padding2 = StylePropertyParser.padding(skin,depth);
-        let margin:Margin = StylePropertyParser.margin(skin);
+        let margin:Margin = StylePropertyParser.margin(skin,depth);
         //var floatType:String = SkinParser.float(skin,depth);
         //if(floatType == CSSConstants.LEFT || floatType == "" || floatType == null) DisplayObjectModifier.position(displayObject, new Point(margin.left + offset.x, margin.top + offset.y));
         //else if(floatType == CSSConstants.RIGHT) DisplayObjectModifier.position(displayObject, new Point(padding.right + margin.right + offset.x, margin.top + padding.top + offset.y));

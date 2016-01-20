0b24748fd7fd6953537cbf832cf191a6cc260f96
Eonist <unicornhive@gmail.com>
2016-01-20 14:29:42 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/SVGAsset.swift

--- src/skin/graphic/decorator/SVGAsset.swift
+++ src/skin/graphic/decorator/SVGAsset.swift
@@ -60,6 +60,6 @@ private class Utils{
         //Swift.print("lineStyle.capStyle: " + "\(lineStyle.capStyle)")
         //Swift.print("lineStyle.jointStyle: " + "\(lineStyle.jointStyle)")
         //Swift.print("lineStyle.miterLimit: " + "\(lineStyle.miterLimit)")
-        return SVGStyle(fillStyle != nil ? Double(fillStyle!.color.hex) : nil, fillStyle != nil ? fillStyle!.color.alphaComponent : nil, nil,lineStyle != nil ? lineStyle!.thickness : nil,lineStyle != nil ? lineStyle.color : nil,lineStyle != nil ? lineStyle.color.alphaComponent : nil,lineStyle != nil ? LineStyleParser.lineCapType(lineStyle.lineCap) : nil , lineStyle != nil ? LineStyleParser.lineJoinType(lineStyle.lineJoin): nil,lineStyle != nil ? lineStyle.miterLimit : nil)
+        return SVGStyle(fillStyle != nil ? Double(fillStyle!.color.hex) : nil)
     }
 }

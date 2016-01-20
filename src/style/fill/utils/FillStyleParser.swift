5f5ca7d752c62e13d37739c4d774497a91e82c0a
Eonist <unicornhive@gmail.com>
2016-01-20 14:43:30 +0100

    Files modified: 1
    Modified 1 file:src/style/fill/utils/FillStyleParser.swift

--- src/style/fill/utils/FillStyleParser.swift
+++ src/style/fill/utils/FillStyleParser.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class FillStyleParser {
-
+    /**
+     *
+     */
+    class func describe(fillStyle:IFillStyle){
+        Swift.print("fillStyle.color: " + "\(fillStyle.color)")
+        Swift.print("fillStyle.color.alphaComponent: " + "\(fillStyle.color.alphaComponent)")
+    }
 }

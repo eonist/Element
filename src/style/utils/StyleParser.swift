09e8689c7be794752fb3e410ad99fa7254737d11
Eonist <unicornhive@gmail.com>
2016-01-20 20:07:10 +0100

    Files modified: 1
    Modified 1 file:src/style/utils/StyleParser.swift

--- src/style/utils/StyleParser.swift
+++ src/style/utils/StyleParser.swift
@@ -46,11 +46,11 @@ class StyleParser {// :TODO: rename to StyleResolver, it doesnt feel like a norm
     /**
     * @param name the propertyname
     */
-    class func index(style:IStyle, _ name:String, depth:Int = 0) -> Int {
+    class func index(style:IStyle, _ name:String, _ depth:Int = 0) -> Int {
         let len:Int = style.styleProperties.count;
         for (var i : Int = 0; i < len; i++) {
             let styleProperty : IStyleProperty  = style.styleProperties[i];
-            if(styleProperty.name == name){ return i }
+            if(styleProperty.name == name && styleProperty.depth == depth){ return i }
         }
         return -1;
     }

744cfe8d2c4426e3b753c3a2ea267b2c064d1dd5
Eonist <unicornhive@gmail.com>
2016-01-21 14:18:59 +0100

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyParser.swift

--- src/style/property/utils/StylePropertyParser.swift
+++ src/style/property/utils/StylePropertyParser.swift
@@ -72,7 +72,7 @@ class StylePropertyParser{
     class func color(skin:ISkin, _ propertyName:String, _ depth:Int = 0) -> Double {
         let color:Any? = value(skin, propertyName,depth);
         Swift.print("color: " + "\(color)")
-        return (String(color) == CSSConstants.none || color == nil) ? Double.NaN : Double(color as! UInt);
+        return color == nil || String(color!) == CSSConstants.none ? Double.NaN : Double(color as! UInt);
     }
     /**
      * Returns an Offset instance

389d079a2365c9064b6b5f13c9d2c61ea94a6c86
Eonist <unicornhive@gmail.com>
2016-01-22 10:27:09 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -66,3 +66,14 @@ class GraphicSkin:Skin{
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class*/
 }
+/**
+ *
+ */
+private class Utils{
+    /**
+     *
+     */
+    class func size(sizableDecorator:SizeableDecorator,cgSize:CGSize){
+        
+    }
+}

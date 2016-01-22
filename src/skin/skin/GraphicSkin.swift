3344d40fbebc9a88d53e7c337d1c62b425120bf9
Eonist <unicornhive@gmail.com>
2016-01-22 10:31:09 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -28,7 +28,7 @@ class GraphicSkin:Skin{
             let depthCount:Int = StyleParser.depthCount(style!);
             //Swift.print("depthCount: " + "\(depthCount)")
             for (var depth : Int = 0; depth < depthCount; depth++) {
-                if(hasSizeChanged){Utils.size(<#T##sizableDecorator: SizeableDecorator##SizeableDecorator#>, size: <#T##CGSize#>)}//do sizing of the sizable here
+                if(hasSizeChanged){Utils.size(<#T##sizableDecorator: SizeableDecorator##SizeableDecorator#>, CGSize(width,height))}//do sizing of the sizable here
                 //Rect3Modifier.size(decoratables[depth], width + padding.left + padding.right, height + padding.top + padding.bottom);// :TODO: width and height here is prob wrong
                 if(hasStateChanged || hasStyleChanged) {applyProperties(&decoratables[0],depth)}
                 /*decoratable = */SkinModifier.align(self,decoratables[0] as! IPositional,depth)/* as! IGraphicDecoratable;*/

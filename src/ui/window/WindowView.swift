fcd30da95f92d197167e26e0b298ec2438463217
Eonist <unicornhive@gmail.com>
2016-01-23 23:37:41 +0100

    Files modified: 1
    Modified 1 file:src/ui/window/WindowView.swift

--- src/ui/window/WindowView.swift
+++ src/ui/window/WindowView.swift
@@ -3,20 +3,18 @@ import Cocoa
 /**
  * NOTE: we dont extend Element or InteractiveView here because this view does not need the features that InteractiveView brings
  */
-class WindowView:NSVisualEffectView,IElement{
+class WindowView:FlippedView,IElement{
     var id : String?/*css selector id*/
     var parent:IElement?
-    //var state:String = SkinStates.none
+    var state:String = SkinStates.none
     var skin:ISkin?
     var style:IStyle = Style.clear
-    override var allowsVibrancy :Bool {return true}
     init(_ width: CGFloat, _ height: CGFloat, _ id:String? = nil) {
         self.id = id;
         super.init(frame: NSRect(0,0,width,height))//<--This can be a zero rect since the children contains the actual graphics. And when you use Layer-hosted views the subchildren doesnt clip
         self.wantsLayer = true/*if true then view is layer backed*/
         layer = CALayer()/*needs to be layer-hosted so that we dont get clipping of children*/
         layer!.masksToBounds = false//this is needed!!!
-        blendingMode = NSVisualEffectBlendingMode
         resolveSkin()
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
@@ -31,7 +29,7 @@ class WindowView:NSVisualEffectView,IElement{
      * Toggles between css style sheets and have them applied to all Element instances
      */
     func getSkinState() -> String {// :TODO: the skin should have this state not the element object!!!===???
-        return ""
+        return state
     }
     /**
      * Sets the current state of the button, which determins the current drawing of the skin

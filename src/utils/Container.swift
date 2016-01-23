b3549507d995fbe924f693b0ee312ad821c7a48a
Eonist <unicornhive@gmail.com>
2016-01-23 20:30:10 +0100

    Files modified: 2
    Modified 2 files:src/utils/Container.swiftsrc/utils/Section.swift

--- src/utils/Container.swift
+++ src/utils/Container.swift
@@ -4,17 +4,19 @@ import Cocoa
  * NOTE: Container does not add the skin to the stage, Use Section if you need a skin added to
  * // :TODO: rename to Div,Division,Section,Segment? or? Div sounds best and is closley related to css
  */
-class Container:FlippedView{
-    init(_ width:Int = 100, _ height:Int = 100) {
-        let frame = NSRect(x: 0, y: 0, width: width, height: height)
-        super.init(frame: frame)
+class Container:Element{
+    override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = nil){
+        super.init(width, height, parent, id)
+    }
+    override func resolveSkin() {
+        self.skin = SkinResolver.skin(self)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class*/
     /**
      * Returns "Container"
      * @Note This function is used to find the correct class type when synthezing the element cascade
      */
-    func getClassType()->String{
-        return String(self.dynamicType)
+    override func getClassType()->String{
+        return String(Container)
     }
 }
--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -6,15 +6,12 @@ class Section:Element {
     override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = nil){
         super.init(width, height, parent, id)
     }
-    func resolveSkin() {
-        su
-    }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class */
     /**
      * Returns "Section"
      * NOTE: This function is used to find the correct class type when synthezing the element cascade
      */
     override func getClassType()->String{
-        return String(self.dynamicType)
+        return String(Section)
     }
 }

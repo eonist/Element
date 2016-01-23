733b5d049cd9f2426192ae89d3d6dff611356240
Eonist <unicornhive@gmail.com>
2016-01-23 23:31:44 +0100

    Files modified: 1
    Modified 1 file:src/ui/window/WindowView.swift

--- src/ui/window/WindowView.swift
+++ src/ui/window/WindowView.swift
@@ -9,12 +9,14 @@ class WindowView:FlippedView,IElement{
     var state:String = SkinStates.none
     var skin:ISkin?
     var style:IStyle = Style.clear
+    override var allowsVibrancy :Bool {return true}
     init(_ width: CGFloat, _ height: CGFloat, _ id:String? = nil) {
         self.id = id;
         super.init(frame: NSRect(0,0,width,height))//<--This can be a zero rect since the children contains the actual graphics. And when you use Layer-hosted views the subchildren doesnt clip
         self.wantsLayer = true/*if true then view is layer backed*/
         layer = CALayer()/*needs to be layer-hosted so that we dont get clipping of children*/
         layer!.masksToBounds = false//this is needed!!!
+        blendingMode = NSVisualEffectBlendingModeWithinWindow
         resolveSkin()
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}

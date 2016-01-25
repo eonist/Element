f7229137381ec1473d0f8d500771083342a15eb7
Eonist <unicornhive@gmail.com>
2016-01-25 18:02:36 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/SkinParser.swift

--- src/skin/skin/utils/SkinParser.swift
+++ src/skin/skin/utils/SkinParser.swift
@@ -1,5 +1,23 @@
 import Foundation
 
 class SkinParser {
-
+    /**
+     * Returns the position when margin and padding is taken into account
+     */
+    class func relativePosition(skin:Skin)->CGPoint {
+        var margin:Margin = margin(skin);
+        var border:Border = border(skin);
+        var padding:Padding = padding(skin);
+        var offset:Point = offset(skin);
+        //Swift.print("padding.left: " + padding.left);
+        //Swift.print("margin.left: " + margin.left);
+        //Swift.print("skin.x: " + skin.x);
+        return CGPoint(/*(skin.element as DisplayObject).x*/ margin.left + border.left + padding.left + offset.x, /*(skin.element as DisplayObject).y*/ margin.top + border.top + padding.top + offset.y);
+    }
+    /**
+     *
+     */
+    class func margin(skin:ISkin)->Margin{// :TODO: possibly rename to relativeMargin
+        return StylePropertyParser.margin(skin);
+    }
 }

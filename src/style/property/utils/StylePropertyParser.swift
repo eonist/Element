a9e942e7113dc7c832a99c4366ce9dd0340e32c8
Eonist <unicornhive@gmail.com>
2016-01-26 18:42:31 +0100

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyParser.swift

--- src/style/property/utils/StylePropertyParser.swift
+++ src/style/property/utils/StylePropertyParser.swift
@@ -219,6 +219,10 @@ class StylePropertyParser{
     class func padding(skin:ISkin,_ depth:Int = 0) -> Padding {
         let value:Any? = self.value(skin, CSSConstants.padding, depth)
         Swift.print("value: " + "\(value)")
+        let padding:Padding
+        if(value != nil){
+            
+        }
         let padding:Padding = value != nil ? Padding(value!) : Padding()
         let array:Array<CGFloat> = value is Array<CGFloat> ? value as! Array<CGFloat> : [value as! CGFloat]
         let padding:Padding = Padding(array)

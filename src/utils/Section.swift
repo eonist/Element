e3d2c284b4307768fbc7010c896596d11db4415b
Eonist <unicornhive@gmail.com>
2016-01-27 15:19:14 +0100

    Files modified: 1
    Modified 1 file:src/utils/Section.swift

--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -3,5 +3,5 @@ import Foundation
  * NOTE: Unlike Container, section can have a style applied
  * NOTE: you dont need anything in this method actually, the getClassType works if Section is the last SubClass in the subclass hierarchy
  */
-class Section:Element {
+class Section:WindowView {
 }

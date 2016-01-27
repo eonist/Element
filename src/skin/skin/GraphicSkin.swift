dbb48a7c20249cc2b3aac3087dea09a2cc8d2125
Eonist <unicornhive@gmail.com>
2016-01-27 18:00:28 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/GraphicSkin.swift

--- src/skin/skin/GraphicSkin.swift
+++ src/skin/skin/GraphicSkin.swift
@@ -29,7 +29,7 @@ class GraphicSkin:Skin{
             //if(hasSizeChanged)
             for (var depth : Int = 0; depth < depthCount; depth++) {
                 if(hasSizeChanged){
-                    let padding:Padding = StylePropertyParser.padding(self,depth);// :TODO: what about margin?<----not sure this is needed, the padding
+                    let padding:Padding = Padding()//StylePropertyParser.padding(self,depth);// :TODO: what about margin?<----not sure this is needed, the padding
                     Utils.size(decoratables[depth], CGSize(width! + padding.left + padding.right,height! + padding.top + padding.bottom))
                 }//do sizing of the sizable here
                 if(hasStateChanged || hasStyleChanged) {applyProperties(&decoratables[depth],depth)}

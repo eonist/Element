d361e0f34198de311d55dd88b97fe728549bfd11
Eonist <unicornhive@gmail.com>
2016-02-04 18:18:47 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/text/SelectTextButton.swift

--- src/ui/button/text/SelectTextButton.swift
+++ src/ui/button/text/SelectTextButton.swift
@@ -9,9 +9,9 @@ class SelectTextButton:TextButton,ISelectable {
         super.init(text, width, height, parent, id);
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
-    override func mouseUpInside(theEvent: NSEvent) {
+    override func mouseUpInside(event: MouseEvent) {
         isSelected = true
-        super.mouseUpInside(theEvent)
+        super.mouseUpInside(event)
         NSNotificationCenter.defaultCenter().postNotificationName(SelectEvent.select, object:self)/*bubbles:true because i.e: radioBulet may be added to RadioButton and radioButton needs to dispatch Select event if the SelectGroup is to work*/
     }
     /**

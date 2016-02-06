e68024edac09e5e3f14f4248c33da1333ae44d96
Eonist <unicornhive@gmail.com>
2016-02-06 06:38:24 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/events/ButtonEvent.swift

--- src/ui/button/events/ButtonEvent.swift
+++ src/ui/button/events/ButtonEvent.swift
@@ -1,10 +1,13 @@
 import Foundation
-
+/**
+ * TODO: add for toutch aswell: toutchUp,Down,upOutSide,upInside,out,over etc
+ */
 class ButtonEvent:Event{
     static var down:String = "buttonEventDown"
     static var up:String = "buttonEventUp"/*new*/
-    static var releaseInside:String = "buttonEventReleaseInside"
-    static var releaseOutside:String = "buttonEventReleaseOutside"
-    static var mouseOut:String = "buttonEventMouseOut"
-    static var mouseOver:String = "buttonEventMouseOut"
+    static var upInside:String = "buttonEventUpInside"
+    static var upOutside:String = "buttonEventUpOutside"
+    static var out:String = "buttonEventOut"
+    static var over:String = "buttonEventOut"
+    
 }

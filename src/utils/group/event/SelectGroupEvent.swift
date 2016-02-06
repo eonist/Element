c6993d6a65137c860f9e40dedab108575ee55fd3
Eonist <unicornhive@gmail.com>
2016-02-06 14:47:35 +0100

    Files modified: 1
    Modified 1 file:src/utils/group/event/SelectGroupEvent.swift

--- src/utils/group/event/SelectGroupEvent.swift
+++ src/utils/group/event/SelectGroupEvent.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class SelectGroupEvent {
+class SelectGroupEvent:Event {
     static var change:String = "selectGroupChange";/*this event is dispatched after the _selected variable is set in the SelectGroup instance*/// :TODO: possibly rename to SELECT_GROUP_SELECTED
     static var select : String = "selectGroupSelect";/*This event is dispatched before the _selected variable is set in the SelectGroup instance*/
     //static var deSelect : String = "selectGroupDeSelect";

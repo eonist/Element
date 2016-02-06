bddc40c8eeca148378215a4938c4ffb8c8bf011a
Eonist <unicornhive@gmail.com>
2016-02-06 17:29:06 +0100

    Files modified: 1
    Modified 1 file:src/utils/Section.swift

--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -3,8 +3,11 @@ import Foundation
  * NOTE: Unlike Container, section can have a style applied
  * NOTE: you dont need anything in this method actually, the getClassType works if Section is the last SubClass in the subclass hierarchy
  */
-class Section:WindowView {
-    override func getClassType()->String{
-        return String(Section)
+class Section:Element {
+    /*override func getClassType()->String{
+    return String(Section)
+    }*/
+    override func onEvent(event: Event) {
+        Swift.print("event: " + "\(event)")
     }
 }

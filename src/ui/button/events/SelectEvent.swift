37be53b88fd4f059ed3527416cb720947a569bde
Eonist <unicornhive@gmail.com>
2016-02-06 09:56:30 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/events/SelectEvent.swift

--- src/ui/button/events/SelectEvent.swift
+++ src/ui/button/events/SelectEvent.swift
@@ -3,7 +3,7 @@ import Foundation
  * “Unselect” vs “Deselect”: Unselected is used to qualify something that has not been selected, not something that was selected and isn't anymore.
  * NOTE: to get the selected state: ((sender as! NSNotification).object as ISelectable).isSelected
  */
-class SelectEvent {
+class SelectEvent:Event {
     static var select:String = "select"
     //static var deSelect:String = "deSelect"//use
 }

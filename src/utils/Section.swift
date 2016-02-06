744d1f477fffecf9e8d1b687fb6d0a507fae3b5a
Eonist <unicornhive@gmail.com>
2016-02-06 19:11:57 +0100

    Files modified: 1
    Modified 1 file:src/utils/Section.swift

--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -4,11 +4,4 @@ import Foundation
  * NOTE: you dont need anything in this method actually, the getClassType works if Section is the last SubClass in the subclass hierarchy
  */
 class Section:Element {
-    /*override func getClassType()->String{
-    return String(Section)
-    }*/
-    override func onEvent(event: Event) {
-        Swift.print("Section.event: " + "\(event)")
-        super.onEvent(event)
-    }
 }

6107755d3e70c6d0997911c42060bae5e9dc623c
Eonist <unicornhive@gmail.com>
2016-02-06 15:05:44 +0100

    Files modified: 1
    Modified 1 file:src/utils/group/event/SelectGroupEvent.swift

--- src/utils/group/event/SelectGroupEvent.swift
+++ src/utils/group/event/SelectGroupEvent.swift
@@ -4,8 +4,8 @@ class SelectGroupEvent:Event {
     static var change:String = "selectGroupChange";/*this event is dispatched after the _selected variable is set in the SelectGroup instance*/// :TODO: possibly rename to SELECT_GROUP_SELECTED
     static var select : String = "selectGroupSelect";/*This event is dispatched before the _selected variable is set in the SelectGroup instance*/
     //static var deSelect : String = "selectGroupDeSelect";
-    var selectable : ISelectable
-    init(_ type: String, _ origin: AnyObject,_ selectable:ISelectable) {
+    var selectable : ISelectable?
+    init(_ type: String, _ origin: AnyObject,_ selectable:ISelectable? = nil) {
         self.selectable = selectable
         super.init(type,origin)
     }

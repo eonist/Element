e530cc0322aea068575c1fe4bc8af5241ae78014
Eonist <unicornhive@gmail.com>
2016-02-06 17:33:14 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -8,6 +8,7 @@ class Button:Element {
         //Swift.print("Button.init()")
         super.init(width, height, parent, id)
         //addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
+        
     }
     /**
      * Handles actions and drawing states for the mouseEntered event.

b4e4fcb517b59e0f19381320f6092ad42597ebb8
Eonist <unicornhive@gmail.com>
2016-02-07 16:03:16 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/TextInput.swift

--- src/ui/text/TextInput.swift
+++ src/ui/text/TextInput.swift
@@ -1,5 +1,19 @@
 import Foundation
 
-class TextInput {
-
+class TextInput :Element{
+    var textString:String
+    var inputString:String
+    init(width : CGFloat, height : CGFloat, textString:String, inputString:String, parent : IElement? = nil,  id:String? = nil, classId:String? = nil) {
+        self.textString = textString
+        self.inputString = inputString
+        super.init(width, height, parent, id)
+        
+    }
+    override func resolveSkin() {
+        super.resolveSkin();
+        
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        
+    
 }

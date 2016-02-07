6680a24f6426aeab92bb069cef74604cd9982e71
Eonist <unicornhive@gmail.com>
2016-02-07 08:35:08 +0100

    Files modified: 1
    Modified 1 file:src/ui/window/WindowView.swift

--- src/ui/window/WindowView.swift
+++ src/ui/window/WindowView.swift
@@ -4,42 +4,17 @@ import Cocoa
  * NOTE: we dont extend Element or InteractiveView here because this view does not need the features that InteractiveView brings
  * TODO: window should have a background element to target. check legacy code to confirm theory
  */
-class WindowView:InteractiveView2,IElement{
-    var id : String?/*css selector id*/
-    var parent:IElement?
-    var state:String = SkinStates.none
-    var skin:ISkin?
-    var style:IStyle = Style.clear
-    init(_ width: CGFloat, _ height: CGFloat, _ parent:IElement? = nil, _ id:String? = nil) {
-        self.parent = parent
-        self.id = id
-        super.init(frame: NSRect(0,0,width,height))//<--This can be a zero rect since the children contains the actual graphics. And when you use Layer-hosted views the subchildren doesnt clip
-        resolveSkin()
-    }
-    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+class WindowView:Element{
     /**
      * Draws the graphics
      */
-    func resolveSkin() {
-        self.skin = SkinResolver.skin(self)
-        self.addSubview(self.skin as! NSView)
-    }
-    /**
-     * Toggles between css style sheets and have them applied to all Element instances
-     */
-    func getSkinState() -> String {// :TODO: the skin should have this state not the element object!!!===???
-        return state
-    }
-    /**
-     * Sets the current state of the button, which determins the current drawing of the skin
-     */
-    func setSkinState(state:String) {
-        skin!.setSkinState(state)
+    override func resolveSkin() {
+        super.resolveSkin()
     }
     /**
      * Returns the class type of the Class instance
      */
-    func getClassType()->String{
+    override func getClassType()->String{
         return String(Window)//Window can be targeted via the id so we use Window for all Window subclasses, although this can be overriden in said subclasses
     }
 }

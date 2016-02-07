49e9b299d5282d4ee25300033ae0c66c5f1a2845
Eonist <unicornhive@gmail.com>
2016-02-07 16:33:17 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/TextArea.swift

--- src/ui/text/TextArea.swift
+++ src/ui/text/TextArea.swift
@@ -1,11 +1,20 @@
 import Foundation
-
+/**
+ * :TODO: maybe rename text to defaultText and _text to _text
+ */
 class TextArea:Element {
     var text:Text?
     var textString:String// :TODO: rename to initTextString
     init(_ width:CGFloat,_ height:CGFloat, _ text:String = "defaultText", _ parent:IElement? = nil, _ id:String? = nil) {
         self.textString = text;
         super.init(width, height, parent, id)
+    }
+    func setSize(width:CGFloat, height:CGFloat) {
+        super.setSize(width, height);
+        text!.setSize(width, height);
+    }
+    func setText(text:String) {
+        text.setText(text);
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

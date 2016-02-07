e7467635821006a45622c8e1abf5db1529cc039b
Eonist <unicornhive@gmail.com>
2016-02-07 16:31:18 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/TextInput.swift

--- src/ui/text/TextInput.swift
+++ src/ui/text/TextInput.swift
@@ -14,8 +14,8 @@ class TextInput :Element{
     override func resolveSkin() {
         super.resolveSkin();
         isInteractive = false//<-- only the textField should be interactive
-        text = addChild(new Text(width,height,_textString,this)) as! Text;
-        inputTextArea = addChild(new TextArea(width,height,String(_inputString),this)) as TextArea;
+        text = addSubView(Text(width,height,self.textString,self)) as! Text
+        inputTextArea = addSubView(TextArea(width,height,String(self.inputString),self)) as TextArea;
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
         

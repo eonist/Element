fa4cd0ebee9a09b3ced728a8159fcb3842fc6d52
Eonist <unicornhive@gmail.com>
2016-02-08 13:01:19 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/text/TextSkin.swift

--- src/skin/skin/text/TextSkin.swift
+++ src/skin/skin/text/TextSkin.swift
@@ -30,8 +30,7 @@ class TextSkin:Skin,ITextSkin{
             if(hasStateChanged || hasStyleChanged || hasTextChanged) {applyProperties(textField)}
             if(hasTextChanged) {hasTextChanged = false}
             SkinModifier.align(self, textField)
-            let text = NSText()
-            text.x
+            
         }
         super.draw()
     }

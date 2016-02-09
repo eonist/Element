e7c2d4b622380df99f181de0e2ae2a5b29b8935c
Eonist <unicornhive@gmail.com>
2016-02-09 17:42:00 +0100

    Files modified: 1
    Modified 1 file:src/ui/spinner/event/SpinnerEvent.swift

--- src/ui/spinner/event/SpinnerEvent.swift
+++ src/ui/spinner/event/SpinnerEvent.swift
@@ -1,5 +1,10 @@
-import Foundation
+import Cocoa
 
-class SpinnerEvent {
-
+class SpinnerEvent :Event{
+    static var change : String = "spinnerEventChange"
+    var value:CGFloat
+    init(_ type:String, _ value:CGFloat, _ origin:NSView){
+        self.value = value
+        super.init(type, origin)
+    }
 }

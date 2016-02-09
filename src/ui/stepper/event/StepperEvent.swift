60d6027340bede812207205f3f40dd7ffc050e03
Eonist <unicornhive@gmail.com>
2016-02-09 12:36:30 +0100

    Files modified: 1
    Modified 1 file:src/ui/stepper/event/StepperEvent.swift

--- src/ui/stepper/event/StepperEvent.swift
+++ src/ui/stepper/event/StepperEvent.swift
@@ -1,5 +1,10 @@
-import Foundation
+import Cocoa
 
-class StepperEvent {
-
+class StepperEvent :Event{
+    static var change : String = "stepperEventChange"
+    var value:CGFloat
+    init(_ type:String, _ value:CGFloat, _ origin:NSView){
+        self.value = value
+        super.init("", origin)
+    }
 }

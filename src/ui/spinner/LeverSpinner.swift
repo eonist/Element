dd8febaaab7a25e1a5d79c3d0ac6f245e7892807
Eonist <unicornhive@gmail.com>
2016-02-09 16:32:33 +0100

    Files modified: 1
    Modified 1 file:src/ui/spinner/LeverSpinner.swift

--- src/ui/spinner/LeverSpinner.swift
+++ src/ui/spinner/LeverSpinner.swift
@@ -4,6 +4,7 @@ import Foundation
  * // :TODO: This should maybe be a decorator style pattern that wraps Stepper, then also make RepeatStepper ?
  * @Note if you need to lever a value between 0 and 1 then set min:0, max:1, increment:0.01, decimal:2, leverRange:1,leverHeight:400
  * @Note the reason we dont extend the Stepper component is because the the Stepper component impliments RepeateButton, which LeverStepper doesnt, we could override creating the RepeateButtons but it would obfuscate this class a little, and its nice to have this class as simple as possible since we might need to create different steppers or even extend this class// :TODO: what about Not implimenting RepeatButton in Spinner then and make another class named RepeatStepper and RepeatSpinner?
+ * TODO: you may need to convert CGFloat to an Int if decimal is set to 0, do this in the LeverSpinner class
  */
 class LeverSpinner : Element{
     var textInput:TextInput?

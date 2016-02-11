5e060857626b34a78a03496c3f0dca008be5afe5
Eonist <unicornhive@gmail.com>
2016-02-11 20:49:11 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/check/CheckButton.swift

--- src/ui/button/check/CheckButton.swift
+++ src/ui/button/check/CheckButton.swift
@@ -3,10 +3,16 @@ import Foundation
  * @Note: to force the CheckButton to apply its Checked or unchecked skin, use the setChecked after the instance is created
  */
 class CheckButton :Button{
+    var isChecked:Bool;
     override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = nil){
+        
         super.init(width,height,parent,id);
     }
     
+    override func mouseUpInside(event: MouseEvent) {
+        super.mouseUpInside(event)
+        //onEvent(CheckEvent(CheckEvent.check, isChecked, self))
+    }
     
     
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}

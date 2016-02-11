ee80af3e9090ecd6a75b4cedf40cef3e790b7831
Eonist <unicornhive@gmail.com>
2016-02-11 20:56:06 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/check/CheckButton.swift

--- src/ui/button/check/CheckButton.swift
+++ src/ui/button/check/CheckButton.swift
@@ -8,13 +8,20 @@ class CheckButton :Button{
         self.isChecked = isChecked
         super.init(width,height,parent,id);
     }
-    
     override func mouseUpInside(event: MouseEvent) {
         isChecked = !isChecked
         super.mouseUpInside(event)
         //onEvent(CheckEvent(CheckEvent.check, isChecked, self))
     }
-    
-    
+    /**
+     * Sets the _isChecked variable (Toggles between two states)
+     */
+    func setChecked(isChecked:Bool) {
+        self.isChecked = isChecked
+        setSkinState(getSkinState())
+    }
+    override func getSkinState() -> String {
+        return isChecked ? SkinStates.checked + " " + super.getSkinState() : super.getSkinState()
+    }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

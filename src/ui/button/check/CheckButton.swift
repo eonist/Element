fad9f7891924c422d7df2d1915fe6e530a2a4cd6
Eonist <unicornhive@gmail.com>
2016-02-11 20:46:38 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/check/CheckButton.swift

--- src/ui/button/check/CheckButton.swift
+++ src/ui/button/check/CheckButton.swift
@@ -1,5 +1,13 @@
 import Foundation
-
-class CheckButton {
-
+/**
+ * @Note: to force the CheckButton to apply its Checked or unchecked skin, use the setChecked after the instance is created
+ */
+class CheckButton :Button{
+    override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = nil){
+        super.init(width,height,parent,id);
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

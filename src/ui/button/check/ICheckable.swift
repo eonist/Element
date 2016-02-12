8cb636e3c860565ba0ecc8eb6bc50d43036dc973
Eonist <unicornhive@gmail.com>
2016-02-12 08:48:15 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/check/ICheckable.swift

--- src/ui/button/check/ICheckable.swift
+++ src/ui/button/check/ICheckable.swift
@@ -1,5 +1,6 @@
 import Foundation
 
-class ICheckable {
-
+protocol ICheckable {
+    func setChecked(isChecked:Bool)
+    var isChecked:Bool{get}// :TODO: this should be getChecked since composite classes can impliment ICHeckable and they will need to access a sub instance via a implimcit getter method, same for IDisableable, ISelectable, IFocusable etc
 }

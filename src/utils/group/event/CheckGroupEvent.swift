c1a904d4a30a0673d010803aab3199a26cf42db6
Eonist <unicornhive@gmail.com>
2016-02-12 20:30:58 +0100

    Files modified: 1
    Modified 1 file:src/utils/group/event/CheckGroupEvent.swift

--- src/utils/group/event/CheckGroupEvent.swift
+++ src/utils/group/event/CheckGroupEvent.swift
@@ -1,5 +1,8 @@
 import Foundation
 
 class CheckGroupEvent:Event {
-
+    static var change:String = "checkGroupChange";
+    init(_ type:String, _ origin: AnyObject,_ checkable:ICheckable? = nil) {
+        super(type);
+    }
 }

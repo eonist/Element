34f64e2984581169e503712f2f8b5042346e5396
Eonist <unicornhive@gmail.com>
2016-02-12 08:52:41 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/checkboxbutton/CheckBoxButton.swift

--- src/ui/button/checkboxbutton/CheckBoxButton.swift
+++ src/ui/button/checkboxbutton/CheckBoxButton.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 class CheckBoxButton : Button{
-    
+    init( width:CGFloat = NaN, _ height:CGFloat = NaN, _ isFocused:Boolean = false, _ isDisabled:Boolean = false, _ text:String = "defaultText", _ isChecked:Bool = false, parent:IElement? = nil, id:String? = nil) {
+    _textString = text;
+    _isChecked = isChecked;
+    super(width,height,isFocused, isDisabled,parent,id,classId);
+    }
 }

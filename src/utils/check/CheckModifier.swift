e43359e1359e8589152cf1d4baf59950aa91f82b
Eonist <unicornhive@gmail.com>
2016-02-12 21:24:03 +0100

    Files modified: 1
    Modified 1 file:src/utils/check/CheckModifier.swift

--- src/utils/check/CheckModifier.swift
+++ src/utils/check/CheckModifier.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class CheckModifier {
-
+    /**
+     * UnChecks all in @param items exept @param target
+     */
+    class func unCheckAllExcept(exceptionItem:ICheckable, _ checkables:Array<ICheckable>) {// :TODO: refactor this function// :TODO: rename to unSelectAllExcept
+        for checkable : ICheckable in checkables {if(checkable !== exceptionItem && checkable.isChecked) {checkable.setChecked(false)}}
+    }
 }

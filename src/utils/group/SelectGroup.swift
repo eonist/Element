3e42024aa97ea4d99ae1e8dc35eb68785ca13f19
Eonist <unicornhive@gmail.com>
2016-02-13 12:06:35 +0100

    Files modified: 1
    Modified 1 file:src/utils/group/SelectGroup.swift

--- src/utils/group/SelectGroup.swift
+++ src/utils/group/SelectGroup.swift
@@ -38,10 +38,10 @@ class SelectGroup:EventSender{
         if(event.type == SelectEvent.select){
             //Swift.print("SelectGroup.onEvent() ")
             //NSNotificationCenter.defaultCenter().postNotificationName(SelectGroupEvent.select, object:self/*DOnt forget you can put things inside: userInfo*/)/*bubbles:true because i.e: radioBulet may be added to RadioButton and radioButton needs to dispatch Select event if the SelectGroup is to work*/
-            self.event!(SelectGroupEvent(SelectGroupEvent.select,selected,self,self))
-            selected = event.origin as? ISelectable
+            self.event!(SelectGroupEvent(SelectGroupEvent.select,selected,self/*,self*/))
+            selected = event.immediate as? ISelectable
             SelectModifier.unSelectAllExcept(selected!, selectables);
-            self.event!(SelectGroupEvent(SelectGroupEvent.change,selected,self,self))
+            self.event!(SelectGroupEvent(SelectGroupEvent.change,selected,self/*,self*/))
             //NSNotificationCenter.defaultCenter().postNotificationName(, object:self)
         }
     }

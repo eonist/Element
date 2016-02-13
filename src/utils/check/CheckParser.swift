544c740c57ac7e79a4789dd255f66e895dbddb5d
Eonist <unicornhive@gmail.com>
2016-02-13 06:04:19 +0100

    Files modified: 1
    Modified 1 file:src/utils/check/CheckParser.swift

--- src/utils/check/CheckParser.swift
+++ src/utils/check/CheckParser.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class CheckParser {
-
+    /**
+     * Returns an ICheckable at a spessific index
+     */
+    func getCheckableAt(index:Int)->ICheckable? {// :TODO: consider moving in to util class or just write it up as a note
+        if(index <= checkables.count) {return checkables[index]}
+        else {fatalError("\(self)" + " no ICheckable at the index of: " + "\(index)")}
+        return nil
+    }
 }

c5e91b31b24590c05c398933e40034f554d014d1
Eonist <unicornhive@gmail.com>
2016-02-14 20:26:14 +0100

    Files modified: 1
    Modified 1 file:src/utils/ElementModifier.swift

--- src/utils/ElementModifier.swift
+++ src/utils/ElementModifier.swift
@@ -1,5 +1,15 @@
-import Foundation
+import Cocoa
 
 class ElementModifier {
-
+    /**
+     * @Note refloats @param view children that are of type IElement
+     * @Note i.e: after hideing of an element, or changing the depth order etc
+     */
+    class func floatChildren(view:NSView) {
+        let numChildren:Int = view.subviews.count;
+        for (var i : Int = 0; i < numChildren; i++) {
+            let child:NSView = view.subviews[i]
+            if(child is IElement) {SkinModifier.float((child as! IElement).skin as! Skin)}
+        }
+    }
 }

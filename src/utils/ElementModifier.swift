93cb16c63156f6902ab28f6cd16c0ee6e487480b
Eonist <unicornhive@gmail.com>
2016-02-14 20:54:02 +0100

    Files modified: 1
    Modified 1 file:src/utils/ElementModifier.swift

--- src/utils/ElementModifier.swift
+++ src/utils/ElementModifier.swift
@@ -8,12 +8,12 @@ class ElementModifier {
      */
     class func refresh(element:IElement) {
         if(element.skin!.style!.getStyleProperty("display") != nil && (element.skin!.style!.getStyleProperty("display")!.value as! String) == CSSConstants.none) {return} /*Skip refreshing*/
-        var container:NSView = element as! NSView//element is Window ? Window(element).view : element as NSView;
-        var numChildren:Int = container.subviews.count
+        let container:NSView = element as! NSView//element is Window ? Window(element).view : element as NSView;
+        let numChildren:Int = container.subviews.count
         for (var i : Int = 0; i < numChildren; i++) {
-            var child:NSView = container.getChildAt(i)
+            let child:NSView = container.subviews[i]
             if(child is IElement) {
-                IElement(child).skin.setStyle(IElement(child).skin.style);/*Uses the setStyle since its faster than setSkin*/
+                //(child as! IElement).skin.setStyle((child as! IElement).skin.style);/*Uses the setStyle since its faster than setSkin*/
                 if(child is DisplayObjectContainer && DisplayObjectContainer(child).numChildren > 0) refresh(child as IElement);
             }
         }

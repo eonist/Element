c91bde5a3f1ceb328f848d6ba9fd9d5bfeb11479
Eonist <unicornhive@gmail.com>
2016-02-15 08:00:18 +0100

    Files modified: 1
    Modified 1 file:src/utils/select/SelectModifier.swift

--- src/utils/select/SelectModifier.swift
+++ src/utils/select/SelectModifier.swift
@@ -4,10 +4,18 @@ class SelectModifier {
     /**
      * Unselects all in @param items except @param target
      */
-    class func unSelectAllExcept(exceptionItem:ISelectable, _ aSelectables:AnyObject) {// :TODO: refactor this function// :TODO: rename to unSelectAllExcept
-        var selectables:Array<ISelectable> = []
-        if(aSelectables is NSView) {selectables = SelectParser.selectables(aSelectables as! NSView)}
+    class func unSelectAllExcept(exceptionItem:ISelectable, _ aSelectables:Array<ISelectable>) {// :TODO: refactor this function// :TODO: rename to unSelectAllExcept
+        
+        if(aSelectables is NSView) {}
         else if(aSelectables is Array<ISelectable>) {selectables = aSelectables as! Array<ISelectable>}
         for selectable : ISelectable in selectables {if(selectable !== exceptionItem && selectable.isSelected) {selectable.setSelected(false)}}
+    }
+}
+extension SelectModifier{
+    /**
+     * 
+     */
+    class func unSelectAllExcept(exceptionItem:ISelectable, _ view:NSView){
+        var selectables:Array<ISelectable> = selectables = SelectParser.selectables(aSelectables as! NSView)
     }
 }

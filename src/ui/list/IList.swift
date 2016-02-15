63ad89c16108b50e63d6e424cb3aa3596bbc3cf5
Eonist <unicornhive@gmail.com>
2016-02-15 06:30:47 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/IList.swift

--- src/ui/list/IList.swift
+++ src/ui/list/IList.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class IList {
-
+protocol IList {
+    func getItemsHeight():Number
+    func setSize(width : Number, height : Number) : void
+    func getClassType() : String
+    func get itemHeight() : Number
+    func get height() : Number
+    //----------------------------------
+    //  getters / setters
+    //----------------------------------
+    var dataProvider:DataProvider{get}
+    var lableContainer : {get}
 }

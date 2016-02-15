cfc6d4af6c973ecad35c71598183fbab78851a87
Eonist <unicornhive@gmail.com>
2016-02-15 06:46:29 +0100

    Files modified: 1
    Modified 1 file:src/utils/select/SelectParser.swift

--- src/utils/select/SelectParser.swift
+++ src/utils/select/SelectParser.swift
@@ -1,5 +1,10 @@
-import Foundation
+import Cocoa
 
 class SelectParser {
-
+    /**
+     * Returns an array from every child that is an ISelectable in @param displayObjectContainer
+     */
+    func selectables(displayObjectContainer:NSView)->Array<ISelectable> {
+        return DisplayObjectParser.childrenOfType(displayObjectContainer, ISelectable);
+    }
 }

0d0a578a0270f6a7a40757f556c27e2ec1fdafbe
Eonist <unicornhive@gmail.com>
2016-02-15 06:33:02 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/IList.swift

--- src/ui/list/IList.swift
+++ src/ui/list/IList.swift
@@ -1,14 +1,10 @@
-import Foundation
+import Cocoa
 
 protocol IList {
-    func getItemsHeight():Number
-    func setSize(width : Number, height : Number) : void
-    func getClassType() : String
-    func get itemHeight() : Number
-    func get height() : Number
-    //----------------------------------
-    //  getters / setters
-    //----------------------------------
+    func getItemsHeight()->CGFloat
+    //func setSize(width : CGFloat, height : CGFloat)
+    //var height : CGFloat{get}  //TODO:you can extend IElement
+    var itemHeight : CGFloat{get}
     var dataProvider:DataProvider{get}
-    var lableContainer : {get}
+    var lableContainer :NSView {get}
 }

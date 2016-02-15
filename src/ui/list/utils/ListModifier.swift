5854094c841e6f7dd8d9b7fa5dca56bd98447b40
Eonist <unicornhive@gmail.com>
2016-02-15 08:04:48 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/utils/ListModifier.swift

--- src/ui/list/utils/ListModifier.swift
+++ src/ui/list/utils/ListModifier.swift
@@ -5,8 +5,8 @@ class ListModifier {
      * Selects an item in the _itemContainer
      */
     class func selectAt(list:IList,_ index:Int) {
-        let selectable:ISelectable = list.lableContainer.subviews[index] as! ISelectable;
+        let selectable:ISelectable = list.lableContainer!.subviews[index] as! ISelectable;
         if(!selectable.isSelected) {selectable.setSelected(true)}
-        SelectModifier.unSelectAllExcept(selectable, list.lableContainer)
+        SelectModifier.unSelectAllExcept(selectable, list.lableContainer!)
     }
 }

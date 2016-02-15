d905acceb38d1b9c7233f694b5590f4c78c6bf80
Eonist <unicornhive@gmail.com>
2016-02-15 07:55:37 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/IList.swift

--- src/ui/list/IList.swift
+++ src/ui/list/IList.swift
@@ -1,10 +1,10 @@
 import Cocoa
 
 protocol IList {
-    func getItemsHeight()->CGFloat
+    //func getItemsHeight()->CGFloat
     //func setSize(width : CGFloat, height : CGFloat)
     //var height : CGFloat{get}  //TODO:you can extend IElement
     var itemHeight : CGFloat{get}
     var dataProvider:DataProvider{get}
-    var lableContainer :NSView {get}
+    var lableContainer :Container? {get}
 }

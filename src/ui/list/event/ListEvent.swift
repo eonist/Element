f4e2617aa1e253373c92ad9d3e9ce7585b2a1245
Eonist <unicornhive@gmail.com>
2016-02-15 06:59:52 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/event/ListEvent.swift

--- src/ui/list/event/ListEvent.swift
+++ src/ui/list/event/ListEvent.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class ListEvent : Event{
-
+    private var selected : ISelectable
+    private var index : Int
+    init(_ type: String, _ origin: AnyObject) {
+        selected:ISelectable, 
+        
+    }
 }

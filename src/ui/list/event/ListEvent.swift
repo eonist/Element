0c315023a552ed61e45f0bd8397dc1b45877d5f3
Eonist <unicornhive@gmail.com>
2016-02-15 07:04:18 +0100

    Files modified: 2
    Modified 2 files:src/ui/list/List.swiftsrc/ui/list/event/ListEvent.swift

--- src/ui/list/event/ListEvent.swift
+++ src/ui/list/event/ListEvent.swift
@@ -4,9 +4,16 @@ class ListEvent : Event{
     
     private var index : Int
     init(_ type: String, _ index:Int, _ origin: AnyObject) {
-        //selected:ISelectable
-        //private var selected : ISelectable
         self.index = index
         super.init(type, origin)
+    }
+}
+extension ListEvent{
+    /**
+     * Convenience
+     * NOTE: Keeps the event light-weight by not referencing the item directly
+     */
+    var selected:ISelectable{
+        return (origin as! IList).lableContainer.subviews[index] as! ISelectable
     }
 }

196796e80ca09a02fb5c47f14e5dcb8f8c689f66
Eonist <unicornhive@gmail.com>
2016-02-15 06:46:29 +0100

    Files modified: 1
    Modified 1 file:src/utils/select/SelectModifier.swift

--- src/utils/select/SelectModifier.swift
+++ src/utils/select/SelectModifier.swift
@@ -6,7 +6,7 @@ class SelectModifier {
      */
     class func unSelectAllExcept(exceptionItem:ISelectable, _ aSelectables:AnyObject) {// :TODO: refactor this function// :TODO: rename to unSelectAllExcept
         var selectables:Array<ISelectable> = []
-        if(selectables is DisplayObjectContainer) {selectables = SelectParser.selectables(selectables)}
+        if(selectables is NSView) {selectables = SelectParser.selectables(selectables)}
         else if((selectables is Array<ISelectable>) == false) {selectables = aSelectables as! Array<ISelectable>}
         
         for selectable : ISelectable in selectables {if(selectable !== exceptionItem && selectable.isSelected) {selectable.setSelected(false)}}

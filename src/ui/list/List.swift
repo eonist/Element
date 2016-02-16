908f72e804e2555fd41a11c1f48c07f50fce9c6c
Eonist <unicornhive@gmail.com>
2016-02-16 19:40:27 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/List.swift

--- src/ui/list/List.swift
+++ src/ui/list/List.swift
@@ -65,7 +65,7 @@ class List : Element,IList{
                 break;
             default:fatalError("event type not supported"); break;
         }
-        ElementModifier.floatChildren(lableContainer!)
+        ElementModifier.floatChildren(lableContainer!)/*this call re-floats the list items*/
     }
     /**
      * This is called when a item in the _lableContainer has dispatched the ButtonEvent.TRIGGER_DOWN event

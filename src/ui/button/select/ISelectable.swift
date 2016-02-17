6b96c23dc657b968321dc19cc9126ca0ec10d25d
Eonist <unicornhive@gmail.com>
2016-02-17 07:48:03 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/select/ISelectable.swift

--- src/ui/button/select/ISelectable.swift
+++ src/ui/button/select/ISelectable.swift
@@ -2,5 +2,5 @@ import Foundation
 
 protocol ISelectable: class/*derive only classes for the protocol, not structs, this enables === operator of protocol*/{
     func setSelected(isSelected:Bool)
-    var isSelected:Bool{get}/*This is named isSelected because selected is ocupied by obc and using selected() as a method seems inconsistent*/
+    /*using isSelcted could be the wrong way to go about this-> getSelected would work better->*/var isSelected:Bool{get}/*This is named isSelected because selected is ocupied by obc and using selected() as a method seems inconsistent*/
 }

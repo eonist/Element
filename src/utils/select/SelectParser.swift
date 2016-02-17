8d26d9e18da210e51532cd9d5a42b1356bcfaef7
Eonist <unicornhive@gmail.com>
2016-02-17 07:40:51 +0100

    Files modified: 1
    Modified 1 file:src/utils/select/SelectParser.swift

--- src/utils/select/SelectParser.swift
+++ src/utils/select/SelectParser.swift
@@ -2,6 +2,15 @@ import Cocoa
 
 class SelectParser {
     /**
+     * Returns the  first selected ISelectable in an array of ISelectables or a NSView with ISelectables
+     */
+    class func selected(scope:*):ISelectable {
+        if(scope as NSView) scope = selectables(scope);
+        else if((scope as Array) == false) throw new IllegalOperationError(scope+" type not supported");
+        for each (var selectable : ISelectable in scope) if(selectable.selected) return selectable;
+        return nil;
+    }
+    /**
      * Returns an array from every child that is an ISelectable in @param displayObjectContainer
      */
     class func selectables(view:NSView)->Array<ISelectable> {

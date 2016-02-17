6f2fc29d977cd01f791d45b271e0a48b6e138e4f
Eonist <unicornhive@gmail.com>
2016-02-17 06:45:09 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/utils/ListParser.swift

--- src/ui/list/utils/ListParser.swift
+++ src/ui/list/utils/ListParser.swift
@@ -1,5 +1,12 @@
-import Foundation
+import Cocoa
 
 class ListParser {
-
+    /**
+    * Returns a View instance from @param dataProviderItem
+    * @Note: This is a way to get to the actual "Buttons" directly
+    */
+    func label(list:IList,dataProviderItem:Dictionary<String,String>) -> NSView {// :TODO: rename to toItem and maybe move to Listparser, dataProviderItemToItem? ListUtils
+        let itemIndex:Int = list.dataProvider.getItemIndex(dataProviderItem)
+        return list.lableContainer!.subviews[itemIndex]
+    }
 }

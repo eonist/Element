8040e2a8a2742eb165c184505619ddb5d051bbd9
Eonist <eonist@users.noreply.github.com>
2016-02-17 07:05:11 +0100

    Update CSSLinkResolver.swift

--- src/css/utils/CSSLinkResolver.swift
+++ src/css/utils/CSSLinkResolver.swift
@@ -1,6 +1,6 @@
 import Foundation
 /*
-* TODO: rename this class to CSSPointerResolver? Since it can be confused with resolving css displayObjectContainer urls?
+* TODO: rename this class to CSSPointerResolver? Since it can be confused with resolving css View container urls?
 * EXAMPLE: Swift.print(CSSLinkResolver.resolveLinks("Button{fill:<ButtonBase>;} ButtonBase{fill:green;line:blue;} CheckButton{line:<ButtonBase>;}"))//Button{fill:green;} ButtonBase{fill:green;line:blue;} CheckButton{line:blue;}
 */
 class CSSLinkResolver {

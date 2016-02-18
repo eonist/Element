5079aebc93ce2dc840ab70d920c7572ed57efab4
Eonist <unicornhive@gmail.com>
2016-02-18 17:14:06 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -8,7 +8,6 @@ class Button:Element {
         //Swift.print("Button.init()")
         super.init(width, height, parent, id)
         //addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
-        
     }
     /**
      * Handles actions and drawing states for the mouseEntered event.
@@ -79,7 +78,7 @@ class Button:Element {
         super.onEvent(ButtonEvent(ButtonEvent.up,self/*,self*/))
     }
     override func hitTest(aPoint: NSPoint) -> NSView? {
-        Swift.print("\(self.dynamicType)"+".hitTest() aPoint: " + "\(aPoint)")
+        //Swift.print("\(self.dynamicType)"+".hitTest() aPoint: " + "\(aPoint)")
         return super.hitTest(aPoint)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}

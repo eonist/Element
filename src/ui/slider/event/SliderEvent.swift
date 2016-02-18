23d68bb6436b14ad1ba6e61e9e1cf53052456c18
Eonist <unicornhive@gmail.com>
2016-02-18 20:00:52 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/event/SliderEvent.swift

--- src/ui/slider/event/SliderEvent.swift
+++ src/ui/slider/event/SliderEvent.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class SliderEvent {
+class SliderEvent :Event{
 
+}
+extension ListEvent{
+    /**
+     * Convenience
+     * NOTE: Keeps the event light-weight by not referencing the item directly
+     */
+    var selected:ISelectable{
+        return (origin as! IList).lableContainer!.subviews[index] as! ISelectable
+    }
 }

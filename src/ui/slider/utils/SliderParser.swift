df5fe82140da8c3f78082e8914eda4f4d13468db
Eonist <unicornhive@gmail.com>
2016-02-19 19:10:47 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/utils/SliderParser.swift

--- src/ui/slider/utils/SliderParser.swift
+++ src/ui/slider/utils/SliderParser.swift
@@ -1,13 +1,17 @@
 import Foundation
 
 class SliderParser {
-    //continue here:
-    
-    /*
-    assertSliderVisibility,
-    interval,
-    progress,
-    y,
-    interval;
-    */
+    /**
+     * Returns a slider thumbSize
+     * @param scalar
+     * @param sliderSize (thumbWidth or thumbHeight)
+     * @param scrollBarSize (scrollBarWidth/scrollBarHeight)
+     * @Note: Makes sure that the slider thumb is never to small or to big.
+     */
+    class func thumbSize(scalar:Number, sliderSize:Number)->CGFloat {
+        scalar = Math.min(scalar,1);
+        var size:CGFloat = Math.round(sliderSize * scalar);
+        size = Math.max(size,Math.round(sliderSize/10));/*Makes sure thumbsize isnt smaller than a 10th of the slidersize*/
+        return size;
+    }
 }

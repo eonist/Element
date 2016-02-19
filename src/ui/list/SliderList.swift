eadef9b689ebff1ef2b7082840278e8c072a234b
Eonist <unicornhive@gmail.com>
2016-02-19 20:46:00 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/SliderList.swift

--- src/ui/list/SliderList.swift
+++ src/ui/list/SliderList.swift
@@ -3,16 +3,14 @@ import Cocoa
 class SliderList : List{
     private var slider:VSlider?
     private var sliderInterval:CGFloat?
-    
     override func resolveSkin() {
         super.resolveSkin()
         sliderInterval = floor(ListParser.itemsHeight(self) - height)/itemHeight;// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
         slider = addSubView(VSlider(itemHeight,height,0,0,self)) as? VSlider
         let thumbHeight:CGFloat = SliderParser.thumbSize(height/ListParser.itemsHeight(self), slider!.height);
         slider!.setThumbHeightValue(thumbHeight);
-        ElementModifier.hide(slider, ListParser.itemsHeight(self) > slider!.height)/*<--new adition*/
+        ElementModifier.hide(slider!, ListParser.itemsHeight(self) > slider!.height)/*<--new adition*/
     }
-    
     override func scrollWheel(theEvent: NSEvent) {
         Swift.print("theEvent: " + "\(theEvent)")
         super.scrollWheel(theEvent)

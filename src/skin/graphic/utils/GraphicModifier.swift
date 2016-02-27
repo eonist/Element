222a0a6ab95fef3d16262444102e77d07f275e9a
Eonist <unicornhive@gmail.com>
2016-02-27 13:13:39 +0100

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/GraphicModifier.swift

--- src/skin/graphic/utils/GraphicModifier.swift
+++ src/skin/graphic/utils/GraphicModifier.swift
@@ -10,23 +10,4 @@ class GraphicModifier {
         decoratable.graphic.lineOffsetType = offsetType;
         return decoratable;
     }
-    /**
-     * Finalizes the fill style to the path
-     * NOTE: before it was all bundeled together in the size method, now its move here
-     */
-    class func stylize(path:CGPath, _ graphics:Graphics){
-        //Swift.print("GraphicModifier.stylize()")
-        //path.fill()
-        //path.stroke()
-        graphics.draw(path)//draw everything
-        graphics.stopFill()
-    }
-    /**
-     * Finalizes the stroke style to the path
-     * NOTE: before it was all bundeled together in the size method, now its move here
-     */
-    class func stylizeLine(path:CGPath, _ graphics:Graphics){
-        graphics.draw(path)//draw everything
-        graphics.stopStroke()
-    }
 }

088309580e29d7b61d0aadb7c3af1ec5e75999c9
Eonist <unicornhive@gmail.com>
2016-02-27 13:02:27 +0100

    Files deleted: 12
    Deleted 12 files:src/style/fill/FillStyle.swiftsrc/style/fill/GradientFillStyle.swiftsrc/style/fill/IFileStyle.swiftsrc/style/fill/IGradientFillStyle.swiftsrc/style/fill/utils/FillStyleModifier.swiftsrc/style/fill/utils/FillStyleParser.swiftsrc/style/line/GradientLineStyle.swiftsrc/style/line/IGradientLineStyle.swiftsrc/style/line/ILineStyle.swiftsrc/style/line/LineStyle.swiftsrc/style/line/utils/LineStyleModifier.swiftsrc/style/line/utils/LineStyleParser.swift

--- src/style/fill/FillStyle.swift
+++
@@ -1,8 +0,0 @@
-import Cocoa
-
-class FillStyle:IFillStyle {
-    var color:NSColor
-    init(_ color:NSColor = NSColor.clearColor()){
-        self.color = color
-    }
-}
--- src/style/fill/GradientFillStyle.swift
+++
@@ -1,9 +0,0 @@
-import Cocoa
-
-class GradientFillStyle:FillStyle,IGradientFillStyle{
-    var gradient:IGradient/*IGradient*///TODO:change to IGradient
-    init(_ gradient:IGradient/*IGradient*/, _ color:NSColor = NSColor.clearColor()){
-        self.gradient = gradient
-        super.init(color)
-    }
-}

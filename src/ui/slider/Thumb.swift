54bd9ae6b69ccf6d43affd7a9382ad68f2e309eb
Eonist <unicornhive@gmail.com>
2016-03-05 15:47:27 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/Thumb.swift

--- src/ui/slider/Thumb.swift
+++ src/ui/slider/Thumb.swift
@@ -30,6 +30,7 @@ class Thumb:Button{
     //animate from value to value with: duration and custom property or adhock arg method that you can set your self
     //should have 2 custom transition types: easeIn and easeOut (use log10 or regular easing multiplier, see book for this)
     //you should have a finished call-back method
+    //you should probably make a 
     
     /**
      *

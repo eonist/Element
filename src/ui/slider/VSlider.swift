19923624669ede71e10a58a90163cfa5af436c97
Eonist <unicornhive@gmail.com>
2016-03-05 13:39:49 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/VSlider.swift

--- src/ui/slider/VSlider.swift
+++ src/ui/slider/VSlider.swift
@@ -112,26 +112,7 @@ private class Utils{
         return max(0,min(progress,1))/*Ensures that progress is between 0 and 1 and if its beyond 0 or 1 then it is 0 or 1*/
     }
 }
-/**
- * NOTE: You might need to store the overshoot values for when you resize the button, could conflict if resize and progress changes at the same time, very edge case
- */
-class Thumb:Button{
-    override func getClassType() -> String {
-        return String(Button)
-    }
-    /**
-     * This method facilitates the illusion that the sliderThumb overshoots. As apart of the rubberBand effect
-     */
-    func applyOvershot(progress:CGFloat){
-        if(progress < 0){//top overshot
-            self.skin!.setSize(width, height-(height*abs(progress)))
-        }else if(progress > 1){//bottom overshot
-            let overshot = height*(progress-1)
-            self.skin!.setSize(width, height - overshot)
-            (self.skin! as! Skin).frame.y = overshot
-        }
-    }
-}
+
 /*
 class Thumb:Button2{
     override init(_ width: CGFloat, _ height: CGFloat) {

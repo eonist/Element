66214f6309c794bf3e92970efaf91137e7108905
Eonist <unicornhive@gmail.com>
2016-03-06 08:38:28 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/Thumb.swift

--- src/ui/slider/Thumb.swift
+++ src/ui/slider/Thumb.swift
@@ -90,7 +90,8 @@ class Animator{
         method(val)//call the property method
         if(currentFrameCount == framesToEnd){
             Swift.print("end of anim")/*when the count becomes 0 the frame ticker stops*/
-            ArrayModifier.remove(&view.animators, self)
+            
+            ArrayModifier.removeAt(1)view.animators.indexOf(self)
         }
         self.currentFrameCount++
         //call the method here

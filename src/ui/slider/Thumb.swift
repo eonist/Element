a6d0227e5033b3794157fa0d1eb305e632e2979c
Eonist <unicornhive@gmail.com>
2016-03-06 11:58:40 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/Thumb.swift

--- src/ui/slider/Thumb.swift
+++ src/ui/slider/Thumb.swift
@@ -58,7 +58,7 @@ class Animator{
         Swift.print("onFrame()")
         //var val:CGFloat = NumberParser.interpolate(from, to, currentFrameCount / framesToEnd)//interpolates the value
         //val += Easing.easeOut(val, from, to)
-        val = Easing.easeInSine(0, from, to-from, framesToEnd)
+        val = Easing.easeInSine(currentFrameCount, from, to-from, framesToEnd)
         Swift.print("val: " + "\(val)")
         method(val)//call the property method
         if(currentFrameCount == framesToEnd){

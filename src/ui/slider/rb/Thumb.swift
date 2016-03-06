8765628099b5eadc8a9e15b872ae006baafa74ee
Eonist <unicornhive@gmail.com>
2016-03-06 14:29:26 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/rb/Thumb.swift

--- src/ui/slider/rb/Thumb.swift
+++ src/ui/slider/rb/Thumb.swift
@@ -1,5 +1,27 @@
-import Foundation
+import Cocoa
 
-class Thumb {
-
+/**
+ * NOTE: You might need to store the overshoot values for when you resize the button, could conflict if resize and progress changes at the same time, very edge case
+ */
+class Thumb:Button{
+    let fps:CGFloat = 60
+    var duration:CGFloat?/*in seconds*/
+    var framesToEnd:CGFloat?
+    var currentFrameCount:CGFloat = 0
+    
+    override func getClassType() -> String {
+        return String(Button)
+    }
+    /**
+     * This method facilitates the illusion that the sliderThumb overshoots. As apart of the rubberBand effect
+     */
+    func applyOvershot(progress:CGFloat){
+        if(progress < 0){//top overshot
+            self.skin!.setSize(width, height-(height*abs(progress)))
+        }else if(progress > 1){//bottom overshot
+            let overshot = height*(progress-1)
+            self.skin!.setSize(width, height - overshot)
+            (self.skin! as! Skin).frame.y = overshot
+        }
+    }
 }

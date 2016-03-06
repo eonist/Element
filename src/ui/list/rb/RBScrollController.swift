aa21ad5eb9ff029a8ce1cf67edabeaff4ebf86ed
Eonist <unicornhive@gmail.com>
2016-03-06 19:18:19 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/rb/RBScrollController.swift

--- src/ui/list/rb/RBScrollController.swift
+++ src/ui/list/rb/RBScrollController.swift
@@ -67,7 +67,7 @@ class RBScrollController {
             mover.velocity = velocity/*set the mover velocity to the current mouse gesture velocity, the reason this cant be additive is because you need to be more immediate when you change direction, this could be done vy assering last direction but its not a priority atm*///td try the += on the velocity with more rects to see its effect
             CVDisplayLinkStart(view.displayLink)//'start the frameTicker here, do this part in parent view or use event or Selector
         }else{/*stationary*/
-            CVDisplayLinkStart(view.displayLink)//this needs to start if you in the overshoot areas
+            CVDisplayLinkStart(view.displayLink)//this needs to start if you in the overshoot areas, if its not in the overshoot area it will just stop after a frame tick
             if((view as! RBSliderList).slider?.thumb?.getSkinState() == SkinStates.none){
                 (view as! RBSliderList).slider?.thumb?.fadeOut()
             }

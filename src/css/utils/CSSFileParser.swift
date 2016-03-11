39a5edcc5cecc077599a5b53521c1c7accfb9f8d
Eonist <unicornhive@gmail.com>
2016-03-11 13:15:09 +0100

    Files modified: 1
    Modified 1 file:src/css/utils/CSSFileParser.swift

--- src/css/utils/CSSFileParser.swift
+++ src/css/utils/CSSFileParser.swift
@@ -50,7 +50,8 @@ class CSSFileParser {
     class func separateImportsAndStyles(cssString:String)->(imports:String,style:String){// :TODO: rename to filter or split maybe?
         //^(?P<import>[@\(\)\w\040\.\/";\n]*?(?=(\n[\w\040\[\]\,\#\:\.]+?\{)|$))?(?P<style>.+?$)?
         let importChars:String = "[@\\(\\)\\w\\040\\.\\/\"\\;\n]"
-        let importGroup:String = "(*?(?=(\n[\\w\\040\\[\\]\\,\\#\\:\\.]+?\\{|$))"
+        let 
+        let importGroup:String = "(" + importChars + "*?(?=(\n" + + "+?\\{|$))"
         let styleGroup:String = "(.+?$)"
         let pattern:String = "^" + importGroup + "?" + styleGroup + "?"
         var result:(imports:String,style:String) = ("","")

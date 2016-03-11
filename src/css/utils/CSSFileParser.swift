759d090fe934c8d8d27cfa448391a3964e800cc1
Eonist <unicornhive@gmail.com>
2016-03-11 17:09:31 +0100

    Files modified: 1
    Modified 1 file:src/css/utils/CSSFileParser.swift

--- src/css/utils/CSSFileParser.swift
+++ src/css/utils/CSSFileParser.swift
@@ -12,16 +12,16 @@ class CSSFileParser {
     * @param cssString the recursive string passed down the hierarchy
     */
     class func cssString(url:String)->String {
-        var cssString:String = "";
         var string:String = FileParser.content(url.tildePath)!//TODO: you need to make a tilePath assert
         //cssString = string//temp fix until you implement the recusrive import stuff bellow
         //Swift.print("string: " + "\(string)");
-        //string = RegExpModifier.removeComments(cssString);
+        string = RegExpModifier.removeComments(string);
         let importsAndStyles = CSSFileParser.separateImportsAndStyles(string);
         Swift.print("importsAndStyles.imports: " + "\(importsAndStyles.imports)")
         let importStrings:Array<String> = CSSFileParser.importStrings(importsAndStyles.imports);
         let path:String = StringParser.path(url);
         Swift.print("path: " + "\(path)")
+        var cssString:String = "";
         for importString in importStrings{ cssString += CSSFileParser.cssString(path+importString)}// :TODO: make an if clause tha makes sure it doesnt import it self like path+import != url
         cssString += importsAndStyles.style
         return cssString

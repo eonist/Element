01073466b595f39dc2d23960c3a945a64de8b851
Eonist <unicornhive@gmail.com>
2016-03-12 13:04:01 +0100

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyParser.swift

--- src/style/property/utils/StylePropertyParser.swift
+++ src/style/property/utils/StylePropertyParser.swift
@@ -319,11 +319,14 @@ private class Utils{
     */
     class func totalWidth(element:IElement)->CGFloat {/*beta*/
         if(element.skin != nil){
+            Swift.print("works")
             let margin:Margin = SkinParser.margin(element.skin!)
             let border:Border = SkinParser.border(element.skin!)
             let padding:Padding = SkinParser.padding(element.skin!)
             let width:CGFloat = element.getWidth();/*StylePropertyParser.height(element.skin);*/
-            return margin.left + border.left + padding.left + width - padding.right - border.right - margin.right/*Note used to be + padding.right + border.right + margin.right*/
+            let tot:CGFloat = margin.left + border.left + padding.left + width - padding.right - border.right - margin.right
+            Swift.print("tot: " + "\(tot)")
+            return tot/*Note used to be + padding.right + border.right + margin.right*/
         }else {return element.getWidth()}
     }
     /**

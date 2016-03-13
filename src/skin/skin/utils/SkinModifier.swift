58d9247e94675007e7b9bd08c5dcf7695933062c
Eonist <unicornhive@gmail.com>
2016-03-13 16:21:17 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/SkinModifier.swift

--- src/skin/skin/utils/SkinModifier.swift
+++ src/skin/skin/utils/SkinModifier.swift
@@ -40,6 +40,7 @@ class SkinModifier {// :TODO: consider renaming to ElementModifier (or a better 
         if(skin.element is Text){
             Swift.print("parentTopLeft: " + "\(parentTopLeft)")
             Swift.print("leftSiblingSkin?.element.y: " + "\(leftSiblingSkin?.element!.y)")
+            Swift.print("leftSiblingSkin?.element!: " + "\(leftSiblingSkin?.element!)")
         }
         //if(skin.element!.id == "box2"){/*Swift.print("leftSiblingSkin: " + "\(leftSiblingSkin)")*/}//<--this is how you debug the floating system
         //if(skin is TextSkin){Swift.print("float() leftSiblingSkin.height:" + "\(leftSiblingSkin?.height)" + " clearType: " + "\(clearType)")}//<- or you can debug like this

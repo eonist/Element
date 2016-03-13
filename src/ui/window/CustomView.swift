63d7e9684c06962b223b1efd5a897de5a4fa459a
Eonist <unicornhive@gmail.com>
2016-03-13 09:00:32 +0100

    New files added: 1
    Added 1 file:src/ui/window/CustomView.swift

---
+++ src/ui/window/CustomView.swift
@@ -0,0 +1,80 @@
+import Cocoa
+
+class CustomView:WindowView{
+    var section:Section?
+    var closeButton:Button?
+    var minimizeButton:Button?
+    var maximizeButton:Button?
+    /**
+     * Add content here
+     */
+    override func resolveSkin() {
+        super.resolveSkin()
+        createTitleBar()
+    }
+    /**
+     * Adds close button, min, max
+     */
+    func createTitleBar(){
+        //Swift.print("CustomView.createTitleBar()")
+        StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
+        section = self.addSubView(Section(75,16,self,"titleBar"))
+        closeButton = section!.addSubView(Button(0,0,section!,"close")) /*<--TODO: the w and h should be NaN, test if it supports this*/
+        minimizeButton = section!.addSubView(Button(0,0,section!,"minimize"))
+        maximizeButton = section!.addSubView(Button(0,0,section!,"maximize")) 
+        
+    }
+    /**
+     *
+     */
+    override func setSize(width:CGFloat,_ height:CGFloat){
+        //Swift.print("CustomView.setSize() size: " + "\(size)")
+        //self.skin!.setSize(size.width, size.height)
+        super.setSize(width, height)
+        section!.setSize(width, section!.height)
+    }
+    /**
+     *
+     */
+    func onCloseButtonReleaseInside() {
+        Swift.print("onCloseButtonReleaseInside")
+        //Close window here
+        //self.window?.close()//this closes the window
+        NSApp.terminate(self)//quits the app
+    }
+    /**
+     *
+     */
+    func onMinimizeButtonReleaseInside(){
+        Swift.print("onMinimizeButtonReleaseInside")
+        //minimize the window here
+        
+        //NSApp.miniaturizeAll(self)//minimizes all windows in the app
+        self.window?.miniaturize(self)
+    }
+    /**
+     * TODO: Add support for fullscreen mode aswell: window.setFrame(NSScreen.mainScreen()!.visibleFrame, display: true, animate: true)
+     * TODO: add support for zooming back to normal size
+     */
+    func onMaximizeButtonReleaseInside(){
+        Swift.print("onMaximizeButtonReleaseInside")
+        //maximize the window here
+        self.window?.zoom(self)
+    }
+    /**
+     *
+     */
+    func onTestButtonDown(){
+        Swift.print("onTestButtonDown")
+    }
+    /**
+     *
+     */
+    override func onEvent(event: Event) {
+        //Swift.print( "CustomView.onEvent() event:" + "\(event)")
+        //Swift.print("CustomView.onEvent: " + "\(event)" + " event.origin: " + "\(event.origin)")
+        if(event.origin === closeButton && event.type == ButtonEvent.upInside){onCloseButtonReleaseInside()}
+        else if(event.origin === minimizeButton && event.type == ButtonEvent.upInside){onMinimizeButtonReleaseInside()}
+        else if(event.origin === maximizeButton && event.type == ButtonEvent.upInside){onMaximizeButtonReleaseInside()}
+    }
+}

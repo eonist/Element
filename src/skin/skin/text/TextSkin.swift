51b761f0e414e53a2f02efea5ae2d1ce50121fd0
Eonist <unicornhive@gmail.com>
2016-03-14 17:08:28 +0100

    Files modified: 1
    Modified 1 file:src/skin/skin/text/TextSkin.swift

--- src/skin/skin/text/TextSkin.swift
+++ src/skin/skin/text/TextSkin.swift
@@ -66,7 +66,7 @@ class TextSkin:Skin,ITextSkin{
      * // :TODO: make a similar funciton for getHeight, based on needed space for the height of the textfield
      */
     override func getWidth() -> CGFloat {
-        Swift.print("getWidth()")
+        //Swift.print("TextSkin.getWidth()")
         if((StylePropertyParser.value(self, TextFormatConstants.wordWrap) == nil)){/*if the wordWrap is false the the width of the skin is equal to the width of the textfield (based on needed space for the text)*/
             let padding:Padding = StylePropertyParser.padding(self);
             return textField.frame.width + padding.left + padding.right;

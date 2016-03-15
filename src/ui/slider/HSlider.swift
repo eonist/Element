7508aa536815a7eb9a14c0698e61822a068f11e0
Eonist <unicornhive@gmail.com>
2016-03-15 15:41:19 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/HSlider.swift

--- src/ui/slider/HSlider.swift
+++ src/ui/slider/HSlider.swift
@@ -4,7 +4,7 @@ import Foundation
  * @Note the reason we have two sliders instead of 1 is because otherwise the math and variable naming scheme becomes too complex (same goes for the idea of extending a Slider class)
  * // :TODO: consider having thumbWidth and thumbHeight, its just easier to understand
  */
-class HSlider {
+class HSlider :Element{
     var progress : CGFloat;
     var thumbWidth : CGFloat;
     var thumb : Button;
@@ -12,7 +12,15 @@ class HSlider {
     init(width:CGFloat, _ height:CGFloat, _ thumbWidth:CGFloat, progress:CGFloat = 0, parent:IElement? = nil, id:String? = nil, classId:String? = nil) {
         self.progress = progress
         self.thumbWidth = thumbWidth.isNaN ? height:thumbWidth
-        super.init(width, height, parent, id, classId)
-        addEventListeners()
+        super.init(width,height,parent,id)
     }
+
+    
+    override func resolveSkin() {
+        super.resolveSkin();
+        //skin.mouseEnabled = skin.buttonMode = false;
+        thumb = addSubView(Button(thumbWidth, height,false,false,self)) as! Button
+        //setProgress(progress);
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

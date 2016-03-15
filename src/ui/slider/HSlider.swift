3dfab7ba7dde44ed02c7d76d517d209235331656
Eonist <unicornhive@gmail.com>
2016-03-15 15:24:59 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/HSlider.swift

--- src/ui/slider/HSlider.swift
+++ src/ui/slider/HSlider.swift
@@ -1,5 +1,9 @@
 import Foundation
-
+/**
+ * HSlider is a simple horizontal slider
+ * @Note the reason we have two sliders instead of 1 is because otherwise the math and variable naming scheme becomes too complex (same goes for the idea of extending a Slider class)
+ * 
+ */
 class HSlider {
 
 }

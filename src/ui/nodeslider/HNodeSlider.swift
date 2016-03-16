6a4df55ff0920d73ce4301d13c50777db21dd39b
Eonist <unicornhive@gmail.com>
2016-03-16 14:24:20 +0100

    Files modified: 1
    Modified 1 file:src/ui/nodeslider/HNodeSlider.swift

--- src/ui/nodeslider/HNodeSlider.swift
+++ src/ui/nodeslider/HNodeSlider.swift
@@ -53,6 +53,12 @@ class HNodeSlider:Element {
     func onEndNodeUp(event : MouseEvent)  {
         if(globalMouseMovedHandeler != nil){NSEvent.removeMonitor(globalMouseMovedHandeler!)}//we remove a global mouse move event listener
     }
+    override func onEvent(event: Event) {
+        //Swift.print("\(self.dynamicType)" + ".onEvent() event: " + "\(event)")
+        if(event.origin === startNode && event.type == ButtonEvent.down){onStartNodeDown()}//if thumbButton is down call onThumbDown
+        else if(event.origin === startNode && event.type == ButtonEvent.up){onStartNodeUp()}//if thumbButton is down call onThumbUp
+        //super.onEvent(event)/*forward events, or stop the bubbeling of events by commenting this line out*/
+    }
     /**
      * @param progress (0-1)
      */

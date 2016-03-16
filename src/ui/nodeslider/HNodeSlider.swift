87192bee5c4d2e0e84e1ba18b36e0acc30ca7283
Eonist <unicornhive@gmail.com>
2016-03-16 12:37:46 +0100

    Files modified: 1
    Modified 1 file:src/ui/nodeslider/HNodeSlider.swift

--- src/ui/nodeslider/HNodeSlider.swift
+++ src/ui/nodeslider/HNodeSlider.swift
@@ -3,18 +3,18 @@ import Foundation
  * HorizontalNodeSlider is used when 2 sliders are need, as in section definition or zoom, or gradient values
  */
 class HNodeSlider:Element {
-    var startNode:SelectButton
-    var endNode:SelectButton
-    var selectGroup:SelectGroup
-    var nodeWidth:Number
-    var tempNodeMouseX:Number
-    var startProgress:Number
-    var endProgress:Number
+    var startNode:SelectButton?
+    var endNode:SelectButton?
+    var selectGroup:SelectGroup?
+    var nodeWidth:CGFloat
+    var tempNodeMouseX:CGFloat
+    var startProgress:CGFloat
+    var endProgress:CGFloat
     init(width:CGFloat = NaN, height:CGFloat = NaN, nodeWidth:CGFloat = NaN, startProgress:CGFloat = 0, endProgress:CGFloat = 1, parent:IElement? = nil, id:String? = nil, classId:String? = nil) {
-        _startProgress = startProgress;
-        _endProgress = endProgress;
-        _nodeWidth = isNaN(nodeWidth) ? height:nodeWidth;
-        super(width, height, parent, id,classId);
-        addEventListeners();
+        self.startProgress = startProgress
+        self.endProgress = endProgress
+        self.nodeWidth = isNaN(nodeWidth) ? height:nodeWidth
+        super.init(width, height, parent, id,classId)
+        
     }
 }

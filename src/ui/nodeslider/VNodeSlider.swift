4d84fae9af9d28b26213f6172eceb6e2e45b8ac3
Eonist <unicornhive@gmail.com>
2016-03-16 15:39:09 +0100

    Files modified: 1
    Modified 1 file:src/ui/nodeslider/VNodeSlider.swift

--- src/ui/nodeslider/VNodeSlider.swift
+++ src/ui/nodeslider/VNodeSlider.swift
@@ -1,5 +1,16 @@
 import Foundation
-
+/**
+ * HorizontalNodeSlider is used when 2 sliders are need, as in section definition or zoom, or gradient values
+ * // :TODO: to get the toFront method working you need to support relative positioning, currently the Element framework doesnt support this
+ * // :TODO: support for custom node shape design
+ * // :TODO: when implimenting selectgroup functionality you need to be able to change set the selection on press not on release (You might need to impliment NodeButton that impliments iselectable in order to make this happen)
+ */
 class VNodeSlider {
-
+    private var _startNode : SelectButton;
+    private var _endNode : SelectButton;
+    private var _selectGroup : SelectGroup;
+    private var _nodeHeight : Number;
+    private var _tempNodeMouseY : Number;
+    private var _startProgress : Number;
+    private var _endProgress : Number;
 }

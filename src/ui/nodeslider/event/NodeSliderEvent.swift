fd9b1fc52c56f1a57aa6ff9318799c51203857eb
Eonist <unicornhive@gmail.com>
2016-03-16 14:14:42 +0100

    Files modified: 1
    Modified 1 file:src/ui/nodeslider/event/NodeSliderEvent.swift

--- src/ui/nodeslider/event/NodeSliderEvent.swift
+++ src/ui/nodeslider/event/NodeSliderEvent.swift
@@ -1,5 +1,12 @@
 import Foundation
 
-class NodeSliderEvent {
-
+class NodeSliderEvent :Event{
+    static var change:String = "nodeSliderEventChange"
+    var startProgress:CGFloat
+    var endProgress:CGFloat
+    init(_ type: String, _ startProgress:CGFloat,_ endProgress:CGFloat, _ origin:AnyObject) {
+        self.startProgress = startProgress
+        self.endProgress = endProgress
+        super.init(type, origin)
+    }
 }

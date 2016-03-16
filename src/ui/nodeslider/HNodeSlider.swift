8c3f944c9dd2babcff7815523a65cfee09774357
Eonist <unicornhive@gmail.com>
2016-03-16 12:33:58 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:src/ui/nodeslider/HNodeSlider.swift

--- src/ui/nodeslider/HNodeSlider.swift
+++ src/ui/nodeslider/HNodeSlider.swift
@@ -1,5 +1,7 @@
 import Foundation
-
-class HNodeSlider {
+/**
+ * HorizontalNodeSlider is used when 2 sliders are need, as in section definition or zoom, or gradient values
+ */
+class HNodeSlider:Element {
     
 }

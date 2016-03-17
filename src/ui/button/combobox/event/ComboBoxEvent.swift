e696d19d5254de953472cc0e7622f23d3924f718
Eonist <unicornhive@gmail.com>
2016-03-17 10:35:43 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/combobox/event/ComboBoxEvent.swift

--- src/ui/button/combobox/event/ComboBoxEvent.swift
+++ src/ui/button/combobox/event/ComboBoxEvent.swift
@@ -1,18 +1,8 @@
-class ComboBoxEvent extends Event {
-	public static const click:String = "ComboBoxEventClick";
-	public static const HEADER_CLICK:String = "comboBoxEventHeaderClick";
-	public static const LIST_SELECT : String = "comboBoxEventListSelect";
-	
-	public function ComboBoxEvent(type:String, selected:ISelectable, bubbles:Boolean = false, cancelable:Boolean = false) {
-		_selected = selected;
-		super(type, bubbles, cancelable);
-	}
-}
 /**
- * NOTE: you can derive the slected via the combobox item it self. So no need to pass the selected along in the event. Unless this is easier
+ * NOTE: you can derive the slected via the combobox item it self. So no need to pass the selected along in the event
  */
 class ComboBoxEvent :Event{
-    static var click:String = "ComboBoxEventClick"
+    static var click:String = "comboBoxEventClick"
 	static var headerClick:String = "comboBoxEventHeaderClick"
 	static var listSelect:String = "comboBoxEventListSelect"
     /*private var _selected : ISelectable;*/

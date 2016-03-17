fd12d15f9d18efc8be276314b63a7af6825dbc78
Eonist <unicornhive@gmail.com>
2016-03-17 17:57:14 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/SliderTextArea.swift

--- src/ui/text/SliderTextArea.swift
+++ src/ui/text/SliderTextArea.swift
@@ -1,4 +1,12 @@
 class SliderTextArea{
+	/**
+	 * 13.05.21 15:00 - Added support for mousewheel, and replaced scrollbars with sliders
+	 * @Note it seems difficult to align the HSlider thumb in a relative but correct way, since offsetting the HSlider it self is difficult when thumb is positioned by absolute values
+	 * // :TODO: could possible use a setHtmlText function??!?
+	 * // :TODO: you may need to access gestures to get hold of horzontal mouse delta, when you want to use gestures to scroll horizontally
+	 * // :TODO: the horizontal scroller isnt thourghouly testes. Make sure you set wordwrap to false to test this, and that the input text has break tags \n or br tags
+	 * // :TODO: Impliment a failsafe so that the slider.thumb doesnt get smaller than its width, do the same for both sliders
+	 */
 	init(_ width:CGFloat,_ height:CGFloat, _ text:String = "defaultText", _ scrollBarSize:CGFloat = 24, _ parent:IElement? = nil, _ id:String? = nil){
 		
 	}		

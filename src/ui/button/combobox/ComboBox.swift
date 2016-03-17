60bf1b37dda3246101658852edb73ce99c733d8d
Eonist <unicornhive@gmail.com>
2016-03-17 10:17:59 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/combobox/ComboBox.swift

---
+++ src/ui/button/combobox/ComboBox.swift
@@ -0,0 +1,13 @@
+/** 
+ * @Note for multiSelect option make MultiCheckComboBox.as aand CheckComboBox?
+ * @Note: to get the height while the list is pulled down: comboBox.height * comboBox.maxShowingItems
+ * // :TODO: add isScrollBarVisible as an argument at the end, butbefore, parent and name
+ * // :TODO: add a way to set the init selected list item, and have this update the header, (if headerText != null that is)
+ * // :TODO: add height as an argument to the constructor
+ * // :TODO: find a way to add a mask that can have rounded corners, if a TextButton has a square fill then it overlaps outside the combobox
+ * //closeOnClick
+ * //defaultText
+ */
+class ComboBox{
+	
+}

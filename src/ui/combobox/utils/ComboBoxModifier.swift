e0f569dd73709cf90904a934edb2d441b8c973df
Eonist <unicornhive@gmail.com>
2016-03-17 13:43:44 +0100

    Files modified: 1
    Modified 1 file:src/ui/combobox/utils/ComboBoxModifier.swift

--- src/ui/combobox/utils/ComboBoxModifier.swift
+++ src/ui/combobox/utils/ComboBoxModifier.swift
@@ -4,16 +4,16 @@ class ComboBoxModifier{
 		 * 
 		 */
 		class func selectByProperty(comboBox:ComboBox,_ property:String) {
-			var index:Int = comboBox.list.dataProvider.getItemIndex(DataProviderParser.itemByProperty(comboBox.list.dataProvider, property))
-			ListModifier.selectAt(comboBox.list,index)
-			var text:String = ListParser.selectedTitle(comboBox.list)
-			comboBox.headerButton!.setText(text)
+			var index:Int = comboBox.list!.dataProvider.getItemIndex(DataProviderParser.itemByProperty(comboBox.list.dataProvider, property))
+			ListModifier.selectAt(comboBox.list!,index)
+			var text:String = ListParser.selectedTitle(comboBox.list!)
+			comboBox.headerButton!.setTextValue(text)
 		}
 		/**
 		 * // :TODO: implement asserting that the title exists
 		 */
 		class func select(comboBox:ComboBox,_ title:String) {
 			ListModifier.select(comboBox.list!, title)
-			comboBox.headerButton.setTextValue(title)
+			comboBox.headerButton!.setTextValue(title)
 		}
 }

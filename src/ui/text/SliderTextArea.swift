79635f3235ba3b3f87ac25824510d44fde8d6d5f
Eonist <unicornhive@gmail.com>
2016-03-17 17:55:15 +0100

    Files modified: 1
    Modified 1 file:src/ui/text/SliderTextArea.swift

--- src/ui/text/SliderTextArea.swift
+++ src/ui/text/SliderTextArea.swift
@@ -1,7 +1,5 @@
 class SliderTextArea{
-	init(){
+	init(_ width:CGFloat,_ height:CGFloat, _ text:String = "defaultText", _ scrollBarSize:CGFloat = 24, _ parent:IElement? = nil, _ id:String? = nil){
 		
-	}
-		
-		
+	}		
 }

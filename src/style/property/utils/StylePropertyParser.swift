1374d9f5d483a730f81b2035b6a5484ec1b98836
Eonist <unicornhive@gmail.com>
2016-03-17 17:20:58 +0100

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyParser.swift

--- src/style/property/utils/StylePropertyParser.swift
+++ src/style/property/utils/StylePropertyParser.swift
@@ -202,9 +202,15 @@ class StylePropertyParser{
         let value:Any? = self.value(skin, CSSConstants.offset, depth);
         Swift.print("StylePropertyParser.offset.value: " + "\(value)")
         if(value == nil){return CGPoint(0,0)}//<---temp solution
-        var array:Array<CGFloat> = value is CGFloat ? [value as! CGFloat] : value as! Array<CGFloat>
+        var array:Array<CGFloat>
+        if(value is CGFloat){
+            array = [value as! CGFloat]
+        }else{
+            array = [value[0],value[1]]
+        }
         
-        if((val is CGFloat) || (val is Array<Any>)) {/*(val is String) ||*/fillet = LayoutUtils.instance(val!, Fillet.self) as! Fillet}
+        
+        
         
         //Swift.print("StylePropertyParser.offset.array.count: " + "\(array.count)")
         return array.count == 1 ? CGPoint(array[0],0) : CGPoint(array[0], array[1]);

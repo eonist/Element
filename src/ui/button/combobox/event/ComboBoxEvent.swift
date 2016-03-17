bba1ed5093ec0daeb6826708aa1784c4125e282a
Eonist <unicornhive@gmail.com>
2016-03-17 10:33:48 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/combobox/event/ComboBoxEvent.swift

--- src/ui/button/combobox/event/ComboBoxEvent.swift
+++ src/ui/button/combobox/event/ComboBoxEvent.swift
@@ -8,7 +8,9 @@ class ComboBoxEvent extends Event {
 		super(type, bubbles, cancelable);
 	}
 }
-
+/**
+ * NOTE: you can derive the slected via the combobox item it self. So no need to pass the selected along in the event. Unless this is easier
+ */
 class ComboBoxEvent :Event{
     static var click:String = "ComboBoxEventClick"
 	static var headerClick:String = "comboBoxEventHeaderClick"
@@ -16,5 +18,15 @@ class ComboBoxEvent :Event{
     /*private var _selected : ISelectable;*/
     init(_ type: String, _ origin: AnyObject) {
         super.init(type, origin)
+    }
+}
+extension ListEvent{
+    /**
+     * Convenience
+     * NOTE: Keeps the event light-weight by not referencing the item directly
+     * NOTE: you may have to reconsider this as the selected item may have de-selected before the event arrives (think cpu threads etc)
+     */
+    var selected:ISelectable{
+        return (origin as! ComboBox).list.lableContainer!.subviews[index] as! ISelectable
     }
 }

e7297767c9769801990c752526cdd4271a07a22a
Eonist <unicornhive@gmail.com>
2016-03-20 12:58:02 +0100

    Files modified: 1
    Modified 1 file:src/ui/window/CustomView.swift

--- src/ui/window/CustomView.swift
+++ src/ui/window/CustomView.swift
@@ -26,6 +26,18 @@ class CustomView:WindowView{
     /**
      *
      */
+    func createCustomTitleBar() {
+        StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
+        StyleManager.addStyle("Section#titleBar{padding-top:16px;padding-left:12px;}")
+        
+        section = leftSideBar!.addSubView(Section(75,16,leftSideBar,"titleBar"))
+        closeButton = section!.addSubView(Button(0,0,section!,"close"))/*<--TODO: the w and h should be NaN, test if it supports this*/
+        minimizeButton = section!.addSubView(Button(0,0,section!,"minimize"))
+        maximizeButton = section!.addSubView(Button(0,0,section!,"maximize"))
+    }
+    /**
+     *
+     */
     override func setSize(width:CGFloat,_ height:CGFloat){
         //Swift.print("CustomView.setSize() size: " + "\(size)")
         //self.skin!.setSize(size.width, size.height)

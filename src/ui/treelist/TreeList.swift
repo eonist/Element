5c156fc78151803c593661f4e4077903a2d35cbc
Eonist <unicornhive@gmail.com>
2016-03-25 13:27:29 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/TreeList.swift

--- src/ui/treelist/TreeList.swift
+++ src/ui/treelist/TreeList.swift
@@ -60,14 +60,23 @@ class TreeList:Element/*,ITreeList*/ {
     * @Note the TreeList.addAt is for the internal workings of the Class, use TreeList.database.addAt to add new items
     */
     private func onDatabaseAddAt(event : NodeEvent) {
-        //			trace("onDatabaseAddAt() "+ this);
-        var parentIndex:Array = event.index.slice2(0,event.index.count-1);
-        var parentTreeList:ITreeList = TreeListParser.itemAt(self, parentIndex) as ITreeList//DisplayObjectParser.getAt(_itemContainer,event.index.slice(0,event.index.length-1)) as ITreeList;//this;//TreeListParser.itemAt(this,event.index) as ITreeList;
-        var item:DisplayObject = TreeListUtils.item(event.xml,parentTreeList.itemContainer,new Point(width, _itemHeight));
-        var itemIndex:Int = event.index[event.index.count-1];
-        parentTreeList.addItemAt(item,itemIndex);/*We could use TreeListModifier.addAt(parentTreeList, index, item) here but since we already have the parent since we need it when creating the item we can just use the addAt method directly*/
-        ElementModifier.floatChildren(itemContainer);/*Re aligns the entire treesturcture*/
+        //Swift.print("onDatabaseAddAt() "+ this);
+        let parentIndex:Array = event.index.slice2(0,event.index.count-1)
+        parentIndex
+        //var parentTreeList:ITreeList = TreeListParser.itemAt(self, parentIndex) as ITreeList//DisplayObjectParser.getAt(_itemContainer,event.index.slice(0,event.index.length-1)) as ITreeList;//this;//TreeListParser.itemAt(this,event.index) as ITreeList;
+        //var item:NSView = TreeListUtils.item(event.xml,parentTreeList.itemContainer,CGPoint(width, _itemHeight));
+        let itemIndex:Int = event.index[event.index.count-1]
+        itemIndex
+        //parentTreeList.addItemAt(item,itemIndex);/*We could use TreeListModifier.addAt(parentTreeList, index, item) here but since we already have the parent since we need it when creating the item we can just use the addAt method directly*/
+        ElementModifier.floatChildren(itemContainer!)/*Re aligns the entire treesturcture*/
         super.onEvent(TreeListEvent(TreeListEvent.change,self))
+    }
+    private func onDatabaseSetAttributeAt(event : NodeEvent) {
+        //TreeListModifier.setTitleAt(self, event.index, event.xml["@title"])
+    }
+    private func onBackgroundMouseClick(event:MouseEvent){
+        //Swift.print("onBackgroundMouseClick");
+        //TreeListModifier.unSelectAll(self)
     }
     /**
      *

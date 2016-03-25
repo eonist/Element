29c7194301d14255ec107c7be2562d04473cede0
Eonist <unicornhive@gmail.com>
2016-03-25 17:58:09 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/utils/TreeListParser.swift

--- src/ui/treelist/utils/TreeListParser.swift
+++ src/ui/treelist/utils/TreeListParser.swift
@@ -1,5 +1,28 @@
 import Foundation
 
 class TreeListParser{
-    
+    /**
+     * Returns the total height of the items
+     * @Note another name for this could be getTotalItemsHeight?
+     */
+    class func itemsHeight(treeList:ITreeList)->CGFloat{
+        var height:CGFloat = 0
+        for (var i : Int = 0; i < treeList.itemContainer.subviews.count; i++) {
+            height += treeList.itemContainer.getSubviewAt(i) is TreeListItem ? (treeList.itemContainer.getSubviewAt(i) as! TreeListItem).getHeight() : (treeList.itemContainer.getSubviewAt(i) as! Element).getHeight()
+        }
+        return height
+    }
+    /**
+     * Returns an array of descendants in @param treeList
+     */
+    public static function descendants(treeList:ITreeList):Array{
+    var items:Array = [];
+    var numChildren:int = treeList.itemContainer.numChildren;
+    for (var i : int = 0; i < numChildren; i++) {
+				var displayObject:DisplayObject = treeList.itemContainer.getChildAt(i);
+				items.push(displayObject);
+				if(displayObject is ITreeList) items = items.concat(descendants(displayObject as ITreeList));
+    }
+    return items;
+    }
 }

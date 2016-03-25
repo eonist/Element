ab351cf5a3c1f4dd52b8220d8c3e871d5b344cfc
Eonist <unicornhive@gmail.com>
2016-03-25 14:53:03 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/TreeListItem.swift

--- src/ui/treelist/TreeListItem.swift
+++ src/ui/treelist/TreeListItem.swift
@@ -1,5 +1,9 @@
 import Foundation
-
+/**
+ * @Note: Keep the TreeListItem name, since you might want to create TreeMenuItem one day
+ * // :TODO: why doesnt the treeListItem extend a class that ultimatly extends a TextButton?, has it something to do with the indentation via css?
+ */
 class TreeListItem {
-
+    private var _itemContainer : Container
+    
 }

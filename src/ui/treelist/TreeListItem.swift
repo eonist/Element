dfde579561a297e8c50fbc1003d15ed071f2ed01
Eonist <unicornhive@gmail.com>
2016-03-25 16:22:17 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/TreeListItem.swift

--- src/ui/treelist/TreeListItem.swift
+++ src/ui/treelist/TreeListItem.swift
@@ -3,9 +3,11 @@ import Foundation
  * @Note: Keep the TreeListItem name, since you might want to create TreeMenuItem one day
  * // :TODO: why doesnt the treeListItem extend a class that ultimatly extends a TextButton?, has it something to do with the indentation via css?
  */
-class TreeListItem {
+class TreeListItem:SelectCheckBoxButton{
     //var itemContainer : Container
     init(_ width:CGFloat, _ height:CGFloat, _ text:String = "defaultText", _ isChecked:Bool = false, _ isSelected:Bool = false, parent:IElement? = nil, id:String = "") {
-        super.init(width, height, isFocused, isDisabled, text, isSelected, isChecked, parent, id, classId)
+        super.init(width, height, text, isSelected, isChecked, parent, id)
     }
+    
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

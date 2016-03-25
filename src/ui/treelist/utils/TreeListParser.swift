cb274c3201421aa0d21c523ae42d34e690254f6a
Eonist <unicornhive@gmail.com>
2016-03-25 11:39:19 +0100

    Files modified: 2
    Modified 2 files:src/ui/treelist/TreeList.swiftsrc/ui/treelist/utils/TreeListParser.swift

--- src/ui/treelist/utils/TreeListParser.swift
+++ src/ui/treelist/utils/TreeListParser.swift
@@ -1,19 +1,5 @@
 import Foundation
-/**
- * // :TODO: display:none and display:inline in the css shoud take care of the hiding and revealing of the elements not a method in this class (figure out how to do this)
- * // :TODO: there is a bug when setting the margin of any Text in this class that you have to counter meassure with a negative padding, this should be resolved
- * // :TODO: Why does ITreeListItem need to extend ITreeList, why does TreeList need ITreeList in the first place?
- * // :TODO: it may be wise to remove some of the floatChildren method sprinkled around, and only float after creation and after an event?, if possible, remeber that floatChildren doesnt float descendents aswell? Maybe create a float Decendants method?
- * // :TODO: create a close method that removes all items and eventlisteners
- */
-class TreeListParser:Element/*,ITreeList*/ {
-    var itemHeight : CGFloat
-    var node : Node
-    var itemContainer : Container
-    init(width:CGFloat = NaN, height:CGFloat = NaN, itemHeight:CGFloat = NaN, node:Node = Node(), parent : IElement? = nil, id:String = "") {
-        self.itemHeight = itemHeight;
-        self.node = node
-    }
 
-    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+class TreeListParser{
+    
 }

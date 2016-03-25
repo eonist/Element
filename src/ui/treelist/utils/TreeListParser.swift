608c89415f3ab8156d641fbd2fc77c95b7ff2651
Eonist <unicornhive@gmail.com>
2016-03-25 10:44:06 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/utils/TreeListParser.swift

--- src/ui/treelist/utils/TreeListParser.swift
+++ src/ui/treelist/utils/TreeListParser.swift
@@ -1,5 +1,11 @@
 import Foundation
-
-class TreeListParser {
+/**
+ * // :TODO: display:none and display:inline in the css shoud take care of the hiding and revealing of the elements not a method in this class (figure out how to do this)
+ * // :TODO: there is a bug when setting the margin of any Text in this class that you have to counter meassure with a negative padding, this should be resolved
+ * // :TODO: Why does ITreeListItem need to extend ITreeList, why does TreeList need ITreeList in the first place?
+ * // :TODO: it may be wise to remove some of the floatChildren method sprinkled around, and only float after creation and after an event?, if possible, remeber that floatChildren doesnt float descendents aswell? Maybe create a float Decendants method?
+ * // :TODO: create a close method that removes all items and eventlisteners
+ */
+class TreeListParser:Element,ITreeList {
 
 }

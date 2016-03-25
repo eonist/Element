b42b79b9ca4d6615f92585bc145c20f67a05df88
Eonist <unicornhive@gmail.com>
2016-03-25 16:20:16 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/checkboxbutton/SelectCheckBoxButton.swift

--- src/ui/button/checkboxbutton/SelectCheckBoxButton.swift
+++ src/ui/button/checkboxbutton/SelectCheckBoxButton.swift
@@ -1,5 +1,28 @@
-import Foundation
-
-class SelectCheckBoxButton {
-
+import Cocoa
+/**
+ * @example see TestCheckBoxButton
+ * @Note: Remeber to use the setChecked(true) if you want to change the state and skin after initiating the instance, since it wont do this itself on initiate
+ */
+class SelectCheckBoxButton:CheckBoxButton,ISelectable {
+    var isSelected:Bool
+    init(_ width:CGFloat, _ height:CGFloat,  _ text:String = "defaultText", _ isSelected : Bool = false, _ isChecked:Bool = false, _ parent:IElement? = nil, _ id:String = ""){
+        self.isSelected = isSelected
+        super.init(width, height, text, isChecked, parent, id)
+    }
+    override func mouseDown(event:MouseEvent) {
+        self.isSelected = !self.isSelected;
+        super.mouseDown(event)
+        super.onEvent(SelectEvent(SelectEvent.select,isSelected))/*bubbles:true because i.e: radioBulet may be added to RadioButton and radioButton needs to dispatch Select event if the SelectGroup is to work*/
+    }
+    /**
+     * @Note: do not add a dispatch event here, that is the responsibilyy of the caller
+     */
+    func setSelected(isSelected:Bool) {
+        self.isSelected = isSelected;
+        setSkinState(getSkinState());
+    }
+    override func getSkinState() -> String {
+        return self.isSelected ? SkinStates.selected + " " + super.getSkinState() : super.getSkinState()
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

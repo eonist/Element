346e148376dd550b65d2f4798731e880414c7f9f
Eonist <unicornhive@gmail.com>
2016-03-26 07:44:07 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/ITreeList.swift

--- src/ui/treelist/ITreeList.swift
+++ src/ui/treelist/ITreeList.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-protocol ITreeList/*:IElement*/ {
+protocol ITreeList:IElement {
     var itemContainer:Container{get}
     func addItem(item:NSView)
     func addItemAt(item:NSView,_ index:Int)

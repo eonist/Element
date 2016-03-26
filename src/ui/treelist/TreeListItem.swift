f62e5998d0a2ffdcf905566970e6182d3bd90ff8
Eonist <unicornhive@gmail.com>
2016-03-26 17:04:53 +0100

    Files modified: 2
    Modified 2 files:src/ui/treelist/TreeListItem.swiftsrc/ui/treelist/utils/TreeListUtils.swift

--- src/ui/treelist/TreeListItem.swift
+++ src/ui/treelist/TreeListItem.swift
@@ -3,11 +3,8 @@ import Cocoa
  * @Note: Keep the TreeListItem name, since you might want to create TreeMenuItem one day
  * // :TODO: why doesnt the treeListItem extend a class that ultimatly extends a TextButton?, has it something to do with the indentation via css?
  */
-class TreeListItem:SelectCheckBoxButton{
+class TreeListItem:SelectCheckBoxButton{//this class doesnt need an init method since its exactly the same as the one it extends
     var itemContainer : Container?
-    init(_ width:CGFloat, _ height:CGFloat, _ text:String = "defaultText", _ isChecked:Bool = false, _ isSelected:Bool = false, parent:IElement? = nil, id:String = "") {
-        super.init(width, height, text, isSelected, isChecked, parent, id)
-    }
     override func resolveSkin(){
         super.resolveSkin();
         itemContainer = addSubView(Container(NaN,NaN,self,"lable"))//0. add _itemContainer
--- src/ui/treelist/utils/TreeListUtils.swift
+++ src/ui/treelist/utils/TreeListUtils.swift
@@ -20,7 +20,7 @@ class TreeListUtils {
     class func treeItems(xml:NSXMLElement, _ treeList:ITreeList, _ size:CGPoint) -> ITreeList {
         for var child in xml.children! {
             var itemData:ItemData = Utils.itemData(child)
-            var treeItem:NSView = Utils.treeItem(itemData,treeList.itemContainer as IElement,size)
+            var treeItem:NSView = Utils.treeItem(itemData,treeList.itemContainer as! IElement,size)
             if(itemData.hasChildren) treeItems(child,treeItem as ITreeList,size)// :TODO: move this line into treeitem?
             treeList.addItem(treeItem)
         }
@@ -32,9 +32,9 @@ private class Utils{
      * // :TODO: write java doc
      */
     class func treeItem(itemData:ItemData,_ parent:IElement,_ size:CGPoint) -> NSView {
-        var item:NSView = itemData.hasChildren ? Utils.treeListItem(itemData, parent, size) : Utils.selectTextButton(itemData, parent, size)
-        item.visible = itemData.isVisible// :TODO: should this be here and do we need isVisible?
-        return item;
+        let item:NSView = itemData.hasChildren ? Utils.treeListItem(itemData, parent, size) : Utils.selectTextButton(itemData, parent, size)
+        item.hidden = itemData.isVisible// :TODO: should this be here and do we need isVisible?
+        return item
     }
     /**
      *
@@ -47,6 +47,12 @@ private class Utils{
         let isSelected:Bool = attributes["isSelected"] != nil ? attributes["isSelected"] == "true" : false//<- you can shorten this by doing ??
         let isVisible:Bool = attributes["isVisible"] != nil ?  attributes["isVisible"] == "true" : true//<- you can shorten this by doing ??
         return ItemData(title, hasChildren, isOpen, isVisible,isSelected)
+    }
+    class func treeListItem(itemData:ItemData,_ parent:IElement,_ size:CGPoint) -> TreeListItem {
+        return TreeListItem(size.x,size.y,itemData.title,itemData.isOpen,itemData.isSelected,parent);
+    }
+    class func selectTextButton(itemData:ItemData,_ parent:IElement,_ size:CGPoint) -> SelectTextButton {
+        return SelectTextButton(size.x,size.y,itemData.title,itemData.isSelected,parent);
     }
 }
 class ItemData{

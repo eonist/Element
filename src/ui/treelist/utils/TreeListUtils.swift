41fceb29bae9e5a225c5112850b612c4cd624445
Eonist <unicornhive@gmail.com>
2016-03-26 16:40:02 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/utils/TreeListUtils.swift

--- src/ui/treelist/utils/TreeListUtils.swift
+++ src/ui/treelist/utils/TreeListUtils.swift
@@ -11,4 +11,19 @@ class TreeListUtils {
         if(itemData.hasChildren) {treeItems(xml,item as ITreeList,new Point(size.x, size.y)) as NSView}/*Utils.treeItems(xml) and add each DisplayObject in treeItems*/
         return item
     }
+    /**
+     * @Note: This method is recursive
+     * @Note only use this method to add children to a new TreeList
+     * @param size is the size of each treeItem
+     * // :TODO: this should just return not modify?!? and be moved to TreeListParser
+     */
+    class func treeItems(xml:NSXMLElement, _ treeList:ITreeList, _ size:CGPoint) -> ITreeList {
+        for var child in xml.children! {
+            var itemData:ItemData = Utils.itemData(child)
+            var treeItem:NSView = Utils.treeItem(itemData,treeList.itemContainer as IElement,size)
+            if(itemData.hasChildren) treeItems(child,treeItem as ITreeList,size)// :TODO: move this line into treeitem?
+            treeList.addItem(treeItem)
+        }
+        return treeList;
+    }
 }

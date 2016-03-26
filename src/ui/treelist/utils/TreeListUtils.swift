d7232872712f0af3ea0cc9c3ec9baa615fae78ec
Eonist <unicornhive@gmail.com>
2016-03-26 19:03:57 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/utils/TreeListUtils.swift

--- src/ui/treelist/utils/TreeListUtils.swift
+++ src/ui/treelist/utils/TreeListUtils.swift
@@ -6,9 +6,9 @@ class TreeListUtils {
      * @Note this method is used in the onDataBaseAddAt method in the TreeList class
      */
     class func item(xml:NSXMLElement,parent:IElement,_ size:CGPoint)->NSView {
-        var itemData:ItemData = Utils.itemData(xml)
-        var item:NSView = Utils.treeItem(itemData, parent, size)
-        if(itemData.hasChildren) {treeItems(xml,item as ITreeList,new Point(size.x, size.y)) as NSView}/*Utils.treeItems(xml) and add each DisplayObject in treeItems*/
+        let itemData:ItemData = Utils.itemData(xml)
+        let item:NSView = Utils.treeItem(itemData, parent, size)
+        if(itemData.hasChildren) {treeItems(xml,item as! ITreeList,CGPoint(size.x, size.y)) as! NSView}/*Utils.treeItems(xml) and add each DisplayObject in treeItems*/
         return item
     }
     /**
@@ -17,14 +17,14 @@ class TreeListUtils {
      * @param size is the size of each treeItem
      * // :TODO: this should just return not modify?!? and be moved to TreeListParser
      */
-    class func treeItems(xml:NSXMLElement, _ treeList:ITreeList, _ size:CGPoint) -> ITreeList {
+    class func treeItems(xml:NSXMLElement, _ treeList:ITreeList, _ size:CGPoint) -> ITreeList {//TODO:use CGSize
         for child in xml.children! as! Array<NSXMLElement>{
-            var itemData:ItemData = Utils.itemData(child)
-            var treeItem:NSView = Utils.treeItem(itemData,treeList.itemContainer as! IElement,size)
-            if(itemData.hasChildren) treeItems(child,treeItem as ITreeList,size)// :TODO: move this line into treeitem?
+            let itemData:ItemData = Utils.itemData(child)
+            let treeItem:NSView = Utils.treeItem(itemData,treeList.itemContainer as! IElement,size)
+            if(itemData.hasChildren) {treeItems(child,treeItem as! ITreeList,size)}// :TODO: move this line into treeitem?
             treeList.addItem(treeItem)
         }
-        return treeList;
+        return treeList
     }
 }
 private class Utils{

ea8eb7d96549f7369514c9d984295629c329f1c4
Eonist <unicornhive@gmail.com>
2016-03-26 07:47:58 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/utils/TreeListModifier.swift

--- src/ui/treelist/utils/TreeListModifier.swift
+++ src/ui/treelist/utils/TreeListModifier.swift
@@ -7,4 +7,15 @@ class TreeListModifier {
     class func scrollTo(treeList:ITreeList,_ progress:CGFloat){
         treeList.itemContainer.y = SliderParser.y(progress, treeList.getHeight(),  TreeListParser.itemsHeight(treeList))
     }
+    /**
+     * @Note: to explode the entire treeList pass an empty array as @param index
+     */
+    class func explodeAt(var treeList:ITreeList,_ index:Array<Int>) {
+        treeList = TreeListParser.itemAt(treeList, index) as! ITreeList
+        if(treeList is ITreeListItem){(treeList as! ITreeListItem).open()}
+        var decendants:Array<ITreeList> = TreeListParser.decendantsOfType(treeList,ITreeListItem)
+        for each (var treeListItem : ITreeListItem in decendants){
+            if(!ICheckable(treeListItem).checked) treeListItem.open()
+        }
+    }
 }

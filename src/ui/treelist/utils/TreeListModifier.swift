fd2f02086b53c0d8e5f54ce5d15e36ceabbce392
Eonist <unicornhive@gmail.com>
2016-03-26 07:18:23 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/utils/TreeListModifier.swift

--- src/ui/treelist/utils/TreeListModifier.swift
+++ src/ui/treelist/utils/TreeListModifier.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class TreeListModifier {
-
+    /**
+     * Scrolls the treeList to a scalar position (value 0-1)
+     */
+    public static function scrollTo(treeList:ITreeList,progress:Number):void{
+    treeList.itemContainer.y = SliderParser.y(progress, treeList.getHeight(),  TreeListParser.itemsHeight(treeList));;
+    }
 }

4102f863e3194f15fe3fdef51798ccdda789c297
Eonist <unicornhive@gmail.com>
2016-03-26 06:54:05 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/utils/TreeListAsserter.swift

--- src/ui/treelist/utils/TreeListAsserter.swift
+++ src/ui/treelist/utils/TreeListAsserter.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class TreeListAsserter {
-
+    /**
+     *
+     */
+    class func hasSelected(treeList:ITreeList)->Bool {
+        let selectedIndex:Array = TreeListParser.selectedIndex(treeList)
+        return selectedIndex.count > 0
+    }
 }

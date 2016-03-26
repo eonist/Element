5bc57ac9da3d860649cc1e2f977d2d5a7c54bc60
Eonist <unicornhive@gmail.com>
2016-03-26 16:30:17 +0100

    Files modified: 1
    Modified 1 file:src/ui/treelist/utils/TreeListUtils.swift

--- src/ui/treelist/utils/TreeListUtils.swift
+++ src/ui/treelist/utils/TreeListUtils.swift
@@ -1,5 +1,14 @@
-import Foundation
+import Cocoa
 
 class TreeListUtils {
-    
+    /**
+     * Returns SelectTextButton or a TreeListItem from an xml
+     * @Note this method is used in the onDataBaseAddAt method in the TreeList class
+     */
+    class func item(xml:NSXMLElement,parent:IElement,_ size:CGPoint)->NSView {
+        var itemData:ItemData = Utils.itemData(xml)
+        var item:NSView = Utils.treeItem(itemData, parent, size)
+        if(itemData.hasChildren) {treeItems(xml,item as ITreeList,new Point(size.x, size.y)) as NSView}/*Utils.treeItems(xml) and add each DisplayObject in treeItems*/
+        return item
+    }
 }

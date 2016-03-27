7e59ab1076262d29eee7de787bed15c44d396659
Eonist <unicornhive@gmail.com>
2016-03-27 16:59:38 +0200

    Files modified: 1
    Modified 1 file:src/ui/treelist/TreeListItem.swift

--- src/ui/treelist/TreeListItem.swift
+++ src/ui/treelist/TreeListItem.swift
@@ -26,21 +26,25 @@ class TreeListItem:SelectCheckBoxButton,ITreeListItem{//this class doesnt need a
         ElementModifier.floatChildren(itemContainer!)
     }
     func open(){
+        Swift.print("TreeListItem.open()")
         setChecked(true)
         checkBox?.onEvent(CheckEvent(CheckEvent.check, true, checkBox!))
     }
     func close(){
+        Swift.print("close()")
         setChecked(false)
         checkBox?.onEvent(CheckEvent(CheckEvent.check, false, checkBox!))
     }
+    /**
+     *
+     */
     func onItemCheck(event : CheckEvent) {
         if((event.origin as! NSView).superview === self){itemContainer!.hidden = !event.isChecked}/*Checks if its this.checkButton is dispatching the event*///for (var i : int = 0; i < _itemContainer.numChildren; i++) (_itemContainer.getChildAt(i) as DisplayObject).visible = event.checked;
         if(isChecked) {ElementModifier.floatChildren(itemContainer!)}/*this is called from any decending treeListItem*/
     }
     override func onEvent(event: Event) {
-        
-        if(event.type == CheckEvent.check){onItemCheck(event as! CheckEvent)}/*this listens to all treeListItem decendants*/
         super.onEvent(event)
+        if(event.type == CheckEvent.check){onItemCheck(event as! CheckEvent)}/*this listens to all treeListItem decendants*/
     }
     func getCount()->Int{
         return itemContainer!.subviews.count
@@ -53,7 +57,7 @@ class TreeListItem:SelectCheckBoxButton,ITreeListItem{//this class doesnt need a
             for (var i : Int = 0; i < itemContainer!.subviews.count; i++) {
                 extraHeight += SkinParser.totalHeight((itemContainer!.getSubviewAt(i) as! IElement).skin!)
             }
-            Swift.print("extraHeight: " + "\(extraHeight)")
+            //Swift.print("extraHeight: " + "\(extraHeight)")
         }
         return height + extraHeight
     }

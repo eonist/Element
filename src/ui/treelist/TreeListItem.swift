e4a98cc07d33d8814444047aafcd38bc8669a7d4
Eonist <unicornhive@gmail.com>
2016-03-27 16:44:56 +0200

    Files modified: 1
    Modified 1 file:src/ui/treelist/TreeListItem.swift

--- src/ui/treelist/TreeListItem.swift
+++ src/ui/treelist/TreeListItem.swift
@@ -34,7 +34,7 @@ class TreeListItem:SelectCheckBoxButton,ITreeListItem{//this class doesnt need a
         checkBox?.onEvent(CheckEvent(CheckEvent.check, false, checkBox!))
     }
     func onItemCheck(event : CheckEvent) {
-        if((event.immediate as! NSView).superview === self){itemContainer!.hidden = !event.isChecked}/*Checks if its this.checkButton is dispatching the event*///for (var i : int = 0; i < _itemContainer.numChildren; i++) (_itemContainer.getChildAt(i) as DisplayObject).visible = event.checked;
+        if((event.origin as! NSView).superview === self){itemContainer!.hidden = !event.isChecked}/*Checks if its this.checkButton is dispatching the event*///for (var i : int = 0; i < _itemContainer.numChildren; i++) (_itemContainer.getChildAt(i) as DisplayObject).visible = event.checked;
         if(isChecked) {ElementModifier.floatChildren(itemContainer!)}/*this is called from any decending treeListItem*/
     }
     override func onEvent(event: Event) {
@@ -45,17 +45,13 @@ class TreeListItem:SelectCheckBoxButton,ITreeListItem{//this class doesnt need a
         return itemContainer!.subviews.count
     }
     override func getHeight() -> CGFloat {
-        let height:CGFloat = SkinParser.totalHeight2(skin!)/*<--if we use totalHeight here it creates an infinite call loop*/
-        var extraHeight:CGFloat = 0
+        var height:CGFloat = SkinParser.totalHeight2(skin!)/*<--if we use totalHeight here it creates an infinite call loop*/
         if(isChecked) {
             for (var i : Int = 0; i < itemContainer!.subviews.count; i++) {
-                extraHeight += SkinParser.totalHeight((itemContainer!.getSubviewAt(i) as! IElement).skin!)
+                height += SkinParser.totalHeight((itemContainer!.getSubviewAt(i) as! IElement).skin!)
             }
-            
-            Swift.print("extraHeight: " + "\(extraHeight)")
         }
-        
-        return height + extraHeight
+        return height
     }
     override func setSize(width:CGFloat, _ height:CGFloat){
         super.setSize(width,height)

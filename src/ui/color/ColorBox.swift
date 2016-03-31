a0ac20f4e49461b76289cb0d61817f5892a52726
Eonist <unicornhive@gmail.com>
2016-03-31 08:49:03 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/ColorBox.swift

--- src/ui/color/ColorBox.swift
+++ src/ui/color/ColorBox.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class ColorBox {
-
+class ColorBox:Button{
+    var color:CGFloat
+    init(width:CGFloat = NaN, height:CGFloat = NaN, color:CGFloat = 16711935/*<--Color.MAGENTA*/, parent:IElement? = nil, id:String = "") {
+        self.color = color
+        super.init(width,height,parent,id)
+        //setColor(color)
+    }
+    override func mouseDown(event: MouseEvent) {
+        super.onEvent(ColorBoxEvent(ColorBoxEvent.change,color))
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

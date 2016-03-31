b586822422694191be41160bfe9a1870db8cf1b9
Eonist <unicornhive@gmail.com>
2016-03-31 09:49:15 +0200

    Files modified: 2
    Modified 2 files:src/ui/color/ColorBox.swiftsrc/ui/color/IColorInput.swift

--- src/ui/color/ColorBox.swift
+++ src/ui/color/ColorBox.swift
@@ -1,8 +1,8 @@
 import Cocoa
 
 class ColorBox:Button/*,IColorInput*/{
-    var color:CGFloat
-    init(width:CGFloat = NaN, height:CGFloat = NaN, color:CGFloat = 16711935/*<--Color.MAGENTA*/, parent:IElement? = nil, id:String = "") {
+    var color:NSColor
+    init(width:CGFloat = NaN, height:CGFloat = NaN, color:NSColor = NSColor.magentaColor(), parent:IElement? = nil, id:String = "") {
         self.color = color
         super.init(width,height,parent,id)
         //setColor(color)
@@ -10,7 +10,7 @@ class ColorBox:Button/*,IColorInput*/{
     override func mouseDown(event: MouseEvent) {
         super.onEvent(ColorBoxEvent(ColorBoxEvent.change,color))
     }
-    func setColor(color:NSColor){
+    func setColorValue(color:NSColor){
         self.color = color
         var style:IStyle = StyleModifier.clone(skin.style,skin.style.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
         style.getStyleProperty("fill",1).value = color/*edits the style*/
--- src/ui/color/IColorInput.swift
+++ src/ui/color/IColorInput.swift
@@ -2,5 +2,5 @@ import Cocoa
 
 protocol IColorInput {
     var color:NSColor{get}
-    func setColor(color:NSColor)
+    func setColorvalue(color:NSColor)
 }

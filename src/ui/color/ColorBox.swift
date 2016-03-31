b025839566215480d200241802af022c3db2c36b
Eonist <unicornhive@gmail.com>
2016-03-31 09:51:22 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/ColorBox.swift

--- src/ui/color/ColorBox.swift
+++ src/ui/color/ColorBox.swift
@@ -12,9 +12,9 @@ class ColorBox:Button/*,IColorInput*/{
     }
     func setColorValue(color:NSColor){
         self.color = color
-        var style:IStyle = StyleModifier.clone(skin.style,skin.style.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
-        style.getStyleProperty("fill",1).value = color/*edits the style*/
-        skin.setStyle(style)/*updates the skin*/
+        let style:IStyle = StyleModifier.clone(skin!.style!,skin!.style!.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
+        style.getStyleProperty("fill",1)!.value = color/*edits the style*/
+        skin!.setStyle(style)/*updates the skin*/
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

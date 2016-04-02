b6e67ab83c8f8fca6b0449a1a4a907b1e6f52ba9
Eonist <unicornhive@gmail.com>
2016-04-02 18:33:49 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/utils/ColorSync.swift

--- src/ui/color/utils/ColorSync.swift
+++ src/ui/color/utils/ColorSync.swift
@@ -6,17 +6,21 @@ import Foundation
 class ColorSync {
     static let sharedInstance = ColorSync()
     private init() {} //This prevents others from using the default '()' initializer for this class.
-    private static var _receiver:IColorInput;
-    private static var _broadcaster:IColorInput;
-    public static function onColorChange(event:ColorInputEvent):void{
-    trace("onColorChange"+_receiver);
-    if(_receiver != null) {
-				trace("_receiver: " + _receiver);
-    //				trace("event.target: " + event.target);
-    //				trace("event.currentTarget: " + event.currentTarget);
-				_broadcaster = event.target as IColorInput;
-				_receiver.setColor(event.color);
-				_receiver.dispatchEvent(new ColorInputEvent(ColorInputEvent.CHANGE,event.color));
+    static var receiver:IColorInput?
+    static var broadcaster:IColorInput?
+    func onColorChange(event:ColorInputEvent){
+        print("onColorChange" + "\(ColorSync.receiver)")
+        if(ColorSync.receiver != nil) {
+            print("_receiver: " + "\(ColorSync.receiver)")
+            //print("event.target: " + event.target);
+            //print("event.currentTarget: " + event.currentTarget);
+            ColorSync.broadcaster = event.target as IColorInput
+            ColorSync.receiver.setColor(event.color)
+            ColorSync.receiver.onEvent(ColorInputEvent(ColorInputEvent.change,event.color))
+        }
+    }
+    public static function setColor(color:Number):void {
+    if (_broadcaster != null) _broadcaster.setColor(color);
     }
 }
 

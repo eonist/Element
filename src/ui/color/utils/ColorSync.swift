1bd0ad9c8d4ab57d97c4458193c8efe8863dcd14
Eonist <unicornhive@gmail.com>
2016-04-02 18:29:46 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/utils/ColorSync.swift

--- src/ui/color/utils/ColorSync.swift
+++ src/ui/color/utils/ColorSync.swift
@@ -1,5 +1,22 @@
 import Foundation
-
+/**
+ * let singleton = ColorSync.sharedInstance
+ * singleton.test()
+ */
 class ColorSync {
-
+    static let sharedInstance = ColorSync()
+    private init() {} //This prevents others from using the default '()' initializer for this class.
+    private static var _receiver:IColorInput;
+    private static var _broadcaster:IColorInput;
+    public static function onColorChange(event:ColorInputEvent):void{
+    trace("onColorChange"+_receiver);
+    if(_receiver != null) {
+				trace("_receiver: " + _receiver);
+    //				trace("event.target: " + event.target);
+    //				trace("event.currentTarget: " + event.currentTarget);
+				_broadcaster = event.target as IColorInput;
+				_receiver.setColor(event.color);
+				_receiver.dispatchEvent(new ColorInputEvent(ColorInputEvent.CHANGE,event.color));
+    }
 }
+

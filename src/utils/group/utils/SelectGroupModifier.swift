48f9ff762a6005483f2c7d12ec28d3796941593b
Eonist <unicornhive@gmail.com>
2016-04-03 10:39:12 +0200

    Files modified: 2
    Modified 2 files:src/utils/group/utils/SelectGroupModifier.swiftsrc/utils/group/utils/SelectGroupParser.swift

--- src/utils/group/utils/SelectGroupModifier.swift
+++ src/utils/group/utils/SelectGroupModifier.swift
@@ -1,5 +1,32 @@
 import Foundation
 
 class SelectGroupModifier {
-
+    /**
+     * 
+     */
+    class func selectedAt(selectGroup:SelectGroup,_ index:Int){// :TODO: move this to the SlectUtils since you mihgt not always want to unselectAllExcept when you select, and you may want to impose different functionality, like multi select etc
+        let selectable:ISelectable = SelectGroupParser.selectableAt(selectGroup,index)!
+        selectable.setSelected(true)
+        selectGroup.selected = selectable
+        SelectModifier.unSelectAllExcept(selectable, selectGroup.selectables)
+    }
+    /**
+     * 
+     */
+    class func select(selectGroup:SelectGroup,_ selectable:ISelectable) {
+        selectable.setSelected(true)
+        selectGroup.selected = selectable
+        SelectModifier.unSelectAllExcept(selectable, selectGroup.selectables)
+    }
+    /**
+     * Removes the RadioButton passed through the @param radioButton
+     */
+    class func removeSelectable(selectGroup:SelectGroup,_ item:ISelectable)->ISelectable? {
+        for (var i:Int = 0; i < selectGroup.selectables.count; i++) {
+            if (selectGroup.selectables[i] === item) {
+                return selectGroup.selectables.splice(i,1)[0]	// :TODO: dispatch something?
+            }
+        }
+        return nil
+    }
 }
--- src/utils/group/utils/SelectGroupParser.swift
+++ src/utils/group/utils/SelectGroupParser.swift
@@ -1,5 +1,32 @@
 import Foundation
 
 class SelectGroupParser {
-
+    /**
+     * Returns the selected ISelectable instance in @param selectGroup
+     */
+    class func selected(selectGroup:SelectGroup)->ISelectable? {
+        return SelectParser.selected(selectGroup.selectables)
+    }
+    /**
+     * Returns the index of @param selectable in _selectables
+     */
+    class func index(selectGroup:SelectGroup,_ selectable:ISelectable)->Int {
+        return ArrayParser.indx(selectGroup.selectables, selectable)
+        //return selectGroup.selectables.indexOf(selectable)
+    }
+    /**
+     *
+     */
+    class func indexOfSelected(selectGroup:SelectGroup)->Int {
+        let selected:ISelectable? = SelectGroupParser.selected(selectGroup)
+        return selected != nil ? index(selectGroup, selected!) : -1
+    }
+    /**
+     * Returns an RadioButton at a spessific index
+     */
+    class func selectableAt(selectGroup:SelectGroup,_ index:Int)->ISelectable? {
+        if(index <= selectGroup.selectables.count) {return selectGroup.selectables[index]}
+        Swift.print("no ISelectable at the index of " + "\(index)")
+        return nil
+    }
 }

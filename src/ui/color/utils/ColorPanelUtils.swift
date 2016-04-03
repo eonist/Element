096437e725c0a97bf1f2cfc96f38a1fe322bb9f7
Eonist <unicornhive@gmail.com>
2016-04-03 11:10:01 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/utils/ColorPanelUtils.swift

--- src/ui/color/utils/ColorPanelUtils.swift
+++ src/ui/color/utils/ColorPanelUtils.swift
@@ -9,27 +9,27 @@ class ColorPanelUtils {
         switch(colorType){
         case ColorPanel.RGB:
             var rbgObj:[String:CGFloat] = ColorParser.rgbByHex(color)
-            colorPanel.spinner1.setValue(rbgObj["rb"])
-            colorPanel.spinner2.setValue(rbgObj["gb"])
-            colorPanel.spinner3.setValue(rbgObj["bb"])
+            colorPanel.spinner1.setValue(rbgObj["rb"]!)
+            colorPanel.spinner2.setValue(rbgObj["gb"]!)
+            colorPanel.spinner3.setValue(rbgObj["bb"]!)
             break
         case ColorPanel.HSB:
             var hsbObj:[String:CGFloat] = ColorParser.hsbByRgb(color)
-            colorPanel.spinner1.setValue(hsbObj["hue"])
-            colorPanel.spinner2.setValue(hsbObj["saturation"]*100)
-            colorPanel.spinner3.setValue(hsbObj["brightness"]*100)
+            colorPanel.spinner1.setValue(hsbObj["hue"]!)
+            colorPanel.spinner2.setValue(hsbObj["saturation"]!*100)
+            colorPanel.spinner3.setValue(hsbObj["brightness"]!*100)
             break
         case ColorPanel.HLS:
             var hlsObj:[String:CGFloat] = ColorParser.hlsByNumericRgb(color)
-            colorPanel.spinner1.setValue(hlsObj["h"])
-            colorPanel.spinner2.setValue(hlsObj["l"])
-            colorPanel.spinner3.setValue(hlsObj["s"])
+            colorPanel.spinner1.setValue(hlsObj["h"]!)
+            colorPanel.spinner2.setValue(hlsObj["l"]!)
+            colorPanel.spinner3.setValue(hlsObj["s"]!)
             break
         case ColorPanel.HSV:
             var hsvObj:[String:CGFloat] = ColorParser.hsvByNumericRgb(color)
-            colorPanel.spinner1.setValue(hsvObj["h"])
-            colorPanel.spinner2.setValue(hsvObj["s"]*240)
-            colorPanel.spinner3.setValue(hsvObj["v"]*240)
+            colorPanel.spinner1.setValue(hsvObj["h"]!)
+            colorPanel.spinner2.setValue(hsvObj["s"]!*240)
+            colorPanel.spinner3.setValue(hsvObj["v"]!*240)
             break
         }
     }

7a679eae197b4923501ef8a9f2b2f9ca39ed286f
Eonist <unicornhive@gmail.com>
2016-04-03 09:39:07 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/IColorPanel.swift

--- src/ui/color/IColorPanel.swift
+++ src/ui/color/IColorPanel.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-class IColorPanel: IColorInput{
-    var spinner1 : LeverSpinner {get}
-    var spinner2 : LeverSpinner {get}
-    var spinner3 : LeverSpinner {get}
+protocol IColorPanel:IColorInput{
+    var spinner1:LeverSpinner {get}
+    var spinner2:LeverSpinner {get}
+    var spinner3:LeverSpinner {get}
     func getColorType() -> String
 }

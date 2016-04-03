f776b8f4a1887181656522fe2884b0b18f4cfce6
Eonist <unicornhive@gmail.com>
2016-04-03 07:37:57 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/ColorPanel.swift

--- src/ui/color/ColorPanel.swift
+++ src/ui/color/ColorPanel.swift
@@ -17,4 +17,9 @@ class ColorPanel {
     static var spinner3:LeverSpinner?
     static var itemHeight:CGFloat
     static var colorTypeSelectGroup:SelectGroup?
+    init ColorPanel(width:CGFloat = NaN, height:CGFloat = NaN, itemHeight:CGFloat = NaN, color:CGFloat = NSColor("0xFF00FF"), title:String = "Color", minSize:CGPoint? = nil, maxSize:CGPoint? = nil, parent:IElement? = nil, id:String = "") {
+        self.itemHeight = itemHeight
+        self.color = color
+        super.init(width, height, 24,title,minSize,maxSize,parent, id)
+    }
 }

ed428162173d3007460ae02630fdfd73eea8f51e
Eonist <unicornhive@gmail.com>
2016-04-03 09:16:39 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/ColorPanel.swift

--- src/ui/color/ColorPanel.swift
+++ src/ui/color/ColorPanel.swift
@@ -24,10 +24,18 @@ class ColorPanel:Element{
     }
     override func resolveSkin() {
         super.resolveSkin()
-        var rgbBtn = addSubView(RadioButton(NaN,NaN,RGB,true,self))
-        var hsbBtn = addSubView(RadioButton(NaN,NaN,HSB,false,self))
-        var hlsBtn = addSubView(RadioButton(NaN,NaN,HLS,false,self))
-        var hsvBtn = addSubView(RadioButton(NaN,NaN,HSV,false,self))
+        var rgbBtn = addSubView(RadioButton(NaN,NaN,ColorPanel.RGB,true,self))
+        var hsbBtn = addSubView(RadioButton(NaN,NaN,ColorPanel.HSB,false,self))
+        var hlsBtn = addSubView(RadioButton(NaN,NaN,ColorPanel.HLS,false,self))
+        var hsvBtn = addSubView(RadioButton(NaN,NaN,ColorPanel.HSV,false,self))
+        
+        _colorTypeSelectGroup = addChild(new SelectGroup([rgbRadioButton,hsbRadioButton,hlsRadioButton,hsvRadioButton],rgbRadioButton)) as SelectGroup;
+        _colorInput = addChild(new ColorInput(width,_itemHeight,"Color:",_color,this)) as ColorInput;
+        var rgbObj:Object = ColorParser.rgbByHex(_color);/*LeaverStepper instance ->Red (0 - 255) (Read/write)*/
+        _spinner1 = addChild(new LeverSpinner(width, _itemHeight,"Red:",rgbObj["rb"],1,0,255,1,100,200,this)) as LeverSpinner;
+        _spinner2 = addChild(new LeverSpinner(width, _itemHeight,"Green:",rgbObj["gb"],1,0,255,1,200,200,this)) as LeverSpinner;/*LeaverStepper instance ->Green (0 - 255) (Read/write)*/
+        _spinner3 = addChild(new LeverSpinner(width, _itemHeight,"Blue:",rgbObj["bb"],1,0,255,1,200,200,this)) as LeverSpinner;/*LeaverStepper instance ->Blue (0 - 255) (Read/write)*/
+        ColorSync.broadcaster = this;
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

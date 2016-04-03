c247b0cb7f19f4367402f772460514694ab58548
Eonist <unicornhive@gmail.com>
2016-04-03 07:31:56 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/ColorPanel.swift

--- src/ui/color/ColorPanel.swift
+++ src/ui/color/ColorPanel.swift
@@ -1,5 +1,13 @@
 import Foundation
-
+/**
+ * // :TODO: add an alpha-stepper?
+ */
 class ColorPanel {
-
+    static var title:String = "Color"
+    static var minSize:CGPoint = CGPoint(180, 232)
+    static var maxSize:CGPoint = CGPoint(1200,232)
+    static var RGB_TYPE:String = "RGB"
+    static var HSB_TYPE:String = "HSB"
+    static var HLS_TYPE:String = "HLS"
+    static var HSV_TYPE:String = "HSV"
 }

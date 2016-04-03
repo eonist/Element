57b9476828d83da86f01d75e0c5cde25ef4fb9f8
Eonist <unicornhive@gmail.com>
2016-04-03 07:39:59 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/ColorPanel.swift

--- src/ui/color/ColorPanel.swift
+++ src/ui/color/ColorPanel.swift
@@ -2,7 +2,7 @@ import Cocoa
 /**
  * // :TODO: add an alpha-stepper?
  */
-class ColorPanel {
+class ColorPanel:Element{
     static var title:String = "Color"
     static var minSize:CGPoint = CGPoint(180, 232)
     static var maxSize:CGPoint = CGPoint(1200,232)
@@ -10,16 +10,16 @@ class ColorPanel {
     static var HSB:String = "HSB"
     static var HLS:String = "HLS"
     static var HSV:String = "HSV"
-    static var color:NSColor
-    static var colorInput:ColorInput?
-    static var spinner1:LeverSpinner?
-    static var spinner2:LeverSpinner?
-    static var spinner3:LeverSpinner?
-    static var itemHeight:CGFloat
-    static var colorTypeSelectGroup:SelectGroup?
-    init ColorPanel(width:CGFloat = NaN, height:CGFloat = NaN, itemHeight:CGFloat = NaN, color:CGFloat = NSColor("0xFF00FF"), title:String = "Color", minSize:CGPoint? = nil, maxSize:CGPoint? = nil, parent:IElement? = nil, id:String = "") {
+    var color:NSColor
+    var colorInput:ColorInput?
+    var spinner1:LeverSpinner?
+    var spinner2:LeverSpinner?
+    var spinner3:LeverSpinner?
+    var itemHeight:CGFloat
+    var colorTypeSelectGroup:SelectGroup?
+    init(width:CGFloat = NaN, height:CGFloat = NaN, itemHeight:CGFloat = NaN, color:NSColor = NSColorParser.nsColor("0xFF00FF"), title:String = "Color", minSize:CGPoint? = nil, maxSize:CGPoint? = nil, parent:IElement? = nil, id:String = "") {
         self.itemHeight = itemHeight
         self.color = color
-        super.init(width, height, 24,title,minSize,maxSize,parent, id)
+        super.init(width, height, 24, title, minSize, maxSize, parent, id)
     }
 }

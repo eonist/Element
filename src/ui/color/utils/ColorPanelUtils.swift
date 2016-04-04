88802ffe3706e3266a013c922e74a6568fb43f93
Eonist <unicornhive@gmail.com>
2016-04-04 19:30:24 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/utils/ColorPanelUtils.swift

--- src/ui/color/utils/ColorPanelUtils.swift
+++ src/ui/color/utils/ColorPanelUtils.swift
@@ -7,23 +7,23 @@ class ColorPanelUtils {
     class func applyColor(colorPanel:IColorPanel,_ color:NSColor ) {
         let colorType:String = colorPanel.getColorType()
         switch(colorType){
-            case ColorPanel.RGB:
-                let rbgObj = ColorParser.rgba(color)
-                colorPanel.spinner1!.setValue(rbgObj.r*255)//rb
-                colorPanel.spinner2!.setValue(rbgObj.g*255)//gb
-                colorPanel.spinner3!.setValue(rbgObj.b*255)//bb
-            case ColorPanel.HSB:
-                let hsbObj:HSBColor = ColorParser.hsb(color)
-                colorPanel.spinner1!.setValue(hsbObj.hue)
-                colorPanel.spinner2!.setValue(hsbObj.saturation*100)
-                colorPanel.spinner3!.setValue(hsbObj.brightness*100)
-            case ColorPanel.HLS:
-                let hlsObj:HLS = ColorParser.hls(color)
+            case ColorPanel.rgb:
+                let rbgObj:RGB = color.rgb
+                colorPanel.spinner1!.setValue(rbgObj.r.cgFloat)//rb
+                colorPanel.spinner2!.setValue(rbgObj.g.cgFloat)//gb
+                colorPanel.spinner3!.setValue(rbgObj.b.cgFloat)//bb
+            case ColorPanel.hsb:
+                let hsbObj:HSB = color.hsb
+                colorPanel.spinner1!.setValue(hsbObj.h.cgFloat)
+                colorPanel.spinner2!.setValue(hsbObj.s.cgFloat*100)
+                colorPanel.spinner3!.setValue(hsbObj.b.cgFloat*100)
+            case ColorPanel.hls:
+                let hlsObj:HLS = color.hls
                 colorPanel.spinner1!.setValue(hlsObj.h)
                 colorPanel.spinner2!.setValue(hlsObj.l)
                 colorPanel.spinner3!.setValue(hlsObj.s)
-            case ColorPanel.HSV:
-                let hsvObj:HSV = ColorParser.hsv(color)
+            case ColorPanel.hsv:
+                let hsvObj:HSV = color.hsv
                 colorPanel.spinner1!.setValue(hsvObj.h)
                 colorPanel.spinner2!.setValue(hsvObj.s * 240)
                 colorPanel.spinner3!.setValue(hsvObj.v * 240)

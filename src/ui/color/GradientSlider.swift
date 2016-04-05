02bc1d11d23825c7cf80a6ed6039f229efa8f649
Eonist <unicornhive@gmail.com>
2016-04-05 20:21:23 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientSlider.swift

--- src/ui/color/GradientSlider.swift
+++ src/ui/color/GradientSlider.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 class GradientSlider:HNodeSlider{
-
+    init(_ width : CGFloat = NaN, _ height : CGFloat = NaN, _ nodeWidth : CGFloat = NaN, _ gradient:IGradient? = nil, _ startProgress : CGFloat = 0, _ endProgress : CGFloat = 1, _ parent : IElement? = nil, _ id : String = "") {
+        super.init(width, height, nodeWidth, startProgress, endProgress, parent, id)
+        //setGradient(gradient)
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

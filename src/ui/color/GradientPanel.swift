f0e2851ce074ab0ee38472a52f3e9118be739be1
Eonist <unicornhive@gmail.com>
2016-04-05 20:04:57 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientPanel.swift

--- src/ui/color/GradientPanel.swift
+++ src/ui/color/GradientPanel.swift
@@ -1,9 +1,12 @@
 import Foundation
 
 class GradientPanel:Element{
-    init(_ width:CGFloat = NaN, _ height:CGFloat = NaN, _ itemHeight:CGFloat = NaN, gradient:IGradient? = nil, _ title:String = "Color", _ parent:IElement? = nil, _ id:String = "") {
+    var itemHeight:CGFloat
+    var gradient:IGradient
+    init(_ width:CGFloat = NaN, _ height:CGFloat = NaN, _ itemHeight:CGFloat = NaN, gradient:IGradient? = nil, _ parent:IElement? = nil, _ id:String = "") {
         self.itemHeight = itemHeight
-        self.color = color
+        self.gradient = gradient!
         super.init(width, height, parent, id)
     }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

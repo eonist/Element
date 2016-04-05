9fdd05933d3136291f3513ad6f802e54eeda6ce4
Eonist <unicornhive@gmail.com>
2016-04-05 20:23:22 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientSlider.swift

--- src/ui/color/GradientSlider.swift
+++ src/ui/color/GradientSlider.swift
@@ -5,5 +5,14 @@ class GradientSlider:HNodeSlider{
         super.init(width, height, nodeWidth, startProgress, endProgress, parent, id)
         //setGradient(gradient)
     }
+    func setGradient(gradient:IGradient){
+        //trace("setGradient"+gradient.colors);
+        self.gradient = gradient
+        //trace("_gradient: " + _gradient);
+        var style:IStyle = StyleModifier.clone(skin.style,skin.style.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
+        style.getStyleProperty("fill").value = gradient/*edits the style*/
+        //skin.setState(SkinStates.NONE);/*update the skin*/
+        skin.setStyle(style)/*updates the skin*/
+    }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

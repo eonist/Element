c7c76cda9b92c11075afcffb64d1b5ca788cb532
Eonist <unicornhive@gmail.com>
2016-04-05 19:56:52 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientPanel.swift

--- src/ui/color/GradientPanel.swift
+++ src/ui/color/GradientPanel.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class GradientPanel {
-
+class GradientPanel:Element{
+    init(_ width:CGFloat = NaN, _ height:CGFloat = NaN, _ itemHeight:CGFloat = NaN, gradient:IGradient? = nil, _ title:String = "Color", _ parent:IElement? = nil, _ id:String = "") {
+        self.itemHeight = itemHeight
+        self.color = color
+        super.init(width, height, parent, id)
+    }
 }

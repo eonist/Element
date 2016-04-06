d44d3126fa93d2bd7dcc2f2ee2bef999414437a4
Eonist <unicornhive@gmail.com>
2016-04-06 08:33:57 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientPanel.swift

--- src/ui/color/GradientPanel.swift
+++ src/ui/color/GradientPanel.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 class GradientPanel:Element{
+    var gradientSlider : GradientSlider?
     var itemHeight:CGFloat
     var gradient:IGradient
     var gradientTypeSelectGroup:SelectGroup?
@@ -14,7 +15,7 @@ class GradientPanel:Element{
         let linearRadioButton = addSubView(RadioButton(NaN,NaN,"Linear",true,self))
         let radialRadioButton = addSubView(RadioButton(NaN,NaN,"Radial",false,self))
         gradientTypeSelectGroup = SelectGroup([linearRadioButton,radialRadioButton],linearRadioButton)
-        
+        gradientSlider = addSubView(GradientSlider(width,12/*<--this should be NaN*/,20/*<--this should be NaN*/,gradient,0,1,self))
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

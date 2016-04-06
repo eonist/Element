a29b8f3e2e51cb0e016b8dfcbdf9eb47e2483c7b
Eonist <unicornhive@gmail.com>
2016-04-06 08:40:07 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientPanel.swift

--- src/ui/color/GradientPanel.swift
+++ src/ui/color/GradientPanel.swift
@@ -16,7 +16,8 @@ class GradientPanel:Element{
         let radialRadioButton = addSubView(RadioButton(NaN,NaN,"Radial",false,self))
         gradientTypeSelectGroup = SelectGroup([linearRadioButton,radialRadioButton],linearRadioButton)
         gradientSlider = addSubView(GradientSlider(width,12/*<--this should be NaN*/,20/*<--this should be NaN*/,gradient,0,1,self))
-        _alphaSpinner = addChild(new LeverSpinner(width, NaN,"Alpha:",1,0.01,0,1,2,1,200,this)) as LeverSpinner;
+        colorInput = addSubView(ColorInput(width,NaN,"Color:",gradientSlider.gradient.colors[0],self))
+        alphaSpinner = addSubView(new LeverSpinner(width, NaN,"Alpha:",1,0.01,0,1,2,1,200,this))
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

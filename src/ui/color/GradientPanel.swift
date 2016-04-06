48a06855f45d4c136e48576db79dc5a40de4fc6e
Eonist <unicornhive@gmail.com>
2016-04-06 08:46:15 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientPanel.swift

--- src/ui/color/GradientPanel.swift
+++ src/ui/color/GradientPanel.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 
 class GradientPanel:Element{
     var gradientSlider:GradientSlider?
@@ -19,7 +19,9 @@ class GradientPanel:Element{
         let radialRadioButton = addSubView(RadioButton(NaN,NaN,"Radial",false,self))
         gradientTypeSelectGroup = SelectGroup([linearRadioButton,radialRadioButton],linearRadioButton)
         gradientSlider = addSubView(GradientSlider(width,12/*<--this should be NaN*/,20/*<--this should be NaN*/,gradient,0,1,self))
-        colorInput = addSubView(ColorInput(width,NaN,"Color:",gradientSlider!.gradient!.colors[0],self))
+        let cgColor:CGColor = gradientSlider!.gradient!.colors[0]
+        NSColorParser.nsColor(cgColor)
+        colorInput = addSubView(ColorInput(width,NaN,"Color:",,self))
         alphaSpinner = addSubView(LeverSpinner(width, NaN,"Alpha:",1,0.01,0,1,2,1,200,self))
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}

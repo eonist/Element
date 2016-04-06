a0ec1188d5fcbc7efd93cec05fd8ed5df709f1fe
Eonist <unicornhive@gmail.com>
2016-04-06 19:39:44 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientPanel.swift

--- src/ui/color/GradientPanel.swift
+++ src/ui/color/GradientPanel.swift
@@ -21,8 +21,10 @@ class GradientPanel:Element,IGradientPanel{
         let radialRadioButton = addSubView(RadioButton(NaN,NaN,"Radial",false,self))
         gradientTypeSelectGroup = SelectGroup([linearRadioButton,radialRadioButton],linearRadioButton)
         gradientTypeSelectGroup!.event = onEvent/*attach the selectGroup to self, to handle the events here*/
-        /*
+        
         gradientSlider = addSubView(GradientSlider(width,12/*<--this should be NaN*/,20/*<--this should be NaN*/,gradient,0,1,self))
+        
+        /*
         let nsColor:NSColor = gradientSlider!.gradient!.colors[0].nsColor
         colorInput = addSubView(ColorInput(width,NaN,"Color:",nsColor,self))
         alphaSpinner = addSubView(LeverSpinner(width, NaN,"Alpha:",1,0.01,0,1,2,1,200,self))

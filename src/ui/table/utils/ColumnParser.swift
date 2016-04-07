ac1144d578a0d34da3c6b68e7dc9416b3c4f7830
Eonist <unicornhive@gmail.com>
2016-04-07 19:35:12 +0200

    Files modified: 1
    Modified 1 file:src/ui/table/utils/ColumnParser.swift

--- src/ui/table/utils/ColumnParser.swift
+++ src/ui/table/utils/ColumnParser.swift
@@ -1,17 +1,24 @@
-import Foundation
+import Cocoa
 
 class ColumnParser {
     /**
      * Returns the indices in a a spessific sort order
      */
-    public static function sortOrder(column:Column,options:*):Array {
-    var list:IList = column.list;
-    var sortList:Array = [];
-    var children:Array = DisplayObjectParser.children(list.lableContainer);
-    for each (var selectTextButton : SelectTextButton in children) sortList.push({name:selectTextButton.name,text:selectTextButton.text.getText() != "" ? selectTextButton.text.getText() : "~",index:list.lableContainer.getChildIndex(selectTextButton)});
-    sortList.sortOn(["text"],options);
-    var indices:Array = [];
-    for (var i : int = 0; i < sortList.length; i++) indices.push(sortList[i]["index"]);
-    return indices;
+    class func sortOrder(column:Column,_options:AnyObject)->Array<Int> {
+        /*
+        let list:IList = column.list!
+        var sortList:Array<Int> = []
+        var children:Array = NSViewParser.childrenOfType(list.lableContainer!, SelectTextButton.self)
+        for selectTextButton  in children {
+            sortList.append({name:selectTextButton.name,text:selectTextButton.text.getText() != "" ? selectTextButton.text.getText() : "~",index:list.lableContainer.getChildIndex(selectTextButton)})
+        }
+        sortList.sortOn(["text"],options)
+        var indices:Array<Int> = []
+        for (var i : Int = 0; i < sortList.count; i++) {indices.append(sortList[i]["index"])}
+        return indices
+        */
+        
+        //Continue here: fix the above
+        
     }
 }

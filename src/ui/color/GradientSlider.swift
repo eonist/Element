807c45efac2c0553374ee729e95124baa6fcae48
Eonist <unicornhive@gmail.com>
2016-04-07 08:12:06 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientSlider.swift

--- src/ui/color/GradientSlider.swift
+++ src/ui/color/GradientSlider.swift
@@ -4,7 +4,7 @@ class GradientSlider:HNodeSlider{
     var gradient : IGradient?
     init(_ width : CGFloat = NaN, _ height : CGFloat = NaN, _ nodeWidth : CGFloat = NaN, _ gradient:IGradient? = nil, _ startProgress : CGFloat = 0, _ endProgress : CGFloat = 1, _ parent : IElement? = nil, _ id : String = "") {
         super.init(width, height, nodeWidth, startProgress, endProgress, parent, id)
-        //setGradient(gradient!)
+        setGradient(gradient!)
     }
     func setGradient(gradient:IGradient){
         //trace("setGradient"+gradient.colors);

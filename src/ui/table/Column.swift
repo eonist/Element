75e177ac25fed001191172ab811988ae487270d3
Eonist <unicornhive@gmail.com>
2016-04-07 16:10:16 +0200

    Files modified: 1
    Modified 1 file:src/ui/table/Column.swift

--- src/ui/table/Column.swift
+++ src/ui/table/Column.swift
@@ -4,11 +4,15 @@ class Column:Element{
     private var title:String
     private var dataProvider:DataProvider
     //private var header:CheckTextButton
-    private var list:List;
+    private var list:List?
     init(_ width:CGFloat, _ height:CGFloat, _ title:String, _ dataProvider:DataProvider, _ parent:IElement? = nil, _ id:String = "") {
         self.title = title
         self.dataProvider = dataProvider
         super.init(width,height,parent,id)
 
     }
+
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        
+    
 }

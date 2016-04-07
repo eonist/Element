6106a37532385d41a2701c2c91fdc447971ccf07
Eonist <unicornhive@gmail.com>
2016-04-07 15:53:20 +0200

    Files modified: 1
    Modified 1 file:src/ui/table/Table.swift

--- src/ui/table/Table.swift
+++ src/ui/table/Table.swift
@@ -5,7 +5,7 @@ import Foundation
  * // :TODO: create ScrollTable.as in the future, consider how columns will work though?, since they containe a List instance which is has its own mask and height, how do we begin to scroll all of them in tandem? do we redesign column to contain selectButtons and remove list? Think  it through and draw it out on paper
  * // :TODO: Table header needs to be a SelectableCheckButton, since it needs both be selectable and checkable
  */
-class Table :Element{
+class Table:Element{
     private var node:Node
     private var columns:Array<Column> = []
     private var columnContainer:Container?
@@ -14,6 +14,14 @@ class Table :Element{
         super.init(width,height,parent,id)
         
     }
-
+    override func resolveSkin() {
+        super.resolveSkin()
+        columnContainer = addSubView(new Container(width,height,this,"column"))
+        for (var i : Int = 0; i < database.xml.children().Count(); i++) {
+            var child : XML = _database.xml.children()[i]
+            var itemData:Object = XMLParser.attributes(child)
+            if(itemData["hasChildren"] || child.children().length() > 0) _columns.append(_columnContainer.addSubView(Column(NaN,NaN,itemData["title"],new DataProvider(child),_columnContainer,String(i))))/*we add the columns index to the id so we can set individual css properties to each column*/
+        }
+    }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

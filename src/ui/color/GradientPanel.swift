21901ed31a230a5c86583a662010870a632d46ac
Eonist <unicornhive@gmail.com>
2016-04-07 10:59:17 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/GradientPanel.swift

--- src/ui/color/GradientPanel.swift
+++ src/ui/color/GradientPanel.swift
@@ -16,22 +16,19 @@ class GradientPanel:Element,IGradientPanel{
     }
     override func resolveSkin(){
         super.resolveSkin()
-        
         let linearRadioButton = addSubView(RadioButton(NaN,NaN,"Linear",true,self))
         let radialRadioButton = addSubView(RadioButton(NaN,NaN,"Radial",false,self))
         gradientTypeSelectGroup = SelectGroup([linearRadioButton,radialRadioButton],linearRadioButton)
         gradientTypeSelectGroup!.event = onEvent/*attach the selectGroup to self, to handle the events here*/
-        
         gradientSlider = addSubView(GradientSlider(width,12/*<--this should be NaN*/,20/*<--this should be NaN*/,gradient,0,1,self))
-        
         let nsColor:NSColor = gradientSlider!.gradient!.colors[0].nsColor
-        
         colorInput = addSubView(ColorInput(width,NaN,"Color:",nsColor,self))
         alphaSpinner = addSubView(LeverSpinner(width, NaN,"Alpha:",1,0.01,0,1,2,1,200,self))
         ratioSpinner = addSubView(LeverSpinner(width, NaN,"Ratio:",1,1,0,255,1,100,200,self))
         focalPointRatioSpinner = addSubView(LeverSpinner(width, NaN,"Focal point:",0,0.01,-1,1,2,100,200,self))
     }
     private func onGradientSliderChange(event : NodeSliderEvent) {
+        Swift.print("onGradientSliderChange()")
         let isStartNodeSelected:Bool = event.selected === gradientSlider!.startNode
         let ratio:CGFloat = isStartNodeSelected ? event.startProgress : event.endProgress
         //Swift.print("ratio: " + ratio);
@@ -49,6 +46,7 @@ class GradientPanel:Element,IGradientPanel{
         ratioSpinner!.setValue(gradientSlider!.gradient!.locations[index])
     }
     private func onAlphaSpinnerChange(event:SpinnerEvent){
+        Swift.print("onAlphaSpinnerChange()")
         let isStartNodeSelected:Bool = gradientSlider!.selectGroup!.selected === gradientSlider!.startNode
         let alpha1:CGFloat = isStartNodeSelected ? event.value : gradientSlider!.gradient!.colors[0].nsColor.alphaComponent
         let alpha2:CGFloat = !isStartNodeSelected ? event.value : gradientSlider!.gradient!.colors[1].nsColor.alphaComponent
@@ -56,6 +54,7 @@ class GradientPanel:Element,IGradientPanel{
         gradientSlider!.gradient!.colors[1] = gradientSlider!.gradient!.colors[1].alpha(alpha2)
     }
     private func onRatioSpinnerChange(event:SpinnerEvent) {
+        Swift.print("onRatioSpinnerChange()")
         let isStartNodeSelected:Bool = gradientSlider!.selectGroup!.selected === gradientSlider!.startNode
         let ratio1:CGFloat = isStartNodeSelected ? event.value : gradientSlider!.gradient!.locations[0]
         let ratio2:CGFloat = !isStartNodeSelected ? event.value : gradientSlider!.gradient!.locations[1]

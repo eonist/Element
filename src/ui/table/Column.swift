eaa33f5d3885f91565abd41a6e238984bf9759b2
Eonist <unicornhive@gmail.com>
2016-04-07 16:07:40 +0200

    Files modified: 1
    Modified 1 file:src/ui/table/Column.swift

--- src/ui/table/Column.swift
+++ src/ui/table/Column.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class Column {
+class Column:Element{
+    private var title:String
+    private var dataProvider:DataProvider
+    //private var header:CheckTextButton
+    private var list:List;
+    init(_ width:CGFloat, _ height:CGFloat, _ title:String, _ dataProvider:DataProvider, _ parent:IElement? = nil, _ id:String = "") {
+        self.title = title
+        self.dataProvider = dataProvider
+        super.init(width,height,parent,id)
 
+    }
 }

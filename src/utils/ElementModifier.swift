2ef6e078e2ace3ff78c832991c01d90277ed5a07
Eonist <unicornhive@gmail.com>
2016-04-08 12:14:26 +0200

    Files modified: 1
    Modified 1 file:src/utils/ElementModifier.swift

--- src/utils/ElementModifier.swift
+++ src/utils/ElementModifier.swift
@@ -23,15 +23,15 @@ class ElementModifier {
      * // :TODO: skin should have a dedicated redraw method or a simple workaround
      * @Note keep in mind that this can be Window
      */
-    class func refresh(element:IElement, _ thingToDo: (IElement)->IElement) {
+    class func refresh(element:IElement, _ method: (IElement)->Void = Utils.setStyle) {
         if(element.skin!.style!.getStyleProperty("display") != nil && (element.skin!.style!.getStyleProperty("display")!.value as! String) == CSSConstants.none) {return} /*Skip refreshing*/
         let container:NSView = element as! NSView//element is Window ? Window(element).view : element as NSView;
         let numChildren:Int = container.subviews.count
         for (var i : Int = 0; i < numChildren; i++) {
             let child:NSView = container.subviews[i]
             if(child is IElement) {
-                thingToDo(child as! IElement)
-                if(child.subviews.count > 0) {refresh(child as! IElement,thingToDo)}/*<--this line makes it recursive*/
+                method(child as! IElement)
+                if(child.subviews.count > 0) {refresh(child as! IElement,method)}/*<--this line makes it recursive*/
             }
         }
     }

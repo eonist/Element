59c95e79b70f79a3459f12240894e5a55a09e757
Eonist <unicornhive@gmail.com>
2016-04-12 10:09:13 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/DropShadowDecorator.swift

--- src/skin/graphic/decorator/DropShadowDecorator.swift
+++ src/skin/graphic/decorator/DropShadowDecorator.swift
@@ -11,19 +11,19 @@ class DropShadowDecorator:SizeableDecorator{//TODO: probably should extend Sizea
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     override func fill() {
         //Swift.print("DropShadowDecorator.fill()")
-        if(dropShadow != nil && dropShadow!.inner){
-            graphic.fillShape.graphics.dropShadow = dropShadow;
-        }else if(dropShadow != nil && !dropShadow!.inner){
-            graphic.layer!.shadowColor = NSColor.blackColor().CGColor;//dont forget about the graphic.layer!.shadowPath
-            graphic.layer!.shadowOpacity = 1.0;
-            graphic.layer!.shadowRadius = 5.0;
-            graphic.layer!.shadowOffset = CGSizeMake(0, 3);
-        }else{
+        if(dropShadow != nil && dropShadow!.inner){/*inner*/
+            graphic.fillShape.graphics.dropShadow = dropShadow
+        }else if(dropShadow != nil && !dropShadow!.inner){/*outer*/
+            graphic.layer!.shadowColor = NSColor.blackColor().CGColor//dont forget about the graphic.layer!.shadowPath
+            graphic.layer!.shadowOpacity = 1.0
+            graphic.layer!.shadowRadius = 5.0
+            graphic.layer!.shadowOffset = CGSizeMake(0, 3)
+        }else{/*no shadow*/
             graphic.fillShape.graphics.dropShadow = nil
-            graphic.layer!.shadowColor = NSColor.clearColor().CGColor;
-            graphic.layer!.shadowOpacity = 0.0;
-            graphic.layer!.shadowRadius = 0.0;
-            graphic.layer!.shadowOffset = CGSizeMake(0, 0);
+            graphic.layer!.shadowColor = NSColor.clearColor().CGColor
+            graphic.layer!.shadowOpacity = 0.0
+            graphic.layer!.shadowRadius = 0.0
+            graphic.layer!.shadowOffset = CGSizeMake(0, 0)
         }
         
         //Swift.print(graphic.graphics.dropShadow)

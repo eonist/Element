a307842e86799d7e5a0fca8b14bc5fb3eb3c099b
Eonist <unicornhive@gmail.com>
2016-04-13 20:13:22 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/EmptyGraphic.swift

--- src/skin/graphic/decorator/EmptyGraphic.swift
+++ src/skin/graphic/decorator/EmptyGraphic.swift
@@ -8,7 +8,7 @@ class EmptyGraphic:SizeableGraphic{
     override func drawFill() {
         //Swift.print("RectGraphic.drawFill()")
         //graphic.fillShape.path = CGRect(0,0,width,height).path/*Draws in the local coordinate space of the shape*/
-        let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!.thickness, graphic.lineOffsetType) : CGRect(x,y,width,height)
+        let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!.thickness, graphic.lineOffsetType) : CGRect(x,y,1.0,1.0)
         graphic.fillShape.frame = fillFrame/*,position and set the size of the frame*/
         //Swift.print("graphic.fillShape.frame: " + "\(graphic.fillShape.frame)")
     }

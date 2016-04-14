5faebac12328d5ec62f78204926fd2ea0fdcfb60
Eonist <unicornhive@gmail.com>
2016-04-14 11:14:33 +0200

    Files modified: 1
    Modified 1 file:src/ui/slider/VolumeSlider.swift

--- src/ui/slider/VolumeSlider.swift
+++ src/ui/slider/VolumeSlider.swift
@@ -4,25 +4,26 @@ class VolumeSlider:HSlider{
     lazy var volumeGraphic:Element = Element(1,1,self,"volumeGraphic")
     override func resolveSkin() {
         //add the volume graphic before the thumb is added
-        super.resolveSkin()
         volumeGraphic = addSubView(volumeGraphic)
+        super.resolveSkin()
+        
     }
     override func onMouseMove(event: NSEvent) -> NSEvent? {
-        volumeGraphic.setSize(thumb!.x, getHeight())//this should be set after super
+        volumeGraphic.setSize(thumb!.x+thumb!.width/2, getHeight())//this should be set after super
         return super.onMouseMove(event)
     }
     override func onThumbMove(event: NSEvent) -> NSEvent? {
-        volumeGraphic.setSize(thumb!.x, getHeight())//this should be set after super
+        volumeGraphic.setSize(thumb!.x+thumb!.width/2, getHeight())//this should be set after super
         return super.onThumbMove(event)
     }
     override func setProgressValue(progress: CGFloat) {
         
         super.setProgressValue(progress)
-        volumeGraphic.setSize(thumb!.x, getHeight())
+        volumeGraphic.setSize(thumb!.x+thumb!.width/2, getHeight())
     }
     override func setSize(width: CGFloat, _ height: CGFloat) {
         //setSize
         super.setSize(width,height)
-        volumeGraphic.setSize(thumb!.x, getHeight())
+        volumeGraphic.setSize(thumb!.x+thumb!.width/2, getHeight())
     }
 }

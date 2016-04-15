78a4ae8ac47020f0987d240abc4264790e2eb774
Eonist <unicornhive@gmail.com>
2016-04-15 15:33:21 +0200

    Files modified: 1
    Modified 1 file:src/ui/table/Column.swift

--- src/ui/table/Column.swift
+++ src/ui/table/Column.swift
@@ -26,11 +26,11 @@ class Column:Element{
         Swift.print("Column.onListSelect");
         let rowIndex:Int = ListParser.index(list!, event.selected as! NSView)
         Swift.print("rowIndex: " + "\(rowIndex)")
-		//(ColumnEvent(ColumnEvent.select,rowIndex,true));
+		super.onEvent(ColumnEvent(ColumnEvent.select,rowIndex,true));
     }
     override func onEvent(event: Event) {
         if(event.type == CheckEvent.check && event.origin === header){onHeaderCheck(event as! CheckEvent)}
-        if(event.type == ListEvent.select && event.origin === header){onListSelect(event as! ListEvent)}
+        if(event.type == ListEvent.select && event.origin === list){onListSelect(event as! ListEvent)}
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

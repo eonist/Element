e87af6bc75ccc6479547e23806666277f77cd269
Eonist <unicornhive@gmail.com>
2016-04-15 19:03:29 +0200

    Files modified: 1
    Modified 1 file:src/ui/table/utils/ColumnParser.swift

--- src/ui/table/utils/ColumnParser.swift
+++ src/ui/table/utils/ColumnParser.swift
@@ -5,20 +5,19 @@ class ColumnParser {
      * Returns the indices in a a spessific sort order
      */
     class func sortOrder(column:Column,_options:AnyObject)->Array<Int> {
-        
         let list:IList = column.list!
-        var sortList:Array<[String:String]> = []
-        var children:Array<SelectTextButton> = NSViewParser.childrenOfType(list.lableContainer!, SelectTextButton.self)
-        
-        
-        
+        let children:Array<SelectTextButton> = NSViewParser.childrenOfType(list.lableContainer!, SelectTextButton.self)
+        var sortList:Array<[String:AnyObject]> = []
         for selectTextButton  in children {
-            sortList.append(["text":selectTextButton.text!.getText() != "" ? selectTextButton.text!.getText() : "~","index":list.lableContainer.getChildIndex(selectTextButton)])
+            sortList.append(["text":selectTextButton.text!.getText() != "" ? selectTextButton.text!.getText() : "~","index":list.lableContainer!.indexOf(selectTextButton)])
         }
+        
+        sortList.sortInPlace({$0.firstName > $1.firstName})
+        
         /*
         sortList.sortOn(["text"],options)
         
-        //children.sortInPlace({$0.firstName > $1.firstName})
+        //
         
         var indices:Array<Int> = []
         for (var i : Int = 0; i < sortList.count; i++) {indices.append(sortList[i]["index"])}

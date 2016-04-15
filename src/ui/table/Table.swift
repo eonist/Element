6e916469281b1777d4b831eaa391a62c91def156
Eonist <unicornhive@gmail.com>
2016-04-15 20:27:30 +0200

    Files modified: 1
    Modified 1 file:src/ui/table/Table.swift

--- src/ui/table/Table.swift
+++ src/ui/table/Table.swift
@@ -31,7 +31,7 @@ class Table:Element{
      */
     private func onColumnHeaderCheck(event:CheckEvent) {
         Swift.print("Table.onColumnHeaderCheck()")
-        if(event.origin is Column) {
+        if(event.origin === Column) {
             let indices:Array<Int> = ColumnParser.sortOrder(event.origin as! Column, event.isChecked)// :TODO: maybe we can add the NUMERIC sort so that if a text starts with a number etc
             for column in columns {
                 DepthModifier.sortByIndices(column.list!.lableContainer!, indices)

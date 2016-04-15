d513d523db907e8b2495e7650dd9c872573cd8df
Eonist <unicornhive@gmail.com>
2016-04-15 15:35:27 +0200

    Files modified: 2
    Modified 2 files:src/ui/table/Column.swiftsrc/ui/table/events/ColumnEvent.swift

--- src/ui/table/events/ColumnEvent.swift
+++ src/ui/table/events/ColumnEvent.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-class ColumnEvent :Event{
+class ColumnEvent:Event{
     static var select:String = "columnEventSelect"
+    var rowIndex:Int?
+    init(_ type:String = "", rowIndex:Int, _ origin:AnyObject){
+        self.rowIndex = rowIndex
+        super.init(type, origin)
+    }
 }

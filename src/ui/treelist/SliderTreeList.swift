4a41c0a05c63b80277b099fd3952aeaa3410cba5
Eonist <unicornhive@gmail.com>
2016-04-16 09:51:17 +0200

    Files modified: 1
    Modified 1 file:src/ui/treelist/SliderTreeList.swift

--- src/ui/treelist/SliderTreeList.swift
+++ src/ui/treelist/SliderTreeList.swift
@@ -14,4 +14,24 @@ class SliderTreeList:TreeList{
         slider!.setThumbHeightValue(thumbHeight)
         //slider!.hidden = !SliderParser.assertSliderVisibility(thumbHeight/slider!.getHeight())
     }
+    /**
+     * Updates the thumb position and the position of the itemsContainer
+     */
+    func update(){
+        Swift.print("SliderTreeList.update()");
+        var itemsHeight:CGFloat = TreeListParser.itemsHeight(self)/*total height of the items*/
+        /*
+        Swift.print("itemsHeight: " + itemsHeight);
+        Swift.print("itemHeight: " + itemHeight);
+        Swift.print("getHeight(): " +  getHeight());
+        */
+        sliderInterval = SliderParser.interval(itemsHeight, getHeight(), itemHeight)
+        //			Swift.print("update() _sliderInterval: " + _sliderInterval);
+        var thumbHeight:CGFloat = SliderParser.thumbSize(getHeight()/itemsHeight, slider.getHeight())
+        slider.setThumbHeightValue(thumbHeight:)(thumbHeight)
+        var progress:CGFloat = SliderParser.progress(itemContainer.y, getHeight(), itemsHeight)
+        slider.setProgress(progress)
+        //slider.hidden = !SliderParser.assertSliderVisibility(_slider.thumb.getHeight()/slider.getHeight())
+        TreeListModifier.scrollTo(self, progress)
+    }
 }

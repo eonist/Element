724c9d1499d1d917687c87f7ebb5cb2ad25e38a0
Eonist <unicornhive@gmail.com>
2016-04-16 09:15:45 +0200

    Files modified: 1
    Modified 1 file:src/ui/treelist/SliderTreeList.swift

--- src/ui/treelist/SliderTreeList.swift
+++ src/ui/treelist/SliderTreeList.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class SliderTreeList {
-
+class SliderTreeList:TreeList{
+    override func resolveSkin() {
+        super.resolveSkin()
+        var itemsHeight:Number = TreeListParser.itemsHeight(this)
+        _sliderInterval = SliderParser.interval(itemsHeight, getHeight(), itemHeight)//Math.floor(itemsHeight - getHeight())/itemHeight;// :TODO: use ScrollBarUtils.interval instead?
+        _slider = addChild(new VSlider(itemHeight,getHeight(),itemHeight,0,this)) as VSlider
+        var thumbHeight:Number = SliderParser.thumbSize(getHeight()/itemsHeight, _slider.getHeight())
+        _slider.setThumbHeight(thumbHeight)
+        _slider.visible = SliderParser.assertSliderVisibility(thumbHeight/_slider.getHeight())
+    }
 }

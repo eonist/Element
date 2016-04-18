d8b87eb7498a93f4d4bc00a614a4ee535185205c
Eonist <unicornhive@gmail.com>
2016-04-18 09:19:17 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PathGraphic.swift

--- src/skin/graphic/decorator/PathGraphic.swift
+++ src/skin/graphic/decorator/PathGraphic.swift
@@ -18,15 +18,5 @@ class PathGraphic:SizeableDecorator{
         var offsetPath = graphic.fillShape.path.copy()
         graphic.fillShape.path = CGPathModifier.translate(&offsetPath, offset.x, offset.y)
         
-        /*line*/
-        let strokeBoundingBox:CGRect = SVGStyleUtils.boundingBox(fillShape.path, style!)// + boundingBox.origin
-        //Swift.print("strokeBoundingBox: " + "\(strokeBoundingBox)")
-        let linePathOffset:CGPoint = PointParser.difference(strokeBoundingBox.origin,CGPoint(0,0))
-        //Swift.print("linePathOffset: " + "\(linePathOffset)")
-        lineShape.frame = (strokeBoundingBox + boundingBox.origin).copy()
-        lineShape.path = fillShape.path.copy()
-        var lineOffsetPath = fillShape.path.copy()
-        lineShape.path = CGPathModifier.translate(&lineOffsetPath, linePathOffset.x, linePathOffset.y)
-        
     }
 }

1cf6ed4affe18f73e09b2281c552c290c03b2b75
Eonist <unicornhive@gmail.com>
2016-04-18 10:05:33 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PathGraphic.swift

--- src/skin/graphic/decorator/PathGraphic.swift
+++ src/skin/graphic/decorator/PathGraphic.swift
@@ -12,9 +12,8 @@ class PathGraphic:SizeableDecorator{
     }
     override func drawLine() {
         let cgPath = DisplayPathUtils.compile(CGPathCreateMutable(), path)
-        let boundingBox:CGRect = PathParser.boundingBox(cgPath, graphic.lineStyle!)
-        let lineOffsetRect = RectGraphicUtils.lineOffsetRect(boundingBox, graphic.lineStyle!.thickness, graphic.lineOffsetType)
-        graphic.lineShape.frame = lineOffsetRect
+        let boundingBox:CGRect = PathParser.boundingBox(cgPath, graphic.lineStyle!)/*regardless if the line is inside outside or centered, this will still work, as the path is already exapnded correctly*/
+        graphic.lineShape.frame = boundingBox
         let offset = CGPoint(-boundingBox.x,-boundingBox.y)
         var offsetPath = cgPath.copy()
         graphic.fillShape.path = CGPathModifier.translate(&offsetPath, offset.x, offset.y)

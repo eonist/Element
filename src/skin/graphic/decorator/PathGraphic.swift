0fd9c706080f0d4453bdea392e194e1bbbb86f30
Eonist <unicornhive@gmail.com>
2016-04-18 10:18:17 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PathGraphic.swift

--- src/skin/graphic/decorator/PathGraphic.swift
+++ src/skin/graphic/decorator/PathGraphic.swift
@@ -11,20 +11,19 @@ class PathGraphic:SizeableDecorator{
         let cgPath = DisplayPathUtils.compile(CGPathCreateMutable(), path)
         let boundingBox:CGRect = CGPathGetPathBoundingBox(cgPath)/*there is also CGPathGetPathBoundingBox, CGPathGetBoundingBox, which works a bit different, the difference is probably just support for cruves etc*/
         /*fill*/
-        if(style!.fill != nil){/*Fill*/
-            fillShape.frame = boundingBox
+
+            graphic.fillShape.frame = boundingBox
             let offset = CGPoint(-boundingBox.x,-boundingBox.y)
             var offsetPath = path.copy()
             fillShape.path = CGPathModifier.translate(&offsetPath, offset.x, offset.y)
-        }
+        
     }
     override func drawLine() {
-        
-        let boundingBox:CGRect = PathParser.boundingBox(cgPath, graphic.lineStyle!)/*regardless if the line is inside outside or centered, this will still work, as the path is already exapnded correctly*/
+        let boundingBox:CGRect = PathParser.boundingBox(graphic.fillShape.path, graphic.lineStyle!)/*regardless if the line is inside outside or centered, this will still work, as the path is already exapnded correctly*/
         graphic.lineShape.frame = boundingBox
         let offset = CGPoint(-boundingBox.x,-boundingBox.y)
-        var offsetPath = cgPath.copy()
-        graphic.fillShape.path = CGPathModifier.translate(&offsetPath, offset.x, offset.y)
+        var offsetPath = graphic.fillShape.path.copy()
+        graphic.lineShape.path = CGPathModifier.translate(&offsetPath, offset.x, offset.y)
         
     }
 }

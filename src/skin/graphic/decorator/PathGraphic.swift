a99dc848da6e524fc54b639826b201717b79fdea
Eonist <unicornhive@gmail.com>
2016-04-18 08:38:14 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PathGraphic.swift

--- src/skin/graphic/decorator/PathGraphic.swift
+++ src/skin/graphic/decorator/PathGraphic.swift
@@ -1,11 +1,14 @@
 import Foundation
 
-class PathGraphic {
+class PathGraphic:SizeableDecorator{
+    var path:IPath
+    init(_ path:IPath, _ decoratable: IGraphicDecoratable = BaseGraphic(nil,LineStyle())) {
+        self.path = path
+        
+        super.init(decoratable)
+    }
     override func drawFill() {
-        //Swift.print("EllipseGraphic.drawFill()")
-        let fillFrame = graphic.lineStyle != nil ?  RectGraphicUtils.fillFrame(CGRect(x,y,width,height), graphic.lineStyle!.thickness, graphic.lineOffsetType) : CGRect(x,y,width,height)
-        graphic.fillShape.frame = fillFrame/*,position and set the size of the frame*/
-        graphic.fillShape.path = CGPathParser.ellipse(CGRect(0,0,width,height))
+        
     }
     override func drawLine() {
         

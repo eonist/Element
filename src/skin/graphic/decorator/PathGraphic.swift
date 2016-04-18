52eb1b85f892bb73e3f13a522734f61463978f53
Eonist <unicornhive@gmail.com>
2016-04-18 09:14:56 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PathGraphic.swift

--- src/skin/graphic/decorator/PathGraphic.swift
+++ src/skin/graphic/decorator/PathGraphic.swift
@@ -12,12 +12,21 @@ class PathGraphic:SizeableDecorator{
     }
     override func drawLine() {
         let boundingBox:CGRect = CGPathGetPathBoundingBox(graphic.fillShape.path)/*there is also CGPathGetPathBoundingBox, CGPathGetBoundingBox, which works a bit different, the difference is probably just support for cruves etc*/
-        /*fill*/
-let lineOffsetRect = RectGraphicUtils.lineOffsetRect(rect, graphic.lineStyle!.thickness, graphic.lineOffsetType)
-            graphic.lineShape.frame = boundingBox
-            let offset = CGPoint(-boundingBox.x,-boundingBox.y)
-            var offsetPath = graphic.fillShape.path.copy()
-            graphic.fillShape.path = CGPathModifier.translate(&offsetPath, offset.x, offset.y)
-        }
+        let lineOffsetRect = RectGraphicUtils.lineOffsetRect(boundingBox, graphic.lineStyle!.thickness, graphic.lineOffsetType)
+        graphic.lineShape.frame = boundingBox
+        let offset = CGPoint(-boundingBox.x,-boundingBox.y)
+        var offsetPath = graphic.fillShape.path.copy()
+        graphic.fillShape.path = CGPathModifier.translate(&offsetPath, offset.x, offset.y)
+        
+        /*line*/
+        let strokeBoundingBox:CGRect = SVGStyleUtils.boundingBox(fillShape.path, style!)// + boundingBox.origin
+        //Swift.print("strokeBoundingBox: " + "\(strokeBoundingBox)")
+        let linePathOffset:CGPoint = PointParser.difference(strokeBoundingBox.origin,CGPoint(0,0))
+        //Swift.print("linePathOffset: " + "\(linePathOffset)")
+        lineShape.frame = (strokeBoundingBox + boundingBox.origin).copy()
+        lineShape.path = fillShape.path.copy()
+        var lineOffsetPath = fillShape.path.copy()
+        lineShape.path = CGPathModifier.translate(&lineOffsetPath, linePathOffset.x, linePathOffset.y)
+        
     }
 }

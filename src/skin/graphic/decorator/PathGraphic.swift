9a7caeb143ca5e701a1736fcfbe17f03c7f3003b
Eonist <unicornhive@gmail.com>
2016-04-20 17:02:34 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PathGraphic.swift

--- src/skin/graphic/decorator/PathGraphic.swift
+++ src/skin/graphic/decorator/PathGraphic.swift
@@ -2,8 +2,8 @@ import Foundation
 
 class PathGraphic:SizeableDecorator{
     var path:IPath
-    lazy var cgPath:CGMutablePathRef = DisplayPathUtils.compile(CGPathCreateMutable(), self.path)
-    var fillBoundingBox:CGRect?
+    lazy var cgPath:CGMutablePathRef = DisplayPathUtils.compile(CGPathCreateMutable(), self.path)//this is lazy because both drawFill and drawLine uses it, its risky but convenient
+    lazy var fillBoundingBox:CGRect = CGPathGetPathBoundingBox(self.cgPath)//this is lazy because both drawFill and drawLine uses it, its risky but convenient
     init(_ path:IPath, _ decoratable: IGraphicDecoratable = BaseGraphic(nil,LineStyle())) {
         Swift.print("PathGraphic.init()")
         self.path = path
@@ -13,8 +13,8 @@ class PathGraphic:SizeableDecorator{
         Swift.print("PathGraphic.drawFill()")
         fillBoundingBox = CGPathGetPathBoundingBox(cgPath)/*there is also CGPathGetPathBoundingBox, CGPathGetBoundingBox, which works a bit different, the difference is probably just support for cruves etc*/
         Swift.print("fillPathBoundingBox: " + "\(fillBoundingBox)")
-        graphic.fillShape.frame = fillBoundingBox!
-        let offset = CGPoint(-fillBoundingBox!.x,-fillBoundingBox!.y)
+        graphic.fillShape.frame = fillBoundingBox
+        let offset = CGPoint(-fillBoundingBox.x,-fillBoundingBox.y)
         var offsetPath = cgPath.copy()
         graphic.fillShape.path = CGPathModifier.translate(&offsetPath, offset.x, offset.y)
     }
@@ -22,7 +22,7 @@ class PathGraphic:SizeableDecorator{
         Swift.print("PathGraphic.drawLine()")
         var boundingBox:CGRect = PathParser.boundingBox(cgPath, graphic.lineStyle!)/*regardless if the line is inside outside or centered, this will still work, as the path is already exapnded correctly*/
         Swift.print("boundingBox: " + "\(boundingBox)")
-        boundingBox += fillBoundingBox!.topLeft
+        boundingBox += fillBoundingBox.topLeft
         graphic.lineShape.frame = boundingBox
         let offset = CGPoint(-boundingBox.x,-boundingBox.y)
         var offsetPath = cgPath.copy()

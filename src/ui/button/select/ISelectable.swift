b5664155830a1ccb44daa69686d6535b8be87043
Eonist <unicornhive@gmail.com>
2016-04-21 17:35:49 +0200

    Files modified: 2
    Modified 2 files:src/ui/button/select/ISelectable.swiftsrc/utils/select/SelectModifier.swift

--- src/ui/button/select/ISelectable.swift
+++ src/ui/button/select/ISelectable.swift
@@ -5,5 +5,5 @@ import Foundation
 protocol ISelectable: class/*derive only classes for the protocol, not structs, this enables === operator of protocol*/{
     func setSelected(isSelected:Bool)
     /*using isSelcted could be the wrong way to go about this-> getSelected would work better->*//*var isSelected:Bool{get}*//*This is named isSelected because selected is ocupied by obc and using selected() as a method seems inconsistent*/
-    func getSelected()->Bool//<--shouldnt this be isSelected? :TODO: this should be getChecked since composite classes can impliment ICHeckable and they will need to access a sub instance via a implimcit getter method, same for IDisableable, ISelectable, IFocusable etc
+    func getSelected()->Bool//<--shouldnt this be isSelected? :TODO: this should be getSelected since composite classes can impliment ICHeckable and they will need to access a sub instance via a implimcit getter method, same for IDisableable, ISelectable, IFocusable etc
 }
--- src/utils/select/SelectModifier.swift
+++ src/utils/select/SelectModifier.swift
@@ -7,6 +7,30 @@ class SelectModifier {
     class func unSelectAllExcept(exceptionItem:ISelectable, _ selectables:Array<ISelectable>) {// :TODO: refactor this function// :TODO: rename to unSelectAllExcept
         for selectable : ISelectable in selectables {if(selectable !== exceptionItem && selectable.getSelected()) {selectable.setSelected(false)}}
     }
+    /**
+     *
+     */
+    class func selectAll(selectables:[ISelectable],_ isSelected:Bool = true) {
+        for selectable in selectables {selectable.setSelected(isSelected)}
+    }
+    /**
+     * write doc
+     */
+    class func unSelectAllExceptThese(selectables:Array<ISelectable>, exceptions:Array<ISelectable>) {
+        let unSelectedItems:Array<ISelectable> = ArrayParser.difference(selectables, exceptions)
+        for unSelectedItem in unSelectedItems {if(unSelectedItem.getSelected()){unSelectedItem.setSelected(false)}}
+        for selectedItem in exceptions { if(!selectedItem.getSelected()) {selectedItem.setSelected(true)}}
+    }
+    /**
+     * Selects all selectables within a range (from, to)
+     * // :TODO: use the Range class here!?!?
+     */
+    class func selectRange(selectables:Array<ISelectable>, _ from:Int, _ to:Int,_ isSelected:Bool = true) {
+        for (var i : Int = from; i <= to; i++) {
+            let selectable:ISelectable = selectables[i];
+            if(!selectable.getSelected()) {selectable.setSelected(isSelected)}
+        }
+    }
 }
 extension SelectModifier{
     /**
@@ -15,5 +39,9 @@ extension SelectModifier{
     class func unSelectAllExcept(exceptionItem:ISelectable, _ view:NSView){
         let selectables:Array<ISelectable> = SelectParser.selectables(view)
         unSelectAllExcept(exceptionItem,selectables)
+    }
+    class func selectAll(view:NSView,_ isSelected:Bool = true) {
+        let selectables:Array<ISelectable> = SelectParser.selectables(view)
+        for selectable in selectables {selectable.setSelected(isSelected)}
     }
 }

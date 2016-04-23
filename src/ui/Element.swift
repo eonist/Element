a25be89ca19278923d62796bbb2a7965be012cb4
Eonist <unicornhive@gmail.com>
2016-04-23 12:16:26 +0200

    Files modified: 1
    Modified 1 file:src/ui/Element.swift

--- src/ui/Element.swift
+++ src/ui/Element.swift
@@ -10,8 +10,8 @@ import Cocoa
 class Element:InteractiveView2,IElement {
     var width:CGFloat //{get{return super.frame.width}set{super.frame.width = newValue}}
     var height:CGFloat //{get{return super.frame.height}set{super.frame.height = newValue}}
-    var x:CGFloat{get{return super.frame.x}set{super.frame.x = newValue}}
-    var y:CGFloat{get{return super.frame.y}set{super.frame.y = newValue}}
+    //var x:CGFloat{get{return super.frame.x}set{super.frame.x = newValue}}
+    //var y:CGFloat{get{return super.frame.y}set{super.frame.y = newValue}}
     var state:String = SkinStates.none/*This is protected so that sub-classes can access it when setting the initial state*/
     var skin:ISkin?
     var parent : IElement?

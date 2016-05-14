13be6b5d9f88082215bf7e8fa568873fd089169f
Eonist <unicornhive@gmail.com>
2016-05-14 21:00:05 +0200

    Files modified: 10
    Modified 10 files:src/ui/button/TextButton.swiftsrc/ui/button/check/CheckButton.swiftsrc/ui/button/check/ICheckable.swiftsrc/ui/button/checkboxbutton/CheckBox.swiftsrc/ui/button/checkboxbutton/CheckBoxButton.swiftsrc/ui/button/checkboxbutton/SelectCheckBoxButton.swiftsrc/ui/button/radio/RadioBullet.swiftsrc/ui/button/select/ISelectable.swiftsrc/ui/button/text/CheckTextButton.swiftsrc/ui/button/text/SelectTextButton.swift

--- src/ui/button/check/ICheckable.swift
+++ src/ui/button/check/ICheckable.swift
@@ -5,10 +5,4 @@ protocol ICheckable: class/*derive only classes for the protocol, not structs, t
     //func getChecked()->Bool
     func getChecked()->Bool//<--shouldnt this be isChecked? :TODO: this should be getChecked since composite classes can impliment ICHeckable and they will need to access a sub instance via a implimcit getter method, same for IDisableable, ISelectable, IFocusable etc
 }
-
-
-
 //you probably have to revert to the checked variable as you need to be able to use only one way to assert checkedness. think checkgroup etc
-
-
-
--- src/ui/button/checkboxbutton/CheckBox.swift
+++ src/ui/button/checkboxbutton/CheckBox.swift
@@ -1,5 +1,4 @@
 import Foundation
 
 class CheckBox:CheckButton{//this class could probably be an alias, is it purly exists to differentiate between types in css
-
 }

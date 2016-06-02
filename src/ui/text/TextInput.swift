04b81dcb04776f3612cfced8b9a83ef3de1bc266
Eonist <unicornhive@gmail.com>
2016-06-02 12:46:28 +0200

    Files modified: 1
    Modified 1 file:src/ui/text/TextInput.swift

--- src/ui/text/TextInput.swift
+++ src/ui/text/TextInput.swift
@@ -5,7 +5,7 @@ class TextInput :Element{
     var inputTextArea : TextArea?
     var textString:String
     var inputString:String
-    init(_ width : CGFloat, _ height : CGFloat, _ textString:String, _ inputString:String, _ parent : IElement? = nil,  id:String? = nil) {
+    init(_ width : CGFloat, _ height : CGFloat, _ textString:String, _ inputString:String, _ parent : IElement? = nil,  _ id:String? = nil) {
         self.textString = textString
         self.inputString = inputString
         super.init(width, height, parent, id)

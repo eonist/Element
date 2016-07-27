d46437105d6e988c04c1a006ef9ad6fc405de2cd
Eonist <unicornhive@gmail.com>
2016-07-27 15:39:47 +0200

    Files modified: 1
    Modified 1 file:src/utils/group/utils/SelectGroupExtensions.swift

--- src/utils/group/utils/SelectGroupExtensions.swift
+++ src/utils/group/utils/SelectGroupExtensions.swift
@@ -1,8 +1,11 @@
 import Cocoa
 
 extension SelectGroup {
-    func selected(selectables:Array<ISelectable>) -> ISelectable? {return SelectParser.selected(self.selectables)}//Convenience
+    /*Parsing*/
+    func getSelected() -> ISelectable? {return SelectParser.selected(self.selectables)}//Convenience
     func index(selectable:ISelectable)->Int{return SelectGroupParser.index(self, selectable)}//Convenience
-    func indexOfSelected(selectGroup:SelectGroup)->Int{return SelectGroupParser.indexOfSelected(self)}//Convenience
-    func selectableAt(selectGroup:SelectGroup,_ index:Int)->ISelectable?{return SelectGroupParser.selectableAt(self, index)}//Convenience
+    var indexOfSelected:Int{return SelectGroupParser.indexOfSelected(self)}//Convenience
+    func selectableAt(index:Int)->ISelectable?{return SelectGroupParser.selectableAt(self, index)}//Convenience
+    /*Modifiers*/
+    func selectedAt(index:Int){SelectGroupModifier.selectedAt(self, index)}
 }

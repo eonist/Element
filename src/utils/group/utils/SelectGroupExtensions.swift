138df343bfebf1b8e1b44b4f0e12e0958e9c6102
Eonist <unicornhive@gmail.com>
2016-07-27 15:42:18 +0200

    Files modified: 1
    Modified 1 file:src/utils/group/utils/SelectGroupExtensions.swift

--- src/utils/group/utils/SelectGroupExtensions.swift
+++ src/utils/group/utils/SelectGroupExtensions.swift
@@ -7,5 +7,7 @@ extension SelectGroup {
     var indexOfSelected:Int{return SelectGroupParser.indexOfSelected(self)}//Convenience
     func selectableAt(index:Int)->ISelectable?{return SelectGroupParser.selectableAt(self, index)}//Convenience
     /*Modifiers*/
-    func selectedAt(index:Int){SelectGroupModifier.selectedAt(self, index)}
+    func selectedAt(index:Int){SelectGroupModifier.selectedAt(self, index)}//Convenience
+    func select(selectable:ISelectable){SelectGroupModifier.select(self, selectable)}//Convenience
+    func removeSelectable(item:ISelectable)->ISelectable? {return SelectGroupModifier.removeSelectable(self, item)}//Convenience
 }

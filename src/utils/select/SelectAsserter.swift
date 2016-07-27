884fea2a86bf4f3994b059e7236ced959da59329
Eonist <unicornhive@gmail.com>
2016-07-27 22:59:40 +0200

    Files modified: 1
    Modified 1 file:src/utils/select/SelectAsserter.swift

--- src/utils/select/SelectAsserter.swift
+++ src/utils/select/SelectAsserter.swift
@@ -1,5 +1,16 @@
-import Foundation
+import Cocoa
 
 class SelectAsserter {
-    
+    /**
+     *
+     */
+    class func hasSelected(view:NSView) -> Bool {
+        return SelectParser.selected(view) != nil
+    }
+    /**
+     *
+     */
+    class func hasSelected(selectables:Array<ISelectable>) -> Bool {
+        return SelectParser.selected(selectables) != nil
+    }
 }

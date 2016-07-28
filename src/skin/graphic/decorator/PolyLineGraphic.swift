8c1305e2e1d86c61682408f0f4064199a516326e
Eonist <unicornhive@gmail.com>
2016-07-29 00:52:53 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PolyLineGraphic.swift

--- src/skin/graphic/decorator/PolyLineGraphic.swift
+++ src/skin/graphic/decorator/PolyLineGraphic.swift
@@ -14,6 +14,7 @@ class PolyLineGraphic:SizeableDecorator{
         
         //continue here: Use similar boundingBox code as PathGraphic, actually you could just extend PathGraphic and override init and convert points to a Path
         
+        
         //Swift.print("PolyLineGraphic.drawLine()")
         graphic.lineShape.path = CGPathParser.lines(points, false, CGPoint(0,0))
     }

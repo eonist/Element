011c03f5cbda9f3ce3c41e88b71197e653cee2cb
Eonist <unicornhive@gmail.com>
2016-07-30 19:45:22 +0200

    Files modified: 1
    Modified 1 file:src/ui/button/select/ISelectable.swift

--- src/ui/button/select/ISelectable.swift
+++ src/ui/button/select/ISelectable.swift
@@ -7,3 +7,6 @@ protocol ISelectable: class/*derive only classes for the protocol, not structs, 
     /*using isSelcted could be the wrong way to go about this-> getSelected would work better->*//*var isSelected:Bool{get}*//*This is named isSelected because selected is ocupied by obc and using selected() as a method seems inconsistent*/
     func getSelected()->Bool//<--shouldnt this be isSelected? :TODO: this should be getSelected since composite classes can impliment ICHeckable and they will need to access a sub instance via a implimcit getter method, same for IDisableable, ISelectable, IFocusable etc
 }
+extension ISelectable{/*<-- New, simplifes access, while maininting easy overriding abilities with the functions in the Iselectable protocol*/
+    var selected:Bool {get{return getSelected()}set{ setSelected(newValue)}}
+}

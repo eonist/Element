dcf25feb4e24c5adc8d0fb147090d976e65e0ee1
Eonist <unicornhive@gmail.com>
2016-08-01 13:42:30 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PathGraphic.swift

--- src/skin/graphic/decorator/PathGraphic.swift
+++ src/skin/graphic/decorator/PathGraphic.swift
@@ -4,7 +4,7 @@ import Foundation
  */
 class PathGraphic:SizeableDecorator{
     var path:IPath
-    lazy var cgPath:CGMutablePathRef = DisplayPathUtils.compile(CGPathCreateMutable(), self.path)/*this is lazy because both drawFill and drawLine uses it, its risky but convenient*/
+    lazy var cgPath:CGMutablePathRef = CGPathUtils.compile(CGPathCreateMutable(), self.path)/*this is lazy because both drawFill and drawLine uses it, its risky but convenient*/
     lazy var fillBoundingBox:CGRect = CGPathGetPathBoundingBox(self.cgPath)/*this is lazy because both drawFill and drawLine uses it, its risky but convenient*/
     init(_ path:IPath, _ decoratable: IGraphicDecoratable = BaseGraphic(nil,LineStyle())) {
         //Swift.print("PathGraphic.init()")

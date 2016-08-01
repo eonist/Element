d57e8dbb88879ca90ed64830d9c66378378562be
Eonist <unicornhive@gmail.com>
2016-08-01 14:05:29 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/decorator/PolyLineGraphic.swift

--- src/skin/graphic/decorator/PolyLineGraphic.swift
+++ src/skin/graphic/decorator/PolyLineGraphic.swift
@@ -4,7 +4,7 @@ class PolyLineGraphic:PathGraphic{
     var points:Array<CGPoint>
     init(_ points:Array<CGPoint>, _ decoratable: IGraphicDecoratable = BaseGraphic(nil,LineStyle())) {
         self.points = points
-        let path:IPath = PathParser.path(points)/*convert points to a Path*/
+        let path:IPath = BasicPathParser.path(points)/*convert points to a Path*/
         super.init(path, decoratable)
     }
     //----------------------------------
@@ -23,5 +23,20 @@ extension PolyLineGraphic{
     convenience init(_ points:Array<CGPoint>, _ gradientFillStyle:IGradientFillStyle?, _ gradientlineStyle:IGradientLineStyle?) {
         Swift.print("PolyLineGraphic.init() type: GradientGraphic")
         self.init(points, GradientGraphic(BaseGraphic(gradientFillStyle,gradientlineStyle)))
+    }
+}
+private class BasicPathParser{
+    /**
+     * // :TODO: rename to pathByPoints?
+     */
+    class func path(points:Array<CGPoint>) -> IPath {
+        var commands:Array<Int> = [PathCommand.MOVE_TO]
+        var pathData:Array<CGFloat> = [points[0].x,points[0].y]
+        for (var i : Int = 1; i < points.count; i++) {
+            commands.append(PathCommand.LINE_TO)
+            let p:CGPoint = points[i]
+            pathData += [p.x,p.y]
+        }
+        return Path(commands, pathData)
     }
 }

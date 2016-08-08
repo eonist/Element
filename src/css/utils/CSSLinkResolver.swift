c9211d3d52f81bb363b9ae9aad7759803c96d016
Eonist <unicornhive@gmail.com>
2016-08-08 23:58:39 +0200

    Files modified: 1
    Modified 1 file:src/css/utils/CSSLinkResolver.swift

--- src/css/utils/CSSLinkResolver.swift
+++ src/css/utils/CSSLinkResolver.swift
@@ -1,8 +1,8 @@
 import Foundation
 /*
-* TODO: rename this class to CSSPointerResolver? Since it can be confused with resolving css View container urls?
-* EXAMPLE: Swift.print(CSSLinkResolver.resolveLinks("Button{fill:<ButtonBase>;} ButtonBase{fill:green;line:blue;} CheckButton{line:<ButtonBase>;}"))//Button{fill:green;} ButtonBase{fill:green;line:blue;} CheckButton{line:blue;}
-*/
+ * TODO: rename this class to CSSPointerResolver? Since it can be confused with resolving css View container urls?
+ * EXAMPLE: Swift.print(CSSLinkResolver.resolveLinks("Button{fill:<ButtonBase>;} ButtonBase{fill:green;line:blue;} CheckButton{line:<ButtonBase>;}"))//Button{fill:green;} ButtonBase{fill:green;line:blue;} CheckButton{line:blue;}
+ */
 class CSSLinkResolver {
     static var precedingWith:String = "(?<=\\;|^|\\{)"
     static var whiteSpace:String = "[\\n\\s\\t\\v\\r]*?"
@@ -13,9 +13,9 @@ class CSSLinkResolver {
     enum CSSElementType:Int{ case name = 1, value}
     /**
      * Returns a CSS string with all css links resolved, a css link is where a key points to another key to obtain its value
-     * @Note: this method is recursive and traverses down through all its decendants. Think of a tree diagram
-     * @Note: also resolves the weight of each style and merges styles according to the CSS methodology
-     * // :TODO: the replaceRange method may slow things down research a better replace method
+     * NOTE: this method is recursive and traverses down through all its decendants. Think of a tree diagram
+     * NOTE: also resolves the weight of each style and merges styles according to the CSS methodology
+     * TODO: the replaceRange method may slow things down research a better replace method
      * EXAMPLE: resolveLinks("CustomButton{fill:yellow;} CustomButton:down{fill:green;}")//This is the log if you log the name and values: Name: fill, Value: yellow, Name: fill,Value: green
      * NOTE: you could also do a forward for loop and then offset the range as your traverse the matches
      */
@@ -45,7 +45,7 @@ private class Utils {
      * // :TODO: write an example
      * NOTE: there is no speed benefit of optimizing querrying for linkedStyleProperty
      * NOTE: the sansBracketPattern basically finds the content inside the "<" and the ">"
-     * @param cssString: is the original css string in its entirety (not a form of excerpt)
+     * PARAM: cssString: is the original css string in its entirety (not a form of excerpt)
      * NOTE: you could also maybe loop backwards an replace that way, then you wouldnt have to store a new index
      */
     class func replaceLinks(var string:String,_ linkPropName:String,_ cssString:String)->String{
@@ -70,10 +70,10 @@ private class Utils {
         return string;
     }
     /**
-     * Returns a style property value by parsing through @param string and tries to find a match that matches both @param linkName and @param propertyName
-     * @param linkName the linkname is style name to search for
-     * @param propertyName is the property name for the property value to be returned
-     * @param string is the entire css document combined into 1 string
+     * Returns a style property value by parsing through PARAM: string and tries to find a match that matches both PARAM: linkName and PARAM: propertyName
+     * PARAM: linkName the linkname is style name to search for
+     * PARAM: propertyName is the property name for the property value to be returned
+     * PARAM: string is the entire css document combined into 1 string
      * // :TODO: write an example
      * // :TODO: you can write a more precise expression to match the content of a style block
      */
@@ -90,9 +90,9 @@ private class Utils {
         }
     }
     /**
-     * Returns a propertyValue from @param str (a style block)
-     * @param string a style block
-     * @param propName the property name for the property value to be returned
+     * Returns a propertyValue from PARAM: str (a style block)
+     * PARAM: string a style block
+     * PARAM: propName the property name for the property value to be returned
      * write an example
      * // :TODO: you can write a more precise expression to match the content of a style block
      */
@@ -100,7 +100,7 @@ private class Utils {
         let pattern:String = "(?<=" + propName + "\\:).+?(?=\\;)"
         var match:Array = RegExp.match(string, pattern)
         if(match.count > 0){/*this try catch method is here so its easier to debug which propName threw an error*/
-            return match[0];
+            return match[0]
         }else{
             fatalError(" string:>"+string+"<"+" propName:>"+propName+"<")
         }

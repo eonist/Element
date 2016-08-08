b62ea4a524c8c9e3bdc10f9ef5ff27c04f832c38
Eonist <unicornhive@gmail.com>
2016-08-09 00:35:31 +0200

    Files modified: 1
    Modified 1 file:src/skin/graphic/utils/GraphicModifier.swift

--- src/skin/graphic/utils/GraphicModifier.swift
+++ src/skin/graphic/utils/GraphicModifier.swift
@@ -5,9 +5,9 @@ class GraphicModifier {
      * TODO: fill and linestyle should be graphic spessific see original code
      */
     class func applyProperties(inout decoratable:IGraphicDecoratable,_ fillStyle:IFillStyle,_ lineStyle:ILineStyle?,_ offsetType:OffsetType)->IGraphicDecoratable {
-        decoratable.graphic.fillStyle = fillStyle;
-        decoratable.graphic.lineStyle = lineStyle;
-        decoratable.graphic.lineOffsetType = offsetType;
-        return decoratable;
+        decoratable.graphic.fillStyle = fillStyle
+        decoratable.graphic.lineStyle = lineStyle
+        decoratable.graphic.lineOffsetType = offsetType
+        return decoratable
     }
 }

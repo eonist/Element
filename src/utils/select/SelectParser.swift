3c2795e42bcb1a19b40ef02af13df379dcb159f7
Eonist <unicornhive@gmail.com>
2016-08-09 19:37:23 +0200

    Files modified: 1
    Modified 1 file:src/utils/select/SelectParser.swift

--- src/utils/select/SelectParser.swift
+++ src/utils/select/SelectParser.swift
@@ -3,6 +3,7 @@ import Cocoa
 class SelectParser {
     /**
      * Returns the  first selected ISelectable in an array of an NSView with ISelectables
+     * TODO: Rename to firstSelected
      */
     class func selected(view:NSView) -> ISelectable? {
         let selectables:Array<ISelectable> = self.selectables(view)

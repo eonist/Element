39743c30905c925a7f61c8a9fc8d5008e18152c5
Eonist <unicornhive@gmail.com>
2016-08-09 23:54:50 +0200

    Files modified: 1
    Modified 1 file:src/skin/skin/utils/SkinStates.swift

--- src/skin/skin/utils/SkinStates.swift
+++ src/skin/skin/utils/SkinStates.swift
@@ -1,14 +1,17 @@
 import Foundation
-
+/**
+ * TODO: These will probably be upgraded to enums soon, since swift has awesome syntactic support for enums
+ */
 class SkinStates {
-    static var none:String = "";/*aka idle*/
-    static var over:String = "over";
-    static var down:String = "down";
-    
-    static var selected:String = "selected";
-    static var checked:String = "checked";
-    static var focused:String = "focused";
-    static var disabled:String = "disabled";
+    /*core*/
+    static var none:String = ""/*aka idle*/
+    static var over:String = "over"
+    static var down:String = "down"
+    /*other*/
+    static var selected:String = "selected"
+    static var checked:String = "checked"
+    static var focused:String = "focused"
+    static var disabled:String = "disabled"
 }
 /*
 enum SkinStates:String

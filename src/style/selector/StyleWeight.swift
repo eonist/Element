575acc85794192d5e2e73d22f8e61e1536911fd3
Eonist <unicornhive@gmail.com>
2016-08-10 21:45:00 +0200

    Files modified: 2
    Modified 2 files:src/style/selector/StyleWeight.swiftsrc/style/selector/utils/SelectorAsserter.swift

--- src/style/selector/StyleWeight.swift
+++ src/style/selector/StyleWeight.swift
@@ -3,18 +3,18 @@ import Foundation
  * // :TODO: move into its own class? if other classses uses this code yes if not then no!
  */
 class StyleWeight {
-    var selectorWeights:Array<SelectorWeight>;
-    var idWeight:Int = 0;
-    var elementWeight:Int = 0;
-    var classWeight:Int = 0;
-    var stateWeight:Int = 0;
+    var selectorWeights:Array<SelectorWeight>
+    var idWeight:Int = 0
+    var elementWeight:Int = 0
+    var classWeight:Int = 0
+    var stateWeight:Int = 0
     init(_ selectorWeights:Array<SelectorWeight>) {
-       self.selectorWeights = selectorWeights;
+       self.selectorWeights = selectorWeights
        for selectorItemWeight : SelectorWeight in selectorWeights {
-          self.elementWeight += selectorItemWeight.hasElement ? selectorItemWeight.weight : 0;
-          self.classWeight += selectorItemWeight.hasClassId ? selectorItemWeight.weight : 0;
-          self.idWeight += selectorItemWeight.hasId ? selectorItemWeight.weight : 0;
-          self.stateWeight += selectorItemWeight.hasState ? selectorItemWeight.weight : 0;
+          self.elementWeight += selectorItemWeight.hasElement ? selectorItemWeight.weight : 0
+          self.classWeight += selectorItemWeight.hasClassId ? selectorItemWeight.weight : 0
+          self.idWeight += selectorItemWeight.hasId ? selectorItemWeight.weight : 0
+          self.stateWeight += selectorItemWeight.hasState ? selectorItemWeight.weight : 0
        }
     }
 }
--- src/style/selector/utils/SelectorAsserter.swift
+++ src/style/selector/utils/SelectorAsserter.swift
@@ -11,36 +11,36 @@ class SelectorAsserter {
      *
      */
     class func hasCommonality(styleSel:ISelector,_ querrySelector:ISelector)->Bool {
-       let hasMatchingId:Bool = SelectorAsserter.hasMatchingId(styleSel, querrySelector);
-       let hasMatchingElement:Bool = SelectorAsserter.hasMatchingElement(styleSel, querrySelector);
-       let hasBothSelectorsClassIds:Bool = SelectorAsserter.hasBothSelectorsClassIds(styleSel, querrySelector);
-       let hasMatchingClassIds:Bool = hasBothSelectorsClassIds && SelectorAsserter.hasMatchingClassIds(styleSel, querrySelector);/*all styleSel classIds are present in stackSel classIds*/
-       let numOfMatchingStates:Int = SelectorParser.numOfSimilarStates(styleSel, querrySelector);
-       let hasSimilarState:Bool = SelectorAsserter.hasSimilarState(styleSel, querrySelector);
-       let hasStyleSelStates:Bool = SelectorAsserter.hasStates(styleSel);
-       let a:Bool = (hasMatchingId || styleSel.id == ""/*nil*/);
-       let b:Bool = hasMatchingElement || styleSel.element == ""/*nil*/;
-       let c:Bool = (hasMatchingClassIds || !hasBothSelectorsClassIds);
-       let d:Bool = (!hasStyleSelStates || (hasSimilarState && numOfMatchingStates <= querrySelector.states.count));
-       return a && b && c && d;
+       let hasMatchingId:Bool = SelectorAsserter.hasMatchingId(styleSel, querrySelector)
+       let hasMatchingElement:Bool = SelectorAsserter.hasMatchingElement(styleSel, querrySelector)
+       let hasBothSelectorsClassIds:Bool = SelectorAsserter.hasBothSelectorsClassIds(styleSel, querrySelector)
+       let hasMatchingClassIds:Bool = hasBothSelectorsClassIds && SelectorAsserter.hasMatchingClassIds(styleSel, querrySelector)/*all styleSel classIds are present in stackSel classIds*/
+       let numOfMatchingStates:Int = SelectorParser.numOfSimilarStates(styleSel, querrySelector)
+       let hasSimilarState:Bool = SelectorAsserter.hasSimilarState(styleSel, querrySelector)
+       let hasStyleSelStates:Bool = SelectorAsserter.hasStates(styleSel)
+       let a:Bool = (hasMatchingId || styleSel.id == ""/*nil*/)
+       let b:Bool = hasMatchingElement || styleSel.element == ""/*nil*/
+       let c:Bool = (hasMatchingClassIds || !hasBothSelectorsClassIds)
+       let d:Bool = (!hasStyleSelStates || (hasSimilarState && numOfMatchingStates <= querrySelector.states.count))
+       return a && b && c && d
     }
     class func hasMatchingElement(a:ISelector,_ b:ISelector)->Bool {
-      return a.element == b.element;
+      return a.element == b.element
     }
     class func hasMatchingId(a:ISelector,_ b:ISelector)->Bool {
-      return a.id == b.id;
+      return a.id == b.id
     }
     class func hasMatchingClassIds(a:ISelector,_ b:ISelector)->Bool {
-      return ArrayAsserter.contains(a.classIds, b.classIds, true);//<----this may be wrong as it compares if the instance has the same variables, but the original code compared reference, which seemed wrong to me
+      return ArrayAsserter.contains(a.classIds, b.classIds, true)//<----this may be wrong as it compares if the instance has the same variables, but the original code compared reference, which seemed wrong to me
     }
     class func hasBothSelectorsIds(a:ISelector,_ b:ISelector)->Bool {
-      return a.id != ""/*nil*/ && b.id != ""/*nil*/;
+      return a.id != ""/*nil*/ && b.id != ""/*nil*/
     }
     class func hasBothSelectorsClassIds(a:ISelector,_ b:ISelector)->Bool {
-      return a.classIds.count != 0  /*!= nil*/ && b.classIds.count != 0 /*!= nil*/;
+      return a.classIds.count != 0  /*!= nil*/ && b.classIds.count != 0 /*!= nil*/
     }
     class func hasBothSelectorsStates(a:ISelector,_ b:ISelector)->Bool {
-      return a.states.count != 0/* != nil*/ && b.states.count != 0/* != nil*/;
+      return a.states.count != 0/* != nil*/ && b.states.count != 0/* != nil*/
     }
     class func hasSimilarState(a:ISelector,_ b:ISelector)->Bool {
       return SelectorParser.numOfSimilarStates(a, b) > 0;

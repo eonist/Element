83be077aaf9130f1977d94937025692f80b9be03
Eonist <unicornhive@gmail.com>
2016-08-10 22:00:21 +0200

    Files modified: 1
    Modified 1 file:src/style/selector/WeightedStyleAsserter.swift

--- src/style/selector/WeightedStyleAsserter.swift
+++ src/style/selector/WeightedStyleAsserter.swift
@@ -9,53 +9,53 @@ class WeightedStyleAsserter {
      * TODO: You could just do specificity how w3c does it. with assigning points to the different types. 10,100,1000,10000 etc. it works for them, it can work for you
      */
     class func priority(a:WeightedStyle,_ b:WeightedStyle)->Bool {// :TODO: rename to assertSpecificity
-        //var temp:String = "Window";//Box#tabBarBox SelectTextButton Text
-        var priority:Bool = false;
-        let elementPriority:Bool = a.styleWeight.elementWeight >= b.styleWeight.elementWeight;
-        //if(a.name == temp) trace("elementPriority: " + elementPriority);
-        let elementCountPriority:Bool = elementCount(a.selectors) > elementCount(b.selectors);
-        //if(a.name == temp) trace("elementCountPriority: " + elementCountPriority);
-        let classPriority:Bool = a.styleWeight.classWeight > b.styleWeight.classWeight;
-        //if(a.name == temp) trace("classPriority: " + classPriority);
-        let idPriority:Bool = a.styleWeight.idWeight >= b.styleWeight.idWeight;
-        //if(a.name == temp) trace("idPriority: " + idPriority);
-        let statePriority:Bool = a.styleWeight.stateWeight > b.styleWeight.stateWeight;
-        //if(a.name == temp) trace("statePriority: " + statePriority);
-        let hasSelectorPriority:Bool = (idPriority && elementCountPriority) || (classPriority && b.styleWeight.idWeight == 0) || (elementPriority && idPriority/*&& b.styleWeight.idWeight == 0 && b.styleWeight.classWeight == 0*/);
-        //if(a.name == temp)trace("hasSelectorPriority: " + hasSelectorPriority);
-        //print("b.styleWeight.idWeight: " + b.styleWeight.idWeight);
-        //print("b.styleWeight.classWeight: " + b.styleWeight.classWeight);
-        let hasSelector:Bool = a.styleWeight.elementWeight > 0 || a.styleWeight.classWeight > 0 || a.styleWeight.idWeight > 0;
-        let hasState:Bool = a.styleWeight.stateWeight > 0;
-        let hasEqualStateWeight:Bool = a.styleWeight.stateWeight == b.styleWeight.stateWeight;
-        //print("a.styleWeight.stateWeight: " + a.styleWeight.stateWeight);
-        //print("b.styleWeight.stateWeight: " + b.styleWeight.stateWeight);
-        //print(a.name+ " hasSelector: " + hasSelector);
+        //var temp:String = "Window"//Box#tabBarBox SelectTextButton Text
+        var priority:Bool = false
+        let elementPriority:Bool = a.styleWeight.elementWeight >= b.styleWeight.elementWeight
+        //if(a.name == temp) print("elementPriority: " + elementPriority)
+        let elementCountPriority:Bool = elementCount(a.selectors) > elementCount(b.selectors)
+        //if(a.name == temp) print("elementCountPriority: " + elementCountPriority)
+        let classPriority:Bool = a.styleWeight.classWeight > b.styleWeight.classWeight
+        //if(a.name == temp) print("classPriority: " + classPriority)
+        let idPriority:Bool = a.styleWeight.idWeight >= b.styleWeight.idWeight
+        //if(a.name == temp) print("idPriority: " + idPriority)
+        let statePriority:Bool = a.styleWeight.stateWeight > b.styleWeight.stateWeight
+        //if(a.name == temp) print("statePriority: " + statePriority)
+        let hasSelectorPriority:Bool = (idPriority && elementCountPriority) || (classPriority && b.styleWeight.idWeight == 0) || (elementPriority && idPriority/*&& b.styleWeight.idWeight == 0 && b.styleWeight.classWeight == 0*/)
+        //if(a.name == temp)trace("hasSelectorPriority: " + hasSelectorPriority)
+        //print("b.styleWeight.idWeight: " + b.styleWeight.idWeight)
+        //print("b.styleWeight.classWeight: " + b.styleWeight.classWeight)
+        let hasSelector:Bool = a.styleWeight.elementWeight > 0 || a.styleWeight.classWeight > 0 || a.styleWeight.idWeight > 0
+        let hasState:Bool = a.styleWeight.stateWeight > 0
+        let hasEqualStateWeight:Bool = a.styleWeight.stateWeight == b.styleWeight.stateWeight
+        //print("a.styleWeight.stateWeight: " + a.styleWeight.stateWeight)
+        //print("b.styleWeight.stateWeight: " + b.styleWeight.stateWeight)
+        //print(a.name+ " hasSelector: " + hasSelector)
         if(hasSelectorPriority && a.styleWeight.stateWeight == 0 && b.styleWeight.stateWeight == 0){/*both have no stateWeight*/
-            priority = true;
-            //if(a.name == temp) trace("BLOCK - 0: "+priority);
+            priority = true
+            //if(a.name == temp) trace("BLOCK - 0: "+priority)
         }
         else if(hasSelectorPriority && hasEqualStateWeight) {
-            priority = assertStateWeight(a, b);
-            //if(a.name == temp) trace("BLOCK - 1: "+priority);
+            priority = assertStateWeight(a, b)
+            //if(a.name == temp) trace("BLOCK - 1: "+priority)
         }
         else if(hasState && statePriority) {
-            //if(a.name == temp) trace("BLOCK - 2");
+            //if(a.name == temp) trace("BLOCK - 2")
             priority = true;
         }
         else if(hasSelector && statePriority) {
-            //			if(a.name == temp) trace("BLOCK - 3");
+            //if(a.name == temp) trace("BLOCK - 3")
             priority = true;
         }
         else if(hasSelector && hasEqualStateWeight) {
-            //print("a.name: " + a.name);
-            //print("b.name: " + b.name);
-            //print("a.styleWeight.elementWeight: " + a.styleWeight.elementWeight);
-            //print("b.styleWeight.elementWeight: " + b.styleWeight.elementWeight);
-            //print("a.styleWeight.stateWeight: " + a.styleWeight.stateWeight);
-            //print("b.styleWeight.stateWeight: " + b.styleWeight.stateWeight);
-            priority = a.styleWeight.stateWeight == 0 && b.styleWeight.stateWeight == 0 ? false:assertStateWeight(a, b);/*temp fix*/// :TODO: the problem here is that you need to also have a boolean if both states are 0?!?
-            //if(a.name == temp) trace("BLOCK - 4: "+priority);
+            //print("a.name: " + a.name)
+            //print("b.name: " + b.name)
+            //print("a.styleWeight.elementWeight: " + a.styleWeight.elementWeight)
+            //print("b.styleWeight.elementWeight: " + b.styleWeight.elementWeight)
+            //print("a.styleWeight.stateWeight: " + a.styleWeight.stateWeight)
+            //print("b.styleWeight.stateWeight: " + b.styleWeight.stateWeight)
+            priority = a.styleWeight.stateWeight == 0 && b.styleWeight.stateWeight == 0 ? false:assertStateWeight(a, b)/*temp fix*/// :TODO: the problem here is that you need to also have a boolean if both states are 0?!?
+            //if(a.name == temp) trace("BLOCK - 4: "+priority)
         }
         else if(hasState && hasEqualStateWeight){
             priority = assertStateWeight(a, b);
@@ -81,11 +81,11 @@ class WeightedStyleAsserter {
      */
     private class func assertStateWeight(a:WeightedStyle,_ b:WeightedStyle)->Bool{// :TODO: could use a better name
         //if(a.styleWeight.selectorWeights.length != b.styleWeight.selectorWeights.length) return false;/*<--new code*/
-        //print("a.styleWeight.selectorWeights.length: " + a.styleWeight.selectorWeights.length);
-        //print("b.styleWeight.selectorWeights.length: " + b.styleWeight.selectorWeights.length);
-        //if(a.styleWeight.selectorWeights.length > b.styleWeight.selectorWeights.length) return false;/*<--new code 2,caution this may present unknown problems, this code snippet was added to support styles that the parent has a state ie when you want a text tp react to a Button state*/
-        //print("a.styleWeight.selectorWeights.length: " + a.styleWeight.selectorWeights.length);
-        //print("b.styleWeight.selectorWeights.length: " + b.styleWeight.selectorWeights.length);
+        //print("a.styleWeight.selectorWeights.length: " + a.styleWeight.selectorWeights.length)
+        //print("b.styleWeight.selectorWeights.length: " + b.styleWeight.selectorWeights.length)
+        //if(a.styleWeight.selectorWeights.length > b.styleWeight.selectorWeights.length) return false/*<--new code 2,caution this may present unknown problems, this code snippet was added to support styles that the parent has a state ie when you want a text tp react to a Button state*/
+        //print("a.styleWeight.selectorWeights.length: " + a.styleWeight.selectorWeights.length)
+        //print("b.styleWeight.selectorWeights.length: " + b.styleWeight.selectorWeights.length)
         //for (var i : int = 0; i < a.styleWeight.selectorWeights.length; i++) {
         //	var aSelectorWeight:SelectorWeight = a.styleWeight.selectorWeights[i];
         //	var bSelectorWeight:SelectorWeight = b.styleWeight.selectorWeights[i];

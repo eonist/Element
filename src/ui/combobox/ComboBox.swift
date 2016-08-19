e0e0946ce15f8dd29f302a448c1c99ed0dda4b94
Eonist <unicornhive@gmail.com>
2016-08-19 20:47:45 +0200

    Files modified: 1
    Modified 1 file:src/ui/combobox/ComboBox.swift

--- src/ui/combobox/ComboBox.swift
+++ src/ui/combobox/ComboBox.swift
@@ -176,8 +176,9 @@ class PopupView:WindowView{
 
 //1. on click outside of the popup window needs to be recorded (done)
 //2. click a button in popupWin sends an event and then closes it self (done)
-//3. the popupWindow needs to be stored somewhere, maybe in a static variable or somewhere else in the Element framework
+//3. the popupWindow needs to be stored somewhere, maybe in a static variable or somewhere else in the Element framework (done)
 //4. Make an universal alignment method for aligning windows, you can probably use the regular Align method here with a zeroSize and CGPoint and TopCenter as the alignment type
 //5. try to animate the popup effect
+//6. the popupwin must have an init with size and position, 
 
 

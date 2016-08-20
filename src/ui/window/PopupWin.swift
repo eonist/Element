4e2dbc86dd567a8ea066887e061fa3565c2acd99
Eonist <unicornhive@gmail.com>
2016-08-20 12:37:24 +0200

    Files modified: 1
    Modified 1 file:src/ui/window/PopupWin.swift

--- src/ui/window/PopupWin.swift
+++ src/ui/window/PopupWin.swift
@@ -1,5 +1,40 @@
-import Foundation
+import Cocoa
 
-class PopupWin {
-
+class PopupWindow:Window{
+    required init(_ width: CGFloat, _ height: CGFloat) {
+        super.init(width,height)
+        WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter)
+    }
+    override func resolveSkin() {
+        super.resolveSkin()
+        self.contentView = PopupView(frame.width,frame.height,nil,"special")/*Sets the mainview of the window*/
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+/**
+ * NOTE: the difference between local and global monitors is that local takes care of events that happen inside an app window, and the global handle events that also is detected outside an app window.
+ */
+class PopupView:WindowView{
+    var leftMouseDownEventListener:AnyObject?
+    override func resolveSkin() {
+        Swift.print("PopupView.resolveSkin")
+        StyleManager.addStyle("Window#special{fill:red;}")
+        super.resolveSkin()
+        if(leftMouseDownEventListener == nil) {leftMouseDownEventListener = NSEvent.addGlobalMonitorForEventsMatchingMask([.LeftMouseDownMask], handler:self.onMouseDown ) }//we add a global mouse move event listener
+        else {fatalError("This shouldn't be possible, if it throws this error then you need to remove he eventListener before you add it")}
+    }
+    
+    func onMouseDown(event:NSEvent) {
+        Swift.print("PopupView.onMouseDown()")
+        super.onEvent(Event(Event.update,self))
+        if(leftMouseDownEventListener != nil){
+            NSEvent.removeMonitor(leftMouseDownEventListener!)
+            leftMouseDownEventListener = nil
+        }
+        //TODO: set the event to it self again here
+        self.window!.close()
+        //return event
+    }
 }

f2d2b49043b2e626f483b83fff770f06f10263e8
Eonist <unicornhive@gmail.com>
2016-08-20 13:54:02 +0200

    Files modified: 1
    Modified 1 file:src/ui/combobox/utils/ComboBoxWin.swift

--- src/ui/combobox/utils/ComboBoxWin.swift
+++ src/ui/combobox/utils/ComboBoxWin.swift
@@ -32,7 +32,7 @@ class ComboBoxView:PopupView{
     override func resolveSkin() {
         super.resolveSkin()
         list = addSubView(List(width, height, itemHeight, dataProvider, self))
-        //ListModifier.selectAt(list!, initSelectedIndex)
+        ListModifier.selectAt(list!, initSelectedIndex)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

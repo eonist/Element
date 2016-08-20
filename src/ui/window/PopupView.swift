acc2b9bbdcf9377272c98b1fddadd6bfb6047abb
Eonist <unicornhive@gmail.com>
2016-08-20 18:32:58 +0200

    Files modified: 1
    Modified 1 file:src/ui/window/PopupView.swift

--- src/ui/window/PopupView.swift
+++ src/ui/window/PopupView.swift
@@ -2,6 +2,7 @@ import Cocoa
 
 /**
  * NOTE: the difference between local and global monitors is that local takes care of events that happen inside an app window, and the global handle events that also is detected outside an app window.
+ * TODO: add support for NSNotification: windowDidResignKey -> close the popup window when this event happens
  */
 class PopupView:WindowView{
     var leftMouseDownEventListener:AnyObject?
@@ -12,7 +13,6 @@ class PopupView:WindowView{
         if(leftMouseDownEventListener == nil) {leftMouseDownEventListener = NSEvent.addLocalMonitorForEventsMatchingMask([.LeftMouseDownMask], handler:self.onMouseDown ) }//we add a global mouse move event listener
         else {fatalError("This shouldn't be possible, if it throws this error then you need to remove he eventListener before you add it")}
     }
-    
     func onMouseDown(event:NSEvent) -> NSEvent? {
         Swift.print("PopupView.onMouseDown()")
         super.onEvent(Event(Event.update,self))

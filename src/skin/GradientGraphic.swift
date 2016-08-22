c37404132a355b0ab70cd586ee1f38e66e529ec8
Eonist <unicornhive@gmail.com>
2016-08-22 13:22:50 +0200

    Files modified: 1
    Modified 1 file:src/skin/GradientGraphic.swift

--- src/skin/GradientGraphic.swift
+++ src/skin/GradientGraphic.swift
@@ -33,7 +33,7 @@ class GradientGraphic:SizeableDecorator/*<--recently changed from GraphicDecorat
             var boundingBox:CGRect = CGPathGetBoundingBox(graphic.lineShape.path) // this method can be moved up one level if its better for performance, but wait untill you impliment matrix etc
             boundingBox = boundingBox.outset(graphic.lineStyle!.thickness/2, graphic.lineStyle!.thickness/2)/*Outset the boundingbox to cover the entire stroke*/
             
-            //TODO: the above isn't totally correct, use the outlinestroke method and then get the boundingbox from that, think different caps etc
+            //TODO: the above isn't totally correct, use the outline-stroke method and then get the boundingbox from that, think different caps etc
             
             let graphicsGradient:IGraphicsGradient = Utils.graphicsGradient(boundingBox, gradient)
             graphic.lineShape.graphics.gradientLine(graphicsGradient)

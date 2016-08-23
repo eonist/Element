17880a226004d41282e68a161121234ba9dbbaa8
Eonist <unicornhive@gmail.com>
2016-08-23 12:30:03 +0200

    Files modified: 2
    Modified 2 files:src/ui/Element.swiftsrc/ui/IElement.swift

--- src/ui/IElement.swift
+++ src/ui/IElement.swift
@@ -15,7 +15,6 @@ protocol IElement:class,IView{/*:class <--- derive only classes for the protocol
     /*Getters / Setters*/
     var parent:IElement?{get}
     //var state:String{get set}/*skinState is renamed to state because objc won't allow implicit setter with the same name*/
-    var style:IStyle{get set}
     var skin:ISkin?{get set}
     var id : String?{get};
     var width:CGFloat{get /*set*/}

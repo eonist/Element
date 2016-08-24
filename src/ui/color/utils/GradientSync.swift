2f520876203fda1510666d07ef82fc968b99f9a8
Eonist <unicornhive@gmail.com>
2016-08-24 11:36:55 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/utils/GradientSync.swift

--- src/ui/color/utils/GradientSync.swift
+++ src/ui/color/utils/GradientSync.swift
@@ -1,5 +1,24 @@
-import Foundation
+import Cocoa
 
 class GradientSync {
+    static var receiver:IGradientInput?
+    static var broadcaster:IGradientInput?
+    class func onGradientChange(event:ColorInputEvent){
+        print("onColorChange" + "\(ColorSync.receiver)")
+        if(ColorSync.receiver != nil) {
+            print("_receiver: " + "\(ColorSync.receiver)")
+            //print("event.target: " + event.target);
+            //print("event.currentTarget: " + event.currentTarget);
+            ColorSync.broadcaster = event.origin as? IColorInput
+            ColorSync.receiver!.setColorValue(event.color!)
+            ColorSync.receiver!.onEvent(ColorInputEvent(ColorInputEvent.change,event.color!))
+        }
+    }
+    /**
+     * NOTE: this sets the color of the broadcaster not the receiver
+     */
+    class func setColor(color:NSColor) {
+        if (ColorSync.broadcaster != nil) {ColorSync.broadcaster!.setColorValue(color)}
+    }
 
 }

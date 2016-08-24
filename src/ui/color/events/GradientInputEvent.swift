3cae035701e17714727f5f3b2f4b52f5bfffb450
Eonist <unicornhive@gmail.com>
2016-08-24 11:40:54 +0200

    Files modified: 1
    Modified 1 file:src/ui/color/events/GradientInputEvent.swift

--- src/ui/color/events/GradientInputEvent.swift
+++ src/ui/color/events/GradientInputEvent.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class GradientInputEvent {
-
+class GradientInputEvent:Event{
+    static var change:String = "gradientInputChange"//probably should be ColorInputChange
+    override init(_ type:String = "", _ origin:AnyObject){
+        super.init(type, origin)
+    }
+}
+extension GradientInputEvent{
+    var gradient:IGradient? {return (origin as! IGradientInput).gradient}
 }

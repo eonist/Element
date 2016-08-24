ea96dbb63f732abbb5762844d600a000b98aaa22
Eonist <unicornhive@gmail.com>
2016-08-24 11:38:52 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:src/ui/color/events/ColorInputEvent.swift

--- src/ui/color/events/ColorInputEvent.swift
+++ src/ui/color/events/ColorInputEvent.swift
@@ -1,8 +1,8 @@
 import Cocoa
 
 class ColorInputEvent:Event {
-    static var change:String = "colorBoxChange"
-    static var colorBoxDown:String = "colorBoxDown"
+    static var change:String = "colorBoxChange"//probably should be ColorInputChange
+    static var colorBoxDown:String = "colorBoxDown"//this shouldnt exist?
     override init(_ type:String = "", _ origin:AnyObject){
         super.init(type, origin)
     }

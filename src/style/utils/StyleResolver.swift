93c9dd3a51b4190b7aa798f184e08aeac0c377bd
Eonist <unicornhive@gmail.com>
2016-09-09 23:19:05 +0200

    Files modified: 1
    Modified 1 file:src/style/utils/StyleResolver.swift

--- src/style/utils/StyleResolver.swift
+++ src/style/utils/StyleResolver.swift
@@ -6,6 +6,8 @@ class StyleResolver{
      *
      */
     static func style(element:IElement)->IStyle{
+        return resolveStyle(element)
+        /*
         let elementAddress = ElementParser.stackString(element)
         var index = -1
         for var i = 0; i < recently.count; ++i{
@@ -23,7 +25,8 @@ class StyleResolver{
             let style = resolveStyle(element)
             recently.unshift((style,elementAddress))//add to front
             return style
-        }  
+        }
+        */
     }
     /**
      * Returns a style comprised of all the styleProperties element inherit from

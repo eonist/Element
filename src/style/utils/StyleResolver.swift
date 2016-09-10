c939825c8a936eea6d1a496d4f2d7db63f071e1f
Eonist <unicornhive@gmail.com>
2016-09-10 18:27:44 +0200

    Files modified: 1
    Modified 1 file:src/style/utils/StyleResolver.swift

--- src/style/utils/StyleResolver.swift
+++ src/style/utils/StyleResolver.swift
@@ -58,10 +58,7 @@ class StyleResolver{
             return StyleManager.hashedStyles[i.string]!
         }
         
-        //test how many element selector addresses are similar
-        
-        //
-        
+       
         //let styles = getStyles(querrySelectors.count)
         for style : IStyle in StyleManager.styles/*styles*/ {/*This loop disregards styles that don't apply to the element Selectors*/
             styleLookUpCount++

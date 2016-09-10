9096fb95d0e1c39f261d6c1389a2969071f4970e
Eonist <unicornhive@gmail.com>
2016-09-10 19:42:00 +0200

    Files modified: 1
    Modified 1 file:src/style/manager/utils/StyleManagerUtils.swift

--- src/style/manager/utils/StyleManagerUtils.swift
+++ src/style/manager/utils/StyleManagerUtils.swift
@@ -1,6 +1,24 @@
 import Foundation
 
 class StyleManagerUtils{
-    
+    static func hashStyle(style:IStyle){
+        //when you add styles:
+        if(style.selectors.last!.element != ""){
+            if let styles = stylesByElement[style.selectors.lastItem.element]{
+                styles.append(style)
+            }else{
+                stylesByElement[style.selectors.lastItem.element] = [style]
+            }
+        }
+        if(style.selectors.lastItem.id != ""){
+            
+        }
+        if(style.selectors.lastItem.classIds.count > 0){
+            
+        }
+        if(style.selectors.lastItem.states.count > 0){
+            
+        }    
+    }
 }
 

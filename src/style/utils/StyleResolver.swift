66069aaecc36774145da3e5e05915bbc4ec89592
Eonist <unicornhive@gmail.com>
2016-09-11 17:48:36 +0200

    Files modified: 2
    Modified 2 files:src/skin/skin/SkinResolver.swiftsrc/style/utils/StyleResolver.swift

--- src/style/utils/StyleResolver.swift
+++ src/style/utils/StyleResolver.swift
@@ -37,7 +37,10 @@ class StyleResolver{
         }
         return finalStyle
     }
-    
+    static func style2(element:IElement)->IStyle{
+        let querySelectors:Array<ISelector> = ElementParser.selectors(element)/*Array instance comprised of Selector instances for each (element,classId,id and state) in the element*/
+        return style2(querySelectors,element)
+    }
     class func style2(querySelectors:[ISelector])->IStyle{
         var weightedStyles:Array<WeightedStyle> = StyleResolverUtils.query(querySelectors,StyleManager.styleTree,0)
         //print("weightedStyles: " + "\(weightedStyles.count)")

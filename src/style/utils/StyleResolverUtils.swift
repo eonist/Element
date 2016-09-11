d723038ce6b47c1823aa169fc9538a02c9f6f6d9
Eonist <unicornhive@gmail.com>
2016-09-11 17:09:14 +0200

    Files modified: 1
    Modified 1 file:src/style/utils/StyleResolverUtils.swift

--- src/style/utils/StyleResolverUtils.swift
+++ src/style/utils/StyleResolverUtils.swift
@@ -5,23 +5,23 @@ class StyleResolverUtils {
      * Return an array of WeightedStyle instances
      * @Note while loading the Basic.css flat structure queries 97656  vs treestructure queries 13324 (this is why we use a treestructure querey technique)
      */
-    static func query(querySelectors:[ISelector],_ searchTree:[String:Any],_ cursor:Int = 0, _ selectorWeights:[ISelector] = []) -> [IStyle]{
+    static func query(querySelectors:[ISelector],_ searchTree:[String:Any],_ cursor:Int = 0, _ selectorWeights:[SelectorWeight] = []) -> [WeightedStyle]{
         var weightedStyles:[WeightedStyle] = []
         //var styles:[IStyle] = []
         for key in searchTree.keys {
             //print("key: " + key + " object: "+searchTree[key] + " at cursor: "+cursor);
-            if(key == "style") {WeightedStyle.append(searchTree[key] as! WeightedStyle)}
+            if(key == "style") {weightedStyles.append(WeightedStyle(searchTree[key] as! IStyle, StyleWeight(selectorWeights)))}
             else{
                 for (var i : Int = cursor; i < querySelectors.count; i++) {
                     let querySelector:ISelector = querySelectors[i]
                     if(key == querySelector.element){
                         //print("matching element found, keep digging deeper");
-                        let result:[IStyle] = query(querySelectors, searchTree[key] as! [String:Any],i+1)
-                        if(result.count > 0) {styles += result}
+                        let result:[WeightedStyle] = query(querySelectors, searchTree[key] as! [String:Any],i+1)
+                        if(result.count > 0) {weightedStyles += result}
                     }
                 }
             }
         }
-        return styles
+        return weightedStyles
     }
 }

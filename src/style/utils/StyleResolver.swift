757c0f20d59facc878ff080196ffbd3f5fd3cc5d
Eonist <unicornhive@gmail.com>
2016-09-11 16:05:55 +0200

    Files modified: 1
    Modified 1 file:src/style/utils/StyleResolver.swift

--- src/style/utils/StyleResolver.swift
+++ src/style/utils/StyleResolver.swift
@@ -9,33 +9,33 @@ class StyleResolver{
      * // :TODO: should only inherit when property is marked inherit or from * universal selectors!?!?
      */
     static func style(element:IElement)->IStyle{
-        let querrySelectors:Array<ISelector> = ElementParser.selectors(element)/*Array instance comprised of Selector instances for each (element,classId,id and state) in the element*/
+        let querySelectors:Array<ISelector> = ElementParser.selectors(element)/*Array instance comprised of Selector instances for each (element,classId,id and state) in the element*/
+        return style(querySelectors,element)
+    }
+    /**
+     *
+     */
+    class func style(querySelectors:[ISelector],_ element:IElement)->IStyle{
         var weightedStyles:Array<WeightedStyle> = []
-        let styles = element as? Text != nil ? StyleManager.styles : Utils.getStyles(querrySelectors.last!)
+        let styles = element as? Text != nil ? StyleManager.styles : Utils.getStyles(querySelectors.last!)
         for style : IStyle in styles {/*This loop disregards styles that don't apply to the element Selectors*/
             styleLookUpCount++
-            if(style.selectors.count > querrySelectors.count) {continue;}/*if there are more selectors in style.selectors than in cascade the final styleWeight.weight is 0 and there for it is not included in the weightedStyles array*/
+            if(style.selectors.count > querySelectors.count) {continue;}/*if there are more selectors in style.selectors than in cascade the final styleWeight.weight is 0 and there for it is not included in the weightedStyles array*/
             //Swift.print("style: " + style.name)
-            let selectorWeights:Array<SelectorWeight>? = SelectorParser.selectorWeights(style,querrySelectors)
+            let selectorWeights:Array<SelectorWeight>? = SelectorParser.selectorWeights(style,querySelectors)
             if(selectorWeights != nil) {weightedStyles.append(WeightedStyle(style, StyleWeight(selectorWeights!)))}
         }
         //Swift.print("weightedStyles: " + weightedStyles.count)
         if(weightedStyles.count > 1) {
             weightedStyles = ArrayParser.conditionSort(weightedStyles, WeightedStyleAsserter.priority)/*Sorts each weightedStyle by its weight, the styles with most specificity has a lower index*/
         }
-        let styleName:String = SelectorParser.string(querrySelectors)/*returns the absolute selecter address of the element*/
-        var finalStyle:IStyle = StyleManager.getStyle(styleName) ?? Style(styleName,querrySelectors,[])/*find the exact styleName in the stylemanager or if that doesn't exist then create a new style to merge partily matched styles*/
+        let styleName:String = SelectorParser.string(querySelectors)/*returns the absolute selecter address of the element*/
+        var finalStyle:IStyle = StyleManager.getStyle(styleName) ?? Style(styleName,querySelectors,[])/*find the exact styleName in the stylemanager or if that doesn't exist then create a new style to merge partily matched styles*/
         
         for weightStyle:WeightedStyle in weightedStyles{/*compiles the finalStyle by making sure the last selector matches the last selector in the weightstyle, this works different for font etc. which are inheritable*/
-            StyleModifier.merge(&finalStyle, StyleAsserter.direct(querrySelectors, weightStyle) ? weightStyle : StyleModifier.filter(weightStyle, CSSConstants.textPropertyNames))/*direct styles will be appart of the final style and  you inherit from indirect styles, fonts,*or properties marked inherit*/
+            StyleModifier.merge(&finalStyle, StyleAsserter.direct(querySelectors, weightStyle) ? weightStyle : StyleModifier.filter(weightStyle, CSSConstants.textPropertyNames))/*direct styles will be appart of the final style and  you inherit from indirect styles, fonts,*or properties marked inherit*/
         }
         return finalStyle
-    }
-    /**
-     *
-     */
-    class func style(){
-        
     }
 }
 

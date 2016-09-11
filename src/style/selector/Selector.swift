c4d56f4c00b0f50e0101be4b331f043d4e0c3060
Eonist <unicornhive@gmail.com>
2016-09-11 22:00:54 +0200

    Files modified: 1
    Modified 1 file:src/style/selector/Selector.swift

--- src/style/selector/Selector.swift
+++ src/style/selector/Selector.swift
@@ -2,7 +2,7 @@ import Foundation
 /**
  * NOTE: ObjectC already has a class named Selector that may conflict with this one, use: this syntax instead if you need to use the ObjC Selector: ObjectiveC.Selector
  */
-struct Selector:ISelector,Reflectable{// :TODO: you should probably use a struct instead of class in the future
+struct Selector:ISelector{// :TODO: you should probably use a struct instead of class in the future
     let element:String/*Button*///should be nil
     let classIds:Array<String>/*.customButton*///should be nil
     let id:String/*#customButton*///should be nil

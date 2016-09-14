b84e02d4eab4b139a288f5be815b19f9359cb205
Eonist <unicornhive@gmail.com>
2016-09-15 00:12:07 +0200

    Files modified: 1
    Modified 1 file:src/style/selector/ISelector.swift

--- src/style/selector/ISelector.swift
+++ src/style/selector/ISelector.swift
@@ -4,17 +4,19 @@ protocol ISelector{
     var id:String{get}
     var states:Array<String>{get}
 }
-
 extension ISelector{
     /**
-     *
+     * Converts xml to a Selector
      */
     func selector(xml:XML)->ISelector{
         let element:String = xml.firstNode("element")!.value
         let id:String = xml.firstNode("id")!.value
-        let classIds:[String] = xml.firstNode("classIds")!.children.map{
+        let classIds:[String] = xml.firstNode("classIds")!.children!.map{
             ($0 as! XML).value
         }
-        return Selector()
+        let states:[String] = xml.firstNode("states")!.children!.map{
+            ($0 as! XML).value
+        }
+        return Selector(element,classIds,id,states)
     }
 }

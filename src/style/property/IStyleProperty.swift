e18cca6beb1f4ece2a7d073c4738265ba4b4125e
Eonist <unicornhive@gmail.com>
2016-09-19 23:43:51 +0200

    Files modified: 1
    Modified 1 file:src/style/property/IStyleProperty.swift

--- src/style/property/IStyleProperty.swift
+++ src/style/property/IStyleProperty.swift
@@ -13,16 +13,10 @@ extension IStyleProperty{
         //Swift.print("xml.string: " + "\(xml.string)")
         let name:String = xml.firstNode("name")!.value
         //Swift.print("element: " + "\(element)")
-        let value:Any = xml.firstNode("id")!.value
-        //Swift.print("id: " + "\(id)")
-        let classIds:[String] = xml.firstNode("classIds")!.children?.flatMap{
-            ($0 as! XML).value
-            } ?? []//<--or empty array
-        //Swift.print("classIds.count: " + "\(classIds.count)")
-        let states:[String] = xml.firstNode("states")!.children?.flatMap{
-            ($0 as! XML).value
-            } ?? []//<--or empty array
-        //Swift.print("states.count: " + "\(states.count)")
-        return Selector(element,classIds,id,states)
+        let value:Any = xml.firstNode("value")!.value
+        
+        let depth:Int = xml.firstNode("depth")!.value.int
+       
+        return StyleProperty(name,value,depth)
     }
 }

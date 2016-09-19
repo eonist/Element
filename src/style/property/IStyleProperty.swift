d7e468e36f122eed7656edb2a0a1510a06a3a880
Eonist <unicornhive@gmail.com>
2016-09-19 23:41:59 +0200

    Files modified: 1
    Modified 1 file:src/style/property/IStyleProperty.swift

--- src/style/property/IStyleProperty.swift
+++ src/style/property/IStyleProperty.swift
@@ -5,3 +5,24 @@ protocol IStyleProperty {
     var name:String {get set}
     var depth:Int {get}
 }
+extension IStyleProperty{
+    /**
+     * Converts xml to a Selector
+     */
+    static func styleProperty(xml:XML)->IStyleProperty{
+        //Swift.print("xml.string: " + "\(xml.string)")
+        let name:String = xml.firstNode("name")!.value
+        //Swift.print("element: " + "\(element)")
+        let value:Any = xml.firstNode("id")!.value
+        //Swift.print("id: " + "\(id)")
+        let classIds:[String] = xml.firstNode("classIds")!.children?.flatMap{
+            ($0 as! XML).value
+            } ?? []//<--or empty array
+        //Swift.print("classIds.count: " + "\(classIds.count)")
+        let states:[String] = xml.firstNode("states")!.children?.flatMap{
+            ($0 as! XML).value
+            } ?? []//<--or empty array
+        //Swift.print("states.count: " + "\(states.count)")
+        return Selector(element,classIds,id,states)
+    }
+}

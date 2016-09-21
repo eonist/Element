bce8990eacd5fc3ed36342b5bf01548c03705d36
Eonist <unicornhive@gmail.com>
2016-09-21 20:52:29 +0200

    Files modified: 2
    Modified 2 files:src/skin/skin/text/TextSkin.swiftsrc/ui/text/Text.swift

--- src/ui/text/Text.swift
+++ src/ui/text/Text.swift
@@ -5,7 +5,7 @@ class Text:Element,IText {
     var textField:TextField{get{return (skin as! ITextSkin).textField}}
     //var text:String{get{return textField.string!} set{fatalError("NOT SUPPORTED YET")}}
     init(_ width:CGFloat, _ height:CGFloat, _ text:String = "dafaultText", _ parent:IElement? = nil, _ id:String? = nil){
-        Swift.print("Text.init()")
+        //Swift.print("Text.init()")
         initText = text
         super.init(width, height, parent, id)
     }

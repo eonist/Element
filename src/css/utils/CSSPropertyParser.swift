19cfc30229346a7962c571a07034fd5c99933064
Eonist <unicornhive@gmail.com>
2016-09-22 00:43:31 +0200

    Files modified: 1
    Modified 1 file:src/css/utils/CSSPropertyParser.swift

--- src/css/utils/CSSPropertyParser.swift
+++ src/css/utils/CSSPropertyParser.swift
@@ -140,16 +140,16 @@ class CSSPropertyParser {
             let matches:Array<NSTextCheckingResult> = property.matches("^(\\w+?)\\:(.+?)$")
             for match:NSTextCheckingResult in matches {
                 //TODO: use RegExp.value on the bellow
-                let name:String = (property as NSString).substringWithRange(match.rangeAtIndex(1))//capturing group 1
+                let name:String = RegExp.value(property, match, 1)//capturing group 1
                 //TODO: use RegExp.value on the bellow
-                var value:Any = (property as NSString).substringWithRange(match.rangeAtIndex(2))//capturing group 2
+                var value:Any = RegExp.value(property, match, 2)//capturing group 2
                 if(name == "textColor" || name == "backgroundColor" || name ==  "borderColor") { value = StringParser.nsColor(value as! String)}
                 else if(value as! String == "true") { value = true }
                 else if(value as! String == "false") { value = false }
                 textField[name] = value
             }
         }
-        return textField;
+        return textField
     }
     /**
      * Returns a DropShadowFilter instance

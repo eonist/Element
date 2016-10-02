396a0de3334471946003fab6ac3a0d91f9fef6b8
Eonist <unicornhive@gmail.com>
2016-10-02 20:52:22 +0200

    Files modified: 1
    Modified 1 file:src/style/property/utils/StylePropertyParser.swift

--- src/style/property/utils/StylePropertyParser.swift
+++ src/style/property/utils/StylePropertyParser.swift
@@ -9,7 +9,7 @@ class StylePropertyParser{
     static func value(skin:ISkin, _ propertyName:String, _ depth:Int = 0)->Any!{//TODO: <- try to remove the ! char here
         //Swift.print("StylePropertyParser.value() propertyName: " + propertyName)
         let value:Any? = skin.style!.getValue(propertyName,depth)
-        //Swift.print("value: " + "\(value)")
+        Swift.print("value: " + "\(value)")
         return value
     }
     /**

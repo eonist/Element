e5399664fbd1537cdd1ebd47ef5287a2c0a8015a
Eonist <unicornhive@gmail.com>
2016-10-03 20:37:12 +0200

    Files modified: 1
    Modified 1 file:src/style/property/IStyleProperty.swift

--- src/style/property/IStyleProperty.swift
+++ src/style/property/IStyleProperty.swift
@@ -5,25 +5,3 @@ protocol IStyleProperty {
     var name:String {get set}
     var depth:Int {get}
 }
-extension IStyleProperty{
-    /**
-     * Converts xml to a Selector
-     */
-    static func styleProperty(xml:XML)->IStyleProperty{
-        //Swift.print("xml.string: " + "\(xml.string)")
-        let name:String = xml.firstNode("name")!.value
-        //Swift.print("element: " + "\(element)")
-        let valueXML:XML = xml.firstNode("value")!
-        Swift.print("valueXML.XMLString: " + "\(valueXML.XMLString)")
-        
-        
-        
-        let value:Any? = nil//ReflectionUtils.toType(valueXML)
-        
-        let depth:Int = xml.firstNode("depth")!.value.int
-       
-        fatalError("this is out of order atm")
-        
-        //return StyleProperty(name,value,depth)
-    }
-}

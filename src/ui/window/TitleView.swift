e2cc6c619f47d7ee433359c1e07fe4cead1a9dc7
Eonist <unicornhive@gmail.com>
2016-10-09 17:13:47 +0200

    Files modified: 1
    Modified 1 file:src/ui/window/TitleView.swift

--- src/ui/window/TitleView.swift
+++ src/ui/window/TitleView.swift
@@ -10,38 +10,7 @@ class TitleView:CustomView{
         super.init(width, height, parent, id)
     }
     override func resolveSkin() {
-        var css:String = ""
-        css += "TextArea#winTitle{"
-        css +=     "float:none;"
-        css +=     "clear:none;"
-        css +=     "width:100%;"
-        css +=     "offset:75px,0px;"
-        css +=     "fill-alpha:0;"
-        css +=     "fill:yellow;"
-        css +=     "line-alpha:0;"
-        css +=     "line-thickness:0px;"
-        css +=     "background:true;"
-        css +=     "padding-top:14px;"
-        css += "}"
-        css += "TextArea#winTitle Text{"
-        css +=     "width:100%;"
-        css +=     "height:16px;"
-        css +=     "offset:-75px,0px;"//<- set the width of the text to 100% and the offset to -the width of the titlebarIconContainer
-        css +=     "scrollable:false;"
-        css +=     "multiline:false;"
-        css +=     "wordWrap:true;"
-        css +=     "align:center;"
-        css +=     "margin-top:0px;"
-        css +=     "margin-left:4px;"
-        css +=     "font:Helvetica Neue Medium;"
-        css +=     "backgroundColor:orange;"
-        css +=     "background:false;"
-        css +=     "selectable:false;"
-        css += "}"
-        StyleManager.addStyle(css)
-        
         super.resolveSkin()
-        
         textArea = addSubView(TextArea(NaN,24,"Title goes here",self,"winTitle"))
         textArea!.text?.isInteractive = false
     }

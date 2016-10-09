9fad4d2a130181dc9367b54e44381454f4426505
Eonist <unicornhive@gmail.com>
2016-10-09 16:05:02 +0200

    Files modified: 1
    Modified 1 file:src/style/collection/StyleCollection.swift

--- src/style/collection/StyleCollection.swift
+++ src/style/collection/StyleCollection.swift
@@ -19,7 +19,7 @@ class StyleCollection:IStyleCollection{
     func addStyle(style:IStyle){
         for var styleItem in styles {
             if(styleItem.name == style.name) {/*if there are duplicates merge them*/
-                StyleModifier.combine(&styleItem, style)//<--was merge, but styles that comes later in the array with the same name should hard-override properties, not soft-override like it was previously
+                StyleModifier.combine(&styleItem, style)/*<--was merge, but styles that comes later in the array with the same name should hard-override properties, not soft-override like it was previously*/
                 return//you can also do break
             }
         }

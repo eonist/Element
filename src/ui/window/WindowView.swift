e0b1479066c954eca6c4d9d395a0bcdbc29016e7
Eonist <unicornhive@gmail.com>
2016-10-10 20:34:29 +0200

    Files modified: 1
    Modified 1 file:src/ui/window/WindowView.swift

--- src/ui/window/WindowView.swift
+++ src/ui/window/WindowView.swift
@@ -4,23 +4,19 @@ import Cocoa
  * TODO: WindowView should have a background element to target. check legacy code to confirm theory
  */
 class WindowView:Element{
-    var background:IElement?
     /**
      * Draws the graphics
      */
     override func resolveSkin() {
         super.resolveSkin()
-        //TODO:adding a background is probably not needed after you fix the inheritage problem
-        //background = addSubView(Element(width,height,self,"background"))
     }
     /**
      * Returns the class type of the Class instance
      */
     override func getClassType()->String{
-        return String(Window)//Window can be targeted via the id so we use Window for all Window subclasses, although this can be overriden in said subclasses
+        return String(Window)/*Window can be targeted via the id so we use Window for all Window subclasses, although this can be overriden in said subclasses*/
     }
     override func setSize(width: CGFloat, _ height: CGFloat) {
         super.setSize(width, height)
-        background?.setSize(width, height)
     }
 }

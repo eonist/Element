919db31a44e095bbc5c81be4349ae0e9499397a1
Eonist <unicornhive@gmail.com>
2016-10-16 13:55:15 +0200

    Files modified: 1
    Modified 1 file:src/ui/list/utils/ListParser.swift

--- src/ui/list/utils/ListParser.swift
+++ src/ui/list/utils/ListParser.swift
@@ -7,14 +7,14 @@ class ListParser {
      * Returns a NSView instance from @param dataProviderItem
      * @Note: This is a way to get to the actual "Buttons" directly
      */
-    class func label(list:IList,_ dataProviderItem:Dictionary<String,String>) -> NSView {// :TODO: rename to toItem and maybe move to Listparser, dataProviderItemToItem? ListUtils
+    static func label(list:IList,_ dataProviderItem:Dictionary<String,String>) -> NSView {// :TODO: rename to toItem and maybe move to Listparser, dataProviderItemToItem? ListUtils
         let itemIndex:Int = list.dataProvider.getItemIndex(dataProviderItem)
         return list.lableContainer!.subviews[itemIndex]
     }
     /**
      * Returns the item from the list (NSView instance) from @param list at @param index
      */
-    class func labelAt(list:IList,_ index:Int)->NSView? {
+    static func labelAt(list:IList,_ index:Int)->NSView? {
         let dataProvderItem:Dictionary<String,String>? = list.dataProvider.getItemAt(index)
         if(dataProvderItem != nil){return ListParser.label(list,dataProvderItem!)}
         return nil
@@ -22,7 +22,7 @@ class ListParser {
     /**
      * Returns a dataProviderItem
      */
-    class func dataProviderItem(list:IList, _ view:NSView)->Dictionary<String,String>? {
+    static func dataProviderItem(list:IList, _ view:NSView)->Dictionary<String,String>? {
         let index:Int = self.index(list, view)
         return list.dataProvider.getItemAt(index)
     }
@@ -30,46 +30,46 @@ class ListParser {
      * Returns the index of a "label"
      * @param view is the Label
      */
-    class func index(list:IList,_ view:NSView)->Int {
+    static func index(list:IList,_ view:NSView)->Int {
         return list.lableContainer!.indexOf(view)
     }
     /**
      * Returns the title of the currently selected
      */
-    class func selectedTitle(list:IList)->String {
+    static func selectedTitle(list:IList)->String {
         let index:Int = selectedIndex(list);
         return titleAt(list, index)
     }
     /**
      * Returns the title for index
      */
-    class func titleAt(list:IList, _ index:Int)->String{
+    static func titleAt(list:IList, _ index:Int)->String{
         return list.dataProvider.getItemAt(index)!["title"]!
     }
     /**
      * Returns the title of the currently selected
      */
-    class func selectedProperty(list:IList) -> String {/*<--could be **/
+    static func selectedProperty(list:IList) -> String {/*<--could be **/
         let index:Int = selectedIndex(list);
         return propertyAt(list,index)
     }
     /**
      * Returns the property for index
      */
-    class func propertyAt(list:IList, _ index:Int)->String{
+    static func propertyAt(list:IList, _ index:Int)->String{
         return list.dataProvider.getItemAt(index)!["property"]!
     }
     /**
      * Returns the index of the currentSelected
      */
-    class func selectedIndex(list:IList) -> Int {
+    static func selectedIndex(list:IList) -> Int {
         let selected:ISelectable? = self.selected(list)
         return selected != nil ? list.lableContainer!.indexOf(selected as! NSView) : -1
     }
     /**
      * Returns the current selected item
      */
-    class func selected(list:IList) -> ISelectable? {
+    static func selected(list:IList) -> ISelectable? {
         return SelectParser.selected(list.lableContainer!)
     }
     /**
@@ -77,7 +77,7 @@ class ListParser {
      * @Note another name for this could be getTotalItemsHeight?
      * // :TODO: you must for loop through each item and use the method getHeight() on each item to find the total height, item heights may vary and have different heights
      */
-    class func itemsHeight(list:IList) -> CGFloat {
+    static func itemsHeight(list:IList) -> CGFloat {
         return CGFloat(list.dataProvider.count()) * list.itemHeight
     }
 }

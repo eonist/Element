45e84a3b846f0dee6006335636847788fe0b4044
Eonist <unicornhive@gmail.com>
2016-10-18 14:15:58 +0200

    Files modified: 1
    Modified 1 file:src/ui/button/check/event/CheckEvent.swift

--- src/ui/button/check/event/CheckEvent.swift
+++ src/ui/button/check/event/CheckEvent.swift
@@ -1,10 +1,10 @@
 import Cocoa
 /**
- * // :TODO: include isCheckable? what use would it have?
+ * // :TODO: include isCheckable? what use would it have?, you could add that via an extension
  */
 class CheckEvent:Event{
-    var isChecked:Bool;
-    static var check:String = "checkEventCheck";
+    var isChecked:Bool
+    static var check:String = "checkEventCheck"
     init(_ type:String, _ isChecked:Bool, _ origin:NSView){
         self.isChecked = isChecked
         super.init(type, origin)

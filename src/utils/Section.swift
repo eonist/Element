2ef1e8afa64c2a3c2dd3cd5ec245a1d154f65121
Eonist <unicornhive@gmail.com>
2016-10-19 18:43:07 +0200

    Files modified: 1
    Modified 1 file:src/utils/Section.swift

--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -2,5 +2,6 @@ import Foundation
 /**
  * NOTE: Unlike Container, section can have a style applied
  * NOTE: you dont need anything in this method actually, the getClassType works if Section is the last SubClass in the subclass hierarchy
+ * TODO: make this as an alias instead
  */
 class Section:Element {}

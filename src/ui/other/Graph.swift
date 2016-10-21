eba40175d64d37534ad791b332f5538e6b1196f2
Eonist <unicornhive@gmail.com>
2016-10-21 22:15:30 +0200

    Files modified: 1
    Modified 1 file:src/ui/other/Graph.swift

--- src/ui/other/Graph.swift
+++ src/ui/other/Graph.swift
@@ -84,7 +84,8 @@ class GraphLine:Element{
         super.init(width, height, parent, id)
     }
     override func resolveSkin() {
-        skin = SkinResolver.skin(self)
+        skin = SkinResolver.skin(self)//you could use let style:IStyle = StyleResolver.style(element), but i think skin has to be created to not cause bugs 
+        //I think the most apropriate way is to make a custom skin and add it as a subView wich would implement :ISkin etc, see TextSkin for details
         //Somehow derive the style data and make a basegraphic with it
         let lineStyle:ILineStyle = StylePropertyParser.lineStyle(skin!)!
         let baseGraphic = BaseGraphic(nil,lineStyle)

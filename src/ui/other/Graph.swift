a19b8656e52d6555b4aea64e5211faad7d72cb03
Eonist <unicornhive@gmail.com>
2016-10-24 14:39:35 +0200

    Files modified: 1
    Modified 1 file:src/ui/other/Graph.swift

--- src/ui/other/Graph.swift
+++ src/ui/other/Graph.swift
@@ -221,3 +221,17 @@ class WeekGraph:Graph{
     override var hValNames:[String] {return ["M","T","W","T","F","S","S"]}
 }
 */
+
+
+//day,week,month,year,all (focus on day and week)
+//12a 1a 2a 3a 4a 5a 6a 7a 8a 9a 10a 11a 12p 1p 2p 3p 4p 5p 6p 7p 8p 9p 10p 11p
+//00:00, 01:00, 02
+//00,02,04,06,08,10,12,14,16,18,20,22,24 (12 part time)
+//00:00,04:00,08:00,12:00,16:00,20:00 ()
+//00,04,08,12,16,20 (6 part time)
+
+//Night 00, Morning 06, Noon 12, evening: 18 night 00
+
+//00,03,06,09,12,15,18,21,00
+//12a,03a,06a,09a,12a,03p,06p,09p,12a
+

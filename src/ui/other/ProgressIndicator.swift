603659817c4af6e97ec48a413e2a08a917ed8b35
Eonist <unicornhive@gmail.com>
2016-10-25 15:56:25 +0200

    Files modified: 1
    Modified 1 file:src/ui/other/ProgressIndicator.swift

--- src/ui/other/ProgressIndicator.swift
+++ src/ui/other/ProgressIndicator.swift
@@ -40,6 +40,12 @@ class ProgressIndicator:Element {
     /**
      *
      */
+    func onFrame(){
+        
+    }
+    /**
+     *
+     */
     func start(){
         //assert if animator exist else create animator
         //start anim

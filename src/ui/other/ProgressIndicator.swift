339fde8f891e22770ef8b58b39ce5632aa0d76bf
Eonist <unicornhive@gmail.com>
2016-10-25 13:35:45 +0200

    Files modified: 1
    Modified 1 file:src/ui/other/ProgressIndicator.swift

--- src/ui/other/ProgressIndicator.swift
+++ src/ui/other/ProgressIndicator.swift
@@ -6,6 +6,13 @@ class ProgressIndicator:Element {
         
         //get the linestyle from this skin
         //add round end style 
-        
+        //center of element
+        //radius = width/2
+        //wedge = π/2
+        //for i loop 12 lines
+            //angle = wedge * i
+            //startPos = center.polar(radius/2,angle)
+            //endPos = center.polar(radius,angle)
+            
     }
 }

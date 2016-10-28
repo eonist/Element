43167fd9526d7bedab3dac8b1c8161d474d4ad60
Eonist <unicornhive@gmail.com>
2016-10-28 22:35:18 +0200

    Files modified: 1
    Modified 1 file:src/ui/slider/rb/Thumb.swift

--- src/ui/slider/rb/Thumb.swift
+++ src/ui/slider/rb/Thumb.swift
@@ -51,6 +51,7 @@ extension Thumb{
         if(animator != nil){animator!.stop()}//stop any previous running animation
         let curVal:CGFloat = self.skin!.decoratables[0].getGraphic().fillStyle!.color.alphaComponent
         animator = Animator(Animation.sharedInstance,0.5,curVal,0,interpolateAlpha,Easing.easeInQuad)
+        animator!.event = {(event:Event) -> Void in }
         animator!.start()
     }
 }

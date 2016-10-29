b22726f2c96069cc4a94ecfdec837b54dd8e381f
Eonist <unicornhive@gmail.com>
2016-10-29 15:35:51 +0200

    Files modified: 1
    Modified 1 file:src/ui/other/ProgressIndicator.swift

--- src/ui/other/ProgressIndicator.swift
+++ src/ui/other/ProgressIndicator.swift
@@ -46,7 +46,7 @@ class ProgressIndicator:Element {
         
         let progress:Int = round(12*value).int//value = 0.25 -> 3, value = 0.5 -> 6 etc etc (values from 0 - 12 )
         
-        for i in 0..<7{//iterates 7 times
+        for i in 6..<13{//iterates 7 times
             let alpha:CGFloat = initAlpha + restAlpha * (1/7*i)//we need a half circle with gradually increasing alpha values starting from initAlpha
             let a:Int = progress + i
             let e:Int = IntParser.normalize(a, 12)//clamps the values between 0 and 12

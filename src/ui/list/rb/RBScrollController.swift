1f40ea7c9fe5388f01c377326f47a469c4913e34
Eonist <unicornhive@gmail.com>
2016-10-29 16:19:26 +0200

    Files modified: 1
    Modified 1 file:src/ui/list/rb/RBScrollController.swift

--- src/ui/list/rb/RBScrollController.swift
+++ src/ui/list/rb/RBScrollController.swift
@@ -6,7 +6,11 @@ import Cocoa
  * PARAM: frame: represents the visible part of the content //TODO: could be ranmed to maskRect
  * PARAM: itemsRect: represents the total size of the content //TODO: could be ranmed to contentRect
  * TODO: You could make a class named ScrollController that doesnt have the rubberband and then extend it
+ * TODO: Maybe create var's that store the enter and exit state.
  */
+
+//Continue here: Check the swipe left right code for state code for enter and exit.
+
 class RBScrollController:EventSender{
     //var view:RBSliderList/*holds a ref to the view*/
     var mover:RubberBand

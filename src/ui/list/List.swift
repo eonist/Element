20cbed8d8b8f1a91057e76665713e6c92ef24213
Eonist <unicornhive@gmail.com>
2016-10-30 13:22:33 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/List.swift

--- src/ui/list/List.swift
+++ src/ui/list/List.swift
@@ -20,7 +20,7 @@ class List:Element,IList{
         self.dataProvider = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
         super.init(width,height,parent,id)
         self.dataProvider.event = onEvent/*Add event handler for the dataProvider*/
-        layer!.masksToBounds = true/*masks the children to the frame, I don't think this works*/
+        layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
     }
     /**
      * Creates the components in the List Component

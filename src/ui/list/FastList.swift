53044fa1ba6ed568fdd444487acb684a6f3afa5c
Eonist <unicornhive@gmail.com>
2016-10-30 15:05:41 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -19,11 +19,11 @@ class FastList:Element {
         //Actually: store the visible item indecies in an array that you push and pop when the list goes up and down
             //This has the benefit that you only need to calc the height of the items in view (thinking about variable size support)
             //when an item goes above the top 
-                //the index is removed from the visibleItemIndecies array (one could also use a range here)
+                //the index is removed from the visibleItemIndecies array (one could also use a range here, but a small temp list is faster to read data from, )
                 //if the last index in visibleItemIndecies < items.count 
                     //append items[visibleItemIndecies.last] to visibleItemIndecies
                 //item.removeFromSuperView()
-                //spawn new Item from list(visibleItemIndecies.last)
+                //spawn new Item from visibleItemIndecies.last
                 //place it at y:  visibleItems.last.y+visibleItems.last.height
     }
     override func resolveSkin() {

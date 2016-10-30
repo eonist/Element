e85d601d680b73c7c103d6b067dd1b0e5f5098a2
Eonist <unicornhive@gmail.com>
2016-10-30 14:18:23 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -6,7 +6,7 @@ class FastList:Element {
         //Add 20 rects to a list (random colors) 100x50
         //mask 100x400
         
-        //move a "virtual" list up and down by: (just by values) (se code for this in List.swift)
+        //move a "virtual" list up and down by: (just by values) (see code for this in List.swift)
             //(the index of the item in the list) * itemHeight, represetnts the initY pos
             //when you move the "virtual" list up and down you add the .y value of the "virtual" list to every item
             //when an item is above the top or bellow the bottom of the mask, then remove it

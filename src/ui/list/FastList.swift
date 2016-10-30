9135cb7d24219cd46a8cab727ef14f0068773b55
Eonist <unicornhive@gmail.com>
2016-10-30 15:34:18 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -1,11 +1,15 @@
-import Foundation
+import Cocoa
 
 class FastList:Element {
+    var visibleItems:[NSView] = []
+    var items:[NSColor] = []
+    var itemContainer:Container?
     override init(_ width: CGFloat, _ height: CGFloat, _ parent: IElement?, _ id: String? = nil) {
         super.init(width, height, parent, id)
         //Add 20 rects to a list (random colors) 100x50
         //mask 100x400
-        
+       
+        layer!.masksToBounds = true/*masks the children to the frame*/
         //move a "virtual" list up and down by: (just by values) (see code for this in List.swift)
             //(the index of the item in the list) * itemHeight, represetnts the initY pos
             //when you move the "virtual" list up and down you add the .y value of the "virtual" list to every item
@@ -28,6 +32,7 @@ class FastList:Element {
     }
     override func resolveSkin() {
         super.resolveSkin()
+        itemContainer = addSubView(Container(width,height,self,"item"))
     }
     required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

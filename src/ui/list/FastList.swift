ded94ee400b2ee7faf0ad1d827a3b9922ca8ce54
Eonist <unicornhive@gmail.com>
2016-10-30 13:32:21 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -6,7 +6,7 @@ class FastList:Element {
         //Add 20 rects to a list (random colors) 100x50
         //mask 100x400
         
-        //move a "virtual" list up and down by:
+        //move a "virtual" list up and down by: (just by values) (se code for this in List.swift)
             //
         
     }

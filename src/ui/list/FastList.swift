954927e9b2d9efbeb900733b63555a318ca12db7
Eonist <unicornhive@gmail.com>
2016-10-30 13:22:34 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class FastList {
-
+class FastList:Element {
+    override init(_ width: CGFloat, _ height: CGFloat, _ parent: IElement?, _ id: String? = nil) {
+        super.init(width, height, parent, id)
+    }
+    override func resolveSkin() {
+        
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

4e5de74efa285dc2a5e4199c7368a2a011b1e0bc
Eonist <unicornhive@gmail.com>
2016-10-30 13:28:32 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -3,6 +3,8 @@ import Foundation
 class FastList:Element {
     override init(_ width: CGFloat, _ height: CGFloat, _ parent: IElement?, _ id: String? = nil) {
         super.init(width, height, parent, id)
+        //Add 20 rects to a list (random colors)
+        //
     }
     override func resolveSkin() {
         super.resolveSkin()

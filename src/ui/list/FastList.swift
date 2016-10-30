29ddc6161702efbc95182f27f85907f2b5ab7a0a
Eonist <unicornhive@gmail.com>
2016-10-30 13:43:56 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -9,7 +9,7 @@ class FastList:Element {
         //move a "virtual" list up and down by: (just by values) (se code for this in List.swift)
             //(the index of the item in the list) * itemHeight, represetnts the initY pos
             //when you move the "virtual" list up and down you add the .y value of the "virtual" list to every item
-            //when an item is above the top of the mask, then remove it
+            //when an item is above the top or bellow the bottom of the mask, then hide it
         
     }
     override func resolveSkin() {

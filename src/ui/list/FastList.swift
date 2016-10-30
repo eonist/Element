ab33a53a9eb2fcc4fc6d3dbc6e667694f629cf26
Eonist <unicornhive@gmail.com>
2016-10-30 13:38:17 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -8,7 +8,8 @@ class FastList:Element {
         
         //move a "virtual" list up and down by: (just by values) (se code for this in List.swift)
             //(the index of the item in the list) * itemHeight, represetnts the initY pos
-            //when you move the "virtual" list up and down 
+            //when you move the "virtual" list up and down you add the .y value of the "virtual" list to every item
+            //
         
     }
     override func resolveSkin() {

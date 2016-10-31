2567549843a1ef193d98b7b8a5308ba868e976f1
Eonist <unicornhive@gmail.com>
2016-10-31 21:33:51 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -89,7 +89,7 @@ class FastList:Element {
                 //Swift.print("item is bellow bottom limit - remove()")
                 item.hidden = true
                 surplusItems.append(item)//
-                visibleItems.indexOf(item)
+                visibleItems.indexOf(<#T##element: ListItem##ListItem#>)
 
             }
         }

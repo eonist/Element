69b1b1ad54119fe90034ac5fd4564253ab5aff2f
Eonist <unicornhive@gmail.com>
2016-11-01 17:28:36 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -80,7 +80,7 @@ class FastList:Element {
         //first part
         let firstPartStart:Int = firstItemIndex
         Swift.print("firstPartStart: " + "\(firstPartStart)")
-        let firstPartEnd:Int = firstItemIndex + (firstItemIndex - firstVisibleIdx)
+        let firstPartEnd:Int = firstItemIndex + ((firstVisibleIdx + 1) - firstItemIndex)
         Swift.print("firstPartEnd: " + "\(firstPartEnd)")
         
         for i in firstPartStart..<firstPartEnd{

2bcc19e8bc73a1d60031234b279e509c7145eeda
Eonist <unicornhive@gmail.com>
2016-11-01 21:35:26 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -23,7 +23,7 @@ class FastList:Element {
     var dataProvider:DataProvider
     var items:[NSColor] = []
     var itemContainer:Container?
-    let maxVisibleItems:Int = 6//this will be calculated on init and on setSize calls
+    let maxVisibleItems:Int//this will be calculated on init and on setSize calls
     var itemsHeight:CGFloat {return items.count * itemHeight}//<--the tot items height can be calculated at init, and on list data refresh
     var surplusItems:[ListItem] = []/*repurpouse Items instead of removing and creating new ones*/
     var visibleItems:[ListItem] = []/*Items that are within the mask, since itemContainer has surplus items and visible items we need this array to hold visible items*/
@@ -31,6 +31,8 @@ class FastList:Element {
     init(_ width:CGFloat, _ height:CGFloat, _ itemHeight:CGFloat = CGFloat.NaN,_ dataProvider:DataProvider? = nil, _ parent:IElement?, _ id:String? = nil) {
         self.itemHeight = itemHeight
         self.dataProvider = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
+        maxVisibleItems = ceil(height / itemHeight).int
+        Swift.print("maxVisibleItems: " + "\(maxVisibleItems)")
         super.init(width, height, parent, id)
         layer!.masksToBounds = true/*masks the children to the frame*///mask 100x400
     }
@@ -41,7 +43,7 @@ class FastList:Element {
         for _ in 0..<20{items.append(NSColor.random)}//Add 20 rects to a list (random colors) 100x50, this represents the items to derive data from
         itemContainer = addSubView(Container(width,height,self,"itemContainer"))
         var y:CGFloat = 0
-        for i in 0..<9{/*we need an extra item to cover the entire*/
+        for i in 0...maxVisibleItems{/*we need an extra item to cover the entire*/
             //visibleItemIndecies.append(i)
             let item:ListItem = spawn(i) as! ListItem
             visibleItems.append(item)
@@ -49,7 +51,7 @@ class FastList:Element {
             item.y = y
             y += itemHeight
         }
-        setProgress(0)
+        setProgress(0)//<-not really needed, but nice to have while debugging
     }
     /**
      * PARAM: progress: 0 to 1

5853192d6f8c09c365232902315c53cdf5430d40
Eonist <unicornhive@gmail.com>
2016-11-01 15:18:47 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -82,10 +82,10 @@ class FastList:Element {
         //Swift.print("lastIdx: " + "\(lastIdx)")
         
         //Continue here: Its working, and no tearing!
-            //Turn of the hide/unhide animation (done)
+            
             //Try to use 3 ranges when prepending,appending items instead of the 4 if clauses bellow. or group the if clauses (REfactor)
             //Try to avoid spoofing items when the limit is reached. needs an if statment or alike
-            //also there is still some gaps apearing between items. try to use values derived from the above item when setting y (done)
+        
         
         var subViewIdx:Int = 0
         for i in 0..<maxVisibleItems{//if no items exist then this doesnt iterate

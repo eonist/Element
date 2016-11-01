104876786fa1c6c9785a8ef934c22333f249a6df
Eonist <unicornhive@gmail.com>
2016-11-01 12:25:29 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -160,7 +160,7 @@ class FastList:Element {
      * Reuse item but apply new data
      */
     func spoof(item:ListItem){
-        let color:NSColor = items[item.index]//NSColor.grayColor()//
+        let color:NSColor = items[item.index] ?? NSColor.grayColor()//<--temp bug fix
         let style:IStyle = StyleModifier.clone(item.skin!.style!,item.skin!.style!.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
         var styleProperty = style.getStyleProperty("fill",0) /*edits the style*/
         if(styleProperty != nil){

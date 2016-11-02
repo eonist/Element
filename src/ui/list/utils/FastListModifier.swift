7298cb9b0b7427fdc3b2c50ed353a8174e0fc6c5
Eonist <unicornhive@gmail.com>
2016-11-02 16:43:49 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/utils/FastListModifier.swift

--- src/ui/list/utils/FastListModifier.swift
+++ src/ui/list/utils/FastListModifier.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 class FastListModifier {
-
+    /**
+     * 
+     */
+    static func select(list:IList, _ index:Int, _ isSelected:Bool){
+        ListModifier.selectAt(list, index)
+        list.dataProvider.setValue(index,"selected",String(isSelected))
+        list.dataProvider.setValuesExceptAt(index, "selected", String(!isSelected))
+    }
 }

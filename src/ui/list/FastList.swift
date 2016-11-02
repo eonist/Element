c815805fdbc01532949d5c09f8ae665718597208
Eonist <unicornhive@gmail.com>
2016-11-02 13:01:08 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -8,14 +8,13 @@ import Cocoa
  * NOTE: Placing items to the bottom of the above item is the only way to avoid gaps from apearing from time to time
  * NOTE: Supporting variable item height will require advance caching system for keeping track of item heights. The challenge is to not have to loop through 1000's of items to get the correct .y coordinate (remember setProgress may be called 60 times per second)
  * NOTE: When inserting list items at new indecies is needed, then update the dataprovider and it will in turn spoof the change visually
+ * NOTE: to debug you can: remove the mask and use an outline that is above the itemContainer
  * TODO: the dataProvider.items.count should probably be cached if the count is high, maybe even do this in the dataprovider it self
+ * TODO: try the 1 loop setProgress idea (where you do the adding and appending the same place where you do the hiding)
+ * TODO: test if resize works, by spawning new items etc
  */
 
 //Continue here:
- 
-    //try the 1 loop setProgress idea
-    //remove the mask and use an outline that is above the itemContainer
-    //test if resize works, by spawning new items etc
     //test the FastList with Slider
     //test the FastList with rubberband
     //test the FastList with 1000's of items

9a24d1e198784d59d78409b2c5da22a073eec3c0
Eonist <unicornhive@gmail.com>
2016-11-02 23:27:33 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -87,6 +87,7 @@ class FastList:Element,IList {
                 surplusItems += visibleItems.removeAtIndex(i)//remove item that falls bellow the bottom limit
                 
                 //only add above if there is something to add above (same for the topLimit)
+                    //assert 
                 
                 firstPart.append(reveal(listItem.idx,firstPartY))//place the removed item to the top of the visible items, think lego
                 firstPartY -= itemHeight

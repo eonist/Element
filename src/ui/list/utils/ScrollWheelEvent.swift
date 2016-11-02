a08d2c51ee17f1dd438faed7d1e9f828ebb14b99
Eonist <unicornhive@gmail.com>
2016-11-02 13:19:48 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/utils/ScrollWheelEvent.swift

--- src/ui/list/utils/ScrollWheelEvent.swift
+++ src/ui/list/utils/ScrollWheelEvent.swift
@@ -1,5 +1,7 @@
 import Foundation
 
-class ScrollWheelEvent {
-
+class ScrollWheelEvent:Event{
+    static let enter:String = "scrollWheelEnter"//possibly rename to engage
+    static let exit:String = "scrollWheelExit"//possibly rename to disEngage
+    static let exitAndStationary:String = "scrollWheelExitAndStationary"
 }

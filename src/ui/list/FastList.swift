a4659a784d4d1fd141ea7b85b148549f9223513a
Eonist <unicornhive@gmail.com>
2016-11-02 11:41:24 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -135,13 +135,8 @@ class FastList:Element {
      */
     func spoof(listItem:(item:Element,idx:Int)){
         let item:Element = listItem.item
-        let color:NSColor = items[listItem.idx]//item.index < items.count ? items[item.index] : NSColor.grayColor()//<--temp bug fix
-        let style:IStyle = StyleModifier.clone(item.skin!.style!,item.skin!.style!.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
-        var styleProperty = style.getStyleProperty("fill",0) /*edits the style*/
-        if(styleProperty != nil){
-            styleProperty!.value = color
-            item.skin!.setStyle(style)/*updates the skin*/
-        }
+        let idx:Int = listItem.idx
+        
     }
     required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
@@ -171,8 +166,20 @@ private class Utils{
         CATransaction.commit()
     }
 }
-
-
+/*
+private class ColorList{
+    func spoof(listItem:(item:Element,idx:Int)){
+        let item:Element = listItem.item
+        let color:NSColor = items[listItem.idx]//item.index < items.count ? items[item.index] : NSColor.grayColor()//<--temp bug fix
+        let style:IStyle = StyleModifier.clone(item.skin!.style!,item.skin!.style!.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
+        var styleProperty = style.getStyleProperty("fill",0) /*edits the style*/
+        if(styleProperty != nil){
+            styleProperty!.value = color
+            item.skin!.setStyle(style)/*updates the skin*/
+        }
+    }
+}
+*/
 /*
 itemContainer?.subviews.forEach{//remove items that are above or bellow the limits
     let item:ListItem = $0 as! ListItem

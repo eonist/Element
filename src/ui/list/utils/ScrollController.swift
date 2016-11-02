018a6cf252d9d0a68c6c4671d77b7e2f6b6d7869
Eonist <unicornhive@gmail.com>
2016-11-02 15:18:51 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file: src/ui/list/utils/SliderListUtils.swiftModified 1 file:src/ui/list/utils/ScrollController.swift

--- src/ui/list/utils/ScrollController.swift
+++ src/ui/list/utils/ScrollController.swift
@@ -6,10 +6,22 @@ class ScrollController {
         self.sliderList = sliderList
     }
     func scrollWheel(theEvent:NSEvent) {
-        let currentScroll:CGFloat = SliderListUtils.progress(theEvent.deltaY, sliderList.sliderInterval!, sliderList.slider!.progress)
+        let currentScroll:CGFloat = Utils.progress(theEvent.deltaY, sliderList.sliderInterval!, sliderList.slider!.progress)
         ListModifier.scrollTo(sliderList,currentScroll) /*Sets the target item to correct y, according to the current scrollBar progress*/
         sliderList.slider?.setProgressValue(currentScroll)
         if(theEvent.momentumPhase == NSEventPhase.Ended){sliderList.slider!.thumb!.setSkinState("inActive")}
         else if(theEvent.momentumPhase == NSEventPhase.Began){sliderList.slider!.thumb!.setSkinState(SkinStates.none)}//include may begin here
+    }
+}
+private class Utils{
+    /**
+     * Returns the progress og the sliderList (used when we scroll with the scrollwheel/touchpad)
+     * NOTE: The SliderList and SliderFastList uses this method
+     */
+    static func progress(deltaY:CGFloat,_ sliderInterval:CGFloat,_ sliderProgress:CGFloat)->CGFloat{
+        let scrollAmount:CGFloat = (deltaY/30)/sliderInterval/*_scrollBar.interval*/
+        var currentScroll:CGFloat = sliderProgress - scrollAmount/*the minus sign makes sure the scroll works like in OSX LION*/
+        currentScroll = NumberParser.minMax(currentScroll, 0, 1)/*clamps the num between 0 and 1*/
+        return currentScroll
     }
 }

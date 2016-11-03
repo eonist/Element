6717453d366fd777a24e5589c08ab73a36e5292c
Eonist <unicornhive@gmail.com>
2016-11-03 23:06:56 +0100

    Files modified: 2
    Modified 2 files:src/ui/list/RBSliderList.swiftsrc/ui/list/rb/RBScrollController.swift

--- src/ui/list/rb/RBScrollController.swift
+++ src/ui/list/rb/RBScrollController.swift
@@ -3,8 +3,8 @@ import Cocoa
  * NOTE: You forward the scrollWheel events here
  * NOTE: It all works like a regular MVC system
  * TODO: Create the algorithm that calculates the actual throw speed. By looking at the time that each intervall travles. 
- * PARAM:
- * 
+ * PARAM: frame: represents the visible part of the content //TODO: could be ranmed to maskRect
+ * PARAM: itemsRect: represents the total size of the content //TODO: could be ranmed to contentRect
  * TODO: You could make a class named ScrollController that doesnt have the rubberband and then extend it
  * TODO: Maybe create var's that store the enter and exit state.
  * TODO: It could be possible to do all the methods i this class as an extension to IRBSliderList, and have the mover,prescorldeltay and velocites in Rbsliderlist instead
@@ -14,10 +14,14 @@ import Cocoa
 
 class RBScrollController:EventSender{
     //var view:RBSliderList/*holds a ref to the view*/
-    
+    var mover:RubberBand
+    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
+    var velocities:Array<CGFloat> = [0,0,0,0,0,0,0,0,0,0]/*represents the velocity resolution of the gesture movment*/
     init(_ callBack:(CGFloat)->Void,_ frame:CGRect, _ itemRect:CGRect){
         //self.view = view
-       
+        mover = RubberBand(Animation.sharedInstance,callBack,frame,itemRect)
+        super.init()
+        mover.event = onEvent/*Add an eventHandler for the mover object*/
     }
     
     /*

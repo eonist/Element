a0b1aeb6681dd3f585af309211d1d416771a771d
Eonist <unicornhive@gmail.com>
2016-11-03 19:45:27 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/utils/FastListModifier.swift

--- src/ui/list/utils/FastListModifier.swift
+++ src/ui/list/utils/FastListModifier.swift
@@ -2,15 +2,12 @@ import Foundation
 
 class FastListModifier {
     /**
-     * NOTE: setting the selected attribute in the dataProvider is important because when we spoof list items we recycle old items and so we dont want to inherit the selected state
+     * Sets selectedIndex in fastList and makes the appropriate UI changes to the visibleItems
      */
     static func select(list:FastList, _ index:Int/*dp idx*/, _ isSelected:Bool){
         list.selectedIdx = index//set the cur selectedIdx in fastList
-        var viewIndex:Int?
-        
         for i in 0..<list.visibleItems.count{
-            if(index == visibleItems[i].idx){ListModifier.selectAt(list, index)}//if the index is currently visible then select it to see UI changes
+            if(index == list.visibleItems[i].idx){ListModifier.selectAt(list, i)}//if the index is currently visible then select it to see UI changes
         }
-        
     }
 }

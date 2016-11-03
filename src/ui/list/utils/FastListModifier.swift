16c2df3a1f934151a567726638b0fd53a0d550cc
Eonist <unicornhive@gmail.com>
2016-11-03 19:35:58 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/utils/FastListModifier.swift

--- src/ui/list/utils/FastListModifier.swift
+++ src/ui/list/utils/FastListModifier.swift
@@ -4,7 +4,7 @@ class FastListModifier {
     /**
      * NOTE: setting the selected attribute in the dataProvider is important because when we spoof list items we recycle old items and so we dont want to inherit the selected state
      */
-    static func select(list:IList, _ index:Int, _ isSelected:Bool){
+    static func select(list:FastList, _ index:Int, _ isSelected:Bool){
         ListModifier.selectAt(list, index)//Handles the visible items in the lable container
         list.dataProvider.setValue(index,"selected",String(isSelected))
         list.dataProvider.setValuesExceptAt(index, "selected", String(!isSelected))

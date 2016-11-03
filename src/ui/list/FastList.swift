886ae1c576ab43000ff0368a6a955045706df538
Eonist <unicornhive@gmail.com>
2016-11-03 14:29:52 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList.swift

--- src/ui/list/FastList.swift
+++ src/ui/list/FastList.swift
@@ -94,14 +94,13 @@ class FastList:Element,IList {
                     thirdPartY += itemHeight//we stack under the last one appended
                     thirdPartIdx--
                 }
-                
                 //Swift.print("visibleItems.count: " + "\(visibleItems.count)")
             }else if(listItemY >= bottomLimit){
                 Swift.print("item: \(listItem.idx) at: \(listItemY) is bellow bottom limit")
                 Utils.hide(listItem.item, true)
                 surplusItems += visibleItems.removeAtIndex(i)//remove item that falls bellow the bottom limit
-                if(visibleItems.count > 0 && visibleItems.first.idx > 0){//only add above if there is something to add above (same for the topLimit), assert idx of visibleItems.first.idx > 0
-                    firstPart.prepend(reveal(listItem.idx,firstPartY))//place the removed item to the top of the visible items, think lego
+                if(firstPartIdx >= 0){//only add above if there is something to add above (same for the topLimit), assert idx of visibleItems.first.idx > 0
+                    firstPart.prepend(reveal(firstPartIdx,firstPartY))//place the removed item to the top of the visible items, think lego
                     firstPartY -= itemHeight//We stack on top of the last appended
                 }
                 //Swift.print("visibleItems.count: " + "\(visibleItems.count)")

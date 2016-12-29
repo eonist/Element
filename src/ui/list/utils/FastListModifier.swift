39efe99343db715a1253311ef2911813ac2c2027
Eonist <unicornhive@gmail.com>
2016-12-29 21:28:02 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/utils/FastListModifier.swift

--- src/ui/list/utils/FastListModifier.swift
+++ src/ui/list/utils/FastListModifier.swift
@@ -3,8 +3,9 @@ import Foundation
 class FastListModifier {
     /**
      * Sets selectedIndex in fastList and makes the appropriate UI changes to the visibleItems
+     * PARAM: 
      */
-    static func select(list:FastList, _ index:Int/*dp idx*/, _ isSelected:Bool){
+    static func select(list:FastList, _ index:Int, _ isSelected:Bool){
         list.selectedIdx = index//set the cur selectedIdx in fastList
         for i in 0..<list.visibleItems.count{
             if(index == list.visibleItems[i].idx){ListModifier.selectAt(list, i)}//if the index is currently visible then select it to see UI changes

2667f88ae945e916446d4484c8813a01e3dfc99c
Eonist <unicornhive@gmail.com>
2016-12-30 17:39:36 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList2.swift

--- src/ui/list/FastList2.swift
+++ src/ui/list/FastList2.swift
@@ -4,10 +4,12 @@ class FastList2:Element,IList{
     var itemHeight:CGFloat/*The list item height, each item must have the same height*/
     var dataProvider:DataProvider/*data storage*/
     var lableContainer:Container?/*holds the list items*/
-
+    var maxVisibleItems:Int?/*this will be calculated on init and on setSize calls*/
+    
     init(_ width:CGFloat, _ height:CGFloat, _ itemHeight:CGFloat = NaN,_ dataProvider:DataProvider? = nil, _ parent:IElement?, _ id:String? = nil){
         self.itemHeight = itemHeight
         self.dataProvider = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
+        
         super.init(width, height, parent, id)
         self.dataProvider.event = onEvent/*Add event handler for the dataProvider*/
         layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
@@ -15,6 +17,7 @@ class FastList2:Element,IList{
     override func resolveSkin() {
         super.resolveSkin()
         lableContainer = addSubView(Container(width,height,self,"lable"))
+        maxVisibleItems = round(height / itemHeight).int + 1
     }
     /**
      *

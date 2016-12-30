2e88f3ba4514abecc2a7fff4f9e8aba2e8fe32a8
Eonist <unicornhive@gmail.com>
2016-12-30 16:35:57 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList2.swift

--- src/ui/list/FastList2.swift
+++ src/ui/list/FastList2.swift
@@ -1,5 +1,18 @@
 import Foundation
 
-class FastList2 {
+class FastList2:Element{
+    var itemHeight:CGFloat/*The list item height, each item must have the same height*/
+    var dataProvider:DataProvider/*data storage*/
+    var lableContainer:Container?/*holds the list items*/
 
+    init(_ width:CGFloat, _ height:CGFloat, _ itemHeight:CGFloat = NaN,_ dataProvider:DataProvider? = nil, _ parent:IElement?, _ id:String? = nil){
+        self.itemHeight = itemHeight
+        self.dataProvider = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
+        super.init(width, height, parent, id)
+    }
+    override func resolveSkin() {
+        super.resolveSkin()
+        lableContainer = addSubView(Container(width,height,self,"lable"))
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

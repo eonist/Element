ccc8badb5f9396d40100ffabbb5103418a315b67
Eonist <unicornhive@gmail.com>
2016-12-31 18:02:01 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/SliderFastList2.swift

--- src/ui/list/SliderFastList2.swift
+++ src/ui/list/SliderFastList2.swift
@@ -1,5 +1,24 @@
-import Foundation
+import Cocoa
 
-class SliderFastList2 {
-
+class SliderFastList2:FastList2,ISliderList {
+    var slider:VSlider?
+    var sliderInterval:CGFloat?
+    override func resolveSkin() {
+        super.resolveSkin()
+        sliderInterval = floor(ListParser.itemsHeight(self) - height)/itemHeight// :TODO: use SliderParser.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/ListParser.itemsHeight(self), slider!.height)
+        slider!.setThumbHeightValue(thumbHeight)//<--TODO: Rather set the thumbHeight on init?
+    }
+    override func scrollWheel(event:NSEvent) {
+        scroll(event)/*forwards the event to the extension method*/
+        super.scrollWheel(event)/*forwards the event other delegates higher up in the stack*/
+    }
+    func onSliderChange(sliderEvent:SliderEvent){/*Handler for the SliderEvent.change*/
+        ListModifier.scrollTo(self,sliderEvent.progress)
+    }
+    override func onEvent(event:Event) {
+        if(event.assert(SliderEvent.change, slider)){onSliderChange(event.cast())}/*events from the slider*/
+        super.onEvent(event)
+    }
 }

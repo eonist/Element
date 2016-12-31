a5a59c6e12eb1274fc033d8dfdc7bb832043d5fd
Eonist <unicornhive@gmail.com>
2016-12-31 18:16:37 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList2.swift

--- src/ui/list/FastList2.swift
+++ src/ui/list/FastList2.swift
@@ -63,6 +63,8 @@ class FastList2:Element,IList{
         }else if(diff.negative){//cur.start is more than prev.start
             Swift.print("append")
             var items = visibleItems.splice2(0, abs(diff))//grab items from the top
+            Swift.print("visibleItems.count: " + "\(visibleItems.count)")
+            Swift.print("items.count: " + "\(items.count)")
             for i in 0..<items.count {items[i] = (items[i].item, cur.end + i);spoof(items[i])}//assign correct absolute idx
             visibleItems += items/*append to list*/
         }

35babad03d0056c6e8cea73aaa8fe3828de709e9
Eonist <unicornhive@gmail.com>
2016-12-31 17:58:03 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/SliderFastList.swift

--- src/ui/list/SliderFastList.swift
+++ src/ui/list/SliderFastList.swift
@@ -5,7 +5,7 @@ class SliderFastList:FastList,ISliderList {
     var sliderInterval:CGFloat?
     override func resolveSkin() {
         super.resolveSkin()
-        sliderInterval = floor(ListParser.itemsHeight(self) - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        sliderInterval = floor(ListParser.itemsHeight(self) - height)/itemHeight// :TODO: use SliderParser.interval instead?// :TODO: explain what this is in a comment
         slider = addSubView(VSlider(itemHeight,height,0,0,self))
         let thumbHeight:CGFloat = SliderParser.thumbSize(height/ListParser.itemsHeight(self), slider!.height)
         slider!.setThumbHeightValue(thumbHeight)//<--TODO: Rather set the thumbHeight on init?

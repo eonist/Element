abb340a3b3f62e60bbe3a663b7597ae14e08ff59
Eonist <unicornhive@gmail.com>
2017-01-04 18:54:40 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList2.swift

--- src/ui/list/FastList2.swift
+++ src/ui/list/FastList2.swift
@@ -163,7 +163,7 @@ class FastList2:Element,IList{
         
         //Continue here:🏀
             //add remove item button
-            //SliderList should somehow update the slider,sliderinterval etc -> onDataProviderEvent in SliderList -> 
+            //SliderList should somehow update the slider.thumb.height,slider.thumb.y and sliderInterval -> onDataProviderEvent in SliderList -> 
 
     }
     /**

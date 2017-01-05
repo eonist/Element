23e970c1de85d29f9f264c7576e7dbea7ee4575b
Eonist <unicornhive@gmail.com>
2017-01-05 15:15:04 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList2.swift

--- src/ui/list/FastList2.swift
+++ src/ui/list/FastList2.swift
@@ -32,11 +32,11 @@ class FastList2:Element,IList{
      */
     func setProgress(progress:CGFloat){
         Swift.print("FastList2.setProgress() ")
-        Swift.print("lableContainer!.y: " + "\(lableContainer!.y)")
+        //Swift.print("lableContainer!.y: " + "\(lableContainer!.y)")
         ListModifier.scrollTo(self, progress)/*moves the labelContainer up and down*/
         let curVisibleRange:Range<Int> = Utils.curVisibleItems(self, maxVisibleItems!)
-        //Swift.print("curVisibleRange: " + "\(curVisibleRange)")
-        if(curVisibleRange != prevVisibleRange){/*Optimizatioin: only set if it's not the same as prev range*/
+        Swift.print("curVisibleRange: " + "\(curVisibleRange)")
+        if(curVisibleRange != prevVisibleRange){/*Optimization: only set if it's not the same as prev range*/
             spoof(curVisibleRange)/*spoof items in the new range*/
             prevVisibleRange = curVisibleRange
         }

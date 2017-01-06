bc0e16d6af305702bbd342518cb30fc66cd6f849
Eonist <unicornhive@gmail.com>
2017-01-06 16:28:26 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList3.swift

--- src/ui/list/FastList3.swift
+++ src/ui/list/FastList3.swift
@@ -30,4 +30,106 @@ class FastList3:Element,IList{
             prevVisibleRange = curVisibleRange
         }
     }
+    /**
+     * (spoof == apply/reuse)
+     */
+    func spoof(cur:Range<Int>){
+        let prev = prevVisibleRange!
+        let diff = prev.start - cur.start
+        if(abs(diff) >= maxVisibleItems!){//spoof every item
+            Swift.print("all")
+            for i in 0..<visibleItems.count {
+                let idx = cur.start + i
+                visibleItems[i] = (visibleItems[i].item, idx)
+                spoof(visibleItems[i])
+            }
+        }else if(diff.positive){//cur.start is less than prev.start
+            Swift.print("prepend ")
+            var items = visibleItems.splice2(visibleItems.count-diff, diff)//grab the end items
+            for i in 0..<items.count {items[i] = (items[i].item, cur.start + i);spoof(items[i])}//assign correct absolute idx
+            visibleItems = items + visibleItems/*prepend to list*/
+        }else if(diff.negative){//cur.start is more than prev.start
+            Swift.print("append")
+            var items = visibleItems.splice2(0, -1*(diff))//grab items from the top
+            for i in 0..<items.count {
+                items[i] = (items[i].item, prev.end + i)
+                spoof(items[i])
+            }//assign correct absolute idx
+            visibleItems += items/*append to list*/
+        }
+    }
+    /**
+     * (spoof == apply/reuse)
+     */
+    func spoof(listItem:FastListItem){/*override this to use custom ItemList items*/
+        Swift.print("spoof: " + "\(listItem.idx)")
+        let item:SelectTextButton = listItem.item as! SelectTextButton
+        let idx:Int = listItem.idx/*the index of the data in dataProvider*/
+        let dpItem = dataProvider.items[idx]
+        let title:String = dpItem["title"]!
+        item.setTextValue(title)
+        item.y = listItem.idx * itemHeight/*position the item*/
+    }
+    /**
+     * (spawn == create something)
+     */
+    private func spawn(range:Range<Int>){
+        for i in range{/*we need an extra item to cover the entire area*/
+            let item:Element = spawn(i)
+            visibleItems.append((item,i))
+            lableContainer!.addSubView(item)
+            item.y = i * itemHeight
+        }
+    }
+    /**
+     * (spawn == create something)
+     */
+    func spawn(idx:Int)->Element{/*override this to use custom ItemList items*/
+        Swift.print("spawn: " + "\(idx)")
+        let dpItem = dataProvider.items[idx]
+        let title:String = dpItem["title"]!
+        let item:SelectTextButton = SelectTextButton(getWidth(), itemHeight ,title, false, lableContainer)
+        return item
+    }
+    override func getClassType() -> String {return String(List)}
+    required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
+private class Utils{
+    /**
+     *
+     */
+    static func curVisibleItems(list:IList,_ maxVisibleItems:Int)->Range<Int>{
+        let visibleItemsTop:CGFloat = abs(list.lableContainer!.y > 0 ? 0 : list.lableContainer!.y)//NumberParser.minMax(-1*lableContainer!.y, 0, itemHeight * dataProvider.count - height)
+        //Swift.print("visibleItemsTop: " + "\(visibleItemsTop)")
+        //let visibleBottom:CGFloat = visibleItemsTop + height
+        //Swift.print("visibleBottom: " + "\(visibleBottom)")
+        //var topItemY:CGFloat {let remainder = visibleItemsTop % itemHeight;return visibleItemsTop-itemHeight+remainder}
+        //Swift.print("topItemY: " + "\(topItemY)")
+        var topItemIndex:Int = (visibleItemsTop / list.itemHeight).int
+        topItemIndex = topItemIndex < 0 ? 0 : topItemIndex
+        //topItemIndex = NumberParser.minMax(topItemIndex, 0, dataProvider.count-maxVisibleItems!)//clamp the num between min and max
+        //Swift.print("topItemIndex: " + "\(topItemIndex)")
+        var bottomItemIndex:Int = topItemIndex + maxVisibleItems
+        bottomItemIndex = bottomItemIndex > list.dataProvider.count-1 ? max(list.dataProvider.count-1,0) : bottomItemIndex//the max part forces the value to be no less than 0
+        //if(bottomItemIndex >= dataProvider.count){bottomItemIndex = dataProvider.count-1}
+        //Swift.print("bottomItemIndex: " + "\(bottomItemIndex)")
+        //Swift.print("topItemIndex: " + "\(topItemIndex)")
+        let curVisibleRange:Range<Int> = topItemIndex..<bottomItemIndex
+        return curVisibleRange
+    }
+    /**
+     * When you add/remove items from a list, the list changes size. This method returns a value that lets you keep the same position of the list after a add/remove items change
+     * EXAMPLE: let p = progress(100, 500, 0, 700)//(200,0.5)
+     */
+    static func progress(maskHeight:CGFloat,_ newItemsHeight:CGFloat, _ oldLableContainerY:CGFloat, _ oldItemsHeight:CGFloat)->(lableContainerY:CGFloat,progress:CGFloat){
+        if(oldLableContainerY >= 0){//this should be more advance, like assert wether an item was inserted in the visiblepart of the view, and position the list accordingly, to be continued
+            let progress = SliderParser.progress(oldLableContainerY, maskHeight, oldItemsHeight)
+            return (oldLableContainerY,progress)}/*pins the list to the top if its already at the top*/
+        let newItemsHeight = newItemsHeight
+        let dist = -(newItemsHeight-oldItemsHeight)//dist <-> old and new itemsHeight
+        let newProgress = (oldLableContainerY+dist)/(-(newItemsHeight-maskHeight))
+        let newLableContainerY = -(newItemsHeight-maskHeight)*newProgress
+        return (newLableContainerY,newProgress)
+    }
+    
 }

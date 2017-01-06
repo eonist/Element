0c8bb94d1737530d12da901f95108017ca20eabf
Eonist <unicornhive@gmail.com>
2017-01-06 16:24:29 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList3.swift

--- src/ui/list/FastList3.swift
+++ src/ui/list/FastList3.swift
@@ -1,5 +1,25 @@
 import Foundation
 
-class FastList3 {
-
+class FastList3:Element,IList{
+    var itemHeight:CGFloat/*The list item height, each item must have the same height*/
+    var dataProvider:DataProvider/*data storage*/
+    var lableContainer:Container?/*holds the list items*/
+    var maxVisibleItems:Int?/*this will be calculated on init and on setSize calls*/
+    var prevVisibleRange:Range<Int>?
+    var visibleItems:[FastListItem] = []
+    init(_ width:CGFloat, _ height:CGFloat, _ itemHeight:CGFloat = NaN,_ dataProvider:DataProvider? = nil, _ parent:IElement?, _ id:String? = nil){
+        self.itemHeight = itemHeight
+        self.dataProvider = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
+        super.init(width, height, parent, id)
+        self.dataProvider.event = self.onEvent/*Add event handler for the dataProvider*/
+        layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
+    }
+    override func resolveSkin() {
+        super.resolveSkin()
+        maxVisibleItems = round(height / itemHeight).int
+        lableContainer = addSubView(Container(width,height,self,"lable"))
+        let numOfItems:Int = Swift.min(maxVisibleItems!, dataProvider.count)
+        prevVisibleRange = 0..<numOfItems//<--this should be the same range as we set bellow no?
+        spawn(0..<numOfItems)
+    }
 }

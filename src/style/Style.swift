4e222a072851c5b5d6d2e7e61a3226117d14e7d9
Eonist <unicornhive@gmail.com>
2017-01-07 18:12:17 +0100

    Files modified: 1
    Modified 1 file:src/style/Style.swift

--- src/style/Style.swift
+++ src/style/Style.swift
@@ -20,13 +20,15 @@ extension Style{
      * Adds styleProperties
      */
     func addStyleProperties(styleProperties:Array<IStyleProperty>){
-        for styleProperty in styleProperties { addStyleProperty(styleProperty) }
+        for styleProperty in styleProperties {
+            addStyleProperty(styleProperty)
+        }
     }
     /**
      * Add styleProperty
-     * @Note will throw an error if a styleProperty with the same name is allready added
-     * @Note the reason that the following methods are not moved into StyleModifier is because they arent that complex, a minimal set of core methods are ok in a bean style class like this, more advance methods can go into StyleModifier
-     * // :TODO: but this method is cognativly taxing just look how long it is, makes more sense to move it to a modifier class, only simple methods should be in Type classes
+     * NOTE: will throw an error if a styleProperty with the same name is allready added
+     * NOTE: the reason that the following methods are not moved into StyleModifier is because they arent that complex, a minimal set of core methods are ok in a bean style class like this, more advance methods can go into StyleModifier
+     * TODO: but this method is cognativly taxing just look how long it is, makes more sense to move it to a modifier class, only simple methods should be in Type classes
      */
     func addStyleProperty(styleProperty:IStyleProperty) {
         styleProperties.append(styleProperty)//TODO:this method is more elaborate, it checks if the item is original, if its not throw error, implement this when its time
@@ -35,11 +37,13 @@ extension Style{
      * Adds styleProperties
      */
     func addStyleProperty(styleProperties:Array<IStyleProperty>){
-        for styleProperty : IStyleProperty in styleProperties{addStyleProperty(styleProperty) }
+        for styleProperty:IStyleProperty in styleProperties{
+            addStyleProperty(styleProperty)
+        }
     }
     /**
-     * @return a style property by the name given
-     * @Note returning null is fine, no need to make a EmptyStyleProperty class, or is there?
+     * Returns a style property by the name given
+     * NOTE: returning nil is fine, no need to make a EmptyStyleProperty class, or is there?
      */
     func getStyleProperty(name:String,_ depth:Int = 0)->IStyleProperty?{
         //Swift.print("styleProperties.count: " + "\(styleProperties.count)")
@@ -60,23 +64,22 @@ extension Style{
         return styleProperties
     }
     /**
-     * @Note a benefit of having this method is that when used you can use the interface of the return type instantly
+     * NOTE: a benefit of having this method is that when used you can use the interface of the return type instantly
      */
     func getStylePropertyAt(index:Int)->IStyleProperty{
         return styleProperties[index]
     }
     /**
-     * @Note this function is not redundant, its usefull for qucik access in some methods
+     * NOTE: this function is not redundant, its usefull for qucik access in some methods
      */
     func getValue(name:String,_ depth:Int = 0)->Any?{
         var styleProperty:IStyleProperty? = getStyleProperty(name,depth)
         return styleProperty != nil ? styleProperty?.value : nil
     }
     /**
-     * @Note this method is here for convenience (methods that should be contained in an Utils class are thous that are seldom used)
+     * NOTE: this method is here for convenience (methods that should be contained in an Utils class are thous that are seldom used)
      */
     func getValueAt(index:Int)->Any{
         return getStylePropertyAt(index).value
     }
-    
 }

295853ff09109464c73f1521a11c504abb4e3530
Eonist <unicornhive@gmail.com>
2017-01-08 17:08:57 +0100

    Files modified: 1
    Modified 1 file:src/ui/list/FastList3.swift

--- src/ui/list/FastList3.swift
+++ src/ui/list/FastList3.swift
@@ -34,6 +34,7 @@ class FastList3:Element,IList{
             //Pool can only have 1 surplus item at the time, you dont want to hold many items that are not in use -> after resize for instance
             //Figure out the pooling in the context that db.count may change while scrolling🏀
                 //only move items to buffer when it moves outside top or bottom limit
+            //sppof(range) could use an array.diff method and genereate individual spoof(fastlistitem) that way
                 
     }
     override func resolveSkin() {

97c623dbc70b711f07259a538b66c92c04f6775e
Eonist <unicornhive@gmail.com>
2017-01-12 00:37:42 +0100

    Files modified: 1
    Modified 1 file:src/style/selector/utils/SelectorExtensions.swift

--- src/style/selector/utils/SelectorExtensions.swift
+++ src/style/selector/utils/SelectorExtensions.swift
@@ -1,5 +1,18 @@
 import Foundation
 
-class SelectorExtensions {
-
+extension Selector:UnWrappable{
+    /**
+     * Converts xml to a Selector instance
+     */
+    static func unWrap<T>(xml:XML) -> T? {
+        //Swift.print("xml.XMLString: " + "\(xml.XMLString)")
+        let element:String = unWrap(xml, "element") ?? ""
+        //Swift.print("element: " + "\(element)")
+        let id:String = unWrap(xml, "id") ?? ""
+        //Swift.print("id: " + "\(id)")
+        let classIds:[String?] = unWrap(xml, "classIds")
+        let states:[String?] = unWrap(xml, "states")
+        //Swift.print("states.count: " + "\(states.count)")
+        return Selector(element,classIds.flatMap{$0},id,states.flatMap{$0}) as? T
+    }
 }

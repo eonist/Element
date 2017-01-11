fdeeb22e9cbe5f3459b9ba819974030db73cb5de
Eonist <unicornhive@gmail.com>
2017-01-11 13:01:53 +0100

    Files modified: 1
    Modified 1 file:src/style/utils/StyleExtensions.swift

--- src/style/utils/StyleExtensions.swift
+++ src/style/utils/StyleExtensions.swift
@@ -1,5 +1,17 @@
 import Foundation
 
-class StyleExtension {
-
+extension Style:UnWrappable{
+    /**
+     * Converts xml to a Style instance
+     * IMPORTANT: this must be located here because it belongs in the Element lib but uses the swift-utils lib
+     */
+    static func unWrap<T>(xml:XML) -> T? {
+        let name:String = unWrap(xml, "name")!
+        //Swift.print("UnWrap.name: " + "\(name)")
+        let styleProperties:[StyleProperty?] = unWrap(xml, "styleProperties")
+        //Swift.print("styleProperties.count: " + "\(styleProperties.count)")
+        let selectors:[Selector?] = unWrap(xml, "selectors")
+        //Swift.print("selectors.count: " + "\(selectors.count)")
+        return Style(name,selectors.flatMap{$0},styleProperties.flatMap{$0}) as? T
+    }
 }

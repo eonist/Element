092c5ab72d38af848dee8b8b38d01baed064d091
Eonist <unicornhive@gmail.com>
2017-01-22 13:42:34 +0100

    Files modified: 2
    Modified 2 files:src/ui/other/Graph.swiftsrc/ui/other/graph/GraphUtils.swift

--- src/ui/other/graph/GraphUtils.swift
+++ src/ui/other/graph/GraphUtils.swift
@@ -1,5 +1,35 @@
 import Foundation
 
-class GraphUtils {
-
+class GraphUtils{
+    /**
+     * Returns graph points (Basically the coordinates of where to place the visual graph points)
+     */
+    static func points(_ size:CGSize,_ position:CGPoint,_ spacing:CGSize, _ vValues:[CGFloat], _ maxValue:CGFloat) -> [CGPoint]{
+        var points:[CGPoint] = []
+        let x:CGFloat = /*position.x*/ spacing.width
+        let y:CGFloat = /*position.y +*/ size.height// - (spacing.height*2)
+        for i in 0..<vValues.count{//calc the graphPoints:
+            var p = CGPoint()
+            p.x = x + (i * spacing.width)
+            p.y = y - ((size.height-(spacing.height*2))/maxValue * vValues[i])
+            points.append(p)
+        }
+        return points
+    }
+    /**
+     * Generates value indicators that match up with the (data set)
+     */
+    static func verticalIndicators(_ vCount:Int,_ maxValue:CGFloat)->[String]{
+        var strings:[String] = []
+        for i in (0..<vCount).reversed() {//swift 3 update
+            //Swift.print("i: " + "\(i)")
+            var num:CGFloat = (maxValue/(vCount.cgFloat-1))*i
+            //Swift.print("num: " + "\(num)")
+            num = round(num)//NumberModifier.toFixed(num, 1)//
+            let str:String = num.string
+            strings.append(str)
+            //Tip: use skin.getWidth() if you need to align Element items with Align
+        }
+        return strings
+    }
 }

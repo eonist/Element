2fb489132af41088da77ac0f41b95c9ffd526573
Eonist <unicornhive@gmail.com>
2017-01-22 14:49:17 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/Graph.swift

--- src/ui/other/Graph.swift
+++ src/ui/other/Graph.swift
@@ -2,7 +2,7 @@ import Cocoa
 @testable import Utils
 
 class Graph:Element {
-    var vValues:[CGFloat] {return [4,2,3,7,5,0,1]}/*vertical amount*///TODO: rename to y-values?
+    var vValues:[CGFloat]?/*vertical amount*///TODO: rename to y-values?
     var hValNames:[String] {return ["A","B","C","D","E","F","G"]}/*horizontal items*///TODO: rename to x-values? 
     var vCount:Int = 5/*Number of vertical indicators*/
     var leftBar:Section?
@@ -21,6 +21,7 @@ class Graph:Element {
     var spacing:CGSize?
     override func resolveSkin() {
         super.resolveSkin()
+        vValues = [4,2,3,7,5,0,1]
         createUI()
         graphPts = GraphUtils.points(newSize!, newPosition!, spacing!, vValues, maxValue!)//creates the init positions of where the points should lay
         createGraph()

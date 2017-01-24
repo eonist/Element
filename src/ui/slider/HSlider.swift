ceabda4a7de5e425dd2698ab8c30cb2e6fd79a52
Eonist <unicornhive@gmail.com>
2017-01-24 18:47:01 +0100

    Files modified: 1
    Modified 1 file:src/ui/slider/HSlider.swift

--- src/ui/slider/HSlider.swift
+++ src/ui/slider/HSlider.swift
@@ -18,6 +18,12 @@ class HSlider:Element{
     }
     override func resolveSkin() {
         super.resolveSkin()
+        createThumb()
+    }
+    /**
+     * Override this method if you want to add a custom thumb
+     */
+    func createThumb(){
         thumb = addSubView(Thumb(thumbWidth, height,false,self))
         setProgressValue(progress)
     }

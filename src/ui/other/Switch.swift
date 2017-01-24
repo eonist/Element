a14947043d668fb7ac9922bd26130726d4b97648
Eonist <unicornhive@gmail.com>
2017-01-24 15:57:14 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/Switch.swift

--- src/ui/other/Switch.swift
+++ src/ui/other/Switch.swift
@@ -1,5 +1,21 @@
 import Foundation
 
-class Switch {
-
+class Switch:VolumeSlider,ICheckable{
+    private var isChecked:Bool = false
+    /**
+     * Sets the self.isChecked variable (Toggles between two states)
+     */
+    func setChecked(_ isChecked:Bool) {
+        self.isChecked = isChecked
+        setSkinState(getSkinState())
+    }
+    func getChecked() -> Bool {
+        return isChecked
+    }
+    override func getSkinState() -> String {
+        return isChecked ? SkinStates.checked + " " + super.getSkinState() : super.getSkinState()
+    }
+    override func getClassType() -> String {
+        return "\(VolumeSlider.self)"//borrow the VolumSlider look for now
+    }
 }

14de6dcb1c0b2b9ca00fc8e5f5c26ff375b7945c
Eonist <unicornhive@gmail.com>
2017-01-26 12:55:20 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/Switch.swift

--- src/ui/other/Switch.swift
+++ src/ui/other/Switch.swift
@@ -20,15 +20,10 @@ class Switch:HSlider,ICheckable{
         Swift.print("isChecked: " + "\(isChecked)")
         setProgressValue(isChecked ? 1:0)
     }
-    override func onThumbMove(event:NSEvent) -> NSEvent{
-        let event = super.onThumbMove(event: event)
-        Swift.print("progress: " + "\(progress)")
-        /*
-        if(progress < 0.5 && isChecked){
-            setChecked(false)//set disable
-        }else if(progress > 0.5 && !isChecked){
-            setChecked(true)//set enable
-        }*/
+    /**
+     *
+     */
+    func interpolateColor(){
         /*bg*/
         let style:IStyle = StyleModifier.clone(skin!.style!,skin!.style!.name)/*We clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
         var styleProperty = style.getStyleProperty("fill") /*Edits the style*/
@@ -45,6 +40,11 @@ class Switch:HSlider,ICheckable{
             thumbStyleProperty!.value = color
             thumb!.skin!.setStyle(thumbStyle)/*updates the skin*/
         }
+    }
+    override func onThumbMove(event:NSEvent) -> NSEvent{
+        let event = super.onThumbMove(event: event)
+        //Swift.print("progress: " + "\(progress)")
+        interpolateColor()
         return event
     }
     override func onThumbDown() {
@@ -143,6 +143,14 @@ class Switch:HSlider,ICheckable{
     }
     required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
+
+/*
+ if(progress < 0.5 && isChecked){
+ setChecked(false)//set disable
+ }else if(progress > 0.5 && !isChecked){
+ setChecked(true)//set enable
+ }*/
+
 class SwitchButton:Button{
     override func mouseDown(_ event: MouseEvent) {
         super.mouseDown(event)

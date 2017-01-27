3dc4b279866f47e5ea54fbb13e48b8edd93e75de
Eonist <unicornhive@gmail.com>
2017-01-27 16:07:25 +0100

    Files modified: 3
    Modified 3 files:src/ui/other/ProgressIndicator.swiftsrc/ui/other/Switch.swiftsrc/ui/slider/rb/Thumb.swift

--- src/ui/other/Switch.swift
+++ src/ui/other/Switch.swift
@@ -70,13 +70,13 @@ class Switch:HSlider,ICheckable{
         
         /*Thumb Anim*/
         if(thumbAnimator != nil){thumbAnimator!.stop()}
-        thumbAnimator = Animator(Animation.sharedInstance,0.2,0,1,thumbAnim,Linear.easeLinear)
+        thumbAnimator = Animator(Animation.sharedInstance,0.2,0,1,thumbAnim,Linear.ease)
         thumbAnimator!.start()
         
         /*bg Anim*/
         if(progress == 0){//must be in off state
             if(bgAnimator != nil){bgAnimator!.stop()}
-            bgAnimator = Animator(Animation.sharedInstance,0.2,0,1,bgAnim,Linear.easeLinear)
+            bgAnimator = Animator(Animation.sharedInstance,0.2,0,1,bgAnim,Linear.ease)
             bgAnimator!.start()
         }
         interpolateColor()//ticks the thumb line to be the correct color, or else the line would be derived from css
@@ -101,13 +101,13 @@ class Switch:HSlider,ICheckable{
         thumb!.skin!.setStyle(style)/*updates the skin*/
         /*Thumb Anim*/
         if(thumbAnimator != nil){thumbAnimator!.stop()}
-        thumbAnimator = Animator(Animation.sharedInstance,0.2,1,0,thumbAnim,Linear.easeLinear)
+        thumbAnimator = Animator(Animation.sharedInstance,0.2,1,0,thumbAnim,Linear.ease)
         thumbAnimator!.start()
         
         /*Bg Anim*/
         if(progress == 0){//must be in off state
             if(bgAnimator != nil){bgAnimator!.stop()}
-            bgAnimator = Animator(Animation.sharedInstance,0.2,1,0,bgAnim,Linear.easeLinear)
+            bgAnimator = Animator(Animation.sharedInstance,0.2,1,0,bgAnim,Linear.ease)
             bgAnimator!.start()
         }else{
             offColor = NSColor.white//temp fix
@@ -172,9 +172,9 @@ class Switch:HSlider,ICheckable{
         Swift.print("setChecked: " + "\(isChecked)")
         if(progressAnimator != nil){progressAnimator!.stop()}
         if(self.isChecked && !isChecked){/*Animate setProgress from 1 - 0*/
-            progressAnimator = Animator(Animation.sharedInstance,0.2,1,0,progressAnim,Linear.easeLinear)
+            progressAnimator = Animator(Animation.sharedInstance,0.2,1,0,progressAnim,Linear.ease)
         }else if (!self.isChecked && isChecked){/*Animate setProgress from 0 - 1*/
-            progressAnimator = Animator(Animation.sharedInstance,0.2,0,1,progressAnim,Linear.easeLinear)
+            progressAnimator = Animator(Animation.sharedInstance,0.2,0,1,progressAnim,Linear.ease)
         }
         progressAnimator!.start()
         self.isChecked = isChecked

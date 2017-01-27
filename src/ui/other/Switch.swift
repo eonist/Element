55bcda490ce26487ecd2b190f9b5200c1e1661ef
Eonist <unicornhive@gmail.com>
2017-01-27 20:03:13 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/Switch.swift

--- src/ui/other/Switch.swift
+++ src/ui/other/Switch.swift
@@ -160,7 +160,9 @@ class Switch:HSlider,ICheckable{
         let style:IStyle = StyleModifier.clone(skin!.style!,skin!.style!.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
         var fillProp = style.getStyleProperty("fill") /*edits the style*/
         //let curColor:NSColor = fillProp!.value as! NSColor
-        offColor = NSColor.white.blended(withFraction: value, of: grey)!
+        let initColor = isChecked ? NSColor.green : NSColor.white
+        let endColor = isChecked ? NSColor.green : grey
+        offColor = initColor.blended(withFraction: value, of: endColor)!
         fillProp!.value = offColor
         skin!.setStyle(style)
     }

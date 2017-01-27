24315605012f4e208fe376b7dcd3800713c13b15
Eonist <unicornhive@gmail.com>
2017-01-27 15:50:34 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/graph/bar/BarGraph.swift

--- src/ui/other/graph/bar/BarGraph.swift
+++ src/ui/other/graph/bar/BarGraph.swift
@@ -70,7 +70,7 @@ class BarGraph:Graph {
         graphPts = GraphUtils.points(newSize!, newPosition!, spacing!, vValues, maxValue!)
        
         if(animator != nil){animator!.stop()}/*stop any previous running animation*/
-        animator = Animator(Animation.sharedInstance,0.5,0,1,interpolateValue,Quad.easeInQuad)
+        animator = Animator(Animation.sharedInstance,0.5,0,1,interpolateValue,Quad.easeIn)
         animator!.start()
     }
     /**

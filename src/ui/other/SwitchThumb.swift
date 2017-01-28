ee465f1b67a95854d4ca5142a69510de3622c6c0
Eonist <unicornhive@gmail.com>
2017-01-28 16:16:36 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/SwitchThumb.swift

--- src/ui/other/SwitchThumb.swift
+++ src/ui/other/SwitchThumb.swift
@@ -2,5 +2,5 @@ import Foundation
 
 class SwitchThumb {
     //continue here:
-        //the entire Switch is the button, the thumb should be non interactive and click-through
+        //the entire Switch is the button, the thumb should be non interactive and click-through, similar to how RadioBullet works in RadioButton i guess
 }

3e22a777e4940ea3939ee75082a20744114a7754
Eonist <unicornhive@gmail.com>
2017-01-28 14:43:28 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/SwitchSlider.swift

--- src/ui/other/SwitchSlider.swift
+++ src/ui/other/SwitchSlider.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-class SwitchSlider {
-
+class SwitchSlider:Element {
+    var progress:CGFloat
+    init(_ width:CGFloat, _ height:CGFloat, _ progress:CGFloat = 0, _ parent:IElement? = nil, _ id:String? = nil, _ classId:String? = nil) {
+        self.progress = progress
+        super.init(width,height,parent,id)
+    }
+    required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

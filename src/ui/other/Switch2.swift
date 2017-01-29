6eb99515f6cbdfee826b37653bd6d046e1960d13
Eonist <unicornhive@gmail.com>
2017-01-29 13:32:19 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/Switch2.swift

--- src/ui/other/Switch2.swift
+++ src/ui/other/Switch2.swift
@@ -13,16 +13,6 @@ class Switch2:SwitchSlider,ICheckable{
         self.isChecked = isChecked
         super.init(width,height,isChecked ? 1:0,parent,id)
     }
-    func thumbAnim(value:CGFloat){
-        //Swift.print("thumbAnim: " + "\(value)")
-        let style:IStyle = self.skin!.style!//StyleModifier.clone(thumb!.skin!.style!, thumb!.skin!.style!.name)
-        var marginProp = style.getStyleProperty("margin-left",2) /*edits the style*/
-        marginProp!.value = isChecked ? 20 * (1-value) : 0
-        var widthProp = style.getStyleProperty("width",2)
-        widthProp!.value = 80 + (20 * value)
-        //Swift.print("thumbStyleProperty!.value: " + "\(thumbStyleProperty!.value)")
-        self.skin!.setStyle(style)
-    }
     override func mouseDown(_ event:MouseEvent) {
         Swift.print("Switch2.mouseDown isChecked: \(isChecked)")
         let style:IStyle = self.skin!.style!//StyleModifier.clone(thumb!.skin!.style!, thumb!.skin!.style!.name)
@@ -70,4 +60,16 @@ class Switch2:SwitchSlider,ICheckable{
         return isChecked ? SkinStates.checked + " " + super.getSkinState() : super.getSkinState()
     }
     required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
+extension Switch2{
+    func thumbAnim(value:CGFloat){
+        //Swift.print("thumbAnim: " + "\(value)")
+        let style:IStyle = self.skin!.style!//StyleModifier.clone(thumb!.skin!.style!, thumb!.skin!.style!.name)
+        var marginProp = style.getStyleProperty("margin-left",2) /*edits the style*/
+        marginProp!.value = isChecked ? 20 * (1-value) : 0
+        var widthProp = style.getStyleProperty("width",2)
+        widthProp!.value = 80 + (20 * value)
+        //Swift.print("thumbStyleProperty!.value: " + "\(thumbStyleProperty!.value)")
+        self.skin!.setStyle(style)
+    }
 }

202ce6c953cd261da9d23e4a8548139110561a75
Eonist <unicornhive@gmail.com>
2017-01-29 14:24:59 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/Switch2.swift

--- src/ui/other/Switch2.swift
+++ src/ui/other/Switch2.swift
@@ -114,7 +114,7 @@ extension Switch2{
         let style:IStyle = StyleModifier.clone(skin!.style!,skin!.style!.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
         var fillProp = style.getStyleProperty("fill",1) /*edits the style*/
         //let curColor:NSColor = fillProp!.value as! NSColor
-        if(isChecked){
+        if(getChecked()){
             Swift.print("initColor : grey")
             Swift.print("endColor : grey")
         }else{

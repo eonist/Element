04c95fe8843beefebc8e2ba90813d00d4e0adeed
Eonist <unicornhive@gmail.com>
2017-01-29 13:30:29 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/SwitchSlider.swift

--- src/ui/other/SwitchSlider.swift
+++ src/ui/other/SwitchSlider.swift
@@ -9,8 +9,8 @@ class SwitchSlider:Element {
         super.init(width,height,parent,id)
     }
     func onMouseMove(event:NSEvent)-> NSEvent?{
-        Swift.print("SwitchSlider.onMouseMove")
         progress = HSliderUtils.progress(event.localPos(self).x, 0/*thumbWidth/2*/, width, /*thumbWidth*/ 0)
+        Swift.print("SwitchSlider.onMouseMove progress: " + "\(progress)")
         super.onEvent(SliderEvent(SliderEvent.change,progress,self))
         return event
     }

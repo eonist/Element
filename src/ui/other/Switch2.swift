0fef1f3557aad94322b822f82baebbca7b1814e4
Eonist <unicornhive@gmail.com>
2017-01-29 13:13:46 +0100

    Files modified: 1
    Modified 1 file:src/ui/other/Switch2.swift

--- src/ui/other/Switch2.swift
+++ src/ui/other/Switch2.swift
@@ -13,7 +13,18 @@ class Switch2:SwitchSlider,ICheckable{
         super.init(width,height,isChecked ? 1:0,parent,id)
     }
     override func mouseDown(_ event:MouseEvent) {
-        Swift.print("Switch2.mouseDown")
+        Swift.print("Switch2.mouseDown isChecked: \(isChecked)")
+        let style:IStyle = thumb!.skin!.style!//StyleModifier.clone(thumb!.skin!.style!, thumb!.skin!.style!.name)
+        var widthProp = style.getStyleProperty("width",1)
+        widthProp!.value = 80
+        var marginProp = style.getStyleProperty("margin-left",1) /*edits the style*/
+        marginProp!.value = progress == 1 ? 20  : 0
+        thumb!.skin!.setStyle(style)/*updates the skin*/
+        
+        /*Thumb Anim*/
+        if(thumbAnimator != nil){thumbAnimator!.stop()}
+        thumbAnimator = Animator(Animation.sharedInstance,0.2,0,1,thumbAnim,Linear.ease)
+        thumbAnimator!.start()
         super.mouseDown(event)
     }
     override func mouseUpInside(_ event: MouseEvent) {

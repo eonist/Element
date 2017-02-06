a768f9bd0a5bc7b389ea508a9456808c87331c69
Eonist <unicornhive@gmail.com>
2017-02-06 14:26:32 +0100

    Files modified: 1
    Modified 1 file:Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "Element",
-	dependencies: [],
+	dependencies: [
+		.Package(url: "https://github.com/eonist/swift-utils.git", majorVersion: 1, minor:1)
+    ],
 	exclude: ["README.md"]
 )

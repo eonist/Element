d5c5be372a4e2909e485eac4908bb70360ca7fc0
Eonist <unicornhive@gmail.com>
2017-02-17 14:09:11 +0100

    Files modified: 3
    Modified 3 files:src/Element/ui/list/rb/RBSliderFastList.swiftsrc/Element/ui/list/rb/RBSliderFastList2.swiftsrc/Element/ui/list/rb/RBSliderList.swift

--- src/Element/ui/list/rb/RBSliderFastList.swift
+++ src/Element/ui/list/rb/RBSliderFastList.swift
@@ -9,7 +9,7 @@ class RBSliderFastList:FastList,IRBSliderList{
     var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
     /*Slider*/
     var slider:VSlider?
-    private var sliderInterval:CGFloat?
+    var sliderInterval:CGFloat?
     override func resolveSkin() {
         super.resolveSkin()
         /*RubberBand*/

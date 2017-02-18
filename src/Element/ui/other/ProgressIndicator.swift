bccd51afb14f3934adec7fe24a33dce0fa0d64d2
Eonist <unicornhive@gmail.com>
2017-02-18 16:06:48 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/ProgressIndicator.swift

--- src/Element/ui/other/ProgressIndicator.swift
+++ src/Element/ui/other/ProgressIndicator.swift
@@ -36,7 +36,7 @@ class ProgressIndicator:Element {
      * PARAM: value: 0 - 1
      */
     func progress(_ value:CGFloat){
-        //Swift.print("progress: " + "\(value)")
+        Swift.print("ProgressIndicator.progress: " + "\(value)")
         //Could the bellow be done simpler: think sequence looping in a video.
         let initAlpha = lineStyle.color.alphaComponent//<--can be moved to a global scope
         let restAlpha = 1 - initAlpha//<--can be moved to a global scope
@@ -62,7 +62,7 @@ class ProgressIndicator:Element {
      * TODO: The final tick should be 0, to make this happen you need to offset the i, possibly
      */
     func reveal(_ value:CGFloat){//value goes from 0 to 1
-        //Swift.print("ProgressIndicator.reveal() value: " + "\(value)")
+        Swift.print("ProgressIndicator.reveal() value: " + "\(value)")
         revealProgress = value
         let initAlpha = lineStyle.color.alphaComponent//<--can be moved to a global scope
         let restAlpha = 1 - initAlpha//<--can be moved to a global scope
@@ -91,17 +91,19 @@ class ProgressIndicator:Element {
      * Esentially you start a repeating animation that modulates a value from 0 - 1 of a defined time over n-times
      */
     func start(){
+        Swift.print("💚 ProgressIndicator.start")
         //assert if animator exist else create animator w/ repeatCount : 0 and 0 to 1 sec w/ progress as the call back method
         //start anim
         //lineStyle.color = lineStyle.color.alpha(0.5)
         if(animator != nil){animator!.stop()}//stop any previous running animation
-        animator!.start()
+        animator!.start()/*start animator*/
     }
     /**
      *
      */
     func stop(){
-        animator!.stop()//stop animator
+        Swift.print("❤️️ ProgressIndicator.stop")
+        animator!.stop()/*stop animator*/
     }
     required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

24b233c24dc9eb316d539e8f7c0987a8a358bfc0
Eonist <30n1st@gmail.com>
2017-03-05 16:39:21 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:src/Element/ui/list/utils/ListModifier.swift

--- src/Element/ui/list/utils/ListModifier.swift
+++ src/Element/ui/list/utils/ListModifier.swift
@@ -24,22 +24,5 @@ class ListModifier {
         let y:CGFloat = ListModifier.scrollTo(progress, list.height, list.itemsHeight)
         list.lableContainer!.y = y/*we offset the y position of the lableContainer*/
     }
-    /**
-     * Returns the y position of a "virtual" list
-     */
-    static func scrollTo(_ progress:CGFloat,_ maskHeight:CGFloat,_ itemsHeight:CGFloat)->CGFloat{
-        let scrollHeight:CGFloat = itemsHeight - maskHeight/*allItems.height - mask.height*/
-        //Swift.print("scrollHeight: " + "\(scrollHeight)")
-        /*
-        if(scrollHeight.isNegative && progress.isNegative){
-            scrollHeight = scrollHeight * -1//Swift.max(scrollHeight,0)
-        }
-        Swift.print("progress: " + "\(progress)")
-        Swift.print("scrollHeight: " + "\(scrollHeight)")
-        */
-        //continue here: get both directions working, clip the values in sliderlist only
-        
-        let y:CGFloat = round(progress * scrollHeight)//Things may actually be smoother if you remove the round variable
-        return -y
-    }
+    
 }

cf69170c29b3de912bafe169bfcc12d7d892f3d6
Eonist <30n1st@gmail.com>
2017-03-05 17:14:33 +0100

    Files modified: 2
    Modified 2 files:src/Element/ui/list/slider/protocols/ISlidable.swiftsrc/Element/ui/list/utils/protocols/IScrollable.swift

--- src/Element/ui/list/slider/protocols/ISlidable.swift
+++ src/Element/ui/list/slider/protocols/ISlidable.swift
@@ -1,10 +1,6 @@
 import Foundation
 
 protocol ISlidable:class{
-    var height:CGFloat{get}//used to represent the maskHeight aka the visible part.
-    var itemHeight:CGFloat{get}//item of one item, used to calculate interval
-    var itemsHeight:CGFloat{get}//total height of the items
-    var lableContainer:Container? {get}
     /**/
     var slider:VSlider?{get}
     var sliderInterval:CGFloat?{get set}
--- src/Element/ui/list/utils/protocols/IScrollable.swift
+++ src/Element/ui/list/utils/protocols/IScrollable.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-class IScrollable {
-
+protocol IScrollable {
+    var height:CGFloat{get}//used to represent the maskHeight aka the visible part.
+    var itemHeight:CGFloat{get}//item of one item, used to calculate interval
+    var itemsHeight:CGFloat{get}//total height of the items
+    var lableContainer:Container? {get}
 }

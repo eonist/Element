6448fc301ec06a327a77396317494a9a1e35aaad
Eonist <30n1st@gmail.com>
2017-03-05 13:22:51 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/rb/utils/RBSliderView.swift

--- src/Element/ui/list/rb/utils/RBSliderView.swift
+++ src/Element/ui/list/rb/utils/RBSliderView.swift
@@ -1,11 +1,51 @@
 import Foundation
 
 class RBSliderView:Element {
+    var lableContainer:Container?
+    /*RubberBand*/
+    var mover:RubberBand?
+    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
+    var velocities:[CGFloat] = Array(repeating: 0, count: 10)/*represents the velocity resolution of the gesture movment*/
+    var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
+    /*Slider*/
+    var slider:VSlider?
+    var sliderInterval:CGFloat?
+    var itemsHeight:CGFloat = 600
+    var itemHeight:CGFloat = 24
     override func resolveSkin() {
-        
+        lableContainer = addSubView(Container(width,height,self,"items"))
+        /*RubberBand*/
+        let frame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
+        let itemsRect = CGRect(0,0,width,itemsHeight)/*represents the total size of the content *///TODO: could be ranmed to contentRect
+        mover = RubberBand(Animation.sharedInstance,setProgress,frame,itemsRect)
+        mover!.event = onEvent/*Add an eventHandler for the mover object, avoids logging missing eventHandler, this has no functionality in this class, but may have in classes that extends this class*/
+        /*slider*/
+        let itemHeight:CGFloat = 24
+        sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
+        slider!.setThumbHeightValue(thumbHeight)
     }
+    /**
+     * NOTE: this method overides the Native NSView scrollWheel method
+     */
+    /*
+     override func scrollWheel(with event: NSEvent) {
+        scroll(event)//forward the event to the scrollExtension
+        if(event.phase == NSEventPhase.changed){setProgress(mover!.result)}/*direct manipulation*/
+        super.scrollWheel(with: event)/*keep forwarding the scrollWheel event for NSViews higher up the hierarcy to listen to*/
+     }
+     */
 }
 extension RBSliderView{
-    
-    
+    /**
+     * PARAM value: is the final y value for the lableContainer
+     * TODO: Try to use a preCalculated itemsHeight, as this can be heavy to calculate for lengthy lists
+     */
+    func setProgress(_ value:CGFloat){
+        //Swift.print("RBSliderList.setProgress() value: " + "\(value)")
+        lableContainer!.frame.y = value/*<--this is where we actully move the labelContainer*/
+        progressValue = value / -(itemsHeight - height)/*get the the scalar values from value.*/
+        slider!.setProgressValue(progressValue!)
+    }
 }

ada51729ac13b479f909aff6a174ab2287b3b8fd
Eonist <30n1st@gmail.com>
2017-03-05 11:49:46 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/slider/ISliderList.swift

--- src/Element/ui/list/slider/ISliderList.swift
+++ src/Element/ui/list/slider/ISliderList.swift
@@ -1,29 +1,6 @@
 import Cocoa
 @testable import Utils
 
-protocol ISlidable:class{
-    var height:CGFloat{get}//used to represent the maskHeight aka the visible part.
-    var itemHeight:CGFloat{get}//item of one item, used to calculate interval
-    var itemsHeight:CGFloat{get}//total height of the items
-    var lableContainer:Container? {get}
-    /**/
-    var slider:VSlider?{get}
-    var sliderInterval:CGFloat?{get set}
-    func setProgress(_ progress:CGFloat)
-    func updateSlider()
-}
-extension ISlidable{
-    /**
-     * Updates the slider interval and the sliderThumbSize (after DP events: add/remove etc)
-     */
-    func updateSlider(){
-        sliderInterval = floor(self.itemsHeight - height)/itemHeight
-        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height/*<--this should probably be .getHeight()*/);
-        slider!.setThumbHeightValue(thumbHeight)
-        let progress:CGFloat = SliderParser.progress(lableContainer!.y, height, itemsHeight)//TODO: use getHeight() instead of height
-        slider!.setProgressValue(progress)
-    }
-}
 protocol ISliderList:IList,ISlidable {/*Convenience*/
     func scroll(_ theEvent:NSEvent)
 }

2cd6c722a8bfb49f4b96459008c80e159ed345f3
Eonist <30n1st@gmail.com>
2017-03-05 12:24:40 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift

--- src/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift
+++ src/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift
@@ -2,5 +2,14 @@ import Foundation
 
 protocol IRBScrollableSlidable:class,ISlidable,IRBScrollable{}//Convenience, almost like a typalias
 extension IRBScrollableSlidable{
-    
+    func scrollWheelEnter(){
+        slider!.thumb!.fadeIn()
+    }
+    func scrollWheelExit(){}
+    func scrollWheelExitedAndIsStationary(){
+        if(slider?.thumb?.getSkinState() == SkinStates.none){slider?.thumb?.fadeOut()}
+    }
+    func scrollAnimStopped(){
+        slider!.thumb!.fadeOut()
+    }
 }

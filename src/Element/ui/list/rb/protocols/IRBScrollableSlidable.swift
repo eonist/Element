b6d153af9b33dab43e2c1c6a0cafb125f1b62b9f
Eonist <30n1st@gmail.com>
2017-03-05 13:57:20 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift

--- src/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift
+++ src/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift
@@ -6,22 +6,25 @@ protocol IRBScrollableSlidable:class,ISlidable,IRBScrollable{
 
 }//Convenience, almost like a typalias
 extension IRBScrollableSlidable{
+    func scrollWheelExit() {defaultScrollWheelEntersScrollWheelExit()}
+    func scrollWheelEnter() {defaultScrollWheelEnter()}
+    func scrollWheelEnterScrollWheelExitedAndIsStationary(){defaultScrollWheelEnterScrollWheelExitedAndIsStationary()}
     /**
      * When two fingers touches the track-pad this method is called
      * NOTE: this method is called from: onScrollWheelEnter
      */
-    func scrollWheelEnter(){//2. spring to refreshStatePosition
+    func defaultScrollWheelEnter(){//2. spring to refreshStatePosition
         //Swift.print("RBSliderFastList.scrollWheelEnter()" + "\(progressValue)")
         if(itemsHeight >= height){slider!.thumb!.fadeIn()}/*fades in the slider*/
     }
     /**
      * NOTE: This method is called from onScrollWheelExit
      */
-    func scrollWheelExit(){}
-    func scrollWheelExitedAndIsStationary(){
+    func defaultScrollWheelEntersScrollWheelExit(){}
+    func defaultScrollWheelEnterScrollWheelExitedAndIsStationary(){
         if(slider?.thumb?.getSkinState() == SkinStates.none){slider?.thumb?.fadeOut()}/*only fade out if the state is none, aka not over*/
     }
-    func scrollAnimStopped(){
+    func scrollWheelEnterScrollAnimStopped(){
         slider!.thumb!.fadeOut()
     }
 }

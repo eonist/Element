0b36c90f0464c77781d9b5e77ddd3e8901df867e
Eonist <30n1st@gmail.com>
2017-03-05 12:26:43 +0100

    Files modified: 2
    Modified 2 files:src/Element/ui/list/rb/RBSliderFastList.swiftsrc/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift

--- src/Element/ui/list/rb/RBSliderFastList.swift
+++ src/Element/ui/list/rb/RBSliderFastList.swift
@@ -105,22 +105,4 @@ extension RBSliderFastList{//TODO:rather extend IRBSliderFastList
         ListModifier.scrollTo(self, sliderEvent.progress)
         mover!.value = lableContainer!.frame.y
     }
-    /**
-     * When two fingers are touches the track-pad this method is called
-     */
-    func scrollWheelEnter(){//2. spring to refreshStatePosition
-        //Swift.print("RBSliderFastList.scrollWheelEnter()" + "\(progressValue)")
-        if(itemsHeight >= height){slider!.thumb!.fadeIn()}/*fades in the slider*/
-    }
-    func scrollWheelExit(){}
-    func scrollWheelExitedAndIsStationary(){
-        //Swift.print("RBSliderFastList.scrollWheelExitedAndIsStationary() ")
-        if(slider?.thumb?.getSkinState() == SkinStates.none){/*only fade out if the state is none, aka not over*/
-            slider?.thumb?.fadeOut()
-        }
-    }
-    func scrollAnimStopped(){
-        //Swift.print("RBSliderList.scrollAnimStopped()")
-        slider!.thumb!.fadeOut()
-    }
 }
--- src/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift
+++ src/Element/ui/list/rb/protocols/IRBScrollableSlidable.swift
@@ -2,14 +2,19 @@ import Foundation
 
 protocol IRBScrollableSlidable:class,ISlidable,IRBScrollable{}//Convenience, almost like a typalias
 extension IRBScrollableSlidable{
-    func scrollWheelEnter(){
-        slider!.thumb!.fadeIn()
+    /**
+     * When two fingers are touches the track-pad this method is called
+     */
+    func scrollWheelEnter(){//2. spring to refreshStatePosition
+        //Swift.print("RBSliderFastList.scrollWheelEnter()" + "\(progressValue)")
+        if(itemsHeight >= height){slider!.thumb!.fadeIn()}/*fades in the slider*/
     }
     func scrollWheelExit(){}
     func scrollWheelExitedAndIsStationary(){
-        if(slider?.thumb?.getSkinState() == SkinStates.none){slider?.thumb?.fadeOut()}
+        if(slider?.thumb?.getSkinState() == SkinStates.none){slider?.thumb?.fadeOut()}/*only fade out if the state is none, aka not over*/
     }
     func scrollAnimStopped(){
         slider!.thumb!.fadeOut()
     }
 }
+

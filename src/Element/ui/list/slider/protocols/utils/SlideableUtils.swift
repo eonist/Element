4390cb3088c8e954485c28aaf36cde97ca1adae4
Eonist <30n1st@gmail.com>
2017-03-05 16:49:18 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/slider/protocols/utils/SlideableUtils.swift

--- src/Element/ui/list/slider/protocols/utils/SlideableUtils.swift
+++ src/Element/ui/list/slider/protocols/utils/SlideableUtils.swift
@@ -1,5 +1,29 @@
 import Foundation
 
 class SlideableUtils {
-
+    /**
+     * Scrolls the list to a scalar position (value 0-1)
+     */
+    static func scrollTo(_ list:IList,_ progress:CGFloat){
+        let y:CGFloat = ListModifier.scrollTo(progress, list.height, list.itemsHeight)
+        list.lableContainer!.y = y/*we offset the y position of the lableContainer*/
+    }
+    /**
+     * Returns the y position of a "virtual" list
+     */
+    static func scrollTo(_ progress:CGFloat,_ maskHeight:CGFloat,_ itemsHeight:CGFloat)->CGFloat{
+        let scrollHeight:CGFloat = itemsHeight - maskHeight/*allItems.height - mask.height*/
+        //Swift.print("scrollHeight: " + "\(scrollHeight)")
+        /*
+         if(scrollHeight.isNegative && progress.isNegative){
+         scrollHeight = scrollHeight * -1//Swift.max(scrollHeight,0)
+         }
+         Swift.print("progress: " + "\(progress)")
+         Swift.print("scrollHeight: " + "\(scrollHeight)")
+         */
+        //continue here: get both directions working, clip the values in sliderlist only
+        
+        let y:CGFloat = round(progress * scrollHeight)//Things may actually be smoother if you remove the round variable
+        return -y
+    }
 }

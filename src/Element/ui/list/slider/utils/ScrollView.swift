6f7d4854d2ab1f0761b6477a756ad098f9d444ae
Eonist <30n1st@gmail.com>
2017-03-05 20:16:54 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/slider/utils/ScrollView.swift

--- src/Element/ui/list/slider/utils/ScrollView.swift
+++ src/Element/ui/list/slider/utils/ScrollView.swift
@@ -9,3 +9,15 @@ class ScrollView:Element,IScrollable{
         lableContainer = addSubView(Container(width,height,self,"lable"))
     }
 }
+extension ScrollView{
+    /**
+     * PARAM value: is the final y value for the lableContainer
+     * TODO: Try to use a preCalculated itemsHeight, as this can be heavy to calculate for lengthy lists
+     */
+    func setProgress(_ value:CGFloat){
+        //Swift.print("RBSliderList.setProgress() value: " + "\(value)")
+        lableContainer!.frame.y = value/*<--this is where we actully move the labelContainer*/
+        progressValue = value / -(itemsHeight - height)/*get the the scalar values from value.*/
+        slider!.setProgressValue(progressValue!)
+    }
+}

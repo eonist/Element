7cb9bc0a8fb0fede97125053446e8d4ef7daf388
Eonist <30n1st@gmail.com>
2017-03-05 16:47:15 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/utils/ListModifier.swift

--- src/Element/ui/list/utils/ListModifier.swift
+++ src/Element/ui/list/utils/ListModifier.swift
@@ -17,12 +17,4 @@ class ListModifier {
         if(!selectable.getSelected()) {selectable.setSelected(true)}
         SelectModifier.unSelectAllExcept(selectable, list.lableContainer!)
     }
-    /**
-     * Scrolls the list to a scalar position (value 0-1)
-     */
-    static func scrollTo(_ list:IList,_ progress:CGFloat){
-        let y:CGFloat = ListModifier.scrollTo(progress, list.height, list.itemsHeight)
-        list.lableContainer!.y = y/*we offset the y position of the lableContainer*/
-    }
-    
 }

eee4ad3d20c8bf24f4b09f1b59511ce8d937c5d5
Eonist <30n1st@gmail.com>
2017-03-05 15:24:54 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/rb/utils/RBSliderView.swift

--- src/Element/ui/list/rb/utils/RBSliderView.swift
+++ src/Element/ui/list/rb/utils/RBSliderView.swift
@@ -1,7 +1,7 @@
-import Foundation
+import Cocoa
 @testable import Utils
 
-class RBSliderView:Element {
+class RBSliderView:Element,IRBScrollableSlidable {
     var lableContainer:Container?
     /*RubberBand*/
     var mover:RubberBand?
@@ -31,13 +31,11 @@ class RBSliderView:Element {
     /**
      * NOTE: this method overides the Native NSView scrollWheel method
      */
-    /*
-     override func scrollWheel(with event: NSEvent) {
+     override func scrollWheel(with event:NSEvent) {
         scroll(event)//forward the event to the scrollExtension
         if(event.phase == NSEventPhase.changed){setProgress(mover!.result)}/*direct manipulation*/
         super.scrollWheel(with: event)/*keep forwarding the scrollWheel event for NSViews higher up the hierarcy to listen to*/
      }
-     */
 }
 extension RBSliderView{
     /**

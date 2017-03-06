42ac32e9657d2e038dd524effbc154052651cd8e
Eonist <30n1st@gmail.com>
2017-03-06 11:34:39 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/slider/protocols/ISliderList.swift

--- src/Element/ui/list/slider/protocols/ISliderList.swift
+++ src/Element/ui/list/slider/protocols/ISliderList.swift
@@ -9,17 +9,7 @@ extension ISliderList{
     //Continue here:
         //move the scroll into Scrollable
     
-    /**
-     * NOTE: Slider list and SliderFastList uses this method
-     */
-    func scroll(_ theEvent:NSEvent) {
-        let progress:CGFloat = SliderListUtils.progress(theEvent.deltaY, self.sliderInterval!, self.slider!.progress)
-        //Swift.print("progress: " + "\(progress)")
-        setProgress(progress)/*Sets the target item to correct y, according to the current scrollBar progress*/
-        self.slider?.setProgressValue(progress)/*Positions the slider.thumb*/
-        if(theEvent.momentumPhase == NSEventPhase.ended){self.slider!.thumb!.setSkinState("inActive")}
-        else if(theEvent.momentumPhase == NSEventPhase.began){self.slider!.thumb!.setSkinState(SkinStates.none)}//include may begin here
-    }
+    
 }
 class SliderListUtils{
     /**

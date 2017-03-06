199ebabf7efd52ed102b76ef03c8f7d437a7ced1
Eonist <30n1st@gmail.com>
2017-03-06 10:17:36 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/SliderView.swift

--- src/Element/ui/other/view/SliderView.swift
+++ src/Element/ui/other/view/SliderView.swift
@@ -8,6 +8,8 @@ class SliderView:ScrollView,ISlidable {
     override func resolveSkin() {
         super.resolveSkin()
         /*slider*/
+        Swift.print("SliderView.height: " + "\(height)")
+        Swift.print("SliderView.getHeight(): " + "\(getHeight())")
         sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
         slider = addSubView(VSlider(itemHeight,height,0,0,self))
         let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)

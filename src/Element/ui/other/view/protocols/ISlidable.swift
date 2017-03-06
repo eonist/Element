4c4b1c16324d2cd6cfb09f59c3dd77518f6d589b
Eonist <30n1st@gmail.com>
2017-03-06 11:36:44 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/ISlidable.swift

--- src/Element/ui/other/view/protocols/ISlidable.swift
+++ src/Element/ui/other/view/protocols/ISlidable.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 /**
  * Slidable is for Elements that has a slider attached
  */
@@ -26,5 +26,16 @@ extension ISlidable{
         let progressValue = self.itemsHeight < height ? 0 : progress/*pins the lableContainer to the top if itemsHeight is less than height*/
         //Swift.print("progressValue: " + "\(progressValue)")
         ScrollableUtils.scrollTo(self,progressValue)/*Sets the target item to correct y, according to the current scrollBar progress*/
+    }
+    /**
+     * NOTE: Slider list and SliderFastList uses this method
+     */
+    func scroll(_ theEvent:NSEvent) {
+        let progress:CGFloat = SliderListUtils.progress(theEvent.deltaY, self.sliderInterval!, self.slider!.progress)
+        //Swift.print("progress: " + "\(progress)")
+        setProgress(progress)/*Sets the target item to correct y, according to the current scrollBar progress*/
+        self.slider?.setProgressValue(progress)/*Positions the slider.thumb*/
+        if(theEvent.momentumPhase == NSEventPhase.ended){self.slider!.thumb!.setSkinState("inActive")}
+        else if(theEvent.momentumPhase == NSEventPhase.began){self.slider!.thumb!.setSkinState(SkinStates.none)}//include may begin here
     }
 }

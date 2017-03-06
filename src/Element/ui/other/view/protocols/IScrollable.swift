b4dfdd98f1683f082e9e8123509e329776ef2cd5
Eonist <30n1st@gmail.com>
2017-03-06 12:37:29 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/IScrollable.swift

--- src/Element/ui/other/view/protocols/IScrollable.swift
+++ src/Element/ui/other/view/protocols/IScrollable.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 /**
  * Scrollable is for scroling things, basically content within a mask
  */
@@ -7,4 +7,18 @@ protocol IScrollable:class {
     var itemHeight:CGFloat{get}//item of one item, used to calculate interval
     var itemsHeight:CGFloat{get}//total height of the items
     var lableContainer:Container? {get set}
+}
+
+extension IScrollable{
+    /**
+     * NOTE: Slider list and SliderFastList uses this method
+     */
+    func scroll(_ theEvent:NSEvent) {
+        let progress:CGFloat = SliderListUtils.progress(theEvent.deltaY, self.sliderInterval!, self.slider!.progress)
+        //Swift.print("progress: " + "\(progress)")
+        setProgress(progress)/*Sets the target item to correct y, according to the current scrollBar progress*/
+        self.slider?.setProgressValue(progress)/*Positions the slider.thumb*/
+        if(theEvent.momentumPhase == NSEventPhase.ended){self.slider!.thumb!.setSkinState("inActive")}
+        else if(theEvent.momentumPhase == NSEventPhase.began){self.slider!.thumb!.setSkinState(SkinStates.none)}//include may begin here
+    }
 }

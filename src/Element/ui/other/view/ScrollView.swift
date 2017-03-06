2bae3e49971f140f8da9bef4494158e67aed6d0b
Eonist <30n1st@gmail.com>
2017-03-06 13:18:16 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/ScrollView.swift

--- src/Element/ui/other/view/ScrollView.swift
+++ src/Element/ui/other/view/ScrollView.swift
@@ -3,8 +3,8 @@ import Cocoa
 
 class ScrollView:Element,IScrollable{
     var lableContainer:Container?
-    var itemsHeight:CGFloat = 600//override this for custom value
-    var itemHeight:CGFloat = 24//override this for custom value
+    var itemsHeight:CGFloat {fatalError("Must be override in subClass")}//override this for custom value
+    var itemHeight:CGFloat {fatalError("Must be override in subClass")}//override this for custom value
     override func resolveSkin() {
         super.resolveSkin()//self.skin = SkinResolver.skin(self)//
         lableContainer = self.addSubView(Container(width,height,self,"lable"))

d1e6df5c205e09126fd024eecf07eb1f17334898
Eonist <30n1st@gmail.com>
2017-03-07 09:55:39 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/RBSliderView.swift

--- src/Element/ui/other/view/RBSliderView.swift
+++ src/Element/ui/other/view/RBSliderView.swift
@@ -1,24 +1,14 @@
 import Cocoa
 @testable import Utils
 
-class RBSliderView:RBScrollView,IRBScrollableSlidable/*:SliderView,*/ {
+class RBSliderView:SliderView,IRBScrollableSlidable/*:SliderView,*/ {
     /*Slider*/
-    var slider:VSlider?
-    var sliderInterval:CGFloat?
+    
     override func resolveSkin() {
         super.resolveSkin()
-        sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
-        slider = addSubView(VSlider(itemHeight,height,0,0,self))
-        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
-        slider!.setThumbHeightValue(thumbHeight)
+        
     }
-    override func onEvent(_ event:Event) {
-        Swift.print("RBSliderView.event: " + "\(event)")
-        if(event === (SliderEvent.change,slider!)){
-            setProgress((event as! SliderEvent).progress)
-        }/*events from the slider*/
-        super.onEvent(event)
-    }
+    
     /**
      * When the the user scrolls
      * NOTE: this method overides the Native NSView scrollWheel method

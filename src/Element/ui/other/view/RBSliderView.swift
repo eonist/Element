883e66040297439fe1ca230c082b835ccead6566
Eonist <30n1st@gmail.com>
2017-03-07 09:32:54 +0100

    Files modified: 2
    Modified 2 files:src/Element/ui/other/view/RBSliderView.swiftsrc/Element/ui/other/view/SliderView.swift

--- src/Element/ui/other/view/RBSliderView.swift
+++ src/Element/ui/other/view/RBSliderView.swift
@@ -1,7 +1,15 @@
 import Cocoa
 @testable import Utils
 
-class RBSliderView/*:SliderView,IRBScrollableSlidable*/ {
-    
+class RBSliderView:RBScrollView,IRBScrollableSlidable/*:SliderView,*/ {
+    /*Slider*/
+    var slider:VSlider?
+    var sliderInterval:CGFloat?
+    override func resolveSkin() {
+        sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
+        slider!.setThumbHeightValue(thumbHeight)
+    }
 }
 

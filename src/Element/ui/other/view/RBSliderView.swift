6c2ae7a1a9019a22086889047dc3fb6420e64789
Eonist <30n1st@gmail.com>
2017-03-07 09:35:04 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/RBSliderView.swift

--- src/Element/ui/other/view/RBSliderView.swift
+++ src/Element/ui/other/view/RBSliderView.swift
@@ -11,5 +11,20 @@ class RBSliderView:RBScrollView,IRBScrollableSlidable/*:SliderView,*/ {
         let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
         slider!.setThumbHeightValue(thumbHeight)
     }
+    override func onEvent(_ event:Event) {
+        Swift.print("event: " + "\(event)")
+        if(event === (SliderEvent.change,slider!)){
+            setProgress((event as! SliderEvent).progress)
+        }/*events from the slider*/
+        super.onEvent(event)
+    }
+    /**
+     * When the the user scrolls
+     */
+    override func scrollWheel(with event: NSEvent) {//swift 3 update
+        scroll(event)/*forward the event to the extension which adjust Slider and calls setProgress in this method*/
+        //IMPORTANT: for now let's not pass on the scrollWheel.if this backfires, aka wee need scroolwheel for NSView at another level, then make a scheme that calls the correct scroll, aka make scroll inheritable and overridable and then call doScroll with the extension method attached
+        //super.scrollWheel(with: event)/*forward the event other delegates higher up in the stack*/
+    }
 }
 

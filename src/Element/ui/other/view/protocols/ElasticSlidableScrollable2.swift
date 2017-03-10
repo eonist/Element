4a23525606c9df65c8502f365c48a6756e01e418
Eonist <30n1st@gmail.com>
2017-03-10 13:38:56 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/ElasticSlidableScrollable2.swift

--- src/Element/ui/other/view/protocols/ElasticSlidableScrollable2.swift
+++ src/Element/ui/other/view/protocols/ElasticSlidableScrollable2.swift
@@ -4,26 +4,21 @@ protocol ElasticSlidableScrollable2:ElasticScrollable2,Slidable2{}
 
 extension ElasticSlidableScrollable2{
     func scroll(_ event: NSEvent) {
-        if(event.phase == NSEventPhase.ended || event == NSEventPhase.cancelled){
+        if(event.phase == NSEventPhase.ended || event.phase == NSEventPhase.cancelled){
             hideSlider()
-        }else if(event == "enter"){
+        }else if(event.phase == NSEventPhase.mayBegin || event.phase == NSEventPhase.began){
             showSlider()
         }
-        (self as Scrollable2).scroll(event)//👈 this shouldnt work! protocol ambiguity, code reuse across inheritance, shared inheritance
+        (self as Scrollable2).scroll(event)//👈 this shouldn't work! protocol ambiguity, code reuse across inheritance, shared inheritance
     }
 }
-switch {
-case NSEventPhase.changed:onScrollWheelChange(theEvent)/*Fires everytime there is direct scrollWheel gesture movment.*/
-case NSEventPhase.mayBegin:onScrollWheelEnter()/*Can be used to detect if two fingers are touching the trackpad*/
-case NSEventPhase.began:onScrollWheelEnter()/*The mayBegin phase doesnt fire if you begin the scrollWheel gesture very quickly*/
-case NSEventPhase.ended:onScrollWheelExit();//Swift.print("ended")/*if you release your touch-gesture and the momentum of the gesture has stopped.*/
-case NSEventPhase.cancelled:onScrollWheelExit();
 /*
- extension Slidable where Self:ElasticScrollable{
- func scroll(_ event: String) {
- if(event == "change"){
- print("🏂")
- }
- (self as Scrollable).scroll(event)
- }
- }*/
+extension Slidable where Self:ElasticScrollable{
+    func scroll(_ event: String) {
+        if(event == "change"){
+            print("🏂")
+        }
+        (self as Scrollable).scroll(event)
+    }
+}
+*/

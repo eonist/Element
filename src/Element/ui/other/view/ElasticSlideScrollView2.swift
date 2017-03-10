2e9eaeb97f1b3535b4f47fea351be28e20959752
Eonist <30n1st@gmail.com>
2017-03-10 10:52:14 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/ElasticSlideScrollView2.swift

--- src/Element/ui/other/view/ElasticSlideScrollView2.swift
+++ src/Element/ui/other/view/ElasticSlideScrollView2.swift
@@ -1,5 +1,7 @@
 import Foundation
 
-class ElasticSlideScrollView {
-
+class ElasticSlideScrollView2:DisplaceView2,ElasticSlidableScrollable2{
+    override func scrollWheel(_ event: String) {
+        scroll(event)
+    }
 }

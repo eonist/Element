86446876539691608d445381bfd0425f1a8b7cd1
Eonist <30n1st@gmail.com>
2017-03-11 00:11:59 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/composite/ElasticSlideScrollView.swift

--- src/Element/ui/other/view/composite/ElasticSlideScrollView.swift
+++ src/Element/ui/other/view/composite/ElasticSlideScrollView.swift
@@ -7,7 +7,7 @@ class ElasticSlideScrollView2:SlideView2,ElasticSlidableScrollable2{
     var mover:RubberBand?
     var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
     var velocities:[CGFloat] = Array(repeating: 0, count: 10)/*represents the velocity resolution of the gesture movment*/
-    var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
+    var progressValue:CGFloat?//<--👈Might not need this anymore, same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
     override func resolveSkin() {
         super.resolveSkin()
         /*RubberBand*/

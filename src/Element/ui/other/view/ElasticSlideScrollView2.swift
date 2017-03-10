cf4d989998c6a2c60fbe740054ccc09121d63fe1
Eonist <30n1st@gmail.com>
2017-03-10 16:28:17 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/ElasticSlideScrollView2.swift

--- src/Element/ui/other/view/ElasticSlideScrollView2.swift
+++ src/Element/ui/other/view/ElasticSlideScrollView2.swift
@@ -1,8 +1,7 @@
 import Cocoa
-/*
+
 class ElasticSlideScrollView2:DisplaceView2,ElasticSlidableScrollable2{
     override func scrollWheel(with event: NSEvent) {//you can probably remove this method and do it in base?"!?
         scroll(event)
     }
 }
-*/

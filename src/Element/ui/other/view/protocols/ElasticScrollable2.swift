1755a8e6dc85523c9d655888c2b0e8f47bdce412
Eonist <30n1st@gmail.com>
2017-03-10 16:18:16 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/ElasticScrollable2.swift

--- src/Element/ui/other/view/protocols/ElasticScrollable2.swift
+++ src/Element/ui/other/view/protocols/ElasticScrollable2.swift
@@ -12,11 +12,14 @@ extension ElasticScrollable2{
     func onScrollWheelChange(_ event:NSEvent){
         Swift.print("👻📜 (ElasticScrollable).onScrollWheelChange : \(event.type)")
         //Swift.print("IRBScrollable.onScrollWheelChange")
-        prevScrollingDeltaY = event.scrollingDeltaY/*is needed when figuring out which dir the wheel is spinning and if its spinning at all*/
-        _ = self.velocities.pushPop(event.scrollingDeltaY)/*insert new velocity at the begining and remove the last velocity to make room for the new*/
-        mover!.value += event.scrollingDeltaY/*directly manipulate the value 1 to 1 control*/
-        mover!.updatePosition()/*the mover still governs the resulting value, in order to get the displacement friction working*/
-        if(mover!.isDirectlyManipulating){setProgress(mover!.result)}//👈NEW
+        
+        if(mover!.isDirectlyManipulating){
+            prevScrollingDeltaY = event.scrollingDeltaY/*is needed when figuring out which dir the wheel is spinning and if its spinning at all*/
+            _ = self.velocities.pushPop(event.scrollingDeltaY)/*insert new velocity at the begining and remove the last velocity to make room for the new*/
+            mover!.value += event.scrollingDeltaY/*directly manipulate the value 1 to 1 control*/
+            mover!.updatePosition()/*the mover still governs the resulting value, in order to get the displacement friction working*/
+            setProgress(mover!.result)
+        }//👈NEW
     }
     /**
      * NOTE: Basically when you enter your scrollWheel gesture

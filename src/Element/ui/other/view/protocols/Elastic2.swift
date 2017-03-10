9e687283a7ecb6314249709f3b37049309c0a6cf
Eonist <30n1st@gmail.com>
2017-03-10 12:52:03 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/Elastic2.swift

--- src/Element/ui/other/view/protocols/Elastic2.swift
+++ src/Element/ui/other/view/protocols/Elastic2.swift
@@ -1,3 +1,12 @@
 import Foundation
-
-protocol Elastic2:Displacable2{}
+@testable import Utils
+/**
+ * This protocol exist because other that Lists may want to be Elastic scrollable, like A container of things
+ */
+protocol Elastic2:Displacable2{
+    var mover:RubberBand?{get set}
+    var prevScrollingDeltaY:CGFloat{get set}
+    var velocities:[CGFloat]{get set}
+    //⚠️️ you may be able to remove progressvalue in the future. as it works differently now!=!=?
+    var progressValue:CGFloat?{get set}//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
+}

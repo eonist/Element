39d96eae61049376fed49f1a7fbb91613fbfb459
Eonist <30n1st@gmail.com>
2017-03-10 11:34:42 +0100

    Files modified: 2
    Modified 2 files:src/Element/ui/other/view/protocols/Displacable2.swiftsrc/Element/ui/other/view/protocols/Slidable2.swift

--- src/Element/ui/other/view/protocols/Displacable2.swift
+++ src/Element/ui/other/view/protocols/Displacable2.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol Displacable2 {
+protocol Displacable2:class {
     var height:CGFloat{get}//used to represent the maskHeight aka the visible part.
     var itemHeight:CGFloat{get}//item of one item, used to calculate interval
     var itemsHeight:CGFloat{get}//total height of the items
--- src/Element/ui/other/view/protocols/Slidable2.swift
+++ src/Element/ui/other/view/protocols/Slidable2.swift
@@ -4,19 +4,34 @@ protocol Slidable2:Displacable2{
     var interval:CGFloat {get}
     func updateSlider()
     var slider:VSlider?{get}
-    var sliderInterval:CGFloat?{get set}
-    //func setProgress(_ progress:CGFloat)//this could go in IScrollable
-    func updateSlider()
-    //func scroll(_ theEvent:NSEvent)
+    var sliderInterval:CGFloat?{get set}//i think this is the same as intervall, remove
 }
 extension Slidable2{
+    /**
+     * Updates the slider interval and the sliderThumbSize (after DP events: add/remove etc)
+     */
     func updateSlider(){
-        //dp can update slider etc
+        sliderInterval = floor(self.itemsHeight - height)/itemHeight
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height/*<--this should probably be .getHeight()*/);
+        slider!.setThumbHeightValue(thumbHeight)
+        let progress:CGFloat = SliderParser.progress(lableContainer!.y, height, itemsHeight)//TODO: use getHeight() instead of height
+        slider!.setProgressValue(progress)
     }
     func hideSlider(){
         Swift.print("🏂 hide slider")
     }
     func showSlider(){
         Swift.print("🏂 show slider")
+    }
+    /**
+     * NOTE: Slider list and SliderFastList uses this method
+     */
+    func scroll(_ theEvent:NSEvent) {
+        let progress:CGFloat = SliderListUtils.progress(theEvent.deltaY, self.sliderInterval!, self.slider!.progress)
+        //Swift.print("Slideable.scroll() progress: " + "\(progress)")
+        setProgress(progress)/*Sets the target item to correct y, according to the current scrollBar progress*/
+        self.slider?.setProgressValue(progress)/*Positions the slider.thumb*/
+        if(theEvent.momentumPhase == NSEventPhase.ended){self.slider!.thumb!.setSkinState("inActive")}
+        else if(theEvent.momentumPhase == NSEventPhase.began){self.slider!.thumb!.setSkinState(SkinStates.none)}//include may begin here
     }
 }

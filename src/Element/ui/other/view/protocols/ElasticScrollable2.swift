ea1139c6e010f172a2f55f7b8b7497c71b2ec798
Eonist <30n1st@gmail.com>
2017-03-10 13:02:08 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/ElasticScrollable2.swift

--- src/Element/ui/other/view/protocols/ElasticScrollable2.swift
+++ src/Element/ui/other/view/protocols/ElasticScrollable2.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 
 protocol ElasticScrollable2:Elastic2,Scrollable2{}
 
@@ -10,6 +10,17 @@ extension ElasticScrollable2{
         Swift.print("👻📜 (ElasticScrollable).onScrollWheelExit")
     }
     func onScrollWheelChange(_ event: String) {
+        
+    }
+    /**
+     * NOTE: Basically when you perform a scroll-gesture on the touch-pad
+     */
+    func onScrollWheelChange(_ event:NSEvent){
         Swift.print("👻📜 (ElasticScrollable).onScrollWheelChange : \(event)")
+        //Swift.print("IRBScrollable.onScrollWheelChange")
+        prevScrollingDeltaY = event.scrollingDeltaY/*is needed when figuring out which dir the wheel is spinning and if its spinning at all*/
+        _ = self.velocities.pushPop(event.scrollingDeltaY)/*insert new velocity at the begining and remove the last velocity to make room for the new*/
+        mover!.value += event.scrollingDeltaY/*directly manipulate the value 1 to 1 control*/
+        mover!.updatePosition()/*the mover still governs the resulting value, in order to get the displacement friction working*/
     }
 }

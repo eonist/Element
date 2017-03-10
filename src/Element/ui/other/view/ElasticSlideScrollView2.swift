d785b6585816713ff5287406a69c03f38ec64a81
Eonist <30n1st@gmail.com>
2017-03-10 16:30:19 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/ElasticSlideScrollView2.swift

--- src/Element/ui/other/view/ElasticSlideScrollView2.swift
+++ src/Element/ui/other/view/ElasticSlideScrollView2.swift
@@ -1,6 +1,10 @@
 import Cocoa
 
-class ElasticSlideScrollView2:DisplaceView2,ElasticSlidableScrollable2{
+class ElasticSlideScrollView2:SlideView2,ElasticSlidableScrollable2{
+    var mover:RubberBand?
+    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
+    var velocities:[CGFloat] = Array(repeating: 0, count: 10)/*represents the velocity resolution of the gesture movment*/
+    var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
     override func scrollWheel(with event: NSEvent) {//you can probably remove this method and do it in base?"!?
         scroll(event)
     }

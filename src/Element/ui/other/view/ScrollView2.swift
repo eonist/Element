8dbb418b848d41bd75ff2d444d41136a7389d426
Eonist <30n1st@gmail.com>
2017-03-10 11:30:37 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/ScrollView2.swift

--- src/Element/ui/other/view/ScrollView2.swift
+++ src/Element/ui/other/view/ScrollView2.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
 class ScrollView2:DisplaceView2,Scrollable2{
-    override func scrollWheel(with event: NSEvent) {
+    override func scrollWheel(with event: NSEvent) {//TODO: move to displaceview
         scroll(event)
     }
 }

c01447fed602b03eb97e1eaa84c6d6f1082bfe1b
Eonist <30n1st@gmail.com>
2017-03-10 11:32:39 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/Slidable2.swift

--- src/Element/ui/other/view/protocols/Slidable2.swift
+++ src/Element/ui/other/view/protocols/Slidable2.swift
@@ -3,6 +3,11 @@ import Foundation
 protocol Slidable2:Displacable2{
     var interval:CGFloat {get}
     func updateSlider()
+    var slider:VSlider?{get}
+    var sliderInterval:CGFloat?{get set}
+    //func setProgress(_ progress:CGFloat)//this could go in IScrollable
+    func updateSlider()
+    //func scroll(_ theEvent:NSEvent)
 }
 extension Slidable2{
     func updateSlider(){

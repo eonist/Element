10248a132528db22c7ca013f96fa86bb7f76688c
Eonist <30n1st@gmail.com>
2017-03-10 13:28:39 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/ElasticSlidableScrollable2.swift

--- src/Element/ui/other/view/protocols/ElasticSlidableScrollable2.swift
+++ src/Element/ui/other/view/protocols/ElasticSlidableScrollable2.swift
@@ -4,15 +4,20 @@ protocol ElasticSlidableScrollable2:ElasticScrollable2,Slidable2{}
 
 extension ElasticSlidableScrollable2{
     func scroll(_ event: NSEvent) {
-        if(event == "exit"){
+        if(event.phase == NSEventPhase.ended || NSEventPhase.cancelled){
             hideSlider()
         }else if(event == "enter"){
             showSlider()
         }
-        (self as Scrollable2).scroll(event)//protocol ambiguity, code reuse across inheritance, shared inheritance
+        (self as Scrollable2).scroll(event)//👈 this shouldnt work! protocol ambiguity, code reuse across inheritance, shared inheritance
     }
 }
-
+switch {
+case NSEventPhase.changed:onScrollWheelChange(theEvent)/*Fires everytime there is direct scrollWheel gesture movment.*/
+case NSEventPhase.mayBegin:onScrollWheelEnter()/*Can be used to detect if two fingers are touching the trackpad*/
+case NSEventPhase.began:onScrollWheelEnter()/*The mayBegin phase doesnt fire if you begin the scrollWheel gesture very quickly*/
+case NSEventPhase.ended:onScrollWheelExit();//Swift.print("ended")/*if you release your touch-gesture and the momentum of the gesture has stopped.*/
+case NSEventPhase.cancelled:onScrollWheelExit();
 /*
  extension Slidable where Self:ElasticScrollable{
  func scroll(_ event: String) {

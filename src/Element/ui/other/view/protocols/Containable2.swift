7c00c65984ac8032bfe71175601b9bf8280b6830
Eonist <30n1st@gmail.com>
2017-03-10 10:27:09 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/Containable2.swift

--- src/Element/ui/other/view/protocols/Containable2.swift
+++ src/Element/ui/other/view/protocols/Containable2.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class Containable2 {
-
+protocol Containable2 {
+    var height:CGFloat{get}//used to represent the maskHeight aka the visible part.
+    var itemHeight:CGFloat{get}//item of one item, used to calculate interval
+    var itemsHeight:CGFloat{get}//total height of the items
+    var progress:CGFloat {get}
+    var itemContainer:String {get set}
 }

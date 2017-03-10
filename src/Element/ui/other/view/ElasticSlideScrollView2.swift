2073d9a84ca03b099277ef8b1a0d7c6737088bfd
Eonist <30n1st@gmail.com>
2017-03-10 16:32:23 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/ElasticSlideScrollView2.swift

--- src/Element/ui/other/view/ElasticSlideScrollView2.swift
+++ src/Element/ui/other/view/ElasticSlideScrollView2.swift
@@ -1,5 +1,8 @@
 import Cocoa
-
+@testable import Utils
+/**
+ * Might be better to not extend SlideView2
+ */
 class ElasticSlideScrollView2:SlideView2,ElasticSlidableScrollable2{
     var mover:RubberBand?
     var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
@@ -7,5 +10,13 @@ class ElasticSlideScrollView2:SlideView2,ElasticSlidableScrollable2{
     var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
     override func scrollWheel(with event: NSEvent) {//you can probably remove this method and do it in base?"!?
         scroll(event)
+    }
+    override func resolveSkin() {
+        super.resolveSkin()
+        /*RubberBand*/
+        let frame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
+        let itemsRect = CGRect(0,0,width,itemsHeight)/*represents the total size of the content *///TODO: could be ranmed to contentRect
+        mover = RubberBand(Animation.sharedInstance,setProgress/*👈important*/,frame,itemsRect)
+        mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
     }
 }

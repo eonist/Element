2063a71dec72e8e1b3ebf0a683f5e1d5b8b56d67
Eonist <30n1st@gmail.com>
2017-03-10 11:08:29 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/DisplacableView2.swift

--- src/Element/ui/other/view/DisplacableView2.swift
+++ src/Element/ui/other/view/DisplacableView2.swift
@@ -4,10 +4,9 @@ class DisplaceView2:Element,Displacable2 {
     var height:CGFloat {fatalError("Must override in subClass")}
     var interval:CGFloat {fatalError("Must override in subClass")}
     var progress:CGFloat {fatalError("Must override in subClass")}
-    var itemContainer:Container
-    //func scrollWheel(_ event:String){}
     var itemsHeight:CGFloat {fatalError("Must override in subClass")}//override this for custom value
     var itemHeight:CGFloat {fatalError("Must override in subClass")}//override this for custom value
+    var itemContainer:Container
     override func resolveSkin() {
         super.resolveSkin()//self.skin = SkinResolver.skin(self)//
         lableContainer = self.addSubView(Container(width,height,self,"lable"))

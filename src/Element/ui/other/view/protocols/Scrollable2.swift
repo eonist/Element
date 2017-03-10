bba2db3c0c87d6d703c74e75226661cb9b1a68f6
Eonist <30n1st@gmail.com>
2017-03-10 15:35:27 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/protocols/Scrollable2.swift

--- src/Element/ui/other/view/protocols/Scrollable2.swift
+++ src/Element/ui/other/view/protocols/Scrollable2.swift
@@ -19,7 +19,7 @@ extension Scrollable2{
             //Swift.print("theEvent.phase: " + "\(theEvent.phase)")
         }
         switch theEvent.phase{
-            case NSEventPhase.changed:onScrollWheelChange(theEvent)/*Fires everytime there is direct scrollWheel gesture movment.*/
+            case NSEventPhase.changed:onScrollWheelChange(theEvent)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
             case NSEventPhase.mayBegin:onScrollWheelEnter()/*Can be used to detect if two fingers are touching the trackpad*/
             case NSEventPhase.began:onScrollWheelEnter()/*The mayBegin phase doesnt fire if you begin the scrollWheel gesture very quickly*/
             case NSEventPhase.ended:onScrollWheelExit();//Swift.print("ended")/*if you release your touch-gesture and the momentum of the gesture has stopped.*/

72d06858098e0432ed65bffc4e2176188d24568a
Eonist <30n1st@gmail.com>
2017-03-10 10:37:55 +0100

    Files modified: 2
    Modified 2 files:src/Element/ui/other/view/ElasticSlideScrollView2.swiftsrc/Element/ui/other/view/SlideScrollView2.swift

--- src/Element/ui/other/view/ElasticSlideScrollView2.swift
+++ src/Element/ui/other/view/ElasticSlideScrollView2.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class ElasticSlidableScrollableView {
+class ElasticSlideScrollView {
 
 }
--- src/Element/ui/other/view/SlideScrollView2.swift
+++ src/Element/ui/other/view/SlideScrollView2.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class SlidableScrollableView2 {
+class SlideScrollView2 {
 
 }

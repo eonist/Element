bf49120f640f676e844ef8bb51ec8148aa214658
Eonist <30n1st@gmail.com>
2017-03-10 11:06:24 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/DisplacableView2.swift

--- src/Element/ui/other/view/DisplacableView2.swift
+++ src/Element/ui/other/view/DisplacableView2.swift
@@ -1,11 +1,16 @@
 import Cocoa
 
-class DisplaceView2:NSView,Displacable2 {
-    var height:CGFloat = 0
-    var interval:CGFloat = 0
-    var progress:CGFloat = 0
-    var itemHeight:CGFloat = 0
-    var itemsHeight:CGFloat = 0
-    var itemContainer:String = ""
+class DisplaceView2:Element,Displacable2 {
+    var height:CGFloat {fatalError("Must override in subClass")}
+    var interval:CGFloat {fatalError("Must override in subClass")}
+    var progress:CGFloat {fatalError("Must override in subClass")}
+    var itemContainer:Container
     //func scrollWheel(_ event:String){}
+    var itemsHeight:CGFloat {fatalError("Must override in subClass")}//override this for custom value
+    var itemHeight:CGFloat {fatalError("Must override in subClass")}//override this for custom value
+    override func resolveSkin() {
+        super.resolveSkin()//self.skin = SkinResolver.skin(self)//
+        lableContainer = self.addSubView(Container(width,height,self,"lable"))
+        layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
+    }
 }

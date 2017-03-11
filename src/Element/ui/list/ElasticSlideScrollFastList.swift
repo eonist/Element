2010b5b3d2c29c471df2b7d824a38266f3b5f665
Eonist <30n1st@gmail.com>
2017-03-11 20:12:59 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/ElasticSlideScrollFastList.swift

--- src/Element/ui/list/ElasticSlideScrollFastList.swift
+++ src/Element/ui/list/ElasticSlideScrollFastList.swift
@@ -1,7 +1,36 @@
 import Foundation
 
-class ElasticSlideScrollFastList:SlideFastList2 {
-
+class ElasticSlideScrollFastList:SlideFastList2,ElasticSlidableScrollableFast {
+    var mover:RubberBand?
+    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
+    var velocities:[CGFloat] = Array(repeating: 0, count: 10)/*represents the velocity resolution of the gesture movment*/
+    var progressValue:CGFloat?//<--👈Might not need this anymore, same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
+    override func resolveSkin() {
+        super.resolveSkin()
+        /*RubberBand*/
+        let frame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
+        let itemsRect = CGRect(0,0,width,itemsHeight)/*represents the total size of the content *///TODO: could be ranmed to contentRect
+        mover = RubberBand(Animation.sharedInstance,setProgress/*👈important*/,frame,itemsRect)
+        mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
+    }
+    /**
+     * When the the user scrolls
+     * NOTE: this method overides the Native NSView scrollWheel method
+     */
+    override func scrollWheel(with event: NSEvent) {//you can probably remove this method and do it in base?"!?
+        //Swift.print("ElasticSlideScrollView2.scrollWheel()")
+        scroll(event)
+        super.scrollWheel(with: event)/*forward the event other delegates higher up in the stack*/
+    }
+    override func onEvent(_ event:Event) {
+        if(event === (AnimEvent.stopped, mover!)){
+            Swift.print("anim stopped")
+            hideSlider()/*hides the slider when bounce back anim stopps*/
+        }else if(event === (SliderEvent.change,slider!)){
+            mover!.value = lableContainer!.frame.y//quick fix, ⚠️️⚠️️⚠️️TODO:  move into onSliderEvent in Slidable, and in ElasticSlidableScrollable
+        }
+        super.onEvent(event)
+    }
 }
 
 //you need SlideFastList

8ce5c2360a787395c68de8345e909a5b9d3a32b4
Eonist <30n1st@gmail.com>
2017-03-11 19:06:43 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/ElasticSlideScrollFastList.swift

--- src/Element/ui/list/ElasticSlideScrollFastList.swift
+++ src/Element/ui/list/ElasticSlideScrollFastList.swift
@@ -1,28 +1,8 @@
-import Cocoa
-@testable import Utils
-/**
- * NOTE: this Method is totally academic, can be usefull for UI where you dont want or need slider.
- */
+import Foundation
 
-class ElasticSlideScrollFastList:FastList2, ElasticScrollable {
-    /*RubberBand*/
-    var mover:RubberBand?
-    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
-    var velocities:[CGFloat] = Array(repeating: 0, count: 10)/*represents the velocity resolution of the gesture movment*/
-    var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
-    override func resolveSkin() {
-        super.resolveSkin()
-        /*RubberBand*/
-        let frame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
-        let itemsRect = CGRect(0,0,width,itemsHeight)/*represents the total size of the content *///TODO: could be ranmed to contentRect
-        mover = RubberBand(Animation.sharedInstance,setProgress/*👈important*/,frame,itemsRect)
-        mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
-    }
-    /**
-     * When the the user scrolls
-     * NOTE: this method overides the Native NSView scrollWheel method
-     */
-    override func scrollWheel(with event: NSEvent) {
-        scroll(event)
-    }
+class ElasticSlideScrollFastList {
+
 }
+
+//you need SlideFastList
+// and ElasticSlidableScrollable

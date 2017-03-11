abc9d0b20acd2edb85c7e63cd5d4f8fcedc71ae6
Eonist <30n1st@gmail.com>
2017-03-11 16:28:02 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/protocol/Fast.swift

--- src/Element/ui/list/protocol/Fast.swift
+++ src/Element/ui/list/protocol/Fast.swift
@@ -1,5 +1,12 @@
 import Foundation
 
-class Fast {
+protocol Fast{}
 
+//Continue here: 🏀
+    //move the FastListSetProgress into the fast protocol!
+
+extension Fast{
+    func setProgress(_ progress:CGFloat){
+        Swift.print("🍌 setProgress: progress")
+    }
 }

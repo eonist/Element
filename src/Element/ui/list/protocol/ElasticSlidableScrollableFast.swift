c414451cedc81ee1340ac99de0173713c06296a9
Eonist <30n1st@gmail.com>
2017-03-11 22:46:24 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/protocol/ElasticSlidableScrollableFast.swift

--- src/Element/ui/list/protocol/ElasticSlidableScrollableFast.swift
+++ src/Element/ui/list/protocol/ElasticSlidableScrollableFast.swift
@@ -1,9 +1,6 @@
 import Cocoa
 @testable import Utils
 
-protocol ElasticSlidableScrollableFast:IFastList2,ElasticScrollable, Slidable {
-    var rbContainer:Container?{get set}
-}
 extension ElasticSlidableScrollableFast{
     /**
      * ⚠️️⚠️️⚠️️SUPER IMPORTANT CONCEPT⚠️️⚠️️⚠️️: methods that are called from shallow can overide downstream in POP

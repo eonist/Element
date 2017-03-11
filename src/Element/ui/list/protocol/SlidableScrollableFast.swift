d4e16798ba412ca44ccf0b7f23054cb77a463abb
Eonist <30n1st@gmail.com>
2017-03-11 16:50:40 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/protocol/SlidableScrollableFast.swift

--- src/Element/ui/list/protocol/SlidableScrollableFast.swift
+++ src/Element/ui/list/protocol/SlidableScrollableFast.swift
@@ -2,6 +2,9 @@ import Cocoa
 
 protocol SlidableScrollableFast:Fast, SlidableScrollable{}
 extension SlidableScrollableFast{
+    /**
+     *
+     */
     func onScrollWheelChange(_ event:NSEvent) {
         let progressVal:CGFloat = SliderListUtils.progress(event.deltaY, interval, slider!.progress)
         slider!.setProgressValue(progressVal)

e4a38243094d09ef338785d822afb8977c65431d
Eonist <30n1st@gmail.com>
2017-03-11 20:04:14 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/SlideFastList2.swift

--- src/Element/ui/list/SlideFastList2.swift
+++ src/Element/ui/list/SlideFastList2.swift
@@ -1,5 +1,26 @@
 import Foundation
+@testable import Utils
 
-class SlideFastList {
-
+class SlideFastList2:FastList2,Slidable {
+    var slider:VSlider?
+    var sliderInterval:CGFloat?
+    override var itemsHeight: CGFloat {return dp.count * itemHeight}//👈 temp, move into protocol extension, if possible
+    override func resolveSkin() {
+        super.resolveSkin()
+        Swift.print("itemsHeight: " + "\(itemsHeight)")
+        Swift.print("height: " + "\(height)")
+        /*slider*/
+        sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
+        slider!.setThumbHeightValue(thumbHeight)
+        slider!.thumb!.fadeOut()//inits fade out anim on init
+    }
+    override func onEvent(_ event:Event) {
+        //Swift.print("event: " + "\(event)")
+        if(event === (SliderEvent.change,slider!)){
+            setProgress((event as! SliderEvent).progress)
+        }/*events from the slider*/
+        super.onEvent(event)
+    }
 }

7988be92031e1da04fed551832afe7ef918b6cce
Eonist <30n1st@gmail.com>
2017-03-11 17:07:24 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/FastList2.swift

--- src/Element/ui/list/FastList2.swift
+++ src/Element/ui/list/FastList2.swift
@@ -40,25 +40,7 @@ class FastList2:DisplaceView,IList2 {
         let range:Range<Int> = visibleRange.start..<min(dp.count,visibleRange.end)/*clip the range*/
         renderItems(range)
     }
-    /**
-     * PARAM: progress (0-1)
-     * NOTE: setProgress is in this class because RBFastSliderList doesn't extend SliderList, and both classes needs to extend this method
-     * NOTE: override this method in SliderFastList and RBSliderFastList
-     *
-     * The concept is simple, you only show items that are within the limits as you scroll up and down. (these items only exists virtually, untill they are revealed if they are within the limits)
-     * With these two rules: you should be able to create the algorithm that lay out items at a progress value
-     * Stage.1: Remove items outside Limits
-     * Stage.2: stack items to cover the visible area
-     */
-    func setProgress(_ progress:CGFloat){
-        Swift.print("🐎 FastList2.setProgress() ")
-        //ScrollableUtils.scrollTo(self, progress)/*moves the labelContainer up and down*/
-        super.setProgress(progress)//moves lableContainer
-        let range:Range<Int> = visibleItemRange.start..<Swift.min(visibleItemRange.end,dp.count)
-        if(currentVisibleItemRange != range){/*Optimization: only set if it's not the same as prev range*/
-            renderItems(range)
-        }
-    }
+    
     /**
      * Apply new data / align items
      * NOTE: override this to use custom ItemList items

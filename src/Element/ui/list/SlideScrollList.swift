a2c15dc276813b5efb7e2835c8071ac595f4802a
Eonist <30n1st@gmail.com>
2017-03-11 12:34:46 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/SlideScrollList.swift

--- src/Element/ui/list/SlideScrollList.swift
+++ src/Element/ui/list/SlideScrollList.swift
@@ -1,6 +1,25 @@
 import Cocoa
+@testable import Utils
 
-class SlideScrollList:SlideView,SlidableScrollable {
+class SlideScrollList:List2,SlidableScrollable {
+    var slider:VSlider?
+    var sliderInterval:CGFloat?
+    override func resolveSkin() {
+        super.resolveSkin()
+        /*slider*/
+        sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
+        slider!.setThumbHeightValue(thumbHeight)
+        slider!.thumb!.fadeOut()//inits fade out anim on init
+    }
+    override func onEvent(_ event:Event) {
+        //Swift.print("event: " + "\(event)")
+        if(event === (SliderEvent.change,slider!)){
+            setProgress((event as! SliderEvent).progress)
+        }/*events from the slider*/
+        super.onEvent(event)
+    }
     /**
      * When the the user scrolls
      * NOTE: this method overides the Native NSView scrollWheel method

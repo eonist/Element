17e34d4cbfdcfb98cc6c7187b357b27f99532fa9
Eonist <30n1st@gmail.com>
2017-03-11 15:02:28 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/SlideScrollFastList2.swift

--- src/Element/ui/list/SlideScrollFastList2.swift
+++ src/Element/ui/list/SlideScrollFastList2.swift
@@ -1,5 +1,23 @@
-import Foundation
+import Cocoa
+@testable import Utils
 
-class SlideScrollFastList2 {
-
+class SlideScrollFastList2:FastList2,SlidableScrollable {
+    var slider:VSlider?
+    var sliderInterval:CGFloat?
+    override func resolveSkin() {
+        super.resolveSkin()
+        /*slider*/
+        sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
+        slider!.setThumbHeightValue(thumbHeight)
+        slider!.thumb!.fadeOut()//inits fade out anim on init
+    }
+    override func onEvent(_ event:Event) {
+        //Swift.print("event: " + "\(event)")
+        if(event === (SliderEvent.change,slider!)){
+            setProgress((event as! SliderEvent).progress)
+        }/*events from the slider*/
+        super.onEvent(event)
+    }
 }

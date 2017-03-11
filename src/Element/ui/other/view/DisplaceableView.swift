0a6824ff3d2271be49efe008a16f8fa5b0e6d63d
Eonist <30n1st@gmail.com>
2017-03-11 11:50:22 +0100

    Files modified: 2
    Modified 2 files:src/Element/ui/other/view/DisplaceableView.swiftsrc/Element/ui/other/view/protocols/Displaceable.swift

--- src/Element/ui/other/view/DisplaceableView.swift
+++ src/Element/ui/other/view/DisplaceableView.swift
@@ -8,9 +8,8 @@ class DisplaceView:Element, Displaceable {
     //var interval:CGFloat {fatalError("Must override in subClass")}
     //var progress:CGFloat {fatalError("Must override in subClass")}
     var itemsHeight:CGFloat {fatalError("Must override in subClass")}//override this for custom value
-    var itemHeight:CGFloat!// {fatalError("Must override in subClass")}//⚠️️ remove this, not a computed var override this for custom value
+    var itemHeight:CGFloat {fatalError("Must override in subClass")}//override this for custom value
     var lableContainer:Element?
-    
     override func resolveSkin() {
         super.resolveSkin()//self.skin = SkinResolver.skin(self)//
         lableContainer = addSubView(Container(width,height,self,"lable"))

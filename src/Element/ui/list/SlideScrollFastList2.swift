98f7adf449a06e735bec47248375fba88e50078d
Eonist <30n1st@gmail.com>
2017-03-11 20:02:11 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:src/Element/ui/list/SlideScrollFastList2.swift

--- src/Element/ui/list/SlideScrollFastList2.swift
+++ src/Element/ui/list/SlideScrollFastList2.swift
@@ -1,28 +1,7 @@
 import Cocoa
 @testable import Utils
 
-class SlideScrollFastList2:FastList2,SlidableScrollableFast {
-    var slider:VSlider?
-    var sliderInterval:CGFloat?
-    override var itemsHeight: CGFloat {return dp.count * itemHeight}//👈 temp, move into protocol extension, if possible
-    override func resolveSkin() {
-        super.resolveSkin()
-        Swift.print("itemsHeight: " + "\(itemsHeight)")
-        Swift.print("height: " + "\(height)")
-        /*slider*/
-        sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
-        slider = addSubView(VSlider(itemHeight,height,0,0,self))
-        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
-        slider!.setThumbHeightValue(thumbHeight)
-        slider!.thumb!.fadeOut()//inits fade out anim on init
-    }
-    override func onEvent(_ event:Event) {
-        //Swift.print("event: " + "\(event)")
-        if(event === (SliderEvent.change,slider!)){
-            setProgress((event as! SliderEvent).progress)
-        }/*events from the slider*/
-        super.onEvent(event)
-    }
+class SlideScrollFastList2:SlideFastList2,SlidableScrollableFast {
     /**
      * When the the user scrolls
      * NOTE: this method overides the Native NSView scrollWheel method

32ff3404e9507ba455a13b8ee4812d66d406023d
Eonist <30n1st@gmail.com>
2017-03-11 18:10:44 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/protocol/SlidableScrollableFast.swift

--- src/Element/ui/list/protocol/SlidableScrollableFast.swift
+++ src/Element/ui/list/protocol/SlidableScrollableFast.swift
@@ -2,5 +2,13 @@ import Cocoa
 
 protocol SlidableScrollableFast:IFastList2, SlidableScrollable{}
 extension SlidableScrollableFast{
-    
+    /**
+     * TODO: you could also override scroll and hock after the forward scroll call and then retrive the progress from the var. less code, but the value must be written in Displaceview, it could mess up Elastic, because it needs different progress. etc, do later
+     */
+    func onScrollWheelChange(_ event:NSEvent) {
+        Swift.print("🏂📜🐎 SlidableScrollable2.onScrollWheelChange: \(event)")
+        let progressVal:CGFloat = SliderListUtils.progress(event.deltaY, interval, slider!.progress)
+        slider!.setProgressValue(progressVal)
+        setProgress(progressVal)
+    }
 }

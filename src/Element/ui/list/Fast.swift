879caa310f3fc49dbaea9e4d7cbe1a3df6f8fd49
Eonist <30n1st@gmail.com>
2017-03-11 16:25:17 +0100

    Files modified: 2
    Modified 2 files:src/Element/ui/list/Fast.swiftsrc/Element/ui/list/protocol/SlidableScrollableFast.swift

--- src/Element/ui/list/Fast.swift
+++ src/Element/ui/list/Fast.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class Fast {
+protocol Fast {
 
 }
--- src/Element/ui/list/protocol/SlidableScrollableFast.swift
+++ src/Element/ui/list/protocol/SlidableScrollableFast.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class SlidableScrollableFast {
-
+protocol SlidableScrollableFast:Fast, SlidableScrollable{}
+extension SlidableScrollableFast{
+    func onScrollWheelChange(_ event:NSEvent) {
+        let progressVal:CGFloat = SliderListUtils.progress(event.deltaY, interval, slider!.progress)
+        slider!.setProgressValue(progressVal)
+        (self as Fast).setProgress(progressVal)
+    }
+    
 }

abfd534cc2643b5803ec0b510518dcf3a84faa84
Eonist <30n1st@gmail.com>
2017-03-12 15:17:32 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/protocol/ElasticSlidableScrollableFast.swift

--- src/Element/ui/list/protocol/ElasticSlidableScrollableFast.swift
+++ src/Element/ui/list/protocol/ElasticSlidableScrollableFast.swift
@@ -2,6 +2,7 @@ import Cocoa
 @testable import Utils
 protocol ElasticSlidableScrollableFast:IFastList2,ElasticScrollable, Slidable {
     var rbContainer:Container?{get set}
+    func frameTick(_ value:CGFloat)
 }
 extension ElasticSlidableScrollableFast{
     /**

9cd700b583895ff4052089388dd36150f46f19c3
Eonist <30n1st@gmail.com>
2017-03-12 14:46:34 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/list/ElasticSlideScrollFastList.swift

--- src/Element/ui/list/ElasticSlideScrollFastList.swift
+++ src/Element/ui/list/ElasticSlideScrollFastList.swift
@@ -18,8 +18,11 @@ class ElasticSlideScrollFastList:SlideFastList2,ElasticSlidableScrollableFast {
         /*RubberBand*/
         let frame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
         let itemsRect = CGRect(0,0,width,itemsHeight)/*represents the total size of the content *///TODO: could be ranmed to contentRect
-        mover = RubberBand(Animation.sharedInstance,setProgress/*👈important*/,frame,itemsRect)
+        mover = RubberBand(Animation.sharedInstance,tempSetProgress/*👈important*/,frame,itemsRect)
         mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
+    }
+    func tempSetProgress(_ value:CGFloat){//real value
+        setProgress(value)
     }
     /**
      * When the the user scrolls

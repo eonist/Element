84a4851faa6de33d48ab5c765e6ca71343af6b76
Eonist <30n1st@gmail.com>
2017-03-14 20:43:39 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/misc/abstract/Disablable.swift

--- src/Element/ui/other/misc/abstract/Disablable.swift
+++ src/Element/ui/other/misc/abstract/Disablable.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class Disablable {
-
+protocol Disableable:class{
+    var isDisabled:Bool {get set}
 }
+extension Disableable{
+    func setDisabled(_ disabled:Bool){
+        self.isDisabled = disabled
+    }
+    func getDisabled()->Bool{
+        return self.isDisabled
+    }
+}
+

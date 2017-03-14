30be32e10ff010f7e9a6c59ee597fdfb134cc6df
Eonist <30n1st@gmail.com>
2017-03-14 20:41:24 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/misc/abstract/Focusable.swift

--- src/Element/ui/other/misc/abstract/Focusable.swift
+++ src/Element/ui/other/misc/abstract/Focusable.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class Focusable {
-
+protocol Focusable:class{
+    var isFocused:Bool {get set}
 }
+extension Focusable{
+    func setFocused(_ focused:Bool){
+        self.isFocused = focused
+    }
+    func getFocused()->Bool{
+        return self.isFocused
+    }
+}
+

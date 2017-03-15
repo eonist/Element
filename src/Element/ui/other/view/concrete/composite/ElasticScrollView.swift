028a54fa6ee2a86396d83853b160cf6b9432aa6f
Eonist <30n1st@gmail.com>
2017-03-15 13:34:12 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/concrete/composite/ElasticScrollView.swift

--- src/Element/ui/other/view/concrete/composite/ElasticScrollView.swift
+++ src/Element/ui/other/view/concrete/composite/ElasticScrollView.swift
@@ -7,9 +7,7 @@ import Cocoa
 class ElasticScrollView:ContainerView,ElasticScrollable {
     /*RubberBand*/
     var mover:RubberBand?
-    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
-    var velocities:[CGFloat] = Array(repeating: 0, count: 10)/*represents the velocity resolution of the gesture movment*/
-    var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
+    var iterimScroll
     override func resolveSkin() {
         super.resolveSkin()
         /*RubberBand*/

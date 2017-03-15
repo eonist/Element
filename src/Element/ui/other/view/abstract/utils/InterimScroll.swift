b60abe4a32721c40f60eeaa2758d37939f630c62
Eonist <30n1st@gmail.com>
2017-03-15 13:26:29 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/abstract/utils/InterimScroll.swift

--- src/Element/ui/other/view/abstract/utils/InterimScroll.swift
+++ src/Element/ui/other/view/abstract/utils/InterimScroll.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-class InterimScroll {
-
+struct InterimScroll {
+    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
+    var velocities:[CGFloat]/*represents the velocity resolution of the gesture movment*/
+    var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
+    init(_ velocities:[CGFloat] = Array(repeating: 0, count: 10)) {
+        self.velocities = velocities
+    }
 }

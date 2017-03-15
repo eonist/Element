7749d4cd80a310c48303e6b844a50fb8656aeca3
Eonist <30n1st@gmail.com>
2017-03-15 12:04:18 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/abstract/composition/ElasticScrollable.swift

--- src/Element/ui/other/view/abstract/composition/ElasticScrollable.swift
+++ src/Element/ui/other/view/abstract/composition/ElasticScrollable.swift
@@ -19,7 +19,7 @@ extension ElasticScrollable {
             _ = self.velocities.pushPop(event.scrollingDeltaY)/*insert new velocity at the begining and remove the last velocity to make room for the new*/
             mover!.value += event.scrollingDeltaY/*directly manipulate the value 1 to 1 control*/
             mover!.updatePosition()/*the mover still governs the resulting value, in order to get the displacement friction working*/
-            setProgress(mover!.result)
+            setProgress(mover!.result)//new⚠️️
         }
     }
     /**

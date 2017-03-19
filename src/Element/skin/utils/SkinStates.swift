fd037061b861f33762287cbe07f5f4fccef4deb5
Eonist <30n1st@gmail.com>
2017-03-19 21:55:05 +0100

    Files modified: 1
    Modified 1 file:src/Element/skin/utils/SkinStates.swift

--- src/Element/skin/utils/SkinStates.swift
+++ src/Element/skin/utils/SkinStates.swift
@@ -1,6 +1,7 @@
 import Foundation
 /**
  * TODO: These will probably be upgraded to enums soon, since swift has awesome syntactic support for enums
+ * TODO: Or you could extens String and set receiving params to take SkinStates and then do: .over etc
  */
 class SkinStates {
     /*Core*/

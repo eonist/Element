edfbdccf806737834fce504b5b1176ef4d2f5a01
Eonist <30n1st@gmail.com>
2017-03-19 22:45:23 +0100

    Files modified: 1
    Modified 1 file:src/Element/style/Style.swift

--- src/Element/style/Style.swift
+++ src/Element/style/Style.swift
@@ -1,14 +1,13 @@
 import Foundation
 /**
  * NOTE: The StyleProperty instances are stored in _styleProperties, and then you querry them by name and depth
- * TODO: I think you need to move some of these methods into parsers and modifiers, this class is cognativly heavy to look at
- * TODO: You could probably add most of these methods to an extension, since nothing is subclassing this class
+ * TODO: You could probably add most of these methods to an extension, since nothing is subclassing this class, ⚠️️ weightedStyle overrides style i think, you could jsut make a decorator WeigthedStyle i think, then this could be a struct!
  */
 class Style:IStyle{
     var name:String
-    var selectors:Array<ISelector>
-    var styleProperties:Array<IStyleProperty>
-    init(_ name:String = "",_ selectors:Array<ISelector> = [], _ styleProperties:Array<IStyleProperty> = []){
+    var selectors:[ISelector]
+    var styleProperties:[IStyleProperty]
+    init(_ name:String = "",_ selectors:[ISelector] = [], _ styleProperties:[IStyleProperty] = []){
         self.name = name
         self.selectors = selectors
         self.styleProperties = styleProperties

0329e7b980f839b68ab18e328d13a127b95385b1
Eonist <30n1st@gmail.com>
2017-03-25 11:50:20 +0100

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/concrete/ScrollView.swift

--- src/Element/ui/other/view/concrete/ScrollView.swift
+++ src/Element/ui/other/view/concrete/ScrollView.swift
@@ -1,10 +1,10 @@
 import Cocoa
 /**
  * When the the user scrolls
- * NOTE: this Method is totally academic, can be usefull for UI where you dont want or need slider. 
+ * NOTE: this Method is totally academic, can be usefull for UI where you don't want or need slider.
  */
-class ScrollView: ContainerView, Scrollable {
-    override func scrollWheel(with event: NSEvent) {//TODO: move to displaceview
+class ScrollView:ContainerView,Scrollable {
+    override func scrollWheel(with event:NSEvent) {//TODO: move to displaceview
         scroll(event)/*forward the event to the extension which adjust Slider and calls setProgress in this method*/
         super.scrollWheel(with: event)/*forward the event other delegates higher up in the stack*/
     }

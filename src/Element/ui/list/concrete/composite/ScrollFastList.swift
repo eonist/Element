74c17f4923b1a9c210015b1010c36f53f773c98f
Eonist <30n1st@gmail.com>
2017-03-27 14:32:44 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/concrete/composite/ScrollFastList.swift

--- src/Element/ui/list/concrete/composite/ScrollFastList.swift
+++ src/Element/ui/list/concrete/composite/ScrollFastList.swift
@@ -1,5 +1,16 @@
-import Foundation
-
-class ScrollFastList {
-
+import Cocoa
+@testable import Utils
+/**
+ * New
+ */
+class ScrollFastList:FastList,ScrollableFast {
+    override var itemsHeight: CGFloat {return dp.count * itemHeight}//👈 temp, move into protocol extension, if possible
+    /**
+     * When the the user scrolls
+     * NOTE: this method overides the Native NSView scrollWheel method
+     */
+    override func scrollWheel(with event: NSEvent) {
+        scroll(event)
+        //TODO: ⚠️️ you want to do super.scrollWhe...here, as NSView may need it up hirarchy etc
+    }
 }

f2bd13891bd7978f3269c25b8a530e2123afd917
Eonist <30n1st@gmail.com>
2017-03-28 13:41:21 +0200

    Files modified: 2
    Modified 2 files:src/Element/ui/list/concrete/composite/ScrollList.swiftsrc/Element/ui/other/view/concrete/ContainerView.swift

--- src/Element/ui/other/view/concrete/ContainerView.swift
+++ src/Element/ui/other/view/concrete/ContainerView.swift
@@ -4,17 +4,15 @@ import Cocoa
  * ContainerView holds other UI elements
  */
 class ContainerView:Element,Containable {
-    
+    var maskSize:CGSize {return CGSize(NaN,height)}/*represents the visible part of the content *///TODO: could be ranmed to maskRect
+    var contentSize:CGSize {return CGSize(NaN,itemsHeight)}
+    var itemSize:CGSize {return CGSize(NaN,itemHeight)}
     //var height:CGFloat {fatalError("Must override in subClass")}
     //var interval:CGFloat {fatalError("Must override in subClass")}
     //var progress:CGFloat {fatalError("Must override in subClass")}
     var itemsHeight:CGFloat {fatalError("Must override in subClass")}//override this for custom value
     var itemHeight:CGFloat {fatalError("Must override in subClass")}//override this for custom value
     var lableContainer:Element?
-    
-    var maskSize:CGSize {return CGSize(width,height)}/*represents the visible part of the content *///TODO: could be ranmed to maskRect
-    var contentSize:CGSize {return CGSize(itemsHeight,itemsHeight)}
-    var itemSize:CGSize {return CGSize(itemHeight,itemHeight)}
     override func resolveSkin() {
         super.resolveSkin()//self.skin = SkinResolver.skin(self)//
         lableContainer = addSubView(Container(width,height,self,"lable"))

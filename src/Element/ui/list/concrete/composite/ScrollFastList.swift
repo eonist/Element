f189fe0b888998b1d8e644cf61e3d3b5f6546022
Eonist <30n1st@gmail.com>
2017-03-29 14:02:22 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/concrete/composite/ScrollFastList.swift

--- src/Element/ui/list/concrete/composite/ScrollFastList.swift
+++ src/Element/ui/list/concrete/composite/ScrollFastList.swift
@@ -5,10 +5,18 @@ import Cocoa
  */
 class ScrollFastList:FastList,ScrollableFast {
     /**
+     * NOTE: Basically when you perform a scroll-gesture on the touch-pad
+     */
+    func onScrollWheelChange(_ event:NSEvent) {/*Direct scroll, not momentum*/
+        Swift.print("ScrollFastList.onScrollWheelChange: \(event.type)")
+        let progressVal:CGFloat = SliderListUtils.progress(event.delta[dir], interval, progress)
+        setProgress(progressVal)
+    }
+    /**
      * When the the user scrolls
      * NOTE: this method overides the Native NSView scrollWheel method
      */
-    override func scrollWheel(with event: NSEvent) {
+    override func scrollWheel(with event:NSEvent) {
         scroll(event)
         //TODO: ⚠️️ you want to do super.scrollWhe...here, as NSView may need it up hirarchy etc
     }

76381a90f0152efa8760c443d4a164b69a3b8620
Eonist <30n1st@gmail.com>
2017-03-29 14:25:22 +0200

    Files modified: 2
    Modified 2 files:src/Element/ui/list/concrete/composite/ScrollFastList.swiftsrc/Element/ui/list/utils/SliderListUtils.swift

--- src/Element/ui/list/utils/SliderListUtils.swift
+++ src/Element/ui/list/utils/SliderListUtils.swift
@@ -5,8 +5,8 @@ class SliderListUtils{//⚠️️ This can probably be removed, as the same code
     /**
      * Returns the progress og the sliderList (used when we scroll with the scrollwheel/touchpad)
      */
-    static func progress(_ deltaY:CGFloat,_ sliderInterval:CGFloat,_ sliderProgress:CGFloat)->CGFloat{
-        let scrollAmount:CGFloat = (deltaY/30)/sliderInterval/*_scrollBar.interval*/
+    static func progress(_ delta:CGFloat,_ sliderInterval:CGFloat,_ sliderProgress:CGFloat)->CGFloat{
+        let scrollAmount:CGFloat = (delta/30)/sliderInterval/*_scrollBar.interval*/
         var currentScroll:CGFloat = sliderProgress - scrollAmount/*The minus sign makes sure the scroll works like in OSX LION*/
         currentScroll = currentScroll.clip(0, 1)/*Clamps the num between 0 and 1*/
         return currentScroll

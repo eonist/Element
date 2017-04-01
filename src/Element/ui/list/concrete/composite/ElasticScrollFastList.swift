3d45259e5f3839a4b2a25c4cfe660afc32a0b6b6
Eonist <30n1st@gmail.com>
2017-04-01 23:19:09 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/concrete/composite/ElasticScrollFastList.swift

--- src/Element/ui/list/concrete/composite/ElasticScrollFastList.swift
+++ src/Element/ui/list/concrete/composite/ElasticScrollFastList.swift
@@ -15,9 +15,9 @@ class ElasticScrollFastList:FastList, ElasticScrollableFast {
         rbContainer!.addSubview(lableContainer!)//add lable Container inside rbContainer
         lableContainer!.parent = rbContainer!
         /*RubberBand*/
-        let frame = (min:0,len:maskSize[dir])//CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
-        let itemsRect = (min:0,len:contentSize[dir])/*represents the total size of the content *///TODO: could be ranmed to contentRect
-        mover = RubberBand(Animation.sharedInstance,frameTick/*👈important*/,frame,itemsRect)
+        let frame:RubberBand.Frame = (min:0,len:maskSize[dir])//CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
+        let itemsRect:RubberBand.Frame = (min:0,len:contentSize[dir])/*represents the total size of the content *///TODO: could be ranmed to contentRect
+        mover = RubberBand.init(Animation.sharedInstance, frameTick, frame, itemsRect)
         mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
     }
     func frameTick(_ value:CGFloat){//real value

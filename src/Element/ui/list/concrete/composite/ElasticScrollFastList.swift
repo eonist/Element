b7ef5cf16e4a17cbda1daf752446b92fbc454d75
Eonist <30n1st@gmail.com>
2017-04-01 23:14:12 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/concrete/composite/ElasticScrollFastList.swift

--- src/Element/ui/list/concrete/composite/ElasticScrollFastList.swift
+++ src/Element/ui/list/concrete/composite/ElasticScrollFastList.swift
@@ -15,8 +15,8 @@ class ElasticScrollFastList:FastList, ElasticScrollableFast {
         rbContainer!.addSubview(lableContainer!)//add lable Container inside rbContainer
         lableContainer!.parent = rbContainer!
         /*RubberBand*/
-        let frame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
-        let itemsRect = CGRect(0,0,width,itemsHeight)/*represents the total size of the content *///TODO: could be ranmed to contentRect
+        let frame = (0,maskSize[dir])//CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
+        let itemsRect = (0,contentSize[dir])/*represents the total size of the content *///TODO: could be ranmed to contentRect
         mover = RubberBand(Animation.sharedInstance,frameTick/*👈important*/,frame,itemsRect)
         mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
     }

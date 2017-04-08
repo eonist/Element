09dcadfa858ce64a8826f9486ae923173089bf45
Eonist <30n1st@gmail.com>
2017-04-08 18:29:39 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:src/Element/ui/slider/VolumeSlider.swift

--- src/Element/ui/slider/VolumeSlider.swift
+++ src/Element/ui/slider/VolumeSlider.swift
@@ -2,7 +2,7 @@ import Cocoa
 @testable import Utils
 
 class VolumeSlider:HSlider{
-    lazy var volumeGraphic:Element = Element(1,1,self,"volumeGraphic")//lazy so that its only created once, and we dont have to implement init
+    lazy var volumeGraphic:Element = Element(1,1,self,"volumeGraphic")//lazy so that its only created once, and we don't have to implement init
     override func resolveSkin() {
         super.resolveSkin()
         volumeGraphic = addSubViewAt(volumeGraphic,self.indexOf(thumb!))//TODO: add the volume graphic bellow the thumb

3d767b06f82ead83a956b77ab10b0a6294e421b9
Eonist <30n1st@gmail.com>
2017-04-08 19:56:00 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/slider/Slider.swift

--- src/Element/ui/slider/Slider.swift
+++ src/Element/ui/slider/Slider.swift
@@ -3,7 +3,7 @@ import Foundation
 @testable import Element
 
 class Slider:Element{
-
+    var progress:CGFloat = 0
 }
 extension Slider{
     func setProgressValue(_ progress:CGFloat){/*Can't be named setProgress because of objc*/

fbf94eecb7cd05b8690507f6dccc67532fb7a941
Eonist <30n1st@gmail.com>
2017-04-09 12:19:32 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/slider/Slider.swift

--- src/Element/ui/slider/Slider.swift
+++ src/Element/ui/slider/Slider.swift
@@ -4,6 +4,11 @@ import Foundation
 
 class Slider:Element{
     var progress:CGFloat = 0
+    init(_ width:CGFloat, _ height:CGFloat,_ thumbHeight:CGFloat = NaN, _ progress:CGFloat = 0,_ parent:IElement? = nil, id:String? = nil){
+        self.progress = progress
+        self.thumbHeight = thumbHeight.isNaN ? width:thumbHeight// :TODO: explain in a comment what this does
+        super.init(width,height,parent,id)
+    }
 }
 extension Slider{
     func setProgressValue(_ progress:CGFloat){/*Can't be named setProgress because of objc*/

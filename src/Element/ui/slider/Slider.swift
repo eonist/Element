96c565cf5f673d1cb4b82d93d425d2695a9e2b08
Eonist <30n1st@gmail.com>
2017-04-09 12:24:03 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/slider/Slider.swift

--- src/Element/ui/slider/Slider.swift
+++ src/Element/ui/slider/Slider.swift
@@ -3,12 +3,20 @@ import Foundation
 @testable import Element
 
 class Slider:Element{
+    var thumb:Thumb?
     var progress:CGFloat
     var thumbSize:CGSize
     init(_ width:CGFloat, _ height:CGFloat,_ thumbSize:CGSize, _ progress:CGFloat = 0,_ parent:IElement? = nil, id:String? = nil){
         self.progress = progress
         self.thumbSize = thumbSize
         super.init(width,height,parent,id)
+    }
+    override func resolveSkin() {
+        super.resolveSkin()
+        //skin.isInteractive = false// :TODO: explain why in a comment
+        //skin.useHandCursor = false;// :TODO: explain why in a comment
+        thumb = addSubView(Thumb(width, thumbHeight,false,self))
+        setProgressValue(progress)// :TODO: explain why in a comment, because initially the thumb may be positioned wrongly  due to clear and float being none
     }
     required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

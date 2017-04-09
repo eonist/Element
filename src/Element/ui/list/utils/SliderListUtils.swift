cc3f4fc7e89723a1acd0b5c4520ae1c4c75b41c7
Eonist <30n1st@gmail.com>
2017-04-09 16:08:42 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/utils/SliderListUtils.swift

--- src/Element/ui/list/utils/SliderListUtils.swift
+++ src/Element/ui/list/utils/SliderListUtils.swift
@@ -3,7 +3,7 @@ import Foundation
 
 class SliderListUtils{//⚠️️ This can probably be removed, as the same code is in SliderParser or alike, at least move to SliderParser
     /**
-     * Returns the progress og the sliderList (used when we scroll with the scrollwheel/touchpad)
+     * Returns the progress og the sliderList (used when we scroll with the scrollwheel/touchpad) (0-1)
      */
     static func progress(_ delta:CGFloat,_ sliderInterval:CGFloat,_ sliderProgress:CGFloat)->CGFloat{
         let scrollAmount:CGFloat = (delta/30)/sliderInterval/*_scrollBar.interval*/

c3125047eb02662a8a0d1ce3453fee7d5449e043
Eonist <30n1st@gmail.com>
2017-04-09 13:18:48 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/slider/Slider.swift

--- src/Element/ui/slider/Slider.swift
+++ src/Element/ui/slider/Slider.swift
@@ -29,15 +29,24 @@ extension Slider{
     func setProgressValue(_ progress:CGFloat){/*Can't be named setProgress because of objc*/
         self.progress = progress.clip(0,1)/*if the progress is more than 0 and less than 1 use progress, else use 0 if progress is less than 0 and 1 if its more than 1*/
         thumb!.point[dir] = Utils.thumbPosition(self.progress, frame.size[dir], thumbSize[dir])
-        thumb!.applyOvershot(progress)/*<--we use the unclipped scalar value*/
+        thumb!.applyOvershot(progress,dir)/*<--we use the unclipped scalar value*/
     }
 }
 private class Utils{//TODO:rename to VSliderUtils and make it not private
     /**
      * Returns the position of a thumbs PARAM progress
      */
-    class func thumbPosition(_ progress:CGFloat, _ side:CGFloat, _ thumbSide:CGFloat)->CGFloat {
+    static func thumbPosition(_ progress:CGFloat, _ side:CGFloat, _ thumbSide:CGFloat)->CGFloat {
         let minThumbPos:CGFloat = side - thumbSide/*Minimum thumb position*/
         return progress * minThumbPos
+    }
+    /**
+     * Returns the progress derived from a node
+     * RETURN: a number between 0 and 1
+     */
+    static func progress(_ mouseY:CGFloat,_ tempNodeMouseY:CGFloat,_ height:CGFloat,_ thumbHeight:CGFloat)->CGFloat {
+        if(thumbHeight == height) {return 0}/*if the thumbHeight is the same as the height of the slider then return 0*/
+        let progress:CGFloat = (mouseY-tempNodeMouseY) / (height-thumbHeight)
+        return max(0,min(progress,1))/*Ensures that progress is between 0 and 1 and if its beyond 0 or 1 then it is 0 or 1*/
     }
 }

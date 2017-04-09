487ce3d2c9586189d15ef355405caf208bcd98c3
Eonist <30n1st@gmail.com>
2017-04-09 12:21:52 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/slider/Slider.swift

--- src/Element/ui/slider/Slider.swift
+++ src/Element/ui/slider/Slider.swift
@@ -3,15 +3,17 @@ import Foundation
 @testable import Element
 
 class Slider:Element{
-    var progress:CGFloat = 0
-    init(_ width:CGFloat, _ height:CGFloat,_ thumbHeight:CGFloat = NaN, _ progress:CGFloat = 0,_ parent:IElement? = nil, id:String? = nil){
+    var progress:CGFloat
+    var thumbSize:CGSize
+    init(_ width:CGFloat, _ height:CGFloat,_ thumbSize:CGSize, _ progress:CGFloat = 0,_ parent:IElement? = nil, id:String? = nil){
         self.progress = progress
-        self.thumbHeight = thumbHeight.isNaN ? width:thumbHeight// :TODO: explain in a comment what this does
+        self.thumbSize = thumbSize
         super.init(width,height,parent,id)
     }
+    required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
 extension Slider{
     func setProgressValue(_ progress:CGFloat){/*Can't be named setProgress because of objc*/
-        //
+        self.progress = progress
     }
 }

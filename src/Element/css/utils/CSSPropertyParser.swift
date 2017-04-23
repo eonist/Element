2db037de1fe9875edf3301cef1c26d72a057d55d
Eonist <30n1st@gmail.com>
2017-04-23 17:38:52 +0200

    Files modified: 1
    Modified 1 file:src/Element/css/utils/CSSPropertyParser.swift

--- src/Element/css/utils/CSSPropertyParser.swift
+++ src/Element/css/utils/CSSPropertyParser.swift
@@ -119,10 +119,10 @@ class CSSPropertyParser {
         let textFormat:TextFormat = properties.map {
                 let property:String = $0
                 let matches:[NSTextCheckingResult] = property.matches("^(\\w+?)\\:(.+?)$")
-                let pairs:(name:String,value:Any) = matches.map{
+                let pairs:[(name:String,value:Any)] = matches.map{
                     let match:NSTextCheckingResult = $0
-                    let name:String = matchVal.value(property, 1)/*Capturing group 1*/
-                    var value:Any = matchVal.value(property, 2)/*Capturing group 2*/
+                    let name:String = match.value(property, 1)/*Capturing group 1*/
+                    var value:Any = match.value(property, 2)/*Capturing group 2*/
                     if(name == "color") { value = StringParser.nsColor(value as! String) }
                     else if("\(value)" == "true") {value = true }
                     else if("\(value)" == "false") {value = false }

1597088b077122010daae522b03e6aecffe9f3e5
Eonist <30n1st@gmail.com>
2017-04-24 00:02:39 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/utils/StyleExtensions.swift

--- src/Element/style/utils/StyleExtensions.swift
+++ src/Element/style/utils/StyleExtensions.swift
@@ -3,7 +3,6 @@ import Foundation
 
 extension Style:UnWrappable{
     static var clear:IStyle = Style("clear",[],[StyleProperty("idleColor",0x000000),StyleProperty("idleOpacity",0)])//this won't work since it doesnt have any selectors
-    
     init(_ id:String,_ styleProperty:IStyleProperty){/*Convenience*/
         self.init(id,[],[styleProperty])
     }
@@ -11,16 +10,12 @@ extension Style:UnWrappable{
 extension Style{
     /**
      * Converts xml to a Style instance
-     * IMPORTANT: this must be located here because it belongs in the Element lib but uses the swift-utils lib
+     * IMPORTANT: ⚠️️ this must be located here because it belongs in the Element lib but uses the swift-utils lib
      */
     static func unWrap<T>(_ xml:XML) -> T? {
-        //Swift.print("xml.xmlString: " + "\(xml.xmlString)")
         let name:String = unWrap(xml, "name")!
-        //Swift.print("UnWrap.name: " + "\(name)")
         let styleProperties:[StyleProperty?] = unWrap(xml, "styleProperties")
-        //Swift.print("styleProperties.count: " + "\(styleProperties.count)")
         let selectors:[Selector?] = unWrap(xml, "selectors")
-        //Swift.print("selectors.count: " + "\(selectors.count)")
         return Style(name,selectors.flatMap{$0},styleProperties.flatMap{$0}) as? T
     }
 }

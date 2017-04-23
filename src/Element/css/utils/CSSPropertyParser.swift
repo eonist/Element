b83f5add0be86e7030b47deabd8fe3ae8d5b16f5
Eonist <30n1st@gmail.com>
2017-04-23 17:23:09 +0200

    Files modified: 1
    Modified 1 file:src/Element/css/utils/CSSPropertyParser.swift

--- src/Element/css/utils/CSSPropertyParser.swift
+++ src/Element/css/utils/CSSPropertyParser.swift
@@ -116,11 +116,12 @@ class CSSPropertyParser {
         let propertyString:String = input.match(textFormatPattern)[0]
         let properties:[String] = propertyString.split(",")
         /**/
-        let textFormat:TextFormat = properties.lazy.map {
-            let property:String = $0
+        let textFormat:TextFormat = properties.lazy.map; in -> [NSTextCheckingResult] {
+                let property:String = $0
                 let matches:[NSTextCheckingResult] = property.matches("^(\\w+?)\\:(.+?)$")
                 return matches
-            }.map{ match in
+            }.map{
+                let match:NSTextCheckingResult = $0
                 let name:String = match.value(property, 1)/*Capturing group 1*/
                 var value:Any = match.value(property, 2)/*Capturing group 2*/
                 if(name == "color") { value = StringParser.nsColor(value as! String) }

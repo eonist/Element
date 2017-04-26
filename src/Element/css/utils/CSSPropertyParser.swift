665fa81b9e69e3e148b3b2c7324c1da5babfd7d8
Eonist <30n1st@gmail.com>
2017-04-26 23:42:35 +0200

    Files modified: 1
    Modified 1 file:src/Element/css/utils/CSSPropertyParser.swift

--- src/Element/css/utils/CSSPropertyParser.swift
+++ src/Element/css/utils/CSSPropertyParser.swift
@@ -34,7 +34,7 @@ class CSSPropertyParser {
             case string.test("^radial-gradient\\b"):return radialGradient(string)/*radial-gradient*/// :TODO: create a more complte exprrison for this test
             case string.test("^drop-shadow\\b"):return dropShadow(string)/*drop-shadow*/
             case string.test("^textFormat\\b"):return textFormat(string)
-            case string.test("^rotate\\b"):return transform(string)
+            case string.test("^rotate\\b"):return rotate(string)
             //case RegExp.test(string,"^textField\\b"):return textField(string)
             case string.test(arrayPattern):return array(string)/*corner-radius, line-offset-type, margin, padding, offset, svg asset, font names*/// :TODO: shouldnt the \040 be optional? added ~ char for relative path support
             case string.test(stringPattern):return string/* string (Condition: someName1 | someName | but not just a number by it self);*/ //:TODO: this needs to also test if it is a contining word. ^pattern$ so not to match linear-gradient or you can test that its nothing els than words or number? // :TODO: what does it do?
@@ -45,8 +45,8 @@ class CSSPropertyParser {
      * EXAMPLE: transform:rotation(90deg)
      * NOTE: in the future we will add more transforms
      */
-    static func transform(_ string:String)->CGFloat{
-        Swift.print("transform: " + "\(transform)")
+    static func rotate(_ string:String)->CGFloat{
+        Swift.print("rotate()")
         if let propertyString = string.match(transformRotatePattern).first{
             Swift.print("propertyString: " + "\(propertyString)")
             let rotation:CGFloat = Utils.rotation(propertyString)

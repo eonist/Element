6052f58b8cd9ab8392d13ec6c5ccf6169ed92b08
Eonist <30n1st@gmail.com>
2017-04-26 17:10:01 +0200

    Files modified: 1
    Modified 1 file:src/Element/skin/graphic/utils/GraphicModifier.swift

--- src/Element/skin/graphic/utils/GraphicModifier.swift
+++ src/Element/skin/graphic/utils/GraphicModifier.swift
@@ -11,4 +11,10 @@ class GraphicModifier {
         decoratable.graphic.lineOffsetType = offsetType
         return decoratable
     }
+    /**
+     *
+     */
+    static func applyTransform(_ decoratable:inout IGraphicDecoratable,_ rotation:CGFloat){
+        
+    }
 }

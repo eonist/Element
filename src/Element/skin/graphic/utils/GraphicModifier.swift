fc53f89c40dc081d2cf8510ee134277058d4b7d7
Eonist <30n1st@gmail.com>
2017-04-26 17:41:00 +0200

    Files modified: 2
    Modified 2 files:src/Element/skin/graphic/GraphicSkin.swiftsrc/Element/skin/graphic/utils/GraphicModifier.swift

--- src/Element/skin/graphic/utils/GraphicModifier.swift
+++ src/Element/skin/graphic/utils/GraphicModifier.swift
@@ -15,7 +15,7 @@ class GraphicModifier {
      * New
      * PARAM: roation is in deg -90 90 180 0 etc
      */
-    static func applyTransform(_ decoratable:inout IGraphicDecoratable,_ rotation:CGFloat){
+    static func applyRotation(_ decoratable:inout IGraphicDecoratable,_ rotation:CGFloat){
         //let rot:CGFloat = Trig.normalize2(rotation * ㎭)/*between -π and π*/
         decoratable.graphic.rotate(byDegrees: rotation)
     }

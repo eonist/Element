43d2a85cd72517bac4f471c529358f5e30465f64
Eonist <30n1st@gmail.com>
2017-04-27 16:51:41 +0200

    Files modified: 1
    Modified 1 file:src/Element/skin/graphic/GraphicSkin.swift

--- src/Element/skin/graphic/GraphicSkin.swift
+++ src/Element/skin/graphic/GraphicSkin.swift
@@ -57,7 +57,7 @@ extension GraphicSkin{
     func updateAppearance(_ decoratable:inout IGraphicDecoratable,_ depth:Int){
         Swift.print("refreshLayer")
         Modifier.applyStyle(&decoratable,self,depth)/*derives and applies style to the decoratable*/
-        //decoratable.get(RectGraphic.self)?.setSizeValue(Parser.size(self,depth))
+        decoratable.get(RectGraphic.self)?.setSizeValue(Parser.size(self,depth))
         decoratable.get(RoundRectGraphic.self)?.fillet = StylePropertyParser.fillet(self,depth)/*fillet*/
         decoratable.get(AssetDecorator.self)?.assetURL = StylePropertyParser.asset(self,depth)/*Svg*/
         decoratable.get(DropShadowDecorator.self)?.dropShadow = StylePropertyParser.dropShadow(self,depth)/*dropshadow*/

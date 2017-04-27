625512eae8313d0f3c65bc02fac8e7ff94919601
Eonist <30n1st@gmail.com>
2017-04-27 13:05:03 +0200

    Files modified: 1
    Modified 1 file:src/Element/skin/graphic/utils/GraphicModifier.swift

--- src/Element/skin/graphic/utils/GraphicModifier.swift
+++ src/Element/skin/graphic/utils/GraphicModifier.swift
@@ -14,21 +14,14 @@ class GraphicModifier {
     /**
      * New
      * PARAM: roation is in deg -90 90 180 0 etc
+     * NOTE: possible future normalization: Trig.normalize2(rotation * ㎭)/*between -π and π*/
      */
     static func applyRotation(_ decoratable:inout IGraphicDecoratable,_ rotation:CGFloat){
         Swift.print("applyRotation: rotation")
-        
-        //let rot:CGFloat = Trig.normalize2(rotation * ㎭)/*between -π and π*/
-        
-        //Continue here: 🏀
-            //Look at drawlab. How it rotates shapes with pivot at center
-            //Also Reset the rotation so that you don't keep rotating on every state change
-            //
-        
         //decoratable.graphic.frameCenterRotation = rotation//(byDegrees:rotation)
         let size:CGSize = (decoratable as! ISizeable).size
         let pos:CGPoint = (decoratable as! IPositional).pos
-        let cgRect:CGRect = CGRect(size,pos)
-        NSViewModifier.rotate(decoratable.graphic, rotation, )
+        let rect:CGRect = CGRect(pos, size)
+        NSViewModifier.rotate(decoratable.graphic, rotation, rect.center)
     }
 }

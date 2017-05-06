d6be61fe8f35edc3c4e5fcf6215ee3be052e0e94
Eonist <30n1st@gmail.com>
2017-05-06 17:10:16 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/treelist/utils/TreeList3Utils.swift

--- src/Element/ui/treelist/utils/TreeList3Utils.swift
+++ src/Element/ui/treelist/utils/TreeList3Utils.swift
@@ -1,27 +1,24 @@
 import Foundation
 @testable import Utils
-
-
-
+typealias ItemData3 = (title:String/*,hasChildren:Bool*/,isOpen:Bool,/*isVisible:Bool,*/isSelected:Bool)
 class TreeList3Utils {
-    typealias ItemData = (title:String/*,hasChildren:Bool*/,isOpen:Bool,/*isVisible:Bool,*/isSelected:Bool)
     /**
      * Creates a data instance to make it easier to work with the attributes in the xml
      */
-    static func itemData(_ tree:Tree)->ItemData? {
+    static func itemData(_ tree:Tree)->ItemData3? {
         if let props = tree.props{
             let title:String = props["title"] ?? ""
             let isOpen:Bool = props["isOpen"] != nil ? props["isOpen"] == "true" : false//<- you can shorten this by doing ??
             let isSelected:Bool = props["isSelected"] != nil ? props["isSelected"] == "true" : false//<- you can shorten this by doing ??
-            return ItemData(title, isOpen, isSelected)
+            return ItemData3(title, isOpen, isSelected)
         };return nil
     }
     /**
      * New
      */
-    static func itemData(_ treeList:TreeList3,_ idx3d:[Int]) -> ItemData? {
+    static func itemData(_ treeList:TreeList3,_ idx3d:[Int]) -> ItemData3? {
         if let tree:Tree = treeList.treeDP.tree[idx3d]{
-            return itemData(tree)
+            return ItemData3(tree)
         }
         return nil
     }

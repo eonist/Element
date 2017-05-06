43fc385191f2bff745e061c4873747d4782d0058
Eonist <30n1st@gmail.com>
2017-05-06 13:18:26 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/abstract/concrete/list/fast/ElasticSlideScrollFastList3.swift

--- src/Element/ui/list/abstract/concrete/list/fast/ElasticSlideScrollFastList3.swift
+++ src/Element/ui/list/abstract/concrete/list/fast/ElasticSlideScrollFastList3.swift
@@ -3,11 +3,11 @@ import Foundation
 
 class ElasticSlideScrollFastList3:SlideFastList3,ElasticSlidableScrollableFastListable3 {
     lazy var moverGroup:MoverGroup? = self.moverGrp
-    lazy var rbContainer:Container? = self.rubberBandContainer/*needed for the overshot animation*/
+    lazy var rbContainer:Container? = self.rubberBandContainer/*Needed for the overshot animation*/
     override func onEvent(_ event:Event) {
         if(event.type == AnimEvent.stopped){
             let dir:Dir = event.origin === moverGroup!.yMover ? .ver : .hor
-            hideSlider(dir)/*hides the slider when bounce back anim stopps*/
+            hideSlider(dir)/*Hides the slider when bounce back anim stopps*/
         }
         super.onEvent(event)
     }

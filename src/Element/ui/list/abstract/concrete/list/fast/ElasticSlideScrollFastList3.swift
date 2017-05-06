a905193338de13313d4e9c47c7989a5fbe39cecf
Eonist <30n1st@gmail.com>
2017-05-06 13:31:44 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/abstract/concrete/list/fast/ElasticSlideScrollFastList3.swift

--- src/Element/ui/list/abstract/concrete/list/fast/ElasticSlideScrollFastList3.swift
+++ src/Element/ui/list/abstract/concrete/list/fast/ElasticSlideScrollFastList3.swift
@@ -14,8 +14,15 @@ class ElasticSlideScrollFastList3:SlideFastList3,ElasticSlidableScrollableFastLi
 }
 extension ElasticSlideScrollFastList3{
     var moverGrp:MoverGroup {
+        test()
         let group = MoverGroup(self.setProgressValue,self.maskSize,self.contentSize)
         group.event = self.onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
         return group
+    }
+    /**
+     *
+     */
+    func test(){
+        Swift.print("🏀 test 🏀")
     }
 }

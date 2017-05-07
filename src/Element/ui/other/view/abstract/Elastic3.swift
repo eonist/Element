85320879fc9120de4eeefdc12d8a60217ac11c36
Eonist <30n1st@gmail.com>
2017-05-07 16:38:28 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/abstract/Elastic3.swift

--- src/Element/ui/other/view/abstract/Elastic3.swift
+++ src/Element/ui/other/view/abstract/Elastic3.swift
@@ -12,11 +12,17 @@ extension Elastic3{
     func setProgress(_ value:CGFloat,_ dir:Dir){
         //Swift.print("Elastic3.setProgress(value)")
         //contentContainer!.point[dir] = value
-        contentContainer?.layer?.position[dir] = value
+        disableAnim {
+            contentContainer?.layer?.position[dir] = value
+        }
+        
     }
     func setProgress(_ point:CGPoint){
         //Swift.print("Elastic3.setProgress(\(point))")
         //contentContainer!.point = point
-        contentContainer?.layer?.position = point
+        disableAnim {
+            contentContainer?.layer?.position = point
+        }
+        
     }
 }

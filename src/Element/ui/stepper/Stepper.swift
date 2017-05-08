b57893323c7036cf192b50b8abfe2e0762321e65
Eonist <30n1st@gmail.com>
2017-05-08 23:11:44 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/stepper/Stepper.swift

--- src/Element/ui/stepper/Stepper.swift
+++ src/Element/ui/stepper/Stepper.swift
@@ -2,15 +2,6 @@ import Foundation
 @testable import Utils
 
 class Stepper:Element{
-    var plusButton:Button?
-    var minusButton:Button?
-    override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement?, _ id:String?) {
-        super.init(width, height, parent, id)
-    }
-    override func resolveSkin() {
-        super.resolveSkin()
-        plusButton = addSubView(Button(height,height,self,"plus"))
-        minusButton = addSubView(Button(height,height,self, "minus"))
-    }
-    required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+    lazy var plusButton:Button = {self.addSubView(Button(self.height,self.height,self,"plus"))}()
+    lazy var minusButton:Button = {self.addSubView(Button(self.height,self.height,self, "minus"))}()
 }

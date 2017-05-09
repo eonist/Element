34c1777367d559b320c2f8576fc5add46078caf1
Eonist <30n1st@gmail.com>
2017-05-09 17:52:01 +0200

    Files modified: 3
    Modified 3 files:src/Element/ui/button/radio/RadioButton.swiftsrc/Element/ui/color/ColorBox.swiftsrc/Element/ui/color/ColorInput.swift

--- src/Element/ui/button/radio/RadioButton.swift
+++ src/Element/ui/button/radio/RadioButton.swift
@@ -2,8 +2,6 @@ import Foundation
 @testable import Utils
 /**
  * TODO: Impliment IDisableable also and extend DisableTextButton
- * NOTE: The radioBullet has an id of "radioBullet" (this may be usefull if you extend CheckBoxButton and that subclass has children that are of type Button and you want to identify this button and noth the others)
- * NOTE: When added to stage and if RadioBullet dispatches selct event it will bubble up and through this class (so no need for extra eventlistners and dispatchers in this class)
  */
 class RadioButton:TextButton,ISelectable{
     lazy var radioBullet:RadioBullet = {self.addSubView(RadioBullet(NaN,NaN,self.isSelected,self))}()
@@ -11,6 +9,14 @@ class RadioButton:TextButton,ISelectable{
     init(_ width:CGFloat, _ height:CGFloat, _ text:String = "defaultText", _ isSelected:Bool = false, _ parent:IElement? = nil, _ id:String? = nil) {
         self.isSelected = isSelected
         super.init(width,height,text,parent,id)
+    }
+    /**
+     * NOTE: When added to stage and if RadioBullet dispatches selct event it will bubble up and through this class (so no need for extra eventlistners and dispatchers in this class)
+     * NOTE: The radioBullet has an id of "radioBullet" (this may be usefull if you extend CheckBoxButton and that subclass has children that are of type Button and you want to identify this button and noth the others)
+     */
+    override func resolveSkin() {
+        super.resolveSkin()
+        _ = radioBullet/*Init the UI*/
     }
     func setSelected(_ isSelected:Bool) {
         radioBullet.setSelected(isSelected)
--- src/Element/ui/color/ColorBox.swift
+++ src/Element/ui/color/ColorBox.swift
@@ -14,10 +14,7 @@ class ColorBox:Button,IColorInput{
     func setColorValue(_ color:NSColor){
         self.color = color
         let style:IStyle = StyleModifier.clone(skin!.style!,skin!.style!.name)/*we clone the style so other Element instances doesnt get their style changed aswell*/// :TODO: this wont do if the skin state changes, therefor we need something similar to DisplayObjectSkin
-        //StyleParser.describe(style)
         var styleProperty = style.getStyleProperty("fill",1) /*edits the style*/
-        //Swift.print("styleProperty: " + "\(styleProperty)")
-        //Swift.print("color.hex: " + "\(color.hexString)")
         if(styleProperty != nil){//temp
             styleProperty!.value = color//("0x" + color.hexString).uint
             skin!.setStyle(style)/*updates the skin*/

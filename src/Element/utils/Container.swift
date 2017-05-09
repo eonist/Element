0fbcb6a9d0dceda1395e82b7d7117f9faca20c86
Eonist <30n1st@gmail.com>
2017-05-09 15:17:24 +0200

    Files modified: 1
    Modified 1 file:src/Element/utils/Container.swift

--- src/Element/utils/Container.swift
+++ src/Element/utils/Container.swift
@@ -11,3 +11,9 @@ class Container:Element{
         skin = SkinResolver.skin(self)/*We still need to generate the skin, why? I can't recall*/   
     }
 }
+extension Container{
+    var layerPos:CGPoint?{
+        get{return super.layer?.position}
+        set{super.layer?.position = newValue!}
+    }
+}

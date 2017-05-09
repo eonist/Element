d49ce9848827be560b6c8e466388237f754ecd70
Eonist <30n1st@gmail.com>
2017-05-09 14:21:31 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/other/view/abstract/composition/ElasticScrollable3.swift

--- src/Element/ui/other/view/abstract/composition/ElasticScrollable3.swift
+++ src/Element/ui/other/view/abstract/composition/ElasticScrollable3.swift
@@ -7,7 +7,7 @@ extension ElasticScrollable3{
      * NOTE: Basically when you perform a scroll-gesture on the touch-pad
      */
     func onScrollWheelChange(_ event:NSEvent){/*Direct scroll*/
-        Swift.print("👻📜 (ElasticScrollable3).onScrollWheelChange : \(event.type) event.scrollingDelta: \(event.scrollingDelta)")
+        //Swift.print("👻📜 (ElasticScrollable3).onScrollWheelChange : \(event.type) ")
         moverGroup!.value += event.scrollingDelta/*directly manipulate the value 1 to 1 control*/
         moverGroup!.updatePosition(true)/*the mover still governs the resulting value, in order to get the displacement friction working*/
         let p:CGPoint = moverGroup!.result

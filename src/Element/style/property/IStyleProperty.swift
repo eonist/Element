8382d25f3494d42f7e8ef8d315bb2b75afc25f9f
Eonist <30n1st@gmail.com>
2017-05-11 00:17:34 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/property/IStyleProperty.swift

--- src/Element/style/property/IStyleProperty.swift
+++ src/Element/style/property/IStyleProperty.swift
@@ -1,9 +1,9 @@
 import Foundation
 /**
- * TODO: ⚠️️ rename to StylePropertyKind ?
+ * TODO: ⚠️️ Rename to StylePropertyKind ?
  */
 protocol IStyleProperty {
-    var value: Any {get set}
+    var value:Any {get set}
     var name:String {get set}
     var depth:Int {get}
 }

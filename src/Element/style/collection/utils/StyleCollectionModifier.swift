7504a5518f264ff7cef79985b38839bc1ae6e713
Eonist <30n1st@gmail.com>
2017-05-11 00:07:35 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/collection/utils/StyleCollectionModifier.swift

--- src/Element/style/collection/utils/StyleCollectionModifier.swift
+++ src/Element/style/collection/utils/StyleCollectionModifier.swift
@@ -5,18 +5,13 @@ class StyleCollectionModifier {
      * Merges PARAM: a with PARAM: b (Adds styles that are not present in PARAM: a, and merges but does not override styles that have the same name)
      */
     static func merge(_ a:inout IStyleCollection,_ b:IStyleCollection) {
-        for styleB:IStyle in b.styles {
-            var hasStyle:Bool = a.styles.first(where: { styleA in
-                styleB.name == styleA.name
-            }) != nil
-            /*for var styleA:IStyle in */
-            
-               
-            
-            
-            
-            
-            if(!hasStyle) {a.addStyle(styleB)}
+        b.styles.forEach { styleB in
+            let hasStyle:Bool = a.styles.first(where:{styleA in styleA.name == styleB.name}) != nil
+            if hasStyle{
+                StyleModifier.merge(&styleA, styleB)
+            }else{
+                a.addStyle(styleB)
+            }
         }
     }
 }

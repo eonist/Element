bea0a751f17e28d902b443793951af33b0b9b9ec
Eonist <30n1st@gmail.com>
2017-05-11 00:12:24 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/collection/utils/StyleCollectionParser.swift

--- src/Element/style/collection/utils/StyleCollectionParser.swift
+++ src/Element/style/collection/utils/StyleCollectionParser.swift
@@ -6,11 +6,9 @@ class StyleCollectionParser {
      * Returns an array of style names
      */
     static func styleNames(_ styleCollection:IStyleCollection) -> [String]{
-        var styleNames:[String] = []
-        for i in 0..<styleCollection.styles.count{//TODO:use forEach instead
-            styleNames.append(styleCollection.styles[i].name)
+        return (0..<styleCollection.styles.count).indices.map{ i in
+            return styleCollection.styles[i].name
         }
-        return styleNames
     }
     /**
      * Describes the stylecollection content
@@ -19,15 +17,14 @@ class StyleCollectionParser {
     static func describe(_ styleCollection:IStyleCollection) {
         func printStyleProperties(_ style:IStyle) {
             Swift.print("<style.name>:"+style.name)
-            var propertyNames:Array = StyleParser.stylePropertyNames(style)
+            var propertyNames:[String] = StyleParser.stylePropertyNames(style)
             (0..<style.styleProperties.count).indices.forEach{ e in
                 let property:Any = style.getValueAt(e)
                 let name:String = propertyNames[e]
                 Swift.print("name:" + name + ", property: " +  String(describing: property))
             }
         }
-        for i in 0..<styleCollection.styles.count{//<--swift 3 for loop upgrade
-            let style:IStyle = styleCollection.styles[i]
+        styleCollection.styles.forEach { style in
             printStyleProperties(style)
         }
     }

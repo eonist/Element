d0ad5822838fedafdb81f0e0b6982de1ce5acb90
Eonist <30n1st@gmail.com>
2017-05-11 15:21:47 +0200

    Files modified: 1
    Modified 1 file:src/Element/skin/utils/SkinManager.swift

--- src/Element/skin/utils/SkinManager.swift
+++ src/Element/skin/utils/SkinManager.swift
@@ -4,6 +4,9 @@ class SkinManager{
      * Returns a new SkinClass instance
      */
     static func getSkinInstance(_ skinName:String,_ element:IElement, _ style:IStyle)->ISkin?{
+        Swift.print("skinName: " + "\(skinName)")
+        Swift.print("element: " + "\(element)")
+        Swift.print("style: " + "\(style)")
         switch skinName{
             case SkinFactory.graphicsSkin : return GraphicSkin(style, element.getSkinState(), element)
             case SkinFactory.textSkin : return TextSkin(style, (element as! IText).initText, element.getSkinState(), element)

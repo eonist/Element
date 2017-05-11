8d5e3c06a5b93dd4437e8b7de300b4f27050024a
Eonist <30n1st@gmail.com>
2017-05-11 12:11:26 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/property/utils/StylePropertyParser.swift

--- src/Element/style/property/utils/StylePropertyParser.swift
+++ src/Element/style/property/utils/StylePropertyParser.swift
@@ -215,9 +215,9 @@ private class Utils{
     }
     /**
      * Returns the total width
-     * TODO: Should margin be added to total width? check google for the box model specs (a work around is too add equal amount of margin-right)
+     * TODO: ⚠️️Should margin be added to total width? check google for the box model specs (a work around is too add equal amount of margin-right)
      */
-    static func totalWidth(_ element:IElement)->CGFloat {/*beta*/
+    static func totalWidth(_ element:IElement)->CGFloat {
         if(element.skin != nil){
             let margin:Margin = SkinParser.margin(element.skin!)
             let border:Border = SkinParser.border(element.skin!)

f99522f9c897fa923153ca4e372d10786981b054
Eonist <30n1st@gmail.com>
2017-05-13 01:47:52 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/ElasticScrollView3.swift

--- src/Element/ui/list/ElasticScrollView3.swift
+++ src/Element/ui/list/ElasticScrollView3.swift
@@ -2,5 +2,9 @@ import Cocoa
 @testable import Utils
 
 class ElasticScrollView3:ContainerView3,ElasticScrollable3{
+    override func resolveSkin() {
+        super.resolveSkin()
+        Swift.print("stackString: " + "\(ElementParser.stackString(self))")
+    }
     lazy var moverGroup:MoverGroup? = MoverGroup(self.setProgress,self.maskSize,self.contentSize)
 }

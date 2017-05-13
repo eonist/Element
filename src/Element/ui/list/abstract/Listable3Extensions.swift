084866c70ae07f7cc1458ef172c54df70fed942c
Eonist <30n1st@gmail.com>
2017-05-14 01:11:33 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/abstract/Listable3Extensions.swift

--- src/Element/ui/list/abstract/Listable3Extensions.swift
+++ src/Element/ui/list/abstract/Listable3Extensions.swift
@@ -1,5 +1,21 @@
-import Foundation
-
-class Listable3Extensions {
-
+import Cocoa
+@testable import Utils
+/**
+ * TODO: Continue adding methods here
+ */
+extension Listable3 {
+    /*Parsers*/
+    var selected:ISelectable?{fatalError("not implemented yet")}/*Convenience*/
+    var selectedIndex:Int{return 0}/*Convenience*/
+    //var itemsHeight:CGFloat {fatalError("not implemented yet")}/*Convenience*/
+    /*Modifiers*/
+    func selectAt(_ index:Int){/*convenience*/
+        print("IList.selectAt(\(index)) not implemented yet")
+    }
+    var dp:DataProvider {return self.dp}/*convenience*/
+    //var itemsHeight: CGFloat {return dp.count * itemHeight}//👈 new
+    
+    /*new/temp TODO: maybe use where Self:IList && Self:Scrollable*/
+    var interval:CGFloat{return floor(contentSize[dir] - maskSize[dir])/itemSize[dir]}
+    var progress:CGFloat{return SliderParser.progress(contentContainer.point[dir], maskSize[dir], contentSize[dir])}
 }

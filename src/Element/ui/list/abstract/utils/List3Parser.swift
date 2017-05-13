ca1eeaf2a6c341207c5dbcbba0135f3494d0b912
Eonist <30n1st@gmail.com>
2017-05-13 20:52:42 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/list/abstract/utils/List3Parser.swift

--- src/Element/ui/list/abstract/utils/List3Parser.swift
+++ src/Element/ui/list/abstract/utils/List3Parser.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class List3Parser {
-
+    /**
+     * Returns the index of a "label"
+     * PARAM: view is the Label
+     */
+    static func index(_ list: Listable3, _ view:NSView)->Int {
+        return list.lableContainer!.indexOf(view)
+    }
 }

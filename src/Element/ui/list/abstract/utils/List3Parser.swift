a86fe7f2289d4d7010fee197a19f0ab3dba8a78f
Eonist <30n1st@gmail.com>
2017-05-13 20:57:27 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:src/Element/ui/list/abstract/utils/List3Parser.swift

--- src/Element/ui/list/abstract/utils/List3Parser.swift
+++ src/Element/ui/list/abstract/utils/List3Parser.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 
 class List3Parser {
     /**
@@ -7,5 +7,11 @@ class List3Parser {
      */
     static func index(_ list: Listable3, _ view:NSView)->Int {
         return list.lableContainer!.indexOf(view)
+    }
+    /**
+     * Returns the property for index
+     */
+    static func propertyAt(_ list:IList, _ index:Int)->String{
+        return list.dataProvider.getItemAt(index)!["property"]!
     }
 }

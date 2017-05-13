6545dee65d09e92c75c81b9e6dee6c6c04608681
Eonist <30n1st@gmail.com>
2017-05-13 18:46:40 +0200

    New files added: 2, Files modified: 1, Files deleted: 11
    Added 2 files:src/Element/ui/.DS_Storesrc/Element/ui/slider/.DS_StoreDeleted 11 files:src/Element/ui/slider/DEPRECATEDHSlider.swiftsrc/Element/ui/slider/DEPRECATEDVSlider.swiftsrc/Element/ui/treelist/DEPRECATED/ITreeList.swiftsrc/Element/ui/treelist/DEPRECATED/ITreeListItem.swiftsrc/Element/ui/treelist/DEPRECATED/SliderTreeList.swiftsrc/Element/ui/treelist/DEPRECATED/TreeList.swiftsrc/Element/ui/treelist/DEPRECATED/TreeListItem.swiftsrc/Element/ui/treelist/DEPRECATED/utils/TreeListAsserter.swiftsrc/Element/ui/treelist/DEPRECATED/utils/TreeListModifier.swiftsrc/Element/ui/treelist/DEPRECATED/utils/TreeListParser.swiftsrc/Element/ui/treelist/DEPRECATED/utils/TreeListUtils.swiftModified 1 file:src/Element/ui/treelist/.DS_Store

--- src/Element/ui/treelist/DEPRECATED/utils/TreeListAsserter.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-class TreeListAsserter {
-    class func hasSelected(_ treeList:ITreeList)->Bool {
-        let selectedIndex:Array = TreeListParser.selectedIndex(treeList)
-        return selectedIndex.count > 0
-    }
-}

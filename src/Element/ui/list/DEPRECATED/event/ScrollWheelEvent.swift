8866dc9b4dee4600439be7b75a043b518ee36585
Eonist <30n1st@gmail.com>
2017-05-13 19:50:40 +0200

    Files modified: 2, Files deleted: 20
    Deleted 20 files:src/Element/ui/list/DEPRECATED/.DS_Storesrc/Element/ui/list/DEPRECATED/IList.swiftsrc/Element/ui/list/DEPRECATED/abstract/IFastListExtension.swiftsrc/Element/ui/list/DEPRECATED/abstract/IListExtension.swiftsrc/Element/ui/list/DEPRECATED/concrete/List.swiftsrc/Element/ui/list/DEPRECATED/concrete/composite/ElasticScrollFastList.swiftsrc/Element/ui/list/DEPRECATED/concrete/composite/ElasticSlideScrollFastList.swiftsrc/Element/ui/list/DEPRECATED/concrete/composite/FastList.swiftsrc/Element/ui/list/DEPRECATED/concrete/composite/ScrollFastList.swiftsrc/Element/ui/list/DEPRECATED/concrete/composite/ScrollList.swiftsrc/Element/ui/list/DEPRECATED/concrete/composite/SlideFastList.swiftsrc/Element/ui/list/DEPRECATED/concrete/composite/SlideScrollFastList.swiftsrc/Element/ui/list/DEPRECATED/concrete/composite/SlideScrollList.swiftsrc/Element/ui/list/DEPRECATED/event/ListEvent.swiftsrc/Element/ui/list/DEPRECATED/event/ScrollWheelEvent.swiftsrc/Element/ui/list/DEPRECATED/protocol/.DS_Storesrc/Element/ui/list/DEPRECATED/utils/FastListModifier.swiftsrc/Element/ui/list/DEPRECATED/utils/ListModifier.swiftsrc/Element/ui/list/DEPRECATED/utils/ListParser.swiftsrc/Element/ui/list/DEPRECATED/utils/SliderListUtils.swiftModified 2 files:src/Element/ui/.DS_Storesrc/Element/ui/list/.DS_Store

--- src/Element/ui/list/DEPRECATED/event/ScrollWheelEvent.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-@testable import Utils
-
-class ScrollWheelEvent:Event{
-    static let enter:String = "scrollWheelEnter"//Possibly rename to engage
-    static let exit:String = "scrollWheelExit"//Possibly rename to disEngage
-    static let exitAndStationary:String = "scrollWheelExitAndStationary"
-}

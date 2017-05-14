2292cefea126f2b9894ef27a195293dde8b73d72
Eonist <30n1st@gmail.com>
2017-05-14 22:13:05 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/treelist/utils/TreeList3Utils.swift

--- src/Element/ui/treelist/utils/TreeList3Utils.swift
+++ src/Element/ui/treelist/utils/TreeList3Utils.swift
@@ -5,7 +5,7 @@ class TreeList3Utils {
     /**
      * Creates a data instance to make it easier to work with the attributes in the xml
      */
-    static func itemData(_ tree:Tree)->ItemData3? {
+    private static func itemData(_ tree:Tree)->ItemData3? {
         if let props = tree.props{
             let title:String = props["title"] ?? ""
             let isOpen:Bool = props["isOpen"] != nil ? props["isOpen"] == "true" : false//<- you can shorten this by doing ??
@@ -16,7 +16,7 @@ class TreeList3Utils {
     /**
      * New
      */
-    static func itemData(_ treeList:TreeList3,_ idx3d:[Int]) -> ItemData3? {
+    static func itemData(_ treeList:TreeListable3,_ idx3d:[Int]) -> ItemData3? {
         if let tree:Tree = treeList.treeDP.tree[idx3d]{
             return itemData(tree)
         }

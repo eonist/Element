3cf9a224bfc841c2a667441c1929e28dc2b45633
Eonist <30n1st@gmail.com>
2017-05-14 23:02:51 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/treelist/utils/tree/utils/TreeAsserter.swift

--- src/Element/ui/treelist/utils/tree/utils/TreeAsserter.swift
+++ src/Element/ui/treelist/utils/tree/utils/TreeAsserter.swift
@@ -7,6 +7,7 @@ class TreeAsserter {
     static func hasChildren(_ tree:Tree,_ idx3d:[Int])->Bool{
         if let child:Tree = tree[idx3d]{
             return !child.children.isEmpty
-        };return false
+        }
+        fatalError("no child at: \(idx3d)")
     }
 }

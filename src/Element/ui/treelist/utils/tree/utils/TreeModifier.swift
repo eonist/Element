02e79749afdd3c27e12b447fd1ad78327eaffc48
Eonist <30n1st@gmail.com>
2017-05-15 17:00:11 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/treelist/utils/tree/utils/TreeModifier.swift

--- src/Element/ui/treelist/utils/tree/utils/TreeModifier.swift
+++ src/Element/ui/treelist/utils/tree/utils/TreeModifier.swift
@@ -125,16 +125,17 @@ class TreeModifier {
         if idx3d.isEmpty {fatalError("Index not supported: \(idx3d)")}
         let parentTreeIdx3d:[Int] = TreeUtils.parentIndex(idx3d)
         if idx3d.last! > 0 {_ = tree[parentTreeIdx3d]?.children.displace(idx3d.last!,0)}
-        
+        return parentTreeIdx3d + [idx3d.last!]
     }
     /**
      * Moves the child at PARAM: idx3d to the bottom
      */
-    static func moveToBottom(_ tree: inout Tree,_ idx3d:[Int]){
+    static func moveToBottom(_ tree: inout Tree,_ idx3d:[Int]) -> [Int]{
         if idx3d.isEmpty {fatalError("Index not supported: \(idx3d)")}
         let parentTreeIdx3d:[Int] = TreeUtils.parentIndex(idx3d)
         if let childrenCount:Int = tree[parentTreeIdx3d]?.children.count, idx3d.last! < childrenCount {
             _ = tree[parentTreeIdx3d]?.children.displace(idx3d.last!,childrenCount)
         }
+        return parentTreeIdx3d + 
     }
 }

070d7f41733c704e7582afcc2705bf7b01e6b8b3
Eonist <30n1st@gmail.com>
2017-05-25 16:37:33 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/property/utils/StyleMetricParser.swift

--- src/Element/style/property/utils/StyleMetricParser.swift
+++ src/Element/style/property/utils/StyleMetricParser.swift
@@ -5,7 +5,7 @@ class StyleMetricParser {
     /**
      * Returns Offset
      * TODO: ⚠️️ Merge ver/hor Offset into this one like you did with cornerRadius
-     * TODO: ⚠️️ Add support for % as it isnt implemented yet, see the margin implementation for guidance
+     * TODO: ⚠️️ Add support for % as it isn't implemented yet, see the margin implementation for guidance
      */
     static func offset(_ skin:ISkin,_ depth:Int = 0)->CGPoint {
         guard let value:Any = StylePropertyParser.value(skin, CSSConstants.offset.rawValue, depth) else{

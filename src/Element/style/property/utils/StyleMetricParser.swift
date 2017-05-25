9f9d26530e4eb3498ebcaf37c8cc082f3cbcfca8
Eonist <30n1st@gmail.com>
2017-05-25 16:35:04 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/property/utils/StyleMetricParser.swift

--- src/Element/style/property/utils/StyleMetricParser.swift
+++ src/Element/style/property/utils/StyleMetricParser.swift
@@ -8,7 +8,7 @@ class StyleMetricParser {
      * TODO: ⚠️️ Add support for % as it isnt implemented yet, see the margin implementation for guidance
      */
     static func offset(_ skin:ISkin,_ depth:Int = 0)->CGPoint {
-        guard let value:Any = self.value(skin, CSSConstants.offset.rawValue, depth) else{
+        guard let value:Any = StylePropertyParser.value(skin, CSSConstants.offset.rawValue, depth) else{
             return CGPoint(0,0)//<---temp solution
         }
         let array:[CGFloat] = {

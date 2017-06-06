491114de7226726009d08048caca70eb92384aae
Eonist <30n1st@gmail.com>
2017-06-06 17:19:37 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/window/TranslucentWin.swift

--- src/Element/ui/window/TranslucentWin.swift
+++ src/Element/ui/window/TranslucentWin.swift
@@ -10,7 +10,7 @@ class TranslucentWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
     var visualEffectView:TranslucentView?/*We set the to the background*/
     
     override init(contentRect:NSRect, styleMask style:NSWindowStyleMask, backing bufferingType:NSBackingStoreType, defer flag: Bool) {
-        super.init(contentRect:contentRect, styleMask: [.borderless,.resizable], backing:NSBackingStoreType.buffered, defer: false)
+        super.init(contentRect:contentRect, styleMask: [.titled, .resizable,.fullSizeContentView], backing:NSBackingStoreType.buffered, defer: false)
         WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter)
         Swift.print("frame.origin: " + "\(frame.origin)")
         self.contentView!.wantsLayer = true/*this can and is set in the view*/
@@ -18,9 +18,17 @@ class TranslucentWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         self.makeKeyAndOrderFront(nil)//moves the window to the front
         self.makeMain()//makes it the apps main menu?
         self.hasShadow = true/*you have to set this to true if you want a shadow when using the borderlessmask setting*/
+        self.titleVisibility = .hidden
+        self.titlebarAppearsTransparent = true
         //self.center()/*centers the window, this can also be done via setOrigin and calculating screen size etc*/
         self.isMovableByWindowBackground = false/*This enables you to drag the window around via the background*/
         self.delegate = self/*So that we can use this class as the Window controller aswell*/
+        self.isMovable = false
+        
+        self.standardWindowButton(.miniaturizeButton)?.isHidden = true
+        self.standardWindowButton(.zoomButton)?.isHidden = true
+        self.standardWindowButton(.closeButton)?.isHidden = true
+        
         self.contentView = FlippedView(frame:contentRect)
         visualEffectView = TranslucentView(frame:contentRect)
         self.contentView?.addSubview(visualEffectView!)

f25f1ad482f72e9fa6da987b4f2eb0b8f0c9c146
Eonist <30n1st@gmail.com>
2017-06-06 17:22:09 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/window/TranslucentWin.swift

--- src/Element/ui/window/TranslucentWin.swift
+++ src/Element/ui/window/TranslucentWin.swift
@@ -29,7 +29,7 @@ class TranslucentWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         self.standardWindowButton(.zoomButton)?.isHidden = true
         self.standardWindowButton(.closeButton)?.isHidden = true
         
-        self.contentView = FlippedView(frame:contentRect)
+        self.contentView = InteractiveView2() //FlippedView(frame:contentRect)
         visualEffectView = TranslucentView(frame:contentRect)
         self.contentView?.addSubview(visualEffectView!)
         //override and add view to contentview. 

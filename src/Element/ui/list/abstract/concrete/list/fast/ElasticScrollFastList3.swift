eb74105385691699bb9909126516374414e4e9b2
Eonist <30n1st@gmail.com>
2017-06-07 14:58:41 +0200

    Files modified: 2
    Modified 2 files:src/Element/ui/list/abstract/concrete/list/fast/ElasticScrollFastList3.swiftsrc/Element/ui/other/view/concrete/ContainerView3.swift

--- src/Element/ui/list/abstract/concrete/list/fast/ElasticScrollFastList3.swift
+++ src/Element/ui/list/abstract/concrete/list/fast/ElasticScrollFastList3.swift
@@ -7,7 +7,7 @@ class ElasticScrollFastList3:FastList3,ElasticScrollableFastListable3 {
 }
 extension Elastic3 where Self:FastList3{
     var rubberBandContainer:Container {
-        let rbContainer = addSubView(Container(w,h,self,"rb"))//⚠️️TODO: move to lazy var later
+        let rbContainer = addSubView(Container(NaN,h,self,"rb"))//⚠️️TODO: move to lazy var later
         rbContainer.addSubview(contentContainer)/*adds content Container inside rbContainer*/
         contentContainer.parent = rbContainer/*set the correct parent*/
         return rbContainer

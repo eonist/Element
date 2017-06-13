551d3a648b58c66a19789762f23c0e13a908d40b
Eonist <30n1st@gmail.com>
2017-06-13 13:36:07 +0200

    Files modified: 1
    Modified 1 file:src/Element/utils/ElementExtenions.swift

--- src/Element/utils/ElementExtenions.swift
+++ src/Element/utils/ElementExtenions.swift
@@ -1,5 +1,26 @@
 import Foundation
+@testable import Utils
 
-class ElementExtenions {
-
+extension Element{
+    /**
+     * Convenience
+     */
+    convenience init(_ width: CGFloat, _ height: CGFloat, _ isDisabled:Bool, _ isFocused:Bool = false, _ parent:IElement? = nil,_ id:String? = nil){
+        self.init(width,height,parent,id)
+        self.isDisabled = isDisabled
+        self.isFocused = isFocused
+    }
+    /**
+     * Convenience
+     */
+    convenience init(_ width: CGFloat , _ height: CGFloat , _ x:CGFloat , _ y:CGFloat , _ parent:IElement? = nil,_ id:String? = nil){
+        self.init(width,height,parent,id)
+        setPosition(CGPoint(x,y))
+    }
+    /**
+     * New
+     */
+    func element<T:IElement>(_ id:String,_ type:T.Type? = nil) -> T?{
+        return ElementParser.element(self, id, type)
+    }
 }

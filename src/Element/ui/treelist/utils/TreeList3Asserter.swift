feefd428b96f56721dac336404790d29e8978157
Eonist <30n1st@gmail.com>
2017-06-14 17:27:42 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/treelist/utils/TreeList3Asserter.swift

--- src/Element/ui/treelist/utils/TreeList3Asserter.swift
+++ src/Element/ui/treelist/utils/TreeList3Asserter.swift
@@ -2,6 +2,13 @@ import Foundation
 
 class TreeList3Asserter {
     /**
+     * New
+     * idx3d
+     */
+    static func hasAttribute(_ treeList:TreeListable3, _ idx3d:[Int], _ key:String) -> Bool{
+        return TreeDPParser.getProp(treeList.treeDP, idx3d, key) != nil
+    }
+    /**
      * idx2d
      */
     static func isOpen(_ treeList:TreeListable3, _ idx2d:Int) -> Bool{

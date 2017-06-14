f6100d2203cfef5707dd0f078de0b1edcb216b17
Eonist <30n1st@gmail.com>
2017-06-14 14:55:33 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/treelist/TreeList3.swift

--- src/Element/ui/treelist/TreeList3.swift
+++ src/Element/ui/treelist/TreeList3.swift
@@ -12,7 +12,7 @@ class TreeList3:ElasticScrollFastList3,TreeListable3{//ElasticSlideScrollFastLis
         //Swift.print("🍊 \(listItem.idx)")
         let idx3d:[Int] = treeDP.hashList[listItem.idx]
         //Swift.print("idx3d: " + "\(idx3d)")
-        listItem.item.id = idx3d.count.string/*the indentation level (from 1 and up), should use classID*/
+        listItem.item.id = idx3d.count.string/*The indentation level (from 1 and up), should use classID*/
         disableAnim{listItem.item.setSkinState(listItem.item.getSkinState())}/*Sets correct indent*/
         let isOpenStr = TreeDPParser.getProp(treeDP, idx3d, "isOpen")
         if let checkable = listItem.item as? CheckBoxButton{
@@ -20,11 +20,14 @@ class TreeList3:ElasticScrollFastList3,TreeListable3{//ElasticSlideScrollFastLis
             disableAnim{checkable.checkBox.setChecked(isChecked)}/*Sets correct open/close icon*/
         }
         disableAnim{(listItem.item as! TreeList3Item).checkBox.isHidden = !TreeDPAsserter.hasChildren(treeDP, idx3d)}/*Does item have children?,hides checkBox if item doesn't have children*/
-        super.reUse(listItem)/*sets text and position and select state*/
+        super.reUse(listItem)/*Sets text and position and select state*/
     }
     override func createItem(_ index:Int) -> Element {
         return Utils.createTreeListItem(itemSize,contentContainer)
     }
+    /**
+     * TODO: Use Switch
+     */
     override func onEvent(_ event:Event) {
         if(event.type == CheckEvent.check){onItemCheck(event as! CheckEvent)}
         else if(event.type == SelectEvent.select){onItemSelect(event as! SelectEvent)}
@@ -49,14 +52,15 @@ extension TreeList3{
         }
         moverGroup!.yMover.contentFrame = (0,contentSize.height)/*updates mover group to avoid jumpiness*/
         moverGroup!.xMover.contentFrame = (0,contentSize.width)
-        //onEvent(TreeListEvent(TreeListEvent.change,self))
+        onEvent(TreeListEvent(TreeListEvent.change,self))
     }
     func onItemSelect(_ event:SelectEvent){
         Swift.print("TreeList3.onItemSelect")
+        onEvent(TreeListEvent(TreeListEvent.change,self))
     }
 }
 private class Utils{
-    /*create TreeItem*/
+    /*Create TreeItem*/
     static func createTreeListItem(_ itemSize:CGSize, _ parent:Element) -> TreeList3Item{
         let item:TreeList3Item = TreeList3Item(itemSize.width, itemSize.height ,"", false, false, parent)
         return parent.addSubView(item)

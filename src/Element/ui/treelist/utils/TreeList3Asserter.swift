11018c797cea0c5fd6e27af1e419c32ec68282e6
Eonist <30n1st@gmail.com>
2017-06-14 17:34:39 +0200

    Files modified: 2
    Modified 2 files:src/Element/ui/treelist/utils/TreeList3Asserter.swiftsrc/Element/ui/treelist/utils/tree/utils/TreeAsserter.swift

--- src/Element/ui/treelist/utils/TreeList3Asserter.swift
+++ src/Element/ui/treelist/utils/TreeList3Asserter.swift
@@ -1,13 +1,7 @@
 import Foundation
 
 class TreeList3Asserter {
-    /**
-     * New
-     * idx3d
-     */
-    static func hasAttribute(_ treeList:TreeListable3, _ idx3d:[Int], _ key:String) -> Bool{
-        return TreeDPParser.getProp(treeList.treeDP, idx3d, key) != nil
-    }
+    
     /**
      * idx2d
      */
--- src/Element/ui/treelist/utils/tree/utils/TreeAsserter.swift
+++ src/Element/ui/treelist/utils/tree/utils/TreeAsserter.swift
@@ -2,6 +2,13 @@ import Foundation
 
 class TreeAsserter {
     /**
+     * New
+     * idx3d
+     */
+    static func hasAttribute(_ tree:Tree, _ idx3d:[Int], _ key:String) -> Bool{
+        return tree.getProp(idx3d, key) != nil
+    }
+    /**
      * Asserts if a tree at PARAM: idx3d has children
      */
     static func hasChildren(_ tree:Tree,_ idx3d:[Int])->Bool{

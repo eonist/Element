859dfc99e8cefc0e7d8d8d3c8afa289ffac46c39
Eonist <30n1st@gmail.com>
2017-06-18 16:52:43 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/text/Text.swift

--- src/Element/ui/text/Text.swift
+++ src/Element/ui/text/Text.swift
@@ -3,7 +3,6 @@ import Cocoa
 
 class Text:Element,IText {
     var initText:String;//this value is accessed by the TextSkin (It is not meant for external accessing from other classes)
-    //var text:String{get{return textField.string!} set{fatalError("NOT SUPPORTED YET")}}
     init(_ width:CGFloat, _ height:CGFloat, _ text:String = "dafaultText", _ parent:IElement? = nil, _ id:String? = nil){
         //Swift.print("Text.init()")
         initText = text

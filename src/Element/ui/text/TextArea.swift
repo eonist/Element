dcfe26c868dd2ab1a5e806425242462e11e57821
Eonist <30n1st@gmail.com>
2017-06-19 21:58:03 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/text/TextArea.swift

--- src/Element/ui/text/TextArea.swift
+++ src/Element/ui/text/TextArea.swift
@@ -1,7 +1,7 @@
 import Foundation
 @testable import Utils
 /**
- * :TODO: maybe rename text to defaultText and _text to _text
+ * :TODO: ⚠️️ Maybe rename text to defaultText and _text to _text
  * :TODO: Add support for setting size via css for the TextArea. Its currently not working
  */
 class TextArea:Element {

b4b0e279562f637542a189327b7754229b5daf23
Eonist <30n1st@gmail.com>
2017-06-24 13:38:24 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/Style.swift

--- src/Element/style/Style.swift
+++ src/Element/style/Style.swift
@@ -3,9 +3,9 @@ import Foundation
  * NOTE: The StyleProperty instances are stored in _styleProperties, and then you querry them by name and depth
  */
 struct Style:IStyle{
-    var name:String
-    var selectors:[ISelector]
-    var styleProperties:[IStyleProperty]
+    var name:String/*Name of the class*/
+    var selectors:[ISelector]/*[Type#id:state.class]*/
+    var styleProperties:[IStyleProperty]/*The styleProperties of this style*/
     init(_ name:String = "",_ selectors:[ISelector] = [], _ styleProperties:[IStyleProperty] = []){
         self.name = name
         self.selectors = selectors

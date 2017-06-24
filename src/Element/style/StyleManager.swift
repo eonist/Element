5306ccefdb45af84dad8b64e6e962b6cd7152ec6
Eonist <30n1st@gmail.com>
2017-06-24 15:12:04 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/StyleManager.swift

--- src/Element/style/StyleManager.swift
+++ src/Element/style/StyleManager.swift
@@ -121,7 +121,7 @@ extension StyleManager{
      * New
      */
     func overrideStylePropertyValue(_ styleName:String, _ stylePropertyName:String, _ value:Any, _ depth:Int = 0){
-        if let i = StyleManager.index(name), let style:IStyle = StyleManager.styles[safe:i], let e:Int = StyleParser.idx(style, stylePropertyName) {
+        if let i = StyleManager.index(styleName), let style:IStyle = StyleManager.styles[safe:i], let e:Int = StyleParser.idx(style, stylePropertyName) {
             StyleManager.styles[i].styleProperties[e].value = value
         }
     }

12c34a71c6c34755801828361c67497fed2d6039
Eonist <30n1st@gmail.com>
2017-07-25 22:37:41 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/Element.swift

--- src/Element/ui/Element.swift
+++ src/Element/ui/Element.swift
@@ -30,6 +30,7 @@ class Element:InteractiveView2,IElement {
      * Draws the graphics
      */
     func resolveSkin() {
+        NSColor.green.blended(withFraction: 0.5, of: .blue)
         self.skin = addSubView(SkinResolver.skin(self))
     }
     /**

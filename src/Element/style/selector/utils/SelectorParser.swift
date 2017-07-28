a6525d852a55939e72583d0e01d1766295a15702
Eonist <30n1st@gmail.com>
2017-07-28 17:42:24 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/selector/utils/SelectorParser.swift

--- src/Element/style/selector/utils/SelectorParser.swift
+++ src/Element/style/selector/utils/SelectorParser.swift
@@ -38,9 +38,7 @@ class SelectorParser{
         let element:String = selector.element != "" ? selector.element : ""
         //let classIds:String = selector.classIds.reduce(string) { $0 + ("."+$1) }
         let id:String = selector.id != "" ? "#"+selector.id : ""
-        let states:String = selector.states.reduce("") { $0 + (":"+$1) }
-        
-        
+        let states:String = selector.states.reduce("") { $0 + (":"+$1) }.removeLastChar()
         return element + /*classIds +*/ id + states
     }
     /**

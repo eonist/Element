5235c4955fd9a62d31f4dda0f5fe2143dd1c6f35
Eonist <30n1st@gmail.com>
2017-07-28 17:14:30 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/StyleManager.swift

--- src/Element/style/StyleManager.swift
+++ src/Element/style/StyleManager.swift
@@ -4,7 +4,7 @@ import Foundation
  * NOTE: The reason we use array instead of object: a problem may be that the order will be different every time you read this object, random
  * EXAMPLE: print("StyleManager.getInstance().getStyle(Button): " + StyleManager.getInstance().getStyle("someText").getPropertyNames());//prints style names
  * TODO: ⚠️️ Could potentially extend StyleCollection and just implimnet the extra functions in this class?!?
- * TODO: ⚠️️ This class can be a struct
+ * TODO: ⚠️️⚠️️⚠️️ This class can be a struct
  * TODO: ⚠️️ The tail trick could possibly be even faster if you sorted the hashed styles and used a halfed algo when querrying (but its a dictionary so maybe not, maybe if you stored it in an array etc)
  */
 class StyleManager{

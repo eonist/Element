9b45c326d6c86dd075d5d33cee931037c961cd8a
Eonist <30n1st@gmail.com>
2017-08-01 19:28:46 +0200

    'Files modified: 1'
    'Added 1 file:
    src/Element/style/utils/LiveEdit.swift
    
    '

--- src/Element/style/utils/LiveEdit.swift
+++ src/Element/style/utils/LiveEdit.swift
@@ -1,5 +1,19 @@
 import Foundation
 
 class LiveEdit {
-
+    /**
+     * Removes styles that have been refreshed
+     */
+    static func styles(stylesURL:String) -> [IStyle]{
+        let cssString:String = CSSFileParser.cssString(stylesURL)
+        let styles = StyleManagerUtils.styles(from: cssString)
+        if let cssFile = StyleManager.cssFiles[stylesURL]{/*check if the url already exists in the dictionary*/
+            let cssString:String = CSSLinkResolver.resolveLinks(cssFile)
+            let styles:[IStyle] = StyleManagerUtils.styles(from: cssString)//was CSSParser.styleCollection(cssString).styles
+            StyleManager.removeStyle(styles)/*if url exists then remove the styles that it represents*/
+        }else{/*If the url wasn't in the dictionary, then add it*/
+            StyleManager.cssFiles[stylesURL] = cssString//<--I'm not sure how this works, but it works
+        }
+        return styles
+    }
 }

7370891549ea756448e2af8e759add667be061ec
Eonist <30n1st@gmail.com>
2017-08-01 23:40:24 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/utils/StyleResolver.swift

--- src/Element/style/utils/StyleResolver.swift
+++ src/Element/style/utils/StyleResolver.swift
@@ -50,10 +50,8 @@ private class Utils{
      */
     static func getStyles(_ selector:ISelector)->[IStyle]{
         var styles:[IStyle] = []
-        if(selector.element != ""){
-            if let stylesByElement = StyleManagerUtils.stylesByElement[selector.element]{
-                styles += stylesByElement
-            }
+        if selector.element != "", let stylesByElement = StyleManagerUtils.stylesByElement[selector.element]{
+            styles += stylesByElement
         }
         if selector.id != "", let stylesByID = StyleManagerUtils.stylesByID[selector.id] {
             styles += stylesByID
@@ -61,39 +59,12 @@ private class Utils{
         if selector.classIds.count > 0, let stylesByClassId = StyleManagerUtils.stylesByClassId[selector.classIds.first!]{
             styles += stylesByClassId
         }
-        if(selector.states.count > 0){
-            if let stylesByState = StyleManagerUtils.stylesByState[selector.states.first!]{
-                styles += stylesByState
-            }
+        if selector.states.count > 0, let stylesByState = StyleManagerUtils.stylesByState[selector.states.first!] {
+            styles += stylesByState
         }
         return styles
     }
 }
-/*
-static func style2(_ element:IElement)->IStyle{
-    fatalError("beta")
- 
-     let querySelectors:Array<ISelector> = ElementParser.selectors(element)/*Array instance comprised of Selector instances for each (element,classId,id and state) in the element*/
-     return style2(querySelectors)
- 
-}
-static func style2(_ querySelectors:[ISelector])->IStyle{
-    fatalError("beta")
- 
-     var weightedStyles:Array<WeightedStyle> = StyleResolverUtils.query(querySelectors,StyleManager.styleTree,0)
-     //print("weightedStyles: " + "\(weightedStyles.count)")
-     //if(StyleResolver.stackString(element) == "Window Box#tabBarBox SelectTextButton#first Text") for each (var ws : WeightedStyle in weightedStyles) trace("not.Sorted.ws.name: " + ws.name);
-     if(weightedStyles.count > 1) {weightedStyles = ArrayParser.conditionSort(weightedStyles, WeightedStyleAsserter.priority)}//WeightStyleParser.sortByWeight(weightedStyles);/*Sorts each weightedStyle by its weight, the styles with most specificity has a lower index*/
-     //if(StyleResolver.stackString(element) == "Window Box#tabBarBox SelectTextButton#first Text") for each (var wStyle : WeightedStyle in weightedStyles) trace("sorted.ws.name: " + wStyle.name);
-     let styleName:String = SelectorParser.selectorsString(querySelectors)
-     var finalStyle:IStyle = StyleManager.getStyle(styleName) ?? Style(styleName,querySelectors,[]);/*find the exact styleName in the stylemanager or create a new style to merge partily matched styles*/
-     for weightStyle:WeightedStyle in weightedStyles{
-     StyleModifier.merge(&finalStyle, StyleAsserter.direct(querySelectors, weightStyle) ? weightStyle : StyleModifier.filter(weightStyle, CSSConstants.textPropertyNames))/*direct styles will be appart of the final style and  you inherit from indirect styles, fonts,*or properties marked inherit*/
-     }
-     return finalStyle
- 
-}
-*/
 private class Debug {
     static func appendQuerySelectors(_ querySelectors:[ISelector] ) {
         var selectorsXMLString:String = ""
@@ -101,5 +72,4 @@ private class Debug {
         StyleResolver.selectorsString += "<Selectors>" + selectorsXMLString + "</Selectors>"
     }
 }
-//let styles:[IStyle] = element as? Text != nil ? StyleManager.styles : Utils.getStyles(querySelectors.last!)//<-this is the tail trick
-//styleLookUpCount++
+

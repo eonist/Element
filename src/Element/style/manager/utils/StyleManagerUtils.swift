b0b272bb5757b481f8e6da0835ebe62d69b7942a
Eonist <30n1st@gmail.com>
2017-08-02 02:02:55 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/manager/utils/StyleManagerUtils.swift

--- src/Element/style/manager/utils/StyleManagerUtils.swift
+++ src/Element/style/manager/utils/StyleManagerUtils.swift
@@ -12,29 +12,29 @@ class StyleManagerUtils{
      * TODO: Tail trick is nice, but it complicates the code, try to pursue other avenues in optimization, bisect search comes to mind.
      */
     static func hashStyle(_ style:IStyle){
-        if(style.selectors.last!.element != ""){
-            if let selector = stylesByElement[style.selectors.last!.element]{
+        if style.selectors.last!.element != "" {
+            if stylesByElement[style.selectors.last!.element] != nil {
                 stylesByElement[style.selectors.last!.element]!.append(style)
             }else{
                 stylesByElement[style.selectors.last!.element] = [style]
             }
         }
-        if(style.selectors.last!.id != ""){
+        if style.selectors.last!.id != "" {
             if (stylesByID[style.selectors.last!.id] != nil){
                 stylesByID[style.selectors.last!.id]!.append(style)
             }else{
                 stylesByID[style.selectors.last!.id] = [style]
             }
         }
-        if(style.selectors.last!.classIds.count > 0){//temp solution, you need to flatten the classIds somehow, or test individual etc
+        if !style.selectors.last!.classIds.isEmpty {//temp solution, you need to flatten the classIds somehow, or test individual etc
             if (stylesByClassId[style.selectors.last!.classIds.first!] != nil){
                 stylesByClassId[style.selectors.last!.classIds.first!]!.append(style)
             }else{
                 stylesByClassId[style.selectors.last!.classIds.first!] = [style]
             }
         }
-        if(style.selectors.last!.states.count > 0){//temp solution, you need to flatten the states somehow, or test individual etc
-            if (stylesByState[style.selectors.last!.states.first!] != nil){
+        if !style.selectors.last!.states.isEmpty {//temp solution, you need to flatten the states somehow, or test individual etc
+            if  stylesByState[style.selectors.last!.states.first!] != nil {
                 stylesByState[style.selectors.last!.states.first!]!.append(style)
             }else{
                 stylesByState[style.selectors.last!.states.first!] = [style]
@@ -42,7 +42,7 @@ class StyleManagerUtils{
         }    
     }
     /**
-     * 
+     * New
      */
     static func styles(from cssString:String) -> [IStyle]{
         let resolvedLinksCSS = CSSLinkResolver.resolveLinks(cssString)

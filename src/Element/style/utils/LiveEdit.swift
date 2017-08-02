d88ff579cba9e29f3e6a88b60b378067e7e6734e
Eonist <30n1st@gmail.com>
2017-08-02 13:28:30 +0200

    Files modified: 1
    Modified 1 file:src/Element/style/utils/LiveEdit.swift

--- src/Element/style/utils/LiveEdit.swift
+++ src/Element/style/utils/LiveEdit.swift
@@ -8,8 +8,8 @@ class LiveEdit {
         let cssString:String = CSSFileParser.cssString(stylesURL)
         let styles = StyleManagerUtils.styles(from: cssString)
         if let cssFile = StyleManager.cssFiles[stylesURL]{/*check if the url already exists in the dictionary*/
-            let cssString:String = CSSLinkResolver.resolveLinks(cssFile)
-            let styles:[IStyle] = StyleManagerUtils.styles(from: cssString)
+//            let cssStr:String = CSSLinkResolver.resolveLinks(cssFile)
+            let styles:[IStyle] = StyleManagerUtils.styles(from: cssStr)
             StyleManager.removeStyle(styles)/*if url exists then remove the styles that it represents*/
         }else{/*If the url wasn't in the dictionary, then add it*/
             StyleManager.cssFiles[stylesURL] = cssString//<--I'm not sure how this works, but it works

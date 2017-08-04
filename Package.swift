a95be3df945ee5104c011f89de03372d09b65b22
Eon <30n1st@gmail.com>
2017-08-04 20:44:14 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Element",
 	dependencies: [
-		.Package(url: "https://github.com/eonist/swift-utils.git", Version(0, 0, 0, prereleaseIdentifiers: ["alpha", "4"]))
+		.Package(url: "https://github.com/eonist/swift-utils.git", Version(0, 0, 0, prereleaseIdentifiers: ["alpha", "5"]))
     ],
 	exclude: ["README.md"]
 ) 

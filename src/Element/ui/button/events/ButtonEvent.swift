3aae67813f03273ef19f2678b6c7071a24f548af
Eonist <30n1st@gmail.com>
2017-08-05 16:48:36 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/button/events/ButtonEvent.swift

--- src/Element/ui/button/events/ButtonEvent.swift
+++ src/Element/ui/button/events/ButtonEvent.swift
@@ -5,25 +5,31 @@ import Cocoa
  * NOTE: Reading the event.location can be done by reading the localPos from the button it self
  */
 class ButtonEvent:Event{
-    static let down:String = ButtonEventType.down.hashValue.string
-    static let up:String = ButtonEventType.up.hashValue.string
-    static let upInside:String = ButtonEventType.upInside.hashValue.string
-    static let upOutside:String = ButtonEventType.upOutside.hashValue.string
-    static let out:String = ButtonEventType.out.hashValue.string
-    static let over:String = ButtonEventType.over.hashValue.string
+    static let down:String = ButtonEventType.down.rawValue
+    static let up:String = ButtonEventType.up.rawValue
+    static let upInside:String = ButtonEventType.upInside.rawValue
+    static let upOutside:String = ButtonEventType.upOutside.rawValue
+    static let out:String = ButtonEventType.out.rawValue
+    static let over:String = ButtonEventType.over.rawValue
     /*Right*/
-    static var rightMouseDown:String = ButtonEventType.rightMouseDown.hashValue.string
+    static var rightMouseDown:String = ButtonEventType.rightMouseDown.rawValue
     weak var event:NSEvent?
     init(_ type:String = "", _ origin:AnyObject,_ event:NSEvent? = nil){
         self.event = event
         super.init(type, origin)
     }
 }
-enum ButtonEventType{
-    case down,up,upInside,upOutside,out,over,rightMouseDown
+enum ButtonEventType:String{
+    case down = "buttonEventDown"
+    case up = "buttonEventUp"
+    case upInside = "buttonEventUpInside"
+    case upOutside = "buttonEventUpOutside"
+    case out = "buttonEventOut"
+    case over = "buttonEventOver"
+    case rightMouseDown = "buttonEventRightMouseDown"
 }
 extension Event{
     func assert(_ type:ButtonEventType, id:String) -> Bool{
-        return self.type == type && (self.origin as? ElementKind)?.id == id
+        return self.type == type.rawValue && (self.origin as? ElementKind)?.id == id
     }
 }

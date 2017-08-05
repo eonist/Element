82a3d23b685f6a56599820a9ba9f5b4f23ee4bf5
Eonist <30n1st@gmail.com>
2017-08-05 16:40:49 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/button/events/ButtonEvent.swift

--- src/Element/ui/button/events/ButtonEvent.swift
+++ src/Element/ui/button/events/ButtonEvent.swift
@@ -5,17 +5,20 @@ import Cocoa
  * NOTE: Reading the event.location can be done by reading the localPos from the button it self
  */
 class ButtonEvent:Event{
-    static var down:String = "buttonEventDown"
-    static var up:String = "buttonEventUp"/*<--New*/
-    static var upInside:String = "buttonEventUpInside"
-    static var upOutside:String = "buttonEventUpOutside"
-    static var out:String = "buttonEventOut"
-    static var over:String = "buttonEventOver"
+    static let down:String = ButtonEventType.down.hashValue.string
+    static let up:String = ButtonEventType.up.hashValue.string
+    static let upInside:String = ButtonEventType.upInside.hashValue.string
+    static let upOutside:String = ButtonEventType.upOutside.hashValue.string
+    static let out:String = ButtonEventType.out.hashValue.string
+    static let over:String = ButtonEventType.over.hashValue.string
     /*Right*/
-    static var rightMouseDown:String = "buttonEventRightMouseDown"
+    static var rightMouseDown:String = ButtonEventType.rightMouseDown.hashValue.string
     weak var event:NSEvent?
     init(_ type:String = "", _ origin:AnyObject,_ event:NSEvent? = nil){
         self.event = event
         super.init(type, origin)
     }
+}
+enum ButtonEventType{
+    case down,up,upInside,upOutside,out,over,rightMouseDown
 }

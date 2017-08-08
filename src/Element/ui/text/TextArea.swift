a02a0088ca5a1c0556c7f2d79b12b52d7a8276c9
Eonist <30n1st@gmail.com>
2017-08-08 18:51:18 +0200

    Files modified: 1
    Modified 1 file:src/Element/ui/text/TextArea.swift

--- src/Element/ui/text/TextArea.swift
+++ src/Element/ui/text/TextArea.swift
@@ -23,7 +23,7 @@ class TextArea:Element {
         text.setText(textStr)
     }
     func getTextValue() -> String {
-        text.getText()
+        return text.getText()
     }
     required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }

ae3f961f5522d7170d09819d9977ac321e0c0327
Eonist <30n1st@gmail.com>
2017-08-14 11:17:07 +0200

    Added FilePicker UI Component
    Modified 2 files:
     src/Element/ui/Element.swift
    src/Element/ui/other/misc/FilePicker.swift

--- src/Element/ui/other/misc/FilePicker.swift
+++ src/Element/ui/other/misc/FilePicker.swift
@@ -1,5 +1,16 @@
 import Foundation
+@testable import Utils
 
-class FilePicker {
-
+class FilePicker:Element{
+    init(size:CGSize, parent: IElement?, id: String?) {
+        super.init(size.width, size.height, parent, id)
+    }
+    lazy var textString:String = ""
+    lazy var inputString:String = ""
+    lazy var textInput:TextInput = TextInput()
+    override func resolveSkin() {
+        super.resolveSkin()
+        
+    }
+    required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented") }
 }

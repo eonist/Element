45f129ded9892df34bb89df4ca40eb61d93d5744
Eonist <30n1st@gmail.com>
2017-08-14 12:15:34 +0200

    Removed resolveSkin
    Modified 1 file:
    src/Element/ui/other/misc/FilePicker.swift

--- src/Element/ui/other/misc/FilePicker.swift
+++ src/Element/ui/other/misc/FilePicker.swift
@@ -4,9 +4,6 @@ import Foundation
 class FilePicker:Element{
     lazy var textInput:TextInput = TextInput()
     lazy var button:Button = Button()
-    override func resolveSkin() {
-        super.resolveSkin()
-        
-    }
+    
     required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented") }
 }

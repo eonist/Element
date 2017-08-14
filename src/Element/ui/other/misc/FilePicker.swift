63e0fbfe0a9676f105a4fb6482c98a0525105d84
Eonist <30n1st@gmail.com>
2017-08-14 12:16:57 +0200

    Removed whitespace
    Modified 1 file:
    src/Element/ui/other/misc/FilePicker.swift

--- src/Element/ui/other/misc/FilePicker.swift
+++ src/Element/ui/other/misc/FilePicker.swift
@@ -4,6 +4,5 @@ import Foundation
 class FilePicker:Element{
     lazy var textInput:TextInput = TextInput()
     lazy var button:Button = Button()
-    
     required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented") }
 }

b0c03af34ad8f9e4b97906cc1a29756823aec1fc
Eonist <30n1st@gmail.com>
2017-08-17 12:38:25 +0200

    Files modified: 2
    Modified 2 files:src/Element/ui/button/check/event/CheckEvent.swiftsrc/Element/ui/text/Text.swift

--- src/Element/ui/button/check/event/CheckEvent.swift
+++ src/Element/ui/button/check/event/CheckEvent.swift
@@ -5,9 +5,18 @@ import Cocoa
  */
 class CheckEvent:Event{
     var isChecked:Bool
-    static var check:String = "checkEventCheck"
+    static var check:String = "checkEventCheck"//CheckEventType.check.rawValue
     init(_ type:String, _ isChecked:Bool, _ origin:NSView){
         self.isChecked = isChecked
         super.init(type, origin)
     }
 }
+//enum CheckEventType:String{
+//    case check = "checkEventCheck"
+//}
+//extension Event{
+//    func assert(_ type:CheckEventType) -> Bool{
+//        return self.type == type.rawValue
+//    }
+//}
+
